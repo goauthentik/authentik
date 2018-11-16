de7a2fa0349981779a6390ed66c7b8b07c28a3d9
Jens Langhammer <jens.langhammer@haufe-lexware.com>
2018-11-16 11:40:24 +0100

    add more info to admin overview

--- passbook/admin/views/overview.py
+++ passbook/admin/views/overview.py
@@ -2,7 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 from passbook.admin.mixins import AdminRequiredMixin
-from passbook.core.models import Application, Rule, User
+from passbook.core.models import Application, Rule, User, Provider
 
 
 class AdministrationOverviewView(AdminRequiredMixin, TemplateView):
@@ -13,4 +13,5 @@ class AdministrationOverviewView(AdminRequiredMixin, TemplateView):
         kwargs['application_count'] = len(Application.objects.all())
         kwargs['rule_count'] = len(Rule.objects.all())
         kwargs['user_count'] = len(User.objects.all())
+        kwargs['provider_count'] = len(Provider.objects.all())
         return super().get_context_data(**kwargs)
