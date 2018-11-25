99673192947f2c68e1585a1c31b6e89539ef4d6b
Jens Langhammer <jens.langhammer@beryju.org>
2018-11-25 12:31:21 +0100

    always send email with OpenID userinfo

--- passbook/api/v1/openid.py
+++ passbook/api/v1/openid.py
@@ -16,7 +16,7 @@ class OpenIDUserInfoView(ScopedResourceMixin, View):
             'name': request.user.get_full_name(),
             'given_name': request.user.first_name,
             'family_name': request.user.last_name,
-            'preferred_username': request.user.username
+            'preferred_username': request.user.username,
+            'email': request.user.email,
         }
-
         return JsonResponse(payload)
