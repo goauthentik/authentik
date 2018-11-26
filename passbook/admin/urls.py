2aa12801a86632ce940d0458c470b0f679ccc5a4
Jens Langhammer <jens.langhammer@haufe-lexware.com>
2018-11-26 22:08:18 +0100

    admin: add application admin

--- passbook/admin/urls.py
+++ passbook/admin/urls.py
@@ -5,10 +5,15 @@ from passbook.admin.views import applications, overview, sources
 
 urlpatterns = [
     path('', overview.AdministrationOverviewView.as_view(), name='overview'),
+    # Applications
     path('applications/', applications.ApplicationListView.as_view(),
          name='applications'),
     path('applications/create/', applications.ApplicationCreateView.as_view(),
          name='application-create'),
+    path('applications/<uuid:pk>/update/',
+         applications.ApplicationUpdateView.as_view(), name='application-update'),
+    path('applications/<uuid:pk>/delete/',
+         applications.ApplicationDeleteView.as_view(), name='application-delete'),
     path('sources/', sources.SourceListView.as_view(), name='sources'),
     path('sources/create/', sources.SourceCreateView.as_view(), name='source-create'),
     path('sources/<uuid:pk>/update/', sources.SourceUpdateView.as_view(), name='source-update'),
--- passbook/admin/views/applications.py
+++ passbook/admin/views/applications.py
@@ -1,21 +1,49 @@
-"""passbook application administration"""
-
-from django.views.generic import CreateView, ListView
+"""passbook Application administration"""
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.utils.translation import ugettext as _
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
 from passbook.admin.mixins import AdminRequiredMixin
+from passbook.core.forms.applications import ApplicationForm
 from passbook.core.models import Application
 
 
 class ApplicationListView(AdminRequiredMixin, ListView):
-    """List all applications"""
+    """Show list of all applications"""
 
     model = Application
     template_name = 'administration/application/list.html'
 
+    def get_queryset(self):
+        return super().get_queryset().select_subclasses()
 
-class ApplicationCreateView(AdminRequiredMixin, CreateView):
-    """Create new application"""
+
+class ApplicationCreateView(SuccessMessageMixin, AdminRequiredMixin, CreateView):
+    """Create new Application"""
+
+    form_class = ApplicationForm
+
+    template_name = 'generic/create.html'
+    success_url = reverse_lazy('passbook_admin:applications')
+    success_message = _('Successfully created Application')
+
+
+class ApplicationUpdateView(SuccessMessageMixin, AdminRequiredMixin, UpdateView):
+    """Update application"""
 
     model = Application
-    template_name = 'administration/application/create.html'
-    fields = ['name', 'launch_url', 'icon_url']
+    form_class = ApplicationForm
+
+    template_name = 'generic/update.html'
+    success_url = reverse_lazy('passbook_admin:applications')
+    success_message = _('Successfully updated Application')
+
+
+class ApplicationDeleteView(SuccessMessageMixin, AdminRequiredMixin, DeleteView):
+    """Delete application"""
+
+    model = Application
+
+    success_url = reverse_lazy('passbook_admin:applications')
+    success_message = _('Successfully updated Application')
