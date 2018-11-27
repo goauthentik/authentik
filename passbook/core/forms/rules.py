4b047802c0158dd658da22334eacb4015148dc9f
Jens Langhammer <jens.langhammer@haufe-lexware.com>
2018-11-27 16:23:04 +0100

    core: Add Webhook Rule

--- passbook/core/forms/rules.py
+++ passbook/core/forms/rules.py
@@ -2,7 +2,7 @@
 
 from django import forms
 
-from passbook.core.models import FieldMatcherRule
+from passbook.core.models import FieldMatcherRule, WebhookRule
 
 
 class FieldMatcherRuleForm(forms.ModelForm):
@@ -17,4 +17,20 @@ class FieldMatcherRuleForm(forms.ModelForm):
             'name': forms.TextInput(),
             'user_field': forms.TextInput(),
             'value': forms.TextInput(),
+        }
+
+
+class WebhookRuleForm(forms.ModelForm):
+    """WebhookRuleForm Form"""
+
+    class Meta:
+
+        model = WebhookRule
+        fields = ['url', 'method', 'json_body', 'json_headers',
+                  'result_jsonpath', 'result_json_value', ]
+        widgets = {
+            'json_body': forms.TextInput(),
+            'json_headers': forms.TextInput(),
+            'result_jsonpath': forms.TextInput(),
+            'result_json_value': forms.TextInput(),
         }
--- passbook/core/models.py
+++ passbook/core/models.py
@@ -175,3 +175,40 @@ class FieldMatcherRule(Rule):
 
         verbose_name = _('Field matcher Rule')
         verbose_name_plural = _('Field matcher Rules')
+@reversion.register()
+class WebhookRule(Rule):
+    """Rule that asks webhook"""
+
+    METHOD_GET = 'GET'
+    METHOD_POST = 'POST'
+    METHOD_PATCH = 'PATCH'
+    METHOD_DELETE = 'DELETE'
+    METHOD_PUT = 'PUT'
+
+    METHODS = (
+        (METHOD_GET, METHOD_GET),
+        (METHOD_POST, METHOD_POST),
+        (METHOD_PATCH, METHOD_PATCH),
+        (METHOD_DELETE, METHOD_DELETE),
+        (METHOD_PUT, METHOD_PUT),
+    )
+
+    url = models.URLField()
+    method = models.CharField(max_length=10, choices=METHODS)
+    json_body = models.TextField()
+    json_headers = models.TextField()
+    result_jsonpath = models.TextField()
+    result_json_value = models.TextField()
+
+    name = 'passbook_core.WebhookRule'
+    form = 'passbook.core.forms.rules.WebhookRuleForm'
+
+    def passes(self, user: User):
+        """Call webhook asynchronously and report back"""
+        raise NotImplementedError()
+
+    class Meta:
+
+        verbose_name = _('Webhook Rule')
+        verbose_name_plural = _('Webhook Rules')
+
