43fe9e062df0a771514ee955508c3e74fb3549ab
Jens Langhammer <jens.langhammer@beryju.org>
2018-12-09 22:12:41 +0100

    core: add static root

--- passbook/core/settings.py
+++ passbook/core/settings.py
@@ -22,6 +22,7 @@ VERSION = __version__
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+STATIC_ROOT = BASE_DIR + '/static'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
