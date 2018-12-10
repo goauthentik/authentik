1a8f3fb18a994675768a953c3e1ab92ae6ca1b4b
Jens Langhammer <jens.langhammer@haufe-lexware.com>
2018-12-10 10:49:50 +0100

    Cleanup migrations (again)

--- passbook/core/views/overview.py
+++ passbook/core/views/overview.py
@@ -2,7 +2,7 @@
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
-
+from passbook.core.models import Application
 
 class OverviewView(LoginRequiredMixin, TemplateView):
     """Overview for logged in user, incase user opens passbook directly
@@ -12,4 +12,6 @@ class OverviewView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         kwargs['applications'] = self.request.user.applications.all()
+        if self.request.user.is_superuser:
+            kwargs['applications'] = Application.objects.all()
         return super().get_context_data(**kwargs)
