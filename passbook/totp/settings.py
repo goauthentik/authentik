258d59ef1b21b693fecdd62e991ab6dd43d4b538
Jens Langhammer <jens.langhammer@haufe-lexware.com>
2018-12-14 10:28:25 +0100

    totp: fix invalid settings

--- passbook/totp/settings.py
+++ passbook/totp/settings.py
@@ -1,10 +1,10 @@
 """passbook TOTP Settings"""
 
-OTP_LOGIN_URL = 'passbook_tfa:tfa-verify'
+OTP_LOGIN_URL = 'passbook_totp:totp-verify'
 OTP_TOTP_ISSUER = 'passbook'
 MIDDLEWARE = [
     'django_otp.middleware.OTPMiddleware',
-    'passbook.tfa.middleware.tfa_force_verify',
+    'passbook.totp.middleware.totp_force_verify',
 ]
 INSTALLED_APPS = [
     'django_otp',
