717846813520b11c80d330d917470890835c9d0e
Jens Langhammer <jens.langhammer@beryju.org>
2018-12-26 17:17:24 +0100

    admin: add support for extra links

---
+++ passbook/admin/templatetags/admin_reflection.py
@@ -0,0 +1,26 @@
+"""passbook admin templatetags"""
+import inspect
+from logging import getLogger
+
+from django import template
+from django.db.models import Model
+
+register = template.Library()
+LOGGER = getLogger(__name__)
+
+@register.simple_tag()
+def get_links(model_instance):
+    """Find all link_ methods on an object instance, run them and return as dict"""
+    prefix = 'link_'
+    links = {}
+
+    if not isinstance(model_instance, Model):
+        LOGGER.warning("Model %s is not instance of Model", model_instance)
+        return links
+
+    for name, method in inspect.getmembers(model_instance, predicate=inspect.ismethod):
+        if name.startswith(prefix):
+            human_name = name.replace(prefix, '').replace('_', ' ').capitalize()
+            links[human_name] = method()
+
+    return links
