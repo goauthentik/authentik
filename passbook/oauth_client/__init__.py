b4cb157257407e3124e507f25e625a79a0f047e7
Jens Langhammer <jens.langhammer@haufe-lexware.com>
2019-02-11 17:44:42 +0100

    bump version: 0.0.3-alpha -> 0.0.4-alpha

--- passbook/oauth_client/__init__.py
+++ passbook/oauth_client/__init__.py
@@ -1,2 +1,2 @@
 """passbook oauth_client Header"""
-__version__ = '0.0.3-alpha'
+__version__ = '0.0.4-alpha'
--- passbook/saml_idp/__init__.py
+++ passbook/saml_idp/__init__.py
@@ -1,2 +1,2 @@
 """passbook saml_idp Header"""
-__version__ = '0.0.3-alpha'
+__version__ = '0.0.4-alpha'
