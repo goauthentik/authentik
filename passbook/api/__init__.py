7e9154a0ea9fc044837a2ecf7a58ab3b73a6eb15
Jens Langhammer <jens.langhammer@haufe-lexware.com>
2019-02-25 17:39:39 +0100

    bump version: 0.0.7-alpha -> 0.0.8-alpha

--- passbook/api/__init__.py
+++ passbook/api/__init__.py
@@ -1,2 +1,2 @@
 """passbook api"""
-__version__ = '0.0.7-alpha'
+__version__ = '0.0.8-alpha'
--- passbook/oauth_provider/__init__.py
+++ passbook/oauth_provider/__init__.py
@@ -1,2 +1,2 @@
 """passbook oauth_provider Header"""
-__version__ = '0.0.7-alpha'
+__version__ = '0.0.8-alpha'
