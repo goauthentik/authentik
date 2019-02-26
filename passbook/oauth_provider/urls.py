4ae31d409b6492f29c90a44dfcfe3280f3a4becc
Jens Langhammer <jens.langhammer@haufe-lexware.com>
2019-02-26 09:08:22 +0100

    directly use paths instead of including oauth2_provider's

--- passbook/oauth_provider/urls.py
+++ passbook/oauth_provider/urls.py
@@ -1,6 +1,7 @@
 """passbook oauth_provider urls"""
 
-from django.urls import include, path
+from django.urls import path
+from oauth2_provider import views
 
 from passbook.oauth_provider.views import oauth2
 
@@ -13,5 +14,8 @@ urlpatterns = [
     path('authorize/permission_denied/', oauth2.OAuthPermissionDenied.as_view(),
          name='oauth2-permission-denied'),
     # OAuth API
-    path('', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path("authorize/", views.AuthorizationView.as_view(), name="authorize"),
+    path("token/", views.TokenView.as_view(), name="token"),
+    path("revoke_token/", views.RevokeTokenView.as_view(), name="revoke-token"),
+    path("introspect/", views.IntrospectTokenView.as_view(), name="introspect"),
 ]
