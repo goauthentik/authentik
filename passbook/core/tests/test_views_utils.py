acabb2df54b29dd693f3b202c04c766661893a39
Jens Langhammer <jens.langhammer@haufe-lexware.com>
2019-03-11 11:44:12 +0100

    fix unittests

--- passbook/core/tests/test_views_utils.py
+++ passbook/core/tests/test_views_utils.py
@@ -1,7 +1,10 @@
 """passbook util view tests"""
+import string
+from random import SystemRandom
 
 from django.test import RequestFactory, TestCase
 
+from passbook.core.models import User
 from passbook.core.views.utils import LoadingView, PermissionDeniedView
 
 
@@ -9,6 +12,11 @@ class TestUtilViews(TestCase):
     """Test Utility Views"""
 
     def setUp(self):
+        self.user = User.objects.create_superuser(
+            username='unittest user',
+            email='unittest@example.com',
+            password=''.join(SystemRandom().choice(
+                string.ascii_uppercase + string.digits) for _ in range(8)))
         self.factory = RequestFactory()
 
     def test_loading_view(self):
@@ -21,5 +29,6 @@ class TestUtilViews(TestCase):
     def test_permission_denied_view(self):
         """Test PermissionDeniedView"""
         request = self.factory.get('something')
+        request.user = self.user
         response = PermissionDeniedView.as_view()(request)
         self.assertEqual(response.status_code, 200)
