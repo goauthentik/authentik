11630c9a743ba1fed18cc9805dd78c35af7131b6
Jens Langhammer <jens.langhammer@beryju.org>
2019-04-10 22:38:25 +0200

    switch kubernetes deployment to daphne server

---
+++ passbook/core/asgi.py
@@ -0,0 +1,13 @@
+"""
+ASGI entrypoint. Configures Django and then runs the application
+defined in the ASGI_APPLICATION setting.
+"""
+
+import os
+
+import django
+from channels.routing import get_default_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "passbook.core.settings")
+django.setup()
+application = get_default_application()
