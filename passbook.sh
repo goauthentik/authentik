57de6cbafcae5b56271cde31587c29c0c51b1e32
Jens Langhammer <jens.langhammer@haufe-lexware.com>
2019-04-29 17:05:39 +0200

    repo cleanup, switch to new docker registry

--- passbook.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Check if this file is a symlink, if so, read real base dir
-BASE_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
-
-cd $BASE_DIR
-PYTHONPATH="${BASE_DIR}/vendor/" python3 manage.py $@
