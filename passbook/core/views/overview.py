f576985cc921b2d2cc206ca03b4366c9f96fcb3e
Jens Langhammer <jens.langhammer@beryju.org>
2019-04-29 20:07:18 +0200

    remove django guardian and check application access via PolicyEngine

--- passbook/core/views/overview.py
+++ passbook/core/views/overview.py
@@ -2,7 +2,8 @@
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
-from guardian.shortcuts import get_objects_for_user
+from passbook.core.models import Application
+from passbook.core.policies import PolicyEngine
 
 
 class OverviewView(LoginRequiredMixin, TemplateView):
@@ -12,6 +13,11 @@ class OverviewView(LoginRequiredMixin, TemplateView):
     template_name = 'overview/index.html'
 
     def get_context_data(self, **kwargs):
-        kwargs['applications'] = get_objects_for_user(self.request.user,
-                                                      'passbook_core.view_application')
+        kwargs['applications'] = []
+        for application in Application.objects.all():
+            engine = PolicyEngine(application.policies.all())
+            engine.for_user(self.request.user).with_request(self.request)
+            engine.build()
+            if engine.passing:
+                kwargs['applications'].append(application)
         return super().get_context_data(**kwargs)
