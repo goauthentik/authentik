b1b1a27444f19d88999aa7a1251ac0586fe7432a
Langhammer, Jens <jens.langhammer@haufe-lexware.com>
2019-07-22 15:17:58 +0200

    client-packages: now deprecated

--- client-packages/sentry-auth-passbook/sentry_auth_passbook/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-from sentry.auth import register
-
-from .provider import PassbookOAuth2Provider
-
-register('passbook', PassbookOAuth2Provider)
--- client-packages/sentry-auth-passbook/tests/test_provider.py
+++
@@ -1,6 +0,0 @@
-from sentry.testutils import TestCase
-
-
-class GitHubOAuth2ProviderTest(TestCase):
-    def test_simple(self):
-        pass
