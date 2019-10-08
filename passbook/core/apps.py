088b9592cda47ea07ea79507e90889fb541a8cf9
Langhammer, Jens <jens.langhammer@haufe-lexware.com>
2019-10-08 15:04:38 +0200

    core(minor): remove unused code

--- passbook/core/apps.py
+++ passbook/core/apps.py
@@ -1,11 +1,5 @@
 """passbook core app config"""
-from importlib import import_module
-
 from django.apps import AppConfig
-from django.conf import settings
-from structlog import get_logger
-
-LOGGER = get_logger()
 
 
 class PassbookCoreConfig(AppConfig):
@@ -15,11 +9,3 @@ class PassbookCoreConfig(AppConfig):
     label = 'passbook_core'
     verbose_name = 'passbook Core'
     mountpoint = ''
-
-    def ready(self):
-        for factors_to_load in settings.PASSBOOK_CORE_FACTORS:
-            try:
-                import_module(factors_to_load)
-                LOGGER.info("Loaded factor", factor_class=factors_to_load)
-            except ImportError as exc:
-                LOGGER.debug(exc)
