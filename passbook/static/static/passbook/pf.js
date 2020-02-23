b99d23c11958360cf2eec48209dd7aeb70af2568
Jens Langhammer <jens.langhammer@beryju.org>
2020-02-23 15:32:20 +0100

    all: remove dead code

--- passbook/static/static/passbook/pf.js
+++ passbook/static/static/passbook/pf.js
@@ -52,34 +52,3 @@ document.querySelectorAll("input[name=name]").forEach((input) => {
         slugField.value = convertToSlug(slugField.value);
     });
 });
-
-// Dynamic Array field logic
-window.addEventListener('load', function () {
-
-    function addRemoveEventListener(widgetElement) {
-        widgetElement.querySelectorAll('.array-remove').forEach(function (element) {
-            element.addEventListener('click', function () {
-                this.parentNode.parentNode.remove();
-            });
-        });
-    }
-
-    document.querySelectorAll('.dynamic-array-widget').forEach(function (widgetElement) {
-
-        addRemoveEventListener(widgetElement);
-
-        widgetElement.querySelector('.add-array-item').addEventListener('click', function () {
-            var first = widgetElement.querySelector('.array-item');
-            var newElement = first.cloneNode(true);
-            var id_parts = newElement.querySelector('input').getAttribute('id').split('_');
-            var id = id_parts.slice(0, -1).join('_') + '_' + String(parseInt(id_parts.slice(-1)[0]) + 1);
-            newElement.querySelector('input').setAttribute('id', id);
-            newElement.querySelector('input').value = '';
-
-            addRemoveEventListener(newElement);
-            first.parentElement.insertBefore(newElement, first.parentNode.lastChild);
-        });
-
-    });
-
-});
