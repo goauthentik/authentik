6fd19c0a379ae04f83ccecba1dc05cb5efbe29b8
Jens Langhammer <jens.langhammer@beryju.org>
2020-05-11 11:39:58 +0200

    flows: add caching of plan, add planner unittests

--- passbook/flows/planner.py
+++ passbook/flows/planner.py
@@ -1,11 +1,13 @@
 """Flows Planner"""
 from dataclasses import dataclass, field
 from time import time
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, List, Optional, Tuple
 
+from django.core.cache import cache
 from django.http import HttpRequest
 from structlog import get_logger
 
+from passbook.core.models import User
 from passbook.flows.exceptions import EmptyFlowException, FlowNonApplicableException
 from passbook.flows.models import Flow, Stage
 from passbook.policies.engine import PolicyEngine
@@ -14,6 +16,14 @@ LOGGER = get_logger()
 
 PLAN_CONTEXT_PENDING_USER = "pending_user"
 PLAN_CONTEXT_SSO = "is_sso"
+
+
+def cache_key(flow: Flow, user: Optional[User] = None) -> str:
+    """Generate Cache key for flow"""
+    prefix = f"flow_{flow.pk}"
+    if user:
+        prefix += f"#{user.pk}"
+    return prefix
 
 
 @dataclass
@@ -34,9 +44,11 @@ class FlowPlanner:
     """Execute all policies to plan out a flat list of all Stages
     that should be applied."""
 
+    use_cache: bool
     flow: Flow
 
     def __init__(self, flow: Flow):
+        self.use_cache = True
         self.flow = flow
 
     def _check_flow_root_policies(self, request: HttpRequest) -> Tuple[bool, List[str]]:
@@ -48,13 +60,17 @@ class FlowPlanner:
         """Check each of the flows' policies, check policies for each stage with PolicyBinding
         and return ordered list"""
         LOGGER.debug("f(plan): Starting planning process", flow=self.flow)
-        start_time = time()
-        plan = FlowPlan(flow_pk=self.flow.pk.hex)
         # First off, check the flow's direct policy bindings
         # to make sure the user even has access to the flow
         root_passing, root_passing_messages = self._check_flow_root_policies(request)
         if not root_passing:
             raise FlowNonApplicableException(root_passing_messages)
+        cached_plan = cache.get(cache_key(self.flow, request.user), None)
+        if cached_plan and self.use_cache:
+            LOGGER.debug("f(plan): Taking plan from cache", flow=self.flow)
+            return cached_plan
+        start_time = time()
+        plan = FlowPlan(flow_pk=self.flow.pk.hex)
         # Check Flow policies
         for stage in (
             self.flow.stages.order_by("flowstagebinding__order")
@@ -66,7 +82,7 @@ class FlowPlanner:
             engine.build()
             passing, _ = engine.result
             if passing:
-                LOGGER.debug("f(plan): Stage passing", stage=stage)
+                LOGGER.debug("f(plan): Stage passing", stage=stage, flow=self.flow)
                 plan.stages.append(stage)
         end_time = time()
         LOGGER.debug(
@@ -74,6 +90,7 @@ class FlowPlanner:
             flow=self.flow,
             duration_s=end_time - start_time,
         )
+        cache.set(cache_key(self.flow, request.user), plan)
         if not plan.stages:
             raise EmptyFlowException()
         return plan
