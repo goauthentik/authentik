6adfc84f060d18e3ea3db4988792eb25ff96bc4d
Jens Langhammer <jens.langhammer@beryju.org>
2020-05-24 16:58:01 +0200

    admin: fix PropertyMapping widget not rendering properly

--- passbook/core/migrations/0003_default_user.py
+++ passbook/core/migrations/0003_default_user.py
@@ -10,10 +10,11 @@ def create_default_user(apps: Apps, schema_editor: BaseDatabaseSchemaEditor):
     from passbook.core.models import User
 
     pbadmin = User.objects.create(
-        username="pbadmin", email="root@localhost",  # password="pbadmin"
+        username="pbadmin", email="root@localhost", name="passbook Default Admin"
     )
     pbadmin.set_password("pbadmin")  # noqa # nosec
     pbadmin.is_superuser = True
+    pbadmin.is_staff = True
     pbadmin.save()
 
 
