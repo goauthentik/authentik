d33f632203b98c09c8dcf3c2cd0ca86339878515
Jens Langhammer <jens.langhammer@beryju.org>
2020-06-30 00:11:01 +0200

    flows: add CancelView to cancel current flow execution

--- passbook/flows/urls.py
+++ passbook/flows/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 from passbook.flows.models import FlowDesignation
 from passbook.flows.views import (
+    CancelView,
     FlowExecutorShellView,
     FlowExecutorView,
     FlowPermissionDeniedView,
@@ -36,6 +37,7 @@ urlpatterns = [
         ToDefaultFlow.as_view(designation=FlowDesignation.UNRENOLLMENT),
         name="default-unenrollment",
     ),
+    path("-/cancel/", CancelView.as_view(), name="cancel"),
     path("b/<slug:flow_slug>/", FlowExecutorView.as_view(), name="flow-executor"),
     path(
         "<slug:flow_slug>/", FlowExecutorShellView.as_view(), name="flow-executor-shell"
