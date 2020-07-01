310b31a8b723dd90facb0ab3fdc6ae979e276fdc
Jens Langhammer <jens.langhammer@beryju.org>
2020-07-01 22:35:38 +0200

    core: fix linting

--- passbook/core/tests/tests_tasks.py
+++ passbook/core/tests/tests_tasks.py
@@ -1,10 +1,10 @@
 """passbook user view tests"""
-from guardian.shortcuts import get_anonymous_user
 from django.test import TestCase
 from django.utils.timezone import now
+from guardian.shortcuts import get_anonymous_user
 
-from passbook.core.tasks import clean_tokens
 from passbook.core.models import Token
+from passbook.core.tasks import clean_tokens
 
 
 class TestTasks(TestCase):
@@ -12,14 +12,7 @@ class TestTasks(TestCase):
 
     def test_token_cleanup(self):
         """Test Token cleanup task"""
-        Token.objects.create(
-            expires=now(),
-            user=get_anonymous_user()
-        )
-        self.assertEqual(
-            Token.objects.all().count(), 1
-        )
+        Token.objects.create(expires=now(), user=get_anonymous_user())
+        self.assertEqual(Token.objects.all().count(), 1)
         clean_tokens()
-        self.assertEqual(
-            Token.objects.all().count(), 0
-        )
+        self.assertEqual(Token.objects.all().count(), 0)
