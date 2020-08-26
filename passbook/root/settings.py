c5be90c650807556b6fbbb9f3289c7ce28187e5d
Jens Langhammer <jens.langhammer@beryju.org>
2020-08-26 17:38:30 +0200

    root: implement logger for ASGI requests, use structured logs for gunicorn

--- passbook/root/settings.py
+++ passbook/root/settings.py
@@ -187,7 +187,6 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "passbook.root.wsgi.application"
 ASGI_APPLICATION = "passbook.root.routing.application"
 
 CHANNEL_LAYERS = {
@@ -375,7 +374,7 @@ _LOGGING_HANDLER_MAP = {
     "grpc": LOG_LEVEL,
     "docker": "WARNING",
     "urllib3": "WARNING",
-    "websockets": "WARNING"
+    "websockets": "WARNING",
 }
 for handler_name, level in _LOGGING_HANDLER_MAP.items():
     # pyright: reportGeneralTypeIssues=false
