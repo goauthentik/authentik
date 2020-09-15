e95bbfab9a95c4e523da095497fed38361942635
Jens Langhammer <jens.langhammer@beryju.org>
2020-09-15 11:25:59 +0200

    outposts: disable WIP k8s controller

--- passbook/outposts/settings.py
+++ passbook/outposts/settings.py
@@ -1,10 +1,10 @@
 """Outposts Settings"""
 from celery.schedules import crontab
 
-CELERY_BEAT_SCHEDULE = {
-    "outposts_k8s": {
-        "task": "passbook.outposts.tasks.outpost_k8s_controller",
-        "schedule": crontab(minute="*/5"),  # Run every 5 minutes
-        "options": {"queue": "passbook_scheduled"},
-    }
-}
+# CELERY_BEAT_SCHEDULE = {
+#     "outposts_k8s": {
+#         "task": "passbook.outposts.tasks.outpost_k8s_controller",
+#         "schedule": crontab(minute="*/5"),  # Run every 5 minutes
+#         "options": {"queue": "passbook_scheduled"},
+#     }
+# }
