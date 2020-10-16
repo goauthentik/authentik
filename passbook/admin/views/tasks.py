7154f196689ab20c6d1438e2740e8fecbefe7cc2
Jens Langhammer <jens.langhammer@beryju.org>
2020-10-16 14:36:40 +0200

    admin: fix task list not being sorted

--- passbook/admin/views/tasks.py
+++ passbook/admin/views/tasks.py
@@ -14,7 +14,9 @@ class TaskListView(AdminRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         kwargs = super().get_context_data(**kwargs)
-        kwargs["object_list"] = TaskInfo.all()
+        kwargs["object_list"] = sorted(
+            TaskInfo.all().values(), key=lambda x: x.task_name
+        )
         kwargs["task_successful"] = TaskResultStatus.SUCCESSFUL
         kwargs["task_warning"] = TaskResultStatus.WARNING
         kwargs["task_error"] = TaskResultStatus.ERROR
