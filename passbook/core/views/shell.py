c9712facf34684fe7e83b0266caafedef15a0088
Jens Langhammer <jens.langhammer@beryju.org>
2020-11-22 19:42:47 +0100

    core: fix ShellView not being login only

--- passbook/core/views/shell.py
+++ passbook/core/views/shell.py
@@ -1,8 +1,9 @@
 """core shell view"""
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
 
 
-class ShellView(TemplateView):
+class ShellView(LoginRequiredMixin, TemplateView):
     """core shell view"""
 
     template_name = "shell.html"
