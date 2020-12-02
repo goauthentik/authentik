a26f06c654464ba27b66f191f7e72a7f5a06c739
Jens Langhammer <jens.langhammer@beryju.org>
2020-12-03 00:19:34 +0100

    lifecycle: ignore migration files

--- lifecycle/system_migrations/to_0_10.py
+++ lifecycle/system_migrations/to_0_10.py
@@ -1,34 +1,37 @@
+# flake8: noqa
 from os import system
 
 from lifecycle.migrate import BaseMigration
 
-SQL_STATEMENT = """delete from django_migrations where app = 'passbook_stages_prompt';
-drop table passbook_stages_prompt_prompt cascade;
-drop table passbook_stages_prompt_promptstage cascade;
-drop table passbook_stages_prompt_promptstage_fields;
-drop table corsheaders_corsmodel cascade;
-drop table oauth2_provider_accesstoken cascade;
-drop table oauth2_provider_grant cascade;
-drop table oauth2_provider_refreshtoken cascade;
-drop table oidc_provider_client cascade;
-drop table oidc_provider_client_response_types cascade;
-drop table oidc_provider_code cascade;
-drop table oidc_provider_responsetype cascade;
-drop table oidc_provider_rsakey cascade;
-drop table oidc_provider_token cascade;
-drop table oidc_provider_userconsent cascade;
-drop table passbook_providers_app_gw_applicationgatewayprovider cascade;
-delete from django_migrations where app = 'passbook_flows' and name = '0008_default_flows';
-delete from django_migrations where app = 'passbook_flows' and name = '0009_source_flows';
-delete from django_migrations where app = 'passbook_flows' and name = '0010_provider_flows';
-delete from django_migrations where app = 'passbook_stages_password' and
-name = '0002_passwordstage_change_flow';"""
+SQL_STATEMENT = """
+BEGIN TRANSACTION;
+DELETE FROM django_migrations WHERE app = 'passbook_stages_prompt';
+DROP TABLE passbook_stages_prompt_prompt cascade;
+DROP TABLE passbook_stages_prompt_promptstage cascade;
+DROP TABLE passbook_stages_prompt_promptstage_fields;
+DROP TABLE corsheaders_corsmodel cascade;
+DROP TABLE oauth2_provider_accesstoken cascade;
+DROP TABLE oauth2_provider_grant cascade;
+DROP TABLE oauth2_provider_refreshtoken cascade;
+DROP TABLE oidc_provider_client cascade;
+DROP TABLE oidc_provider_client_response_types cascade;
+DROP TABLE oidc_provider_code cascade;
+DROP TABLE oidc_provider_responsetype cascade;
+DROP TABLE oidc_provider_rsakey cascade;
+DROP TABLE oidc_provider_token cascade;
+DROP TABLE oidc_provider_userconsent cascade;
+DROP TABLE passbook_providers_app_gw_applicationgatewayprovider cascade;
+DELETE FROM django_migrations WHERE app = 'passbook_flows' AND name = '0008_default_flows';
+DELETE FROM django_migrations WHERE app = 'passbook_flows' AND name = '0009_source_flows';
+DELETE FROM django_migrations WHERE app = 'passbook_flows' AND name = '0010_provider_flows';
+DELETE FROM django_migrations WHERE app = 'passbook_stages_password' AND name = '0002_passwordstage_change_flow';
+COMMIT;"""
 
 
 class Migration(BaseMigration):
     def needs_migration(self) -> bool:
         self.cur.execute(
-            "select * from information_schema.tables where table_name='oidc_provider_client'"
+            "select * from information_schema.tables WHERE table_name='oidc_provider_client'"
         )
         return bool(self.cur.rowcount)
 
