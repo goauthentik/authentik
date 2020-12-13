e120d274e94cd88aa9d06c31c475c90bcc0aba50
Jens Langhammer <jens.langhammer@beryju.org>
2020-12-13 20:38:50 +0100

    lib: fix sentry tests not running

--- authentik/lib/tests/test_sentry.py
+++ authentik/lib/tests/test_sentry.py
@@ -7,12 +7,12 @@ from authentik.lib.sentry import SentryIgnoredException, before_send
 class TestSentry(TestCase):
     """test sentry integration"""
 
-    def error_not_sent(self):
+    def test_error_not_sent(self):
         """Test SentryIgnoredError not sent"""
         self.assertIsNone(
             before_send(None, {"exc_info": (0, SentryIgnoredException(), 0)})
         )
 
-    def error_sent(self):
+    def test_error_sent(self):
         """Test error sent"""
         self.assertIsNone(before_send(None, {"exc_info": (0, ValueError(), 0)}))
