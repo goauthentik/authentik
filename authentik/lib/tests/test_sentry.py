ea6ca23f5711aa55d09d5aeae924a209f679b87a
Jens Langhammer <jens.langhammer@beryju.org>
2020-12-13 18:41:43 +0100

    lib: add tests for sentry integration

---
+++ authentik/lib/tests/test_sentry.py
@@ -0,0 +1,14 @@
+"""test sentry integration"""
+from django.test import TestCase
+from authentik.lib.sentry import before_send, SentryIgnoredException
+
+class TestSentry(TestCase):
+    """test sentry integration"""
+
+    def error_not_sent(self):
+        """Test SentryIgnoredError not sent"""
+        self.assertIsNone(before_send(None, {"exc_info": (0, SentryIgnoredException(), 0)}))
+
+    def error_sent(self):
+        """Test error sent"""
+        self.assertIsNone(before_send(None, {"exc_info": (0, ValueError(), 0)}))
