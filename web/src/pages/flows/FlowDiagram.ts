3c71c1351e7d3dc5c56a88b2321516cf1b7b55d2
Jens Langhammer <jens.langhammer@beryju.org>
2020-12-19 00:55:50 +0100

    web: fix formatting for web

--- web/src/pages/flows/FlowDiagram.ts
+++ web/src/pages/flows/FlowDiagram.ts
@@ -13,15 +13,17 @@ export class FlowDiagram extends LitElement {
     }
 
     render(): TemplateResult {
-        Flow.diagram(this.flowSlug!).then((data) => {
-            var diagram = FlowChart.parse(data);
-            diagram.drawSVG(this, {
-                'font-color': '#fafafa',
-                'line-color': '#bebebe',
-                'element-color': '#bebebe',
-                'fill': "#2b2e33",
+        if (this.flowSlug) {
+            Flow.diagram(this.flowSlug).then((data) => {
+                const diagram = FlowChart.parse(data);
+                diagram.drawSVG(this, {
+                    "font-color": "#fafafa",
+                    "line-color": "#bebebe",
+                    "element-color": "#bebebe",
+                    "fill": "#2b2e33",
+                });
             });
-        })
+        }
         return html``;
     }
 
