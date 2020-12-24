9bf2610783730ed29d3fcead4c77010e18b627a9
Jens Langhammer <jens.langhammer@beryju.org>
2020-12-24 11:44:21 +0100

    events: adjust unittests for user handling

--- authentik/events/tests/test_event.py
+++ authentik/events/tests/test_event.py
@@ -1,5 +1,6 @@
 """events event tests"""
 
+from authentik.core.models import Group
 from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
 from guardian.shortcuts import get_anonymous_user
@@ -13,12 +14,22 @@ class TestEvents(TestCase):
 
     def test_new_with_model(self):
         """Create a new Event passing a model as kwarg"""
-        event = Event.new("unittest", test={"model": get_anonymous_user()})
+        test_model = Group.objects.create(name="test")
+        event = Event.new("unittest", test={"model": test_model})
         event.save()  # We save to ensure nothing is un-saveable
-        model_content_type = ContentType.objects.get_for_model(get_anonymous_user())
+        model_content_type = ContentType.objects.get_for_model(test_model)
         self.assertEqual(
             event.context.get("test").get("model").get("app"),
             model_content_type.app_label,
+        )
+
+    def test_new_with_user(self):
+        """Create a new Event passing a user as kwarg"""
+        event = Event.new("unittest", test={"model": get_anonymous_user()})
+        event.save()  # We save to ensure nothing is un-saveable
+        self.assertEqual(
+            event.context.get("test").get("model").get("username"),
+            get_anonymous_user().username,
         )
 
     def test_new_with_uuid_model(self):
