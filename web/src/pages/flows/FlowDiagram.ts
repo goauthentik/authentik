37076d0daf01e25f144b697f8526e0b3751866e2
Jens Langhammer <jens.langhammer@beryju.org>
2020-12-26 16:07:02 +0100

    web: flows/diagram: add support for light mode

--- web/src/pages/flows/FlowDiagram.ts
+++ web/src/pages/flows/FlowDiagram.ts
@@ -1,30 +1,62 @@
 import { customElement, html, LitElement, property, TemplateResult } from "lit-element";
 import FlowChart from "flowchart.js";
 import { Flow } from "../../api/Flows";
+import { loading } from "../../utils";
+
+export const FONT_COLOUR_DARK_MODE = "#fafafa";
+export const FONT_COLOUR_LIGHT_MODE = "#151515";
+export const FILL_DARK_MODE = "#18191a";
+export const FILL_LIGHT_MODE = "#f0f0f0";
 
 @customElement("ak-flow-diagram")
 export class FlowDiagram extends LitElement {
 
     @property()
-    flowSlug?: string;
+    set flowSlug(value: string) {
+        Flow.diagram(value).then((data) => {
+            this.diagram = FlowChart.parse(data);
+        });
+    }
+
+    @property({attribute: false})
+    diagram?: FlowChart.Instance;
+
+    @property()
+    fontColour: string = FONT_COLOUR_DARK_MODE;
+
+    @property()
+    fill: string = FILL_DARK_MODE;
 
     createRenderRoot(): Element | ShadowRoot {
         return this;
     }
 
+    constructor() {
+        super();
+        window.matchMedia("(prefers-color-scheme: light)").addEventListener("change", (ev) => {
+            if (ev.matches) {
+                this.fontColour = FONT_COLOUR_LIGHT_MODE;
+                this.fill = FILL_LIGHT_MODE;
+            } else {
+                this.fontColour = FONT_COLOUR_DARK_MODE;
+                this.fill = FILL_DARK_MODE;
+            }
+        });
+    }
+
     render(): TemplateResult {
-        if (this.flowSlug) {
-            Flow.diagram(this.flowSlug).then((data) => {
-                const diagram = FlowChart.parse(data);
-                diagram.drawSVG(this, {
-                    "font-color": "#fafafa",
-                    "line-color": "#bebebe",
-                    "element-color": "#bebebe",
-                    "fill": "#2b2e33",
-                });
+        if (this.diagram) {
+            this.diagram.drawSVG(this, {
+                "font-color": this.fontColour,
+                "line-color": "#bebebe",
+                "element-color": "#bebebe",
+                "fill": this.fill,
+                "yes-text": "Policy passes",
+                "no-text": "Policy denies",
             });
+            return html``;
         }
-        return html``;
+        return loading(this.diagram, html``);
     }
 
 }
