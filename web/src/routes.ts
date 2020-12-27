9f8eee4f003dea0c87a9a56410aa8dd745e0fb1e
Jens Langhammer <jens.langhammer@beryju.org>
2020-12-27 21:23:49 +0100

    web: don't use trailing slash for spa URLs

--- web/src/routes.ts
+++ web/src/routes.ts
@@ -10,18 +10,18 @@ import "./pages/flows/FlowViewPage";
 
 export const ROUTES: Route[] = [
     // Prevent infinite Shell loops
-    new Route(new RegExp("^/$")).redirect("/library/"),
-    new Route(new RegExp("^#.*")).redirect("/library/"),
-    new Route(new RegExp("^/library/$"), html`<ak-library></ak-library>`),
-    new Route(new RegExp("^/administration/overview/$"), html`<ak-admin-overview></ak-admin-overview>`),
-    new Route(new RegExp("^/applications/$"), html`<ak-application-list></ak-application-list>`),
-    new Route(new RegExp(`^/applications/(?<slug>${SLUG_REGEX})/$`)).then((args) => {
+    new Route(new RegExp("^/$")).redirect("/library"),
+    new Route(new RegExp("^#.*")).redirect("/library"),
+    new Route(new RegExp("^/library$"), html`<ak-library></ak-library>`),
+    new Route(new RegExp("^/administration/overview$"), html`<ak-admin-overview></ak-admin-overview>`),
+    new Route(new RegExp("^/applications$"), html`<ak-application-list></ak-application-list>`),
+    new Route(new RegExp(`^/applications/(?<slug>${SLUG_REGEX})$`)).then((args) => {
         return html`<ak-application-view .args=${args}></ak-application-view>`;
     }),
-    new Route(new RegExp(`^/sources/(?<slug>${SLUG_REGEX})/$`)).then((args) => {
+    new Route(new RegExp(`^/sources/(?<slug>${SLUG_REGEX})$`)).then((args) => {
         return html`<ak-source-view .args=${args}></ak-source-view>`;
     }),
-    new Route(new RegExp(`^/flows/(?<slug>${SLUG_REGEX})/$`)).then((args) => {
+    new Route(new RegExp(`^/flows/(?<slug>${SLUG_REGEX})$`)).then((args) => {
         return html`<ak-flow-view .args=${args}></ak-flow-view>`;
     }),
 ];
