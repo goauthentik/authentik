c8805cc082b9b48178c47455662f6477b7cdb95d
Jens Langhammer <jens.langhammer@beryju.org>
2020-12-28 17:45:57 +0100

    release: 0.14.0-stable

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -28,4 +28,4 @@ export const ColorStyles = css`
         background-color: var(--pf-global--danger-color--100);
     }
 `;
-export const VERSION = "0.14.0-rc2";
+export const VERSION = "0.14.0-stable";
