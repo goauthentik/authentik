82bb179bc212585db11d11fc51ac968ea20b820e
Jens L <jens@beryju.org>
2021-01-05 00:41:10 +0100

    root: global email settings (#448)
    * root: make global email settings configurable
    
    * stages/email: add use_global_settings
    
    * stages/email: add test_email command to test email sending
    
    * stages/email: update email template
    
    * stages/email: simplify email template path
    
    * stages/email: add support for user-supplied email templates
    
    * stages/email: add tests for sending and templates
    
    * stages/email: only add custom template if permissions are correct
    
    * docs: add custom email template docs
    
    * root: add /templates volume in docker-compose by default
    
    * stages/email: fix form not allowing custom templates
    
    * stages/email: use relative path for custom templates
    
    * stages/email: check if all templates exist on startup, reset
    
    * docs: add global email docs for docker-compose
    
    * helm: add email config to helm chart
    
    * helm: load all secrets with env prefix
    
    * helm: move s3 and smtp secret to secret
    
    * stages/email: fix test for relative name
    
    * stages/email: add argument to send email from existing stage
    
    * stages/email: set uid using slug of message id
    
    * stages/email: ensure template validation ignores migration runs
    
    * docs: add email troubleshooting docs
    
    * stages/email: fix long task_name breaking task list

---
+++ authentik/stages/email/tests/test_sending.py
@@ -0,0 +1,83 @@
+"""email tests"""
+from smtplib import SMTPException
+from unittest.mock import MagicMock, patch
+
+from django.core import mail
+from django.core.mail.backends.locmem import EmailBackend
+from django.shortcuts import reverse
+from django.test import Client, TestCase
+
+from authentik.core.models import User
+from authentik.flows.markers import StageMarker
+from authentik.flows.models import Flow, FlowDesignation, FlowStageBinding
+from authentik.flows.planner import PLAN_CONTEXT_PENDING_USER, FlowPlan
+from authentik.flows.views import SESSION_KEY_PLAN
+from authentik.stages.email.models import EmailStage
+
+
+class TestEmailStageSending(TestCase):
+    """Email tests"""
+
+    def setUp(self):
+        super().setUp()
+        self.user = User.objects.create_user(
+            username="unittest", email="test@beryju.org"
+        )
+        self.client = Client()
+
+        self.flow = Flow.objects.create(
+            name="test-email",
+            slug="test-email",
+            designation=FlowDesignation.AUTHENTICATION,
+        )
+        self.stage = EmailStage.objects.create(
+            name="email",
+        )
+        FlowStageBinding.objects.create(target=self.flow, stage=self.stage, order=2)
+
+    def test_pending_user(self):
+        """Test with pending user"""
+        plan = FlowPlan(
+            flow_pk=self.flow.pk.hex, stages=[self.stage], markers=[StageMarker()]
+        )
+        plan.context[PLAN_CONTEXT_PENDING_USER] = self.user
+        session = self.client.session
+        session[SESSION_KEY_PLAN] = plan
+        session.save()
+
+        url = reverse(
+            "authentik_flows:flow-executor", kwargs={"flow_slug": self.flow.slug}
+        )
+        with self.settings(
+            EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend"
+        ):
+            response = self.client.post(url)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(len(mail.outbox), 1)
+            self.assertEqual(mail.outbox[0].subject, "authentik")
+
+    def test_send_error(self):
+        """Test error during sending (sending will be retried)"""
+        plan = FlowPlan(
+            flow_pk=self.flow.pk.hex, stages=[self.stage], markers=[StageMarker()]
+        )
+        plan.context[PLAN_CONTEXT_PENDING_USER] = self.user
+        session = self.client.session
+        session[SESSION_KEY_PLAN] = plan
+        session.save()
+
+        url = reverse(
+            "authentik_flows:flow-executor", kwargs={"flow_slug": self.flow.slug}
+        )
+        with self.settings(
+            EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend"
+        ):
+            with patch(
+                "django.core.mail.backends.locmem.EmailBackend.send_messages",
+                MagicMock(side_effect=[SMTPException, EmailBackend.send_messages]),
+            ):
+                response = self.client.post(url)
+            response = self.client.post(url)
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue(len(mail.outbox) >= 1)
+            self.assertEqual(mail.outbox[0].subject, "authentik")
