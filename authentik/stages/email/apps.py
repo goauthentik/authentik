e683cb455e9af08b360bf8fb3208f6125c408432
Jens Langhammer <jens.langhammer@beryju.org>
2021-01-04 22:43:15 +0100

    stages/email: check if all templates exist on startup, reset

--- authentik/stages/email/apps.py
+++ authentik/stages/email/apps.py
@@ -2,6 +2,11 @@
 from importlib import import_module
 
 from django.apps import AppConfig
+from django.template.exceptions import TemplateDoesNotExist
+from django.template.loader import get_template
+from structlog.stdlib import get_logger
+
+LOGGER = get_logger()
 
 
 class AuthentikStageEmailConfig(AppConfig):
@@ -12,4 +17,24 @@ class AuthentikStageEmailConfig(AppConfig):
     verbose_name = "authentik Stages.Email"
 
     def ready(self):
+        from authentik.stages.email.models import EmailStage, EmailTemplates
+        from authentik.events.models import Event, EventAction
+
         import_module("authentik.stages.email.tasks")
+        for stage in EmailStage.objects.all():
+            try:
+                get_template(stage.template)
+            except TemplateDoesNotExist:
+                LOGGER.warning(
+                    "Stage template does not exist, resetting", path=stage.template
+                )
+                Event.new(
+                    EventAction.CONFIGURATION_ERROR,
+                    stage=stage,
+                    message=(
+                        f"Template {stage.template} does not exist, resetting to default."
+                        f" (Stage {stage.name})"
+                    ),
+                ).save()
+                stage.template = EmailTemplates.ACCOUNT_CONFIRM
+                stage.save()
