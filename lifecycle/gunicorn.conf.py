bfe8bb5e61d3fdcbbc958658211b4faee6d8a18b
Jens Langhammer <jens.langhammer@beryju.org>
2021-01-27 10:13:23 +0100

    lifecycle: fix typo causing single process in docker-compose

--- lifecycle/gunicorn.conf.py
+++ lifecycle/gunicorn.conf.py
@@ -51,7 +51,7 @@ logconfig_dict = {
 if Path("/var/run/secrets/kubernetes.io").exists():
     workers = 2
 else:
-    worker = cpu_count() * 2 + 1
+    workers = cpu_count() * 2 + 1
 threads = 4
 
 warnings.simplefilter("once")
