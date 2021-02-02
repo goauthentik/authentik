0e8583d4be872a199e2a64e4c188fe7e853eb2fc
Jens Langhammer <jens.langhammer@beryju.org>
2021-02-02 21:50:21 +0100

    providers/saml: implement managed objects for SAML Provider

---
+++ authentik/providers/saml/managed.py
@@ -0,0 +1,48 @@
+"""SAML Provider managed objects"""
+from authentik.managed.manager import EnsureExists, ObjectManager
+from authentik.providers.saml.models import SAMLPropertyMapping
+
+
+class SAMLProviderManager(ObjectManager):
+    """SAML Provider managed objects"""
+
+    def reconcile(self):
+        return [
+            EnsureExists(
+                SAMLPropertyMapping,
+                "saml_name",
+                name="authentik default SAML Mapping: UPN",
+                saml_name="http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn",
+                expression="return user.attributes.get('upn', user.email)",
+            ),
+            EnsureExists(
+                SAMLPropertyMapping,
+                "saml_name",
+                name="authentik default SAML Mapping: Name",
+                saml_name="http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name",
+                expression="return user.name",
+            ),
+            EnsureExists(
+                SAMLPropertyMapping,
+                "saml_name",
+                name="authentik default SAML Mapping: Email",
+                saml_name="http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress",
+                expression="return user.email",
+            ),
+            EnsureExists(
+                SAMLPropertyMapping,
+                "saml_name",
+                name="authentik default SAML Mapping: WindowsAccountname (Username)",
+                saml_name=(
+                    "http://schemas.microsoft.com/ws/2008/06/identity/claims/windowsaccountname"
+                ),
+                expression="return user.username",
+            ),
+            EnsureExists(
+                SAMLPropertyMapping,
+                "saml_name",
+                name="authentik default SAML Mapping: Groups",
+                saml_name="http://schemas.xmlsoap.org/claims/Group",
+                expression="for group in user.ak_groups.all():\n    yield group.name",
+            ),
+        ]
