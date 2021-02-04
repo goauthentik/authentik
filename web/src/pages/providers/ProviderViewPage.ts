7e2a471903e4c2273abba6b3a0cf05fc33ebafdc
Jens Langhammer <jens.langhammer@beryju.org>
2021-02-04 10:22:14 +0100

    web: fix linting issues

--- web/src/pages/providers/ProviderViewPage.ts
+++ web/src/pages/providers/ProviderViewPage.ts
@@ -1,12 +1,8 @@
-import { gettext } from "django";
-import { customElement, html, LitElement, property, TemplateResult } from "lit-element";
+import { customElement, LitElement, property } from "lit-element";
 import { Provider } from "../../api/Providers";
-import { PBResponse } from "../../api/Client";
-import { TablePage } from "../../elements/table/TablePage";
 
 import "../../elements/buttons/ModalButton";
 import "../../elements/buttons/SpinnerButton";
-import { TableColumn } from "../../elements/table/Table";
 
 @customElement("ak-provider-view")
 export class ProviderViewPage extends LitElement {
@@ -15,7 +11,7 @@ export class ProviderViewPage extends LitElement {
         this.providerID = value.id;
     }
 
-    @property()
+    @property({type: Number})
     set providerID(value: number) {
         Provider.get(value).then((app) => (this.provider = app));
     }
