a367d8515fa20e7db1db6a20d56819348531f1c3
Jens Langhammer <jens.langhammer@beryju.org>
2021-02-10 20:12:02 +0100

    core: add source endpoint

--- authentik/core/api/sources.py
+++ authentik/core/api/sources.py
@@ -1,8 +1,16 @@
 """Source API Views"""
+from authentik.lib.templatetags.authentik_utils import verbose_name
+from authentik.lib.utils.reflection import all_subclasses
+from drf_yasg2.utils import swagger_auto_schema
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from django.shortcuts import reverse
+from django.utils.translation import gettext_lazy as _
 
-from authentik.core.api.utils import MetaNameSerializer
+from authentik.core.api.utils import MetaNameSerializer, TypeCreateSerializer
 from authentik.core.models import Source
 
 
@@ -40,3 +48,20 @@ class SourceViewSet(ReadOnlyModelViewSet):
 
     def get_queryset(self):
         return Source.objects.select_subclasses()
+
+    @swagger_auto_schema(responses={200: TypeCreateSerializer(many=True)})
+    @action(detail=False)
+    # pylint: disable=unused-argument
+    def types(self, request: Request) -> Response:
+        """Get all creatable source types"""
+        data = []
+        for subclass in all_subclasses(self.queryset.model):
+            data.append(
+                {
+                    "name": verbose_name(subclass),
+                    "description": subclass.__doc__,
+                    "link": reverse("authentik_admin:source-create")
+                    + f"?type={subclass.__name__}",
+                }
+            )
+        return Response(TypeCreateSerializer(data, many=True).data)
