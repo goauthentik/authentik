277b4336d31f7bea2bc3c8e86c4e4bfa2b44675c
Jens Langhammer <jens.langhammer@beryju.org>
2021-02-19 16:00:59 +0100

    stages/authenticator_validate: update autocomplete for code input

--- web/src/elements/notifications/NotificationTrigger.ts
+++ web/src/elements/notifications/NotificationTrigger.ts
@@ -1,7 +1,12 @@
-import { customElement, html, LitElement, TemplateResult } from "lit-element";
+import { CSSResult, customElement, html, LitElement, TemplateResult } from "lit-element";
+import { COMMON_STYLES } from "../../common/styles";
 
 @customElement("ak-notification-trigger")
 export class NotificationRule extends LitElement {
+
+    static get styles(): CSSResult[] {
+        return COMMON_STYLES;
+    }
 
     constructor() {
         super();
@@ -16,7 +21,8 @@ export class NotificationRule extends LitElement {
     }
 
     render(): TemplateResult {
-        return html`<slot></slot>`;
+        // TODO: Show icon with red dot when unread notifications exist
+        return html`<i class="fas fa-bell pf-c-dropdown__toggle-icon" aria-hidden="true"></i>`;
     }
 
 }
