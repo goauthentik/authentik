74c0ed27baca51e1d022394f995d6ff705ffd7b3
Jens Langhammer <jens.langhammer@beryju.org>
2021-02-21 19:03:40 +0100

    web: fix /-/user/ not loading

--- web/src/elements/router/Router404.ts
+++ web/src/elements/router/Router404.ts
@@ -18,7 +18,7 @@ export class Router404 extends LitElement {
                 <i class="fas fa-question-circle pf-c-empty-state__icon" aria-hidden="true"></i>
                 <h1 class="pf-c-title pf-m-lg">${gettext("Not found")}</h1>
                 <div class="pf-c-empty-state__body">
-                    ${gettext(`The url '${this.url}' was not found.`)}
+                    ${gettext(`The URL '${this.url}' was not found.`)}
                 </div>
                 <a href="#/" class="pf-c-button pf-m-primary" type="button">${gettext("Return home")}</a>
             </div>
