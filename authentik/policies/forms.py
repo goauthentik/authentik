a4d5815e1b8321a2d93099e1bc86e59eea2f69db
Jens Langhammer <jens.langhammer@beryju.org>
2021-02-27 13:19:38 +0100

    policies: sort groups in groupmembership policy and binding
    closes #595
    
    # Conflicts:
    #	authentik/policies/group_membership/forms.py

--- authentik/policies/forms.py
+++ authentik/policies/forms.py
@@ -2,6 +2,7 @@
 
 from django import forms
 
+from authentik.core.models import Group
 from authentik.lib.widgets import GroupedModelChoiceField
 from authentik.policies.models import Policy, PolicyBinding, PolicyBindingModel
 
@@ -18,6 +19,9 @@ class PolicyBindingForm(forms.ModelForm):
     )
     policy = GroupedModelChoiceField(
         queryset=Policy.objects.all().select_subclasses(),
+    )
+    group = forms.ModelChoiceField(
+        queryset=Group.objects.all().order_by("name"), required=False
     )
 
     def __init__(self, *args, **kwargs):
--- authentik/policies/group_membership/forms.py
+++ authentik/policies/group_membership/forms.py
@@ -2,12 +2,15 @@
 
 from django import forms
 
+from authentik.core.models import Group
 from authentik.policies.forms import GENERAL_FIELDS
 from authentik.policies.group_membership.models import GroupMembershipPolicy
 
 
 class GroupMembershipPolicyForm(forms.ModelForm):
     """GroupMembershipPolicy Form"""
+
+    group = forms.ModelChoiceField(queryset=Group.objects.all().order_by("name"))
 
     class Meta:
 
