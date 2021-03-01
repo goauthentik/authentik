37b59bb5b9e0641a0de823aae1cd9bbe139aa51e
Jens Langhammer <jens.langhammer@beryju.org>
2021-03-01 18:30:27 +0100

    lib: sort GroupedModelChoiceField by name
    closes #602

--- authentik/lib/widgets.py
+++ authentik/lib/widgets.py
@@ -10,7 +10,7 @@ class GroupedModelChoiceIterator(ModelChoiceIterator):
     def __iter__(self):
         if self.field.empty_label is not None:
             yield ("", self.field.empty_label)
-        queryset = self.queryset
+        queryset = self.queryset.order_by("name")
         # Can't use iterator() when queryset uses prefetch_related()
         if not queryset._prefetch_related_lookups:
             queryset = queryset.iterator()
