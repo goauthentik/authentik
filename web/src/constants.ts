2e659c1ab07201bd196550f4accfcc6e4282ec60
Jens Langhammer <jens.langhammer@beryju.org>
2021-03-02 09:41:09 +0100

    release: 2021.3.1-rc1

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -3,4 +3,4 @@ export const SUCCESS_CLASS = "pf-m-success";
 export const ERROR_CLASS = "pf-m-danger";
 export const PROGRESS_CLASS = "pf-m-in-progress";
 export const CURRENT_CLASS = "pf-m-current";
-export const VERSION = "2021.2.6-stable";
+export const VERSION = "2021.3.1-rc1";
