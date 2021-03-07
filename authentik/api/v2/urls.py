46108efa77763523f349c646ec9e05e50f53e754
Jens Langhammer <jens.langhammer@beryju.org>
2021-03-07 16:46:17 +0100

    api: remove broken swagger UI

--- authentik/api/v2/urls.py
+++ authentik/api/v2/urls.py
@@ -177,12 +177,6 @@ urlpatterns = [
         name="schema-json",
     ),
     path(
-        "swagger/",
-        SchemaView.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
-    path("redoc/", SchemaView.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-    path(
         "flows/executor/<slug:flow_slug>/",
         FlowExecutorView.as_view(),
         name="flow-executor",
