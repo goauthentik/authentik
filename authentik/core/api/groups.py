32fb90e056b8d2668cfebc0cbf181be033a921a5
Jens Langhammer <jens.langhammer@beryju.org>
2021-03-27 15:53:54 +0100

    core: include full users in group API
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/core/api/groups.py
+++ authentik/core/api/groups.py
@@ -2,16 +2,20 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework.viewsets import ModelViewSet
 
+from authentik.core.api.users import UserSerializer
 from authentik.core.models import Group
 
 
 class GroupSerializer(ModelSerializer):
     """Group Serializer"""
 
+    users = UserSerializer(many=True)
+
     class Meta:
 
         model = Group
         fields = ["pk", "name", "is_superuser", "parent", "users", "attributes"]
+        depth = 2
 
 
 class GroupViewSet(ModelViewSet):
