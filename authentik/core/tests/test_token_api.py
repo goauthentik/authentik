330d5047e7dc6d3c24359969c8ebffdc4a1c8507
Jens Langhammer <jens.langhammer@beryju.org>
2021-03-31 20:36:28 +0200

    core: add tests for token API
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

---
+++ authentik/core/tests/test_token_api.py
@@ -0,0 +1,22 @@
+"""Test token API"""
+from django.urls.base import reverse
+from rest_framework.test import APITestCase
+
+from authentik.core.models import Token, User
+
+
+class TestTokenAPI(APITestCase):
+    """Test token API"""
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.user = User.objects.get(username="akadmin")
+        self.client.force_login(self.user)
+
+    def test_test_call(self):
+        """Test token creation endpoint"""
+        response = self.client.post(
+            reverse("authentik_api:token-list"), {"identifier": "test-token"}
+        )
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Token.objects.get(identifier="test-token").user, self.user)
