fc1caf1469330236f7fd8b8318e6ba1b262671c7
Jens Langhammer <jens.langhammer@beryju.org>
2021-04-03 19:07:54 +0200

    api: remove js i18n catalog
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/api/urls.py
+++ authentik/api/urls.py
@@ -1,10 +1,8 @@
 """authentik api urls"""
 from django.urls import include, path
-from django.views.i18n import JavaScriptCatalog
 
 from authentik.api.v2.urls import urlpatterns as v2_urls
 
 urlpatterns = [
     path("v2beta/", include(v2_urls)),
-    path("jsi18n/", JavaScriptCatalog.as_view(), name="javascript-catalog"),
 ]
