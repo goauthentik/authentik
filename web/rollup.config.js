d4982b276ce9f7dacfa7b72d570fcdaa89dd53ad
Jens Langhammer <jens.langhammer@beryju.org>
2021-04-04 18:35:16 +0200

    web: fix mismatched css files between static and SSR
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- web/rollup.config.js
+++ web/rollup.config.js
@@ -15,7 +15,6 @@ const extensions = [
 const resources = [
     { src: "node_modules/rapidoc/dist/rapidoc-min.js", dest: "dist/" },
 
-    { src: "node_modules/@patternfly/patternfly/patternfly-base.css", dest: "dist/" },
     { src: "node_modules/@patternfly/patternfly/patternfly.min.css", dest: "dist/" },
     { src: "node_modules/@patternfly/patternfly/patternfly.min.css.map", dest: "dist/" },
     { src: "src/authentik.css", dest: "dist/" },
