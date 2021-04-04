596431cae7fb24d2c8f710b02518cde1076ce466
Jens Langhammer <jens.langhammer@beryju.org>
2021-04-04 16:45:38 +0200

    web/admin: replace paper-input on user settings
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- web/src/pages/user-settings/UserDetailsPage.ts
+++ web/src/pages/user-settings/UserDetailsPage.ts
@@ -8,13 +8,13 @@ import PFForm from "@patternfly/patternfly/components/Form/form.css";
 import PFFormControl from "@patternfly/patternfly/components/FormControl/form-control.css";
 import { CoreApi, User } from "authentik-api";
 import { me } from "../../api/Users";
+import { FlowURLManager } from "../../api/legacy";
+import { ifDefined } from "lit-html/directives/if-defined";
+import { DEFAULT_CONFIG } from "../../api/Config";
 import "../../elements/forms/FormElement";
 import "../../elements/EmptyState";
-import { FlowURLManager } from "../../api/legacy";
-import "@polymer/paper-input/paper-input";
-import "@polymer/iron-form/iron-form";
-import { DEFAULT_CONFIG } from "../../api/Config";
 import "../../elements/forms/Form";
+import "../../elements/forms/HorizontalFormElement";
 
 @customElement("ak-user-details")
 export class UserDetailsPage extends LitElement {
@@ -53,27 +53,26 @@ export class UserDetailsPage extends LitElement {
                         });
                     }}>
                     <form class="pf-c-form pf-m-horizontal">
-                        <paper-input
-                            name="username"
-                            ?alwaysFloatLabel=${true}
-                            label="${t`Username`}"
-                            value=${this.user.username}>
-                        </paper-input>
-                        <p class="pf-c-form__helper-text">${t`Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.`}</p>
-                        <paper-input
-                            name="name"
-                            ?alwaysFloatLabel=${true}
-                            label="${t`Name`}"
-                            value=${this.user.name}>
-                        </paper-input>
-                        <p class="pf-c-form__helper-text">${t`User's display name.`}</p>
-                        <paper-input
-                            name="email"
-                            ?alwaysFloatLabel=${true}
-                            type="email"
-                            label="${t`Email address`}"
-                            value=${this.user.email || ""}>
-                        </paper-input>
+                        <ak-form-element-horizontal
+                            label=${t`Username`}
+                            ?required=${true}
+                            name="username">
+                            <input type="text" value="${ifDefined(this.user?.username)}" class="pf-c-form-control" required>
+                            <p class="pf-c-form__helper-text">${t`Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.`}</p>
+                        </ak-form-element-horizontal>
+                        <ak-form-element-horizontal
+                            label=${t`Name`}
+                            ?required=${true}
+                            name="name">
+                            <input type="text" value="${ifDefined(this.user?.name)}" class="pf-c-form-control" required>
+                            <p class="pf-c-form__helper-text">${t`User's display name.`}</p>
+                        </ak-form-element-horizontal>
+                        <ak-form-element-horizontal
+                            label=${t`Email`}
+                            ?required=${true}
+                            name="email">
+                            <input type="email" value="${ifDefined(this.user?.email)}" class="pf-c-form-control" required>
+                        </ak-form-element-horizontal>
 
                         <div class="pf-c-form__group pf-m-action">
                             <div class="pf-c-form__horizontal-group">
