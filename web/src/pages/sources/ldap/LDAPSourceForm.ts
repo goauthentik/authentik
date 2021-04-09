963af1ac1eda5457ca9c3e8138ec41d5427e84ce
Jens Langhammer <jens.langhammer@beryju.org>
2021-04-09 11:23:57 +0200

    web/admin: adjust label for ldap password sync
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- web/src/pages/sources/ldap/LDAPSourceForm.ts
+++ web/src/pages/sources/ldap/LDAPSourceForm.ts
@@ -79,9 +79,10 @@ export class LDAPSourceForm extends Form<LDAPSource> {
                 <div class="pf-c-check">
                     <input type="checkbox" class="pf-c-check__input" ?checked=${first(this.source?.syncUsersPassword, true)}>
                     <label class="pf-c-check__label">
-                        ${t`Sync users' passwords`}
+                        ${t`User password writeback`}
                     </label>
                 </div>
+                <p class="pf-c-form__helper-text">${t`Login password is synced from LDAP into authentik automatically. Enable this option only to write password changes in authentik back to LDAP.`}</p>
             </ak-form-element-horizontal>
             <ak-form-element-horizontal name="syncGroups">
                 <div class="pf-c-check">
