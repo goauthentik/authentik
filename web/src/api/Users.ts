71e4936dc35300f8a88d94d293e1c491b4980110
Jens Langhammer <jens.langhammer@beryju.org>
2021-04-22 19:52:01 +0200

    web/admin: fix error when me() returns 403
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- web/src/api/Users.ts
+++ web/src/api/Users.ts
@@ -5,10 +5,16 @@ let _globalMePromise: Promise<SessionUser>;
 export function me(): Promise<SessionUser> {
     if (!_globalMePromise) {
         _globalMePromise = new CoreApi(DEFAULT_CONFIG).coreUsersMe().catch((ex) => {
+            const defaultUser: SessionUser = {
+                user: {
+                    username: "",
+                    name: ""
+                }
+            };
             if (ex.status === 401 || ex.status === 403) {
                 window.location.assign("/");
             }
-            return ex;
+            return defaultUser;
         });
     }
     return _globalMePromise;
