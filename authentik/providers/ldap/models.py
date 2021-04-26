f89479caf303298720c9bca8c535b1b230f20a60
Jens Langhammer <jens.langhammer@beryju.org>
2021-04-26 11:52:42 +0200

    providers/ldap: add LDAP provider
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

---
+++ authentik/providers/ldap/models.py
@@ -0,0 +1,51 @@
+"""LDAP Provider"""
+from typing import Optional, Type
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from rest_framework.serializers import Serializer
+
+from authentik.core.models import Provider
+from authentik.flows.models import Flow
+
+
+class LDAPProvider(Provider):
+    """LDAP Provider"""
+
+    base_dn = models.TextField(
+        default="DC=ldap,DC=goauthentik,DC=io",
+        help_text=_("DN under which objects are accessible."),
+    )
+
+    bind_flow = models.ForeignKey(
+        Flow,
+        null=True,
+        default=None,
+        on_delete=models.SET_DEFAULT,
+        help_text=_(
+            "Flow which is used to bind users. When left empty, no users will be able to bind."
+        ),
+    )
+
+    @property
+    def launch_url(self) -> Optional[str]:
+        """LDAP never has a launch URL"""
+        return None
+
+    @property
+    def component(self) -> str:
+        return "ak-provider-ldap-form"
+
+    @property
+    def serializer(self) -> Type[Serializer]:
+        from authentik.providers.oauth2.api.provider import OAuth2ProviderSerializer
+
+        return OAuth2ProviderSerializer
+
+    def __str__(self):
+        return f"LDAP Provider {self.name}"
+
+    class Meta:
+
+        verbose_name = _("LDAP Provider")
+        verbose_name_plural = _("LDAP Providers")
