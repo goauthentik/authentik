0c1e3cc0d1dee65f3584d66208406b959c9f5753
Jens Langhammer <jens.langhammer@beryju.org>
2021-05-22 13:19:01 +0200

    admin: add worker metrics
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/admin/api/workers.py
+++ authentik/admin/api/workers.py
@@ -1,5 +1,6 @@
 """authentik administration overview"""
 from drf_spectacular.utils import extend_schema, inline_serializer
+from prometheus_client import Gauge
 from rest_framework.fields import IntegerField
 from rest_framework.permissions import IsAdminUser
 from rest_framework.request import Request
@@ -7,6 +8,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from authentik.root.celery import CELERY_APP
+
+GAUGE_WORKERS = Gauge("authentik_admin_workers", "Currently connected workers")
 
 
 class WorkerView(APIView):
@@ -19,4 +22,6 @@ class WorkerView(APIView):
     )
     def get(self, request: Request) -> Response:
         """Get currently connected worker count."""
-        return Response({"count": len(CELERY_APP.control.ping(timeout=0.5))})
+        count = len(CELERY_APP.control.ping(timeout=0.5))
+        GAUGE_WORKERS.set(count)
+        return Response({"count": count})
