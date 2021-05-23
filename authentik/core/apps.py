40549a848bbd70e80f0b465e957f354cd679ad3e
Jens Langhammer <jens.langhammer@beryju.org>
2021-05-23 15:20:47 +0200

    core: add gauge to count all models
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/core/apps.py
+++ authentik/core/apps.py
@@ -3,6 +3,9 @@ from importlib import import_module
 
 from django.apps import AppConfig
 
+from authentik.core.signals import GAUGE_MODELS
+from authentik.lib.utils.reflection import get_apps
+
 
 class AuthentikCoreConfig(AppConfig):
     """authentik core app config"""
@@ -15,3 +18,9 @@ class AuthentikCoreConfig(AppConfig):
     def ready(self):
         import_module("authentik.core.signals")
         import_module("authentik.core.managed")
+        for app in get_apps():
+            for model in app.get_models():
+                GAUGE_MODELS.labels(
+                    model_name=model._meta.model_name,
+                    app=model._meta.app_label,
+                ).set(model.objects.count())
