309d80a92182d7fca2fba8a1120228382d7dc4e7
Jens Langhammer <jens.langhammer@beryju.org>
2021-05-26 10:37:33 +0200

    ci: build image with timestamp for flux
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- scripts/az_do_set_branch.py
+++ scripts/az_do_set_branch.py
@@ -1,5 +1,6 @@
 """Helper script to get the actual branch name, docker safe"""
 import os
+from time import time
 
 env_pr_branch = "SYSTEM_PULLREQUEST_SOURCEBRANCH"
 default_branch = "BUILD_SOURCEBRANCHNAME"
@@ -9,3 +10,4 @@ if env_pr_branch in os.environ:
     branch_name = os.environ[env_pr_branch].replace("/", "-")
 
 print("##vso[task.setvariable variable=branchName]%s" % branch_name)
+print("##vso[task.setvariable variable=timestamp]%s" % int(time()))
