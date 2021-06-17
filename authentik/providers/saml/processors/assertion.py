19cac4bf43b50fae9c3427003b3da0f1d7ce2cd9
Jens Langhammer <jens.langhammer@beryju.org>
2021-06-17 13:52:10 +0200

    providers/saml: fix error when getting transient user identifier
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/providers/saml/processors/assertion.py
+++ authentik/providers/saml/processors/assertion.py
@@ -184,7 +184,7 @@ class AssertionProcessor:
             return name_id
         if name_id.attrib["Format"] == SAML_NAME_ID_FORMAT_TRANSIENT:
             # Use the hash of the user's session, which changes every session
-            session_key: str = self.http_request.user.session.session_key
+            session_key: str = self.http_request.session.session_key
             name_id.text = sha256(session_key.encode()).hexdigest()
             return name_id
         raise UnsupportedNameIDFormat(
