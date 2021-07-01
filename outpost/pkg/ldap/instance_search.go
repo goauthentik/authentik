ade8644da60da80e1b0cb0bb5dab727514e43ca5
Jens Langhammer <jens.langhammer@beryju.org>
2021-07-01 11:50:51 +0200

    outposts/ldap: add support for boolean fields in ldap
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- outpost/pkg/ldap/instance_search.go
+++ outpost/pkg/ldap/instance_search.go
@@ -98,19 +98,9 @@ func (pi *ProviderInstance) UserEntry(u api.User) *ldap.Entry {
 		},
 	}
 
-	if *u.IsActive {
-        attrs = append(attrs, &ldap.EntryAttribute{Name: "accountStatus", Values: []string{"active"}})
-	} else {
-        attrs = append(attrs, &ldap.EntryAttribute{Name: "accountStatus", Values: []string{"inactive"}})
-	}
-
-	if u.IsSuperuser {
-		attrs = append(attrs, &ldap.EntryAttribute{Name: "superuser", Values: []string{"active"}})
-	} else {
-        attrs = append(attrs, &ldap.EntryAttribute{Name: "superuser", Values: []string{"inactive"}})
-	}
-
 	attrs = append(attrs, &ldap.EntryAttribute{Name: "memberOf", Values: pi.GroupsForUser(u)})
+	attrs = append(attrs, &ldap.EntryAttribute{Name: "goauthentik.io/ldap/active", Values: []string{BoolToString(*u.IsActive)}})
+	attrs = append(attrs, &ldap.EntryAttribute{Name: "goauthentik.io/ldap/superuser", Values: []string{BoolToString(u.IsSuperuser)}})
 
 	attrs = append(attrs, AKAttrsToLDAP(u.Attributes)...)
 
