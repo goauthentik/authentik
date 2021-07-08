90fe1c2ce802664b16bb0a71a7d8e50542c6020a
Jens Langhammer <jens.langhammer@beryju.org>
2021-07-08 19:28:35 +0200

    providers/oauth2: allow blank redirect_uris to allow any redirect_uri
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- web/src/pages/providers/oauth2/OAuth2ProviderForm.ts
+++ web/src/pages/providers/oauth2/OAuth2ProviderForm.ts
@@ -117,6 +117,9 @@ export class OAuth2ProviderFormPage extends ModelForm<OAuth2Provider, number> {
                         <p class="pf-c-form__helper-text">
                             ${t`Valid redirect URLs after a successful authorization flow. Also specify any origins here for Implicit flows.`}
                         </p>
+                        <p class="pf-c-form__helper-text">
+                            ${t`If no explicit redirect URIs are specified, any redirect URI is allowed.`}
+                        </p>
                     </ak-form-element-horizontal>
                 </div>
             </ak-form-group>
