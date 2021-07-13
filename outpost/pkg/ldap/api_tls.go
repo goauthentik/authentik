425667268a5ac1abc9afd0395640fef4842a3d54
Jens Langhammer <jens.langhammer@beryju.org>
2021-07-13 17:11:31 +0200

    outposts/ldap: fix logic error for LDAPS
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- outpost/pkg/ldap/api_tls.go
+++ outpost/pkg/ldap/api_tls.go
@@ -4,7 +4,10 @@ import "crypto/tls"
 
 func (ls *LDAPServer) getCertificates(info *tls.ClientHelloInfo) (*tls.Certificate, error) {
 	if len(ls.providers) == 1 {
-		return ls.providers[0].cert, nil
+		if ls.providers[0].cert != nil {
+			ls.log.WithField("server-name", info.ServerName).Debug("We only have a single provider, using their cert")
+			return ls.providers[0].cert, nil
+		}
 	}
 	for _, provider := range ls.providers {
 		if provider.tlsServerName == &info.ServerName {
@@ -15,5 +18,6 @@ func (ls *LDAPServer) getCertificates(info *tls.ClientHelloInfo) (*tls.Certifica
 			return provider.cert, nil
 		}
 	}
+	ls.log.WithField("server-name", info.ServerName).Debug("Fallback to default cert")
 	return ls.defaultCert, nil
 }
