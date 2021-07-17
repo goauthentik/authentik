c256b043e0921707fbb13c749a7951f4e4141d64
Jens Langhammer <jens.langhammer@beryju.org>
2021-07-17 22:53:40 +0200

    outposts: fix error when creating radius outpost
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/providers/oauth2/utils.py
+++ authentik/providers/oauth2/utils.py
@@ -103,7 +103,9 @@ def extract_client_auth(request: HttpRequest) -> tuple[str, str]:
     if re.compile(r"^Basic\s{1}.+$").match(auth_header):
         b64_user_pass = auth_header.split()[1]
         try:
-            client_id, _, client_secret = b64decode(b64_user_pass).decode("utf-8").partition(":")
+            client_id, _, client_secret = (
+                b64decode(b64_user_pass).decode("utf-8").partition(":")
+            )
         except (ValueError, Error):
             client_id = client_secret = ""  # nosec
     else:
