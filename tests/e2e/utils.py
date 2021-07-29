6aebbec2701d2489aeacdd881024a83235f028e2
Jens Langhammer <jens.langhammer@beryju.org>
2021-07-29 18:02:42 +0200

    Revert "tests/e2e: remove chrome requirement"
    This reverts commit b86fd7b71605bea6097860c55fcc3cb1a87bd208.

--- tests/e2e/utils.py
+++ tests/e2e/utils.py
@@ -21,6 +21,7 @@ from selenium.common.exceptions import (
     WebDriverException,
 )
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
@@ -87,6 +88,7 @@ class SeleniumTestCase(StaticLiveServerTestCase):
     def _get_driver(self) -> WebDriver:
         return webdriver.Remote(
             command_executor="http://localhost:4444/wd/hub",
+            desired_capabilities=DesiredCapabilities.CHROME,
         )
 
     def tearDown(self):
