293c479364b060344e0ae0497ac4c7603b56f291
Jens Langhammer <jens.langhammer@beryju.org>
2021-07-31 21:09:38 +0200

    outposts: ensure embedded outpost is created with integration selected
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/outposts/managed.py
+++ authentik/outposts/managed.py
@@ -2,7 +2,13 @@
 from dataclasses import asdict
 
 from authentik.managed.manager import EnsureExists, ObjectManager
-from authentik.outposts.models import Outpost, OutpostConfig, OutpostType
+from authentik.outposts.models import (
+    DockerServiceConnection,
+    KubernetesServiceConnection,
+    Outpost,
+    OutpostConfig,
+    OutpostType,
+)
 
 MANAGED_OUTPOST = "goauthentik.io/outposts/embedded"
 
@@ -11,10 +17,20 @@ class OutpostManager(ObjectManager):
     """Outpost managed objects"""
 
     def reconcile(self):
+        def outpost_created(outpost: Outpost):
+            """When outpost is initially created, and we already have a service connection,
+            auto-assign it."""
+            if KubernetesServiceConnection.objects.exists():
+                outpost.service_connection = KubernetesServiceConnection.objects.first()
+            elif DockerServiceConnection.objects.exists():
+                outpost.service_connection = DockerServiceConnection.objects.first()
+            outpost.save()
+
         return [
             EnsureExists(
                 Outpost,
                 MANAGED_OUTPOST,
+                created_callback=outpost_created,
                 name="authentik Embedded Outpost",
                 type=OutpostType.PROXY,
                 _config=asdict(
