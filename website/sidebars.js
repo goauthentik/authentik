48c980e8e718eadd89b54eaf184fc9c9d7e932e5
Ernie <ernie@iamernie.ca>
2021-08-05 13:17:19 -0400

    website/docs: add Bookstack Integration Instructions (#1228)
    * Create index.mdx
    
    Add Wekan example
    
    * updated to include wekan entry
    
    * Update and rename website/docs/sources.md to website/docs/sources/index.md
    
    Break Sources into individual pages.
    
    * Update and rename website/docs/sources/index.md to website/docs/sources/ldap/index.md
    
    * Create index.md
    
    * Update index.md
    
    * Update index.md
    
    * Create index.md
    
    * Create index.md
    
    * Create index.md
    
    * Update index.md
    
    * Update index.md
    
    * Update index.md
    
    * Create index.md
    
    * discord images
    
    * spacing
    
    * Added discord
    
    * discord changes
    
    * Added sources breakdown to the sidebar
    
    * Fixed the saml title
    
    * Added github examples
    
    * fixed formatting
    
    * Changed file path, updated sidebar, added google.
    
    * fixed a spelling mistake
    
    * Cleaned up formatting
    
    * Fixed Notes
    
    * Added Bookstack to the intergrations
    
    * Fixed a typo with formatting
    
    * Added a line break for Advanced protocol settings

--- website/sidebars.js
+++ website/sidebars.js
@@ -79,6 +79,7 @@ module.exports = {
                         "integrations/services/apache-guacamole/index",
                         "integrations/services/aws/index",
                         "integrations/services/awx-tower/index",
+                        "integrations/services/bookstack/index",
                         "integrations/services/gitlab/index",
                         "integrations/services/grafana/index",
                         "integrations/services/harbor/index",
