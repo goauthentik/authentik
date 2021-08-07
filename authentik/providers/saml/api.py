9a8240bdd1a0d5f730caf1379968776e163b0910
Jens Langhammer <jens.langhammer@beryju.org>
2021-08-07 21:39:30 +0200

    proviers/saml: fix validation error not being raised
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/providers/saml/api.py
+++ authentik/providers/saml/api.py
@@ -162,7 +162,7 @@ class SAMLProviderViewSet(UsedByMixin, ModelViewSet):
             )
         except ValueError as exc:  # pragma: no cover
             LOGGER.warning(str(exc))
-            return ValidationError(
+            raise ValidationError(
                 _("Failed to import Metadata: %(message)s" % {"message": str(exc)}),
             )
         return Response(status=204)
