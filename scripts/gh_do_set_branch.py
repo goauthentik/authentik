5ba526fe57751d8d74e3c31072a338fad9613da3
Jens Langhammer <jens.langhammer@beryju.org>
2021-08-30 17:40:08 +0200

    ci: migrate outpost ci to gh actions
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

---
+++ scripts/gh_do_set_branch.py
@@ -0,0 +1,13 @@
+"""Helper script to get the actual branch name, docker safe"""
+import os
+from time import time
+
+env_pr_branch = "GITHUB_HEAD_REF"
+default_branch = "GITHUB_REF"
+
+branch_name = os.environ[default_branch]
+if env_pr_branch in os.environ:
+    branch_name = os.environ[env_pr_branch].replace("/", "-")
+
+print("##[set-output name=branchName]%s" % branch_name)
+print("##[set-output name=timestamp]%s" % int(time()))
