933336c38b18cd1e5aea5b78772ae5da9826fc0f
Jens L <jens.langhammer@beryju.org>
2021-09-01 16:19:29 +0200

    ci: fix images not being pushed with correct tags
    * ci: debug
    
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>
    
    * ci: fix branch and sha
    
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- scripts/gh_do_set_branch.py
+++ scripts/gh_do_set_branch.py
@@ -4,10 +4,13 @@ from time import time
 
 env_pr_branch = "GITHUB_HEAD_REF"
 default_branch = "GITHUB_REF"
+sha = "GITHUB_SHA"
 
 branch_name = os.environ[default_branch]
-if env_pr_branch in os.environ:
-    branch_name = os.environ[env_pr_branch].replace("/", "-")
+if os.environ.get(env_pr_branch, "") != "":
+    branch_name = os.environ[env_pr_branch]
+branch_name = branch_name.replace("refs/heads/", "").replace("/", "-")
 
 print("##[set-output name=branchName]%s" % branch_name)
 print("##[set-output name=timestamp]%s" % int(time()))
+print("##[set-output name=sha]%s" % os.environ[sha])
