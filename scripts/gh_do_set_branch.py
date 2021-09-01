72c161d1505210f16b222f7e1682e410dbdc5057
Jens Langhammer <jens.langhammer@beryju.org>
2021-09-01 15:44:59 +0200

    ci: debug
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- scripts/gh_do_set_branch.py
+++ scripts/gh_do_set_branch.py
@@ -7,7 +7,10 @@ default_branch = "GITHUB_REF"
 
 branch_name = os.environ[default_branch]
 if env_pr_branch in os.environ:
-    branch_name = os.environ[env_pr_branch].replace("/", "-")
+    branch_name = os.environ[env_pr_branch].replace("refs/heads/", "").replace("/", "-")
+
+print("Debug")
+print(os.environ)
 
 print("##[set-output name=branchName]%s" % branch_name)
 print("##[set-output name=timestamp]%s" % int(time()))
