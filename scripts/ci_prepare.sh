247647517487b80580cc11123c9ac091bc3811b0
Jens L <jens.langhammer@beryju.org>
2021-09-04 14:55:54 +0200

    ci: attempt to cache pipenv (#1347)
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- scripts/ci_prepare.sh
+++ scripts/ci_prepare.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/bash -xe
 docker-compose -f scripts/ci.docker-compose.yml up -d
 sudo apt update
 sudo apt install -y libxmlsec1-dev pkg-config
 sudo pip install -U wheel pipenv
-pipenv install --dev
+if [[ "$INSTALL" != "true" ]]; then
+    pipenv install --dev
+fi
 pipenv run python -m scripts.generate_ci_config
