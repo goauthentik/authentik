5b7e1f97e007218490763566fc1c59f2a14e350a
Jens Langhammer <jens.langhammer@beryju.org>
2021-10-09 19:29:35 +0200

    stages/authenticator_duo: remove signals
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/stages/authenticator_totp/apps.py
+++ authentik/stages/authenticator_totp/apps.py
@@ -1,6 +1,4 @@
 """TOTP"""
-from importlib import import_module
-
 from django.apps import AppConfig
 
 
@@ -10,6 +8,3 @@ class AuthentikStageAuthenticatorTOTPConfig(AppConfig):
     name = "authentik.stages.authenticator_totp"
     label = "authentik_stages_authenticator_totp"
     verbose_name = "authentik Stages.Authenticator.TOTP"
-
-    def ready(self):
-        import_module("authentik.stages.authenticator_totp.signals")
