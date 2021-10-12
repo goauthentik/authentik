6d4ea9992bc5662581b17823fced0143088ec34c
Jens Langhammer <jens.langhammer@beryju.org>
2021-10-12 10:56:42 +0200

    add default for new repos
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- website/netlify/functions/go-get.js
+++ website/netlify/functions/go-get.js
@@ -4,10 +4,13 @@ exports.handler = async function (event, context) {
     let repo = "";
     switch (event.path) {
         case "/":
-            repo = "authentik.git";
+            repo = "/authentik.git";
             break;
         case "/api":
-            repo = "client-go.git";
+            repo = "/client-go.git";
+            break;
+        default:
+            repo = `${event.path}.git`;
             break;
     }
     return {
@@ -15,6 +18,6 @@ exports.handler = async function (event, context) {
         headers: {
             "content-type": "text/html",
         },
-        body: `<meta name="go-import" content="${event.headers.host}${event.path} git https://github.com/${gitHubNamespace}/${repo}">`
+        body: `<meta name="go-import" content="${event.headers.host}${event.path} git https://github.com/${gitHubNamespace}${repo}">`
     };
 }
