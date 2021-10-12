72ea971f48a654e23548109ad822a45c44591651
Jens Langhammer <jens.langhammer@beryju.org>
2021-10-12 10:48:55 +0200

    custom logic for go paths
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- website/netlify/functions/go-get.js
+++ website/netlify/functions/go-get.js
@@ -1,11 +1,18 @@
 const gitHubNamespace = "goauthentik.io";
 
 exports.handler = async function (event, context) {
+    const repo = "";
+    switch (event.path) {
+        case "/":
+            repo = "authentik.git";
+        case "/api":
+            repo = "client-go.git";
+    }
     return {
         statusCode: 200,
         headers: {
             "content-type": "text/html",
         },
-        body: `<meta name="go-import" content="${event.headers.host}${event.path} git https://github.com/${gitHubNamespace}/authentik${event.path}.git">`
+        body: `<meta name="go-import" content="${event.headers.host}${event.path} git https://github.com/${gitHubNamespace}/${repo}.git">`
     };
 }
