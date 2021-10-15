2130658d4164a3c1ed0f6b5f94651c108d3516ee
Jens Langhammer <jens.langhammer@beryju.org>
2021-10-15 20:18:21 +0200

    stages/authenticator_webauthn: migrate to new library version
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/stages/authenticator_webauthn/utils.py
+++ authentik/stages/authenticator_webauthn/utils.py
@@ -1,28 +1,6 @@
 """webauthn utils"""
-import base64
-import os
 
 from django.http import HttpRequest
-
-CHALLENGE_DEFAULT_BYTE_LEN = 32
-
-
-def generate_challenge(challenge_len=CHALLENGE_DEFAULT_BYTE_LEN):
-    """Generate a challenge of challenge_len bytes, Base64-encoded.
-    We use URL-safe base64, but we *don't* strip the padding, so that
-    the browser can decode it without too much hassle.
-    Note that if we are doing byte comparisons with the challenge in collectedClientData
-    later on, that value will not have padding, so we must remove the padding
-    before storing the value in the session.
-    """
-    # If we know Python 3.6 or greater is available, we could replace this with one
-    # call to secrets.token_urlsafe
-    challenge_bytes = os.urandom(challenge_len)
-    challenge_base64 = base64.urlsafe_b64encode(challenge_bytes)
-    # Python 2/3 compatibility: b64encode returns bytes only in newer Python versions
-    if not isinstance(challenge_base64, str):
-        challenge_base64 = challenge_base64.decode("utf-8")
-    return challenge_base64
 
 
 def get_rp_id(request: HttpRequest) -> str:
