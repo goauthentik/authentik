535f2eb27e859c32679200edd4ce97365df98493
Jens Langhammer <jens.langhammer@beryju.org>
2021-10-20 15:24:04 +0200

    website: netlify proxy analytics
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -119,10 +119,9 @@ module.exports = {
 <html <%~ it.htmlAttributes %>>
   <head>
     <meta charset="UTF-8">
-    <meta name="go-import" content="goauthentik.io/api git https://github.com/goauthentik/client-go">
     <meta name="viewport" content="width=device-width, initial-scale=0.86, maximum-scale=3.0, minimum-scale=0.86">
     <meta name="generator" content="Docusaurus v<%= it.version %>">
-    <script async defer data-domain="goauthentik.io" src="https://public.s3.beryju.org/plausibl.js"></script>
+    <script async defer data-domain="goauthentik.io" src="//js/script.js"></script>
     <%~ it.headTags %>
     <% it.metaAttributes.forEach((metaAttribute) => { %>
       <%~ metaAttribute %>
