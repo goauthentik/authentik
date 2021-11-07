c6cc1b1728ce296d3e87aacd09f085e97b534ab7
Jens Langhammer <jens.langhammer@beryju.org>
2021-11-07 21:37:14 +0100

    root: fix defaults for EMAIL_USE_TLS
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/api/urls.py
+++ authentik/api/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 from authentik.api.v3.urls import urlpatterns as v3_urls
 
 urlpatterns = [
-    # Remove in 2022.1
+    # TODO: Remove in 2022.1
     path("v2beta/", include(v3_urls)),
     path("v3/", include(v3_urls)),
 ]
