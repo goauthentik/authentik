4224134a196a2db01962fe945849da53d5b8043b
Jens Langhammer <jens.langhammer@beryju.org>
2021-11-22 11:28:26 +0100

    tests/e2e: remove deprecated desired_capabilities
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- tests/e2e/utils.py
+++ tests/e2e/utils.py
@@ -19,7 +19,6 @@ from docker.models.containers import Container
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, TimeoutException, WebDriverException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
@@ -111,7 +110,7 @@ class SeleniumTestCase(StaticLiveServerTestCase):
             try:
                 return webdriver.Remote(
                     command_executor="http://localhost:4444/wd/hub",
-                    desired_capabilities=DesiredCapabilities.CHROME,
+                    options=webdriver.ChromeOptions(),
                 )
             except WebDriverException:
                 count += 1
