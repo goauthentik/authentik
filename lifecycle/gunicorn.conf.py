39acb044fb7f8c1139cfe6cbb1d5149895b951e5
Jens Langhammer <jens.langhammer@beryju.org>
2021-11-29 14:27:55 +0100

    lifecycle: allow custom worker count in k8s
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- lifecycle/gunicorn.conf.py
+++ lifecycle/gunicorn.conf.py
@@ -54,8 +54,9 @@ logconfig_dict = {
 # if we're running in kubernetes, use fixed workers because we can scale with more pods
 # otherwise (assume docker-compose), use as much as we can
 if SERVICE_HOST_ENV_NAME in os.environ:
-    workers = 2
+    default_workers = 2
 else:
     default_workers = max(cpu_count() * 0.25, 1) + 1  # Minimum of 2 workers
-    workers = int(os.environ.get("WORKERS", default_workers))
+
+workers = int(os.environ.get("WORKERS", default_workers))
 threads = int(os.environ.get("THREADS", 4))
