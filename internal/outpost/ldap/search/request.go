955c62c4ffb644a1862a5d0f487ec78eb3815200
Ilya Kogan <ikogan@mythicnet.org>
2021-12-08 18:58:55 +0000

    outposts/ldap: Fix search case sensitivity.

--- internal/outpost/ldap/search/request.go
+++ internal/outpost/ldap/search/request.go
@@ -25,6 +25,7 @@ type Request struct {
 func NewRequest(bindDN string, searchReq ldap.SearchRequest, conn net.Conn) (*Request, *sentry.Span) {
 	rid := uuid.New().String()
 	bindDN = strings.ToLower(bindDN)
+	searchReq.BaseDN = strings.ToLower(searchReq.BaseDN)
 	span := sentry.StartSpan(context.TODO(), "authentik.providers.ldap.search", sentry.TransactionName("authentik.providers.ldap.search"))
 	span.SetTag("request_uid", rid)
 	span.SetTag("user.username", bindDN)
