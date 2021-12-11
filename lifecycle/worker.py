552ddda909407ce0b626860de2a330e8589c918f
Jens Langhammer <jens.langhammer@beryju.org>
2021-12-11 19:55:09 +0100

    lifecycle: use custom worker class
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

---
+++ lifecycle/worker.py
@@ -0,0 +1,13 @@
+"""Uvicorn worker"""
+from uvicorn.workers import UvicornWorker
+
+
+class DjangoUvicornWorker(UvicornWorker):
+    """Custom configured Uvicorn Worker without lifespan"""
+
+    CONFIG_KWARGS = {
+        "loop": "uvloop",
+        "http": "httptools",
+        "lifespan": "off",
+        "ws": "wsproto",
+    }
