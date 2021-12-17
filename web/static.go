4b20b9954543865c5264d7be6773942e71a006e4
Jens Langhammer <jens.langhammer@beryju.org>
2021-12-17 19:58:18 +0100

    outposts/proxy: always embed static assets, still check local
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- web/static.go
+++ web/static.go
@@ -1,9 +1,14 @@
 package web
 
-import _ "embed"
+import (
+	_ "embed"
+	"net/http"
+)
 
 //go:embed robots.txt
 var RobotsTxt []byte
 
 //go:embed security.txt
 var SecurityTxt []byte
+
+var StaticHandler = http.FileServer(http.Dir("./web/dist/"))
---
+++ web/static_outpost.go
@@ -0,0 +1,19 @@
+//go:build outpost_static_embed
+// +build outpost_static_embed
+
+package web
+
+import (
+	"embed"
+	"net/http"
+)
+
+//go:embed dist/*
+var StaticDist embed.FS
+
+//go:embed authentik
+var StaticAuthentik embed.FS
+
+func init() {
+	StaticHandler = http.FileServer(http.FS(StaticDist))
+}
