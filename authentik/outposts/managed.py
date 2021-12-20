4658018a9050b2083459cdc5e6f30b7c2fe5b61b
Jens Langhammer <jens.langhammer@beryju.org>
2021-12-20 21:37:31 +0100

    Revert "outposts: rename outpost"
    This reverts commit a5c30fd9c7e3d3b1317c43c3e6b46afaf1915666.

--- authentik/outposts/managed.py
+++ authentik/outposts/managed.py
@@ -35,7 +35,7 @@ class OutpostManager(ObjectManager):
                 Outpost,
                 MANAGED_OUTPOST,
                 created_callback=outpost_created,
-                name="authentik Core",
+                name="authentik Embedded Outpost",
                 type=OutpostType.PROXY,
             ),
         ]
