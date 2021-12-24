3c9e426cb953f71aae899a3593ada4200bcdbdbf
Jens Langhammer <jens.langhammer@beryju.org>
2021-12-24 21:20:50 +0100

    use poetry env use
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- scripts/ci_prepare.sh
+++ scripts/ci_prepare.sh
@@ -4,9 +4,10 @@ docker-compose -f scripts/ci.docker-compose.yml up -d
 sudo apt update
 sudo apt install -y libxmlsec1-dev pkg-config
 python3 -m pip install -U wheel poetry
+poetry env use python3.10
 if [[ "$INSTALL" != "true" ]]; then
-    python3 -m poetry install
+    poetry install
 fi
-python3 -m poetry run pip install backports.zoneinfo
-python3 -m poetry run python -m scripts.generate_ci_config
+poetry run pip install backports.zoneinfo
+poetry run python -m scripts.generate_ci_config
 npm install -g pyright@1.1.136
