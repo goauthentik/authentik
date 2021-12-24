198ff40a39aecc3eb68c182d6232432b7f0a7a00
Jens Langhammer <jens.langhammer@beryju.org>
2021-12-24 20:21:27 +0100

    migrate ci
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- scripts/ci_prepare.sh
+++ scripts/ci_prepare.sh
@@ -2,9 +2,9 @@
 docker-compose -f scripts/ci.docker-compose.yml up -d
 sudo apt update
 sudo apt install -y libxmlsec1-dev pkg-config
-sudo pip install -U wheel pipenv
+sudo pip install -U wheel poetry
 if [[ "$INSTALL" != "true" ]]; then
-    pipenv install --dev
+    poetry install --dev
 fi
-pipenv run python -m scripts.generate_ci_config
+poetry run python -m scripts.generate_ci_config
 npm install -g pyright@1.1.136
