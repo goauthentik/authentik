7d7acd84945ac5ed8e159aef8ca15e92cb06df89
Jens Langhammer <jens.langhammer@beryju.org>
2022-01-04 22:13:26 +0100

    root: add ak wrapper script to be installed with poetry
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

---
+++ lifecycle/ak.py
@@ -0,0 +1,15 @@
+"""Wrapper for lifecycle/ak, to be installed by poetry"""
+from os import system
+from pathlib import Path
+from sys import argv
+
+
+def main():
+    """Wrapper around ak bash script"""
+    current_path = Path(__file__)
+    args = " ".join(argv[1:])
+    system(f"{current_path.parent}/ak {args}")  # nosec
+
+
+if __name__ == "__main__":
+    main()
