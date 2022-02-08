7088a6b0e6c9742f18a07569cf3c7d71f0369066
Jens Langhammer <jens.langhammer@beryju.org>
2022-02-08 16:30:26 +0100

    providers/proxy: fix Host/:Authority not being modified
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- internal/outpost/proxyv2/application/mode_proxy.go
+++ internal/outpost/proxyv2/application/mode_proxy.go
@@ -76,15 +76,18 @@ func (a *Application) proxyModifyRequest(ou *url.URL) func(req *http.Request) {
 		claims, _ := a.getClaims(r)
 		r.URL.Scheme = ou.Scheme
 		r.URL.Host = ou.Host
-		if claims.Proxy != nil && claims.Proxy.BackendOverride != "" {
+		r.Host = ou.Host
+		if claims != nil && claims.Proxy != nil && claims.Proxy.BackendOverride != "" {
 			u, err := url.Parse(claims.Proxy.BackendOverride)
 			if err != nil {
 				a.log.WithField("backend_override", claims.Proxy.BackendOverride).WithError(err).Warning("failed parse user backend override")
-				return
+			} else {
+				r.URL.Scheme = u.Scheme
+				r.URL.Host = u.Host
+				r.Host = u.Host
 			}
-			r.URL.Scheme = u.Scheme
-			r.URL.Host = u.Host
 		}
+		a.log.WithField("upstream_url", r.URL.String()).Trace("final upstream url")
 	}
 }
 
