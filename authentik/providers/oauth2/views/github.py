c00b27a4861aba743ee89a26a9813774f94603be
Adam G <25728719+Electromaster232@users.noreply.github.com>
2022-03-16 08:46:19 -0400

    providers/oauth2: Move org template inside loop; Change slug to use Django slugify

--- authentik/providers/oauth2/views/github.py
+++ authentik/providers/oauth2/views/github.py
@@ -1,8 +1,8 @@
 """authentik pretend GitHub Views"""
-from copy import copy
 
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.views import View
+from django.utils.text import slugify
 
 from authentik.providers.oauth2.models import RefreshToken
 
@@ -70,60 +70,58 @@ class GitHubUserTeamsView(View):
         """Emulate GitHub's /user/teams API Endpoint"""
         user = token.user
 
-        org_tpl = {
-            "id": 0,
-            "node_id": "",
-            "url": "",
-            "html_url": "",
-            "name": "",
-            "slug": "",
-            "description": "",
-            "privacy": "",
-            "permission": "",
-            "members_url": "",
-            "repositories_url": "",
-            "parent": None,
-            "members_count": 0,
-            "repos_count": 0,
-            "created_at": "",
-            "updated_at": "",
-            "organization": {
-                "login": "github",
-                "id": 1,
-                "node_id": "",
-                "url": "",
-                "repos_url": "",
-                "events_url": "",
-                "hooks_url": "",
-                "issues_url": "",
-                "members_url": "",
-                "public_members_url": "",
-                "avatar_url": "",
-                "description": "",
-                "name": "Authentik",
-                "company": "",
-                "blog": "",
-                "location": "",
-                "email": "",
-                "is_verified": True,
-                "has_organization_projects": True,
-                "has_repository_projects": True,
-                "public_repos": 0,
-                "public_gists": 0,
-                "followers": 0,
-                "following": 0,
-                "html_url": "",
-                "created_at": "",
-                "updated_at": "",
-                "type": "Organization"
-            }
-        }
-
         orgs_response = []
         for org in user.ak_groups.all():
-            _org = copy(org_tpl)
+            _org = {
+                "id": 0,
+                "node_id": "",
+                "url": "",
+                "html_url": "",
+                "name": "",
+                "slug": "",
+                "description": "",
+                "privacy": "",
+                "permission": "",
+                "members_url": "",
+                "repositories_url": "",
+                "parent": None,
+                "members_count": 0,
+                "repos_count": 0,
+                "created_at": "",
+                "updated_at": "",
+                "organization": {
+                    "login": "github",
+                    "id": 1,
+                    "node_id": "",
+                    "url": "",
+                    "repos_url": "",
+                    "events_url": "",
+                    "hooks_url": "",
+                    "issues_url": "",
+                    "members_url": "",
+                    "public_members_url": "",
+                    "avatar_url": "",
+                    "description": "",
+                    "name": "Authentik",
+                    "company": "",
+                    "blog": "",
+                    "location": "",
+                    "email": "",
+                    "is_verified": True,
+                    "has_organization_projects": True,
+                    "has_repository_projects": True,
+                    "public_repos": 0,
+                    "public_gists": 0,
+                    "followers": 0,
+                    "following": 0,
+                    "html_url": "",
+                    "created_at": "",
+                    "updated_at": "",
+                    "type": "Organization"
+                }
+            }
             _org["id"] = str(org.pk)
-            _org["slug"] = org.name.replace(" ", "-").lower()
+            _org["slug"] = slugify(org.name)
             _org["name"] = org.name
             orgs_response.append(_org)
         return JsonResponse(orgs_response, safe=False)
