a3eb72d16056fd51a6db474524512435b4fe5bc5
Danaël <danaelgiordana@protonmail.com>
2022-03-21 10:45:30 +0100

    website/integrations: Document using pfSense as Provider Integration  (#2534)
    * Add pfSense documentation
    
    * add pfSense to sidebar
    
    * Add pfsense secure setup
    
    * rearrangement of sections for better clarity
    
    * Add port value in unsecure setup
    
    * change admonitions type for unsecure setup
    
    * add `Test you setup` section
    
    * add `Change pfSense default authentication backend` section
    
    * Minor corrections
    
    Co-authored-by: Danaël Giordana <danael@giordana.cc>

--- website/sidebarsIntegrations.js
+++ website/sidebarsIntegrations.js
@@ -27,6 +27,7 @@ module.exports = {
                 "services/onlyoffice/index",
                 "services/opnsense/index",
                 "services/paperless-ng/index",
+                "services/pfsense/index",
                 "services/portainer/index",
                 "services/powerdns-admin/index",
                 "services/proxmox-ve/index",
