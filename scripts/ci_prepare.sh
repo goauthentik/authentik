be68bb0e7e8b82b79aa87dc3c1bf97ce6518c803
Jens Langhammer <jens.langhammer@beryju.org>
2022-04-04 10:11:52 +0000

    ci: use native caching

--- scripts/ci_prepare.sh
+++ scripts/ci_prepare.sh
@@ -5,8 +5,6 @@ sudo apt update
 sudo apt install -y libxmlsec1-dev pkg-config
 python3 -m pip install -U wheel poetry
 poetry env use python3.10
-if [[ "$INSTALL" != "true" ]]; then
-    poetry install
-fi
+poetry install
 poetry run python -m scripts.generate_ci_config
 npm install -g pyright@1.1.136
