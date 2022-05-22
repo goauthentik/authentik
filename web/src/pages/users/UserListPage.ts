8d49705c87532f9a0fb84b8f2b93d5aaceddcf82
Jens Langhammer <jens.langhammer@beryju.org>
2022-05-22 16:46:38 +0200

    web/admin: add set password button to user view page
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- web/src/pages/users/UserListPage.ts
+++ web/src/pages/users/UserListPage.ts
@@ -14,6 +14,7 @@ import { DEFAULT_CONFIG, config, tenant } from "../../api/Config";
 import { me } from "../../api/Users";
 import { uiConfig } from "../../common/config";
 import { PFColor } from "../../elements/Label";
+import { PFSize } from "../../elements/Spinner";
 import "../../elements/buttons/ActionButton";
 import "../../elements/forms/DeleteBulkForm";
 import "../../elements/forms/ModalForm";
@@ -215,7 +216,7 @@ export class UserListPage extends TablePage<User> {
                             </dt>
                             <dd class="pf-c-description-list__description">
                                 <div class="pf-c-description-list__text">
-                                    <ak-forms-modal>
+                                    <ak-forms-modal size=${PFSize.Medium}>
                                         <span slot="submit">${t`Update password`}</span>
                                         <span slot="header">${t`Update password`}</span>
                                         <ak-user-password-form
