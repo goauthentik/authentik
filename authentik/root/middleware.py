2b1a7b4b33c4fa1be020993b3a2ae338870f8c59
Jens Langhammer <jens.langhammer@beryju.org>
2022-05-29 16:55:30 +0200

    fix compatibility with tests
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/root/middleware.py
+++ authentik/root/middleware.py
@@ -41,7 +41,9 @@ class SessionMiddleware(UpstreamSessionMiddleware):
 
     def process_request(self, request):
         session_jwt = request.COOKIES.get(settings.SESSION_COOKIE_NAME)
-        session_key = None
+        # We need to support the standard django format of just a session key
+        # for testing setups, where the session is directly set
+        session_key = session_jwt if settings.TEST else None
         try:
             session_payload = decode(session_jwt, SIGNING_HASH, algorithms=["HS256"])
             session_key = session_payload["sid"]
