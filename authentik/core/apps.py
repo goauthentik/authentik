5e2d647a6c781532d53c0fba0e31644c2cd9ed10
Jens Langhammer <jens.langhammer@beryju.org>
2022-06-09 20:21:31 +0200

    core: trigger bootstrap tasks in server if we're debugging
    closes #3040
    
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/core/apps.py
+++ authentik/core/apps.py
@@ -2,6 +2,7 @@
 from importlib import import_module
 
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class AuthentikCoreConfig(AppConfig):
@@ -15,3 +16,7 @@ class AuthentikCoreConfig(AppConfig):
     def ready(self):
         import_module("authentik.core.signals")
         import_module("authentik.core.managed")
+        if settings.DEBUG:
+            from authentik.root.celery import worker_ready_hook
+
+            worker_ready_hook()
