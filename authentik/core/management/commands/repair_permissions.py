766ceda57a6dfa6d12232aec5e64cb4df2a66088
Jens Langhammer <jens.langhammer@beryju.org>
2022-07-01 17:03:39 +0200

    core: re-create anonymous user when repairing permissions
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/core/management/commands/repair_permissions.py
+++ authentik/core/management/commands/repair_permissions.py
@@ -2,6 +2,7 @@
 from django.apps import apps
 from django.contrib.auth.management import create_permissions
 from django.core.management.base import BaseCommand, no_translations
+from guardian.management import create_anonymous_user
 
 
 class Command(BaseCommand):  # pragma: no cover
@@ -13,3 +14,4 @@ class Command(BaseCommand):  # pragma: no cover
         for app in apps.get_app_configs():
             self.stdout.write(f"Checking app {app.name} ({app.label})\n")
             create_permissions(app, verbosity=0)
+        create_anonymous_user(None, using="default")
