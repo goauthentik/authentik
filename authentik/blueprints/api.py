406072cadee3fe6d6838f374170ec2dda86b62db
Jens Langhammer <jens.langhammer@beryju.org>
2022-08-17 19:04:17 +0200

    add validation for blueprint path
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/blueprints/api.py
+++ authentik/blueprints/api.py
@@ -1,6 +1,7 @@
 """Serializer mixin for managed models"""
 from drf_spectacular.utils import extend_schema, inline_serializer
 from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
 from rest_framework.fields import CharField, DateTimeField, JSONField
 from rest_framework.permissions import IsAdminUser
 from rest_framework.request import Request
@@ -9,7 +10,7 @@ from rest_framework.serializers import ListSerializer, ModelSerializer
 from rest_framework.viewsets import ModelViewSet
 
 from authentik.api.decorators import permission_required
-from authentik.blueprints.models import BlueprintInstance
+from authentik.blueprints.models import BlueprintInstance, BlueprintRetrievalFailed
 from authentik.blueprints.v1.tasks import apply_blueprint, blueprints_find_dict
 from authentik.core.api.used_by import UsedByMixin
 from authentik.core.api.utils import PassiveSerializer
@@ -30,6 +31,14 @@ class MetadataSerializer(PassiveSerializer):
 
 class BlueprintInstanceSerializer(ModelSerializer):
     """Info about a single blueprint instance file"""
+
+    def validate_path(self, path: str) -> str:
+        """Ensure the path specified is retrievable"""
+        try:
+            BlueprintInstance(path=path).retrieve()
+        except BlueprintRetrievalFailed as exc:
+            raise ValidationError(exc) from exc
+        return path
 
     class Meta:
 
