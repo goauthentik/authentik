1bad43c17c2a3ddc786493f21bf84b37313fb81f
Jens Langhammer <jens.langhammer@beryju.org>
2022-09-16 23:37:57 +0200

    limit amount of concurrent tasks on worker
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/root/settings.py
+++ authentik/root/settings.py
@@ -312,6 +312,7 @@ LOCALE_PATHS = ["./locale"]
 # Add a 10 minute timeout to all Celery tasks.
 CELERY_TASK_SOFT_TIME_LIMIT = 600
 CELERY_WORKER_MAX_TASKS_PER_CHILD = 50
+CELERY_WORKER_CONCURRENCY = 2
 CELERY_BEAT_SCHEDULE = {
     "clean_expired_models": {
         "task": "authentik.core.tasks.clean_expired_models",
