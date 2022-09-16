9bebc5e44152cc3ccdc1f6f58df0bd787ee88944
Jens Langhammer <jens.langhammer@beryju.org>
2022-09-16 23:44:27 +0200

    load tasks
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/stages/authenticator_duo/apps.py
+++ authentik/stages/authenticator_duo/apps.py
@@ -1,10 +1,16 @@
 """authentik duo app config"""
-from django.apps import AppConfig
+
+from authentik.blueprints.apps import ManagedAppConfig
 
 
-class AuthentikStageAuthenticatorDuoConfig(AppConfig):
+class AuthentikStageAuthenticatorDuoConfig(ManagedAppConfig):
     """authentik duo config"""
 
     name = "authentik.stages.authenticator_duo"
     label = "authentik_stages_authenticator_duo"
     verbose_name = "authentik Stages.Authenticator.Duo"
+    default = True
+
+    def reconcile_load_tasks(self):
+        """Load tasks"""
+        self.import_module("authentik.stages.authenticator_duo.tasks")
