4118bdf93b3123b9cdf3ab82d17863e2e13d84a5
Jens Langhammer <jens.langhammer@beryju.org>
2022-09-21 09:22:36 +0200

    fix tests
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/flows/tests/test_api.py
+++ authentik/flows/tests/test_api.py
@@ -10,19 +10,19 @@ from authentik.policies.models import PolicyBinding
 from authentik.stages.dummy.models import DummyStage
 
 DIAGRAM_EXPECTED = """graph TD
---> flow_start["Flow
-test-default-context"]
---> stage_0["Stage (Dummy Stage)
-dummy1"]
-stage_1_policy_0 --Policy passed--> stage_1["Stage (Dummy Stage)
-dummy2"]
-stage_0 --> stage_1_policy_0["Policy (Dummy Policy)
-dummy2-policy"]
-stage_1 --> done["End of the flow"]"""
+--> flow_start[["Flow
+test-default-context"]]
+--> stage_0(["Stage (Dummy Stage)
+dummy1"])
+stage_1_policy_0 --Policy passed--> stage_1(["Stage (Dummy Stage)
+dummy2"])
+stage_0 --> stage_1_policy_0{{"Policy (Dummy Policy)
+dummy2-policy"}}
+stage_1 --> done[["End of the flow"]]"""
 DIAGRAM_SHORT_EXPECTED = """graph TD
---> flow_start["Flow
-test-default-context"]
-flow_start --> done["End of the flow"]"""
+--> flow_start[["Flow
+test-default-context"]]
+flow_start --> done[["End of the flow"]]"""
 
 
 class TestFlowsAPI(APITestCase):
