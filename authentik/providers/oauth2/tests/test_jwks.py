f49090c24ae589dc0c226cf0b11ef40fbb7f8ae0
Jens Langhammer <jens.langhammer@beryju.org>
2022-10-18 21:32:09 +0200

    fix issues with es256 and add tests
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/providers/oauth2/tests/test_jwks.py
+++ authentik/providers/oauth2/tests/test_jwks.py
@@ -3,6 +3,7 @@ import json
 
 from django.test import RequestFactory
 from django.urls.base import reverse
+from jwt import PyJWKSet
 
 from authentik.core.models import Application
 from authentik.core.tests.utils import create_test_cert, create_test_flow
@@ -32,6 +33,7 @@ class TestJWKS(OAuthTestCase):
         )
         body = json.loads(response.content.decode())
         self.assertEqual(len(body["keys"]), 1)
+        PyJWKSet.from_dict(body)
 
     def test_hs256(self):
         """Test JWKS request with HS256"""
@@ -46,3 +48,20 @@ class TestJWKS(OAuthTestCase):
             reverse("authentik_providers_oauth2:jwks", kwargs={"application_slug": app.slug})
         )
         self.assertJSONEqual(response.content.decode(), {})
+
+    def test_es256(self):
+        """Test JWKS request with ES256"""
+        provider = OAuth2Provider.objects.create(
+            name="test",
+            client_id="test",
+            authorization_flow=create_test_flow(),
+            redirect_uris="http://local.invalid",
+            signing_key=create_test_cert(use_ec_private_key=True),
+        )
+        app = Application.objects.create(name="test", slug="test", provider=provider)
+        response = self.client.get(
+            reverse("authentik_providers_oauth2:jwks", kwargs={"application_slug": app.slug})
+        )
+        body = json.loads(response.content.decode())
+        self.assertEqual(len(body["keys"]), 1)
+        PyJWKSet.from_dict(body)
