2c42c87689b64603d1a4fb6685dac8dd2b40fedf
Jens Langhammer <jens.langhammer@beryju.org>
2022-12-30 13:43:42 +0100

    release: 2022.12.1

--- authentik/__init__.py
+++ authentik/__init__.py
@@ -2,7 +2,7 @@
 from os import environ
 from typing import Optional
 
-__version__ = "2022.12.0"
+__version__ = "2022.12.1"
 ENV_GIT_HASH_KEY = "GIT_BUILD_HASH"
 
 
--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -29,4 +29,4 @@ func UserAgent() string {
 	return fmt.Sprintf("authentik@%s", FullVersion())
 }
 
-const VERSION = "2022.12.0"
+const VERSION = "2022.12.1"
