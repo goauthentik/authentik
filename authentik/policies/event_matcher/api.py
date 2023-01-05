69c54dc3eb4e9790bc8cc631a6d716bac1837c96
Jens Langhammer <jens.langhammer@beryju.org>
2023-01-05 10:27:13 +0100

    don't save event matcher apps in migrations
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/policies/event_matcher/api.py
+++ authentik/policies/event_matcher/api.py
@@ -1,13 +1,20 @@
 """Event Matcher Policy API"""
+from rest_framework.fields import ChoiceField
 from rest_framework.viewsets import ModelViewSet
+from django.utils.translation import gettext as _
 
 from authentik.core.api.used_by import UsedByMixin
 from authentik.policies.api.policies import PolicySerializer
-from authentik.policies.event_matcher.models import EventMatcherPolicy
+from authentik.policies.event_matcher.models import EventMatcherPolicy, app_choices
 
 
 class EventMatcherPolicySerializer(PolicySerializer):
     """Event Matcher Policy Serializer"""
+
+    app = ChoiceField(choices=app_choices(), required=False, help_text=_(
+        "Match events created by selected application. When left empty, "
+        "all applications are matched."
+    ))
 
     class Meta:
         model = EventMatcherPolicy
