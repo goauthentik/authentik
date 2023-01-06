643b36b7322924689689e7ac7a6031194fabff26
Jens Langhammer <jens.langhammer@beryju.org>
2023-01-06 16:43:50 +0100

    web/admin: fix certificate filtering for LDAP verification certificate
    closes #4376
    
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- web/src/admin/sources/ldap/LDAPSourceForm.ts
+++ web/src/admin/sources/ldap/LDAPSourceForm.ts
@@ -157,7 +157,6 @@ export class LDAPSourceForm extends ModelForm<LDAPSource, string> {
                             ): Promise<CertificateKeyPair[]> => {
                                 const args: CryptoCertificatekeypairsListRequest = {
                                     ordering: "name",
-                                    hasKey: true,
                                     includeDetails: false,
                                 };
                                 if (query !== undefined) {
