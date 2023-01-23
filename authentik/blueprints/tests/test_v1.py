a033327711fc519875962054e8c5e94d9ce45320
Jens Langhammer <jens@goauthentik.io>
2023-01-24 00:05:34 +0100

    fix tests even more
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/blueprints/tests/test_v1.py
+++ authentik/blueprints/tests/test_v1.py
@@ -262,7 +262,11 @@ class TestBlueprintsV1(TransactionTestCase):
         with transaction_rollback():
             # First stage fields
             username_prompt = Prompt.objects.create(
-                field_key="username", label="Username", order=0, type=FieldTypes.TEXT
+                name=generate_id(),
+                field_key="username",
+                label="Username",
+                order=0,
+                type=FieldTypes.TEXT,
             )
             password = Prompt.objects.create(
                 name=generate_id(),
@@ -272,6 +276,7 @@ class TestBlueprintsV1(TransactionTestCase):
                 type=FieldTypes.PASSWORD,
             )
             password_repeat = Prompt.objects.create(
+                name=generate_id(),
                 field_key="password_repeat",
                 label="Password (repeat)",
                 order=2,
