dd1053d5af23682885c4c2d0d90b9d85ada1bb4a
Jens Langhammer <jens@goauthentik.io>
2023-02-12 02:34:52 +0100

    use lxml for cleaner code and less escaping, move to separate file
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/core/models.py
+++ authentik/core/models.py
@@ -28,6 +28,7 @@ from authentik.blueprints.models import ManagedModel
 from authentik.core.exceptions import PropertyMappingExpressionException
 from authentik.core.signals import password_changed
 from authentik.core.types import UILoginButton, UserSettingSerializer
+from authentik.lib.avatars import generate_avatar_from_name
 from authentik.lib.config import CONFIG, get_path_from_dict
 from authentik.lib.generators import generate_id
 from authentik.lib.models import CreatedUpdatedModel, DomainlessURLValidator, SerializerModel
@@ -55,76 +56,6 @@ DEFAULT_AVATAR = static("dist/assets/images/user_default.png")
 
 
 options.DEFAULT_NAMES = options.DEFAULT_NAMES + ("authentik_used_by_shadows",)
-
-
-def _generate_avatar_from_name(  # pragma: no cover
-    name: str,
-    *,
-    bg_hex: str = "ddd",
-    text_hex: str = "222",
-    length: int = 2,
-    size: int = 64,
-    rounded: bool = False,
-    font_size: float = 0.4375,
-    bold: bool = False,
-    uppercase: bool = True,
-) -> str:
-    """ "Generate an avatar with initials in SVG format.
-
-    Inspired from: https://github.com/LasseRafn/ui-avatars
-    """
-    name_parts = name.split()
-
-    if len(name_parts) == 1:
-        initials = name_parts[0][:length]
-    else:
-        initials = "".join([part[0] for part in name_parts[:-1]])
-        initials += name_parts[-1]
-
-    initials = initials[:length]
-
-    bg_hex = bg_hex.strip("#")
-    text_hex = text_hex.strip("#")
-
-    half_size = size // 2
-    shape = "circle" if rounded else "rect"
-    font_weight = "600" if bold else "400"
-
-    return (
-        '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="'
-        f"{size}"
-        'px" height="'
-        f"{size}"
-        'px" viewBox="0 0 '
-        f"{size}"
-        " "
-        f"{size}"
-        '" version="1.1"><'
-        f"{shape}"
-        ' fill="#'
-        f"{bg_hex}"
-        '" cx="'
-        f"{half_size}"
-        '" width="'
-        f"{size}"
-        '" height="'
-        f"{size}"
-        '" cy="'
-        f"{half_size}"
-        '" r="'
-        f"{half_size}"
-        '"/><text x="50%" y="50%" style="color: #'
-        f"{text_hex}"
-        "; line-height: 1;font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;\" alignment-baseline=\"middle\" text-anchor=\"middle\" font-size=\""  # pylint: disable=line-too-long
-        f"{round(size * font_size)}"
-        '" font-weight="'
-        f"{font_weight}"
-        '" dy=".1em" dominant-baseline="middle" fill="#'
-        f"{text_hex}"
-        '">'
-        f"{initials if not uppercase else initials.upper()}"
-        "</text></svg>"
-    )
 
 
 def default_token_duration():
@@ -317,32 +248,10 @@ class User(SerializerModel, GuardianUserMixin, AbstractUser):
             if not self.name:
                 # Render a default avatar abbreviated "AK" and
                 # using authentik's red color
-                svg = _generate_avatar_from_name("a k", bg_hex="fd4b2d", text_hex="fff", length=2)
+                svg = generate_avatar_from_name(User(name="a k"))
             else:
-                color = (
-                    int(md5(self.name.lower().encode("utf-8")).hexdigest(), 16) % 0xFFFFFF  # nosec
-                )
-
-                # Get a (somewhat arbitrarily) reduced scope of colors
-                # to avoid too dark or light backgrounds
-                blue = min(max((color) & 0xFF, 55), 200)
-                green = min(max((color >> 8) & 0xFF, 55), 200)
-                red = min(max((color >> 16) & 0xFF, 55), 200)
-
-                names = self.name.split()
-
-                # Only abbreviate first and last name
-                if len(names) > 2:
-                    names = [names[0], names[-1]]
-
-                svg = _generate_avatar_from_name(
-                    " ".join(names),
-                    bg_hex=f"{red:02x}{green:02x}{blue:02x}",
-                    # Contrasting text color (https://stackoverflow.com/a/3943023)
-                    text_hex="000" if (red * 0.299 + green * 0.587 + blue * 0.114) > 186 else "fff",
-                    length=len(names),
-                )
-
+                svg = generate_avatar_from_name(self)
+            print(svg)
             return (
                 "data:image/svg+xml;base64,"
                 f"{base64.b64encode(svg.encode('utf-8')).decode('utf-8')}"
---
+++ authentik/lib/avatars.py
@@ -0,0 +1,103 @@
+"""Avatar utils"""
+
+from hashlib import md5
+from typing import TYPE_CHECKING
+
+from lxml import etree  # nosec
+from lxml.etree import Element, SubElement  # nosec
+
+if TYPE_CHECKING:
+    from authentik.core.models import User
+
+SVG_XML_NS = "http://www.w3.org/2000/svg"
+SVG_NS_MAP = {None: SVG_XML_NS}
+SVG_FONTS = [
+    "-apple-system",
+    "BlinkMacSystemFont",
+    "'Segoe UI'",
+    "'Roboto'",
+    "'Oxygen'",
+    "'Ubuntu'",
+    "'Fira Sans'",
+    "'Droid Sans'",
+    "'Helvetica Neue'",
+    "sans-serif",
+]
+
+
+def generate_avatar_from_name(
+    user: "User",
+    length: int = 2,
+    size: int = 64,
+    rounded: bool = False,
+    font_size: float = 0.4375,
+    bold: bool = False,
+    uppercase: bool = True,
+) -> str:
+    """ "Generate an avatar with initials in SVG format.
+
+    Inspired from: https://github.com/LasseRafn/ui-avatars
+    """
+    name = user.name
+
+    color = int(md5(name.lower().encode("utf-8")).hexdigest(), 16) % 0xFFFFFF  # nosec
+
+    # Get a (somewhat arbitrarily) reduced scope of colors
+    # to avoid too dark or light backgrounds
+    blue = min(max((color) & 0xFF, 55), 200)
+    green = min(max((color >> 8) & 0xFF, 55), 200)
+    red = min(max((color >> 16) & 0xFF, 55), 200)
+    bg_hex = f"{red:02x}{green:02x}{blue:02x}"
+    # Contrasting text color (https://stackoverflow.com/a/3943023)
+    text_hex = "000" if (red * 0.299 + green * 0.587 + blue * 0.114) > 186 else "fff"
+
+    name_parts = user.name.split()
+    # Only abbreviate first and last name
+    if len(name_parts) > 2:
+        name_parts = [name_parts[0], name_parts[-1]]
+
+    if len(name_parts) == 1:
+        initials = name_parts[0][:length]
+    else:
+        initials = "".join([part[0] for part in name_parts[:-1]])
+        initials += name_parts[-1]
+
+    initials = initials[:length]
+
+    bg_hex = bg_hex.strip("#")
+    text_hex = text_hex.strip("#")
+
+    half_size = size // 2
+    shape = "circle" if rounded else "rect"
+    font_weight = "600" if bold else "400"
+
+    root_element: Element = Element(f"{{{SVG_XML_NS}}}svg", nsmap=SVG_NS_MAP)
+    root_element.attrib["width"] = f"{size}px"
+    root_element.attrib["height"] = f"{size}px"
+    root_element.attrib["viewBox"] = f"0 0 {size} {size}"
+    root_element.attrib["version"] = "1.1"
+
+    shape = SubElement(root_element, f"{{{SVG_XML_NS}}}{shape}", nsmap=SVG_NS_MAP)
+    shape.attrib["fill"] = f"#{bg_hex}"
+    shape.attrib["cx"] = f"{half_size}"
+    shape.attrib["cy"] = f"{half_size}"
+    shape.attrib["width"] = f"{size}"
+    shape.attrib["height"] = f"{size}"
+    shape.attrib["r"] = f"{half_size}"
+
+    text = SubElement(root_element, f"{{{SVG_XML_NS}}}text", nsmap=SVG_NS_MAP)
+    text.attrib["x"] = "50%"
+    text.attrib["y"] = "50%"
+    text.attrib["style"] = (
+        f"color: #{text_hex}; " "line-height: 1; " f"font-family: {','.join(SVG_FONTS)}; "
+    )
+    text.attrib["fill"] = f"#{text_hex}"
+    text.attrib["alignment-baseline"] = "middle"
+    text.attrib["dominant-baseline"] = "middle"
+    text.attrib["text-anchor"] = "middle"
+    text.attrib["font-size"] = f"{round(size * font_size)}"
+    text.attrib["font-weight"] = f"{font_weight}"
+    text.attrib["dy"] = ".1em"
+    text.text = initials if not uppercase else initials.upper()
+
+    return etree.tostring(root_element).decode()
