58e001c3d5165ee5eed935e4398134eafcf6b7a9
Jens Langhammer <jens@goauthentik.io>
2023-02-14 14:31:04 +0100

    internal: fix scheme not being forwarded correctly for host intercepted requests
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- internal/utils/web/http_host_interceptor.go
+++ internal/utils/web/http_host_interceptor.go
@@ -8,12 +8,14 @@ import (
 )
 
 type hostInterceptor struct {
-	inner http.RoundTripper
-	host  string
+	inner  http.RoundTripper
+	host   string
+	scheme string
 }
 
 func (t hostInterceptor) RoundTrip(r *http.Request) (*http.Response, error) {
 	r.Host = t.host
+	r.Header.Set("X-Forwarded-Proto", t.scheme)
 	return t.inner.RoundTrip(r)
 }
 
@@ -24,8 +26,9 @@ func NewHostInterceptor(inner *http.Client, host string) *http.Client {
 	}
 	return &http.Client{
 		Transport: hostInterceptor{
-			inner: inner.Transport,
-			host:  aku.Host,
+			inner:  inner.Transport,
+			host:   aku.Host,
+			scheme: aku.Scheme,
 		},
 	}
 }
