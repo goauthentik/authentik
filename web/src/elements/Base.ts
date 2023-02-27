8fd3f67b17cb7180bbfcd52018b867b31c2287a0
Jens Langhammer <jens@goauthentik.io>
2023-02-27 19:39:01 +0100

    don't hardcode path
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/elements/Base.ts
+++ web/src/elements/Base.ts
@@ -2,16 +2,22 @@ import { EVENT_LOCALE_CHANGE } from "@goauthentik/common/constants";
 
 import { LitElement } from "lit";
 
-let css: Promise<string> | undefined;
-function fetchCustomCSS(): Promise<string> {
+let css: Promise<string[]> | undefined;
+function fetchCustomCSS(): Promise<string[]> {
     if (!css) {
-        css = fetch("/static/dist/custom.css")
-            .then((res) => {
-                return res.text();
-            })
-            .finally(() => {
-                return "";
-            });
+        css = Promise.all(
+            Array.of(...document.head.querySelectorAll<HTMLLinkElement>("link[data-inject]")).map(
+                (link) => {
+                    return fetch(link.href)
+                        .then((res) => {
+                            return res.text();
+                        })
+                        .finally(() => {
+                            return "";
+                        });
+                },
+            ),
+        );
     }
     return css;
 }
@@ -20,15 +26,20 @@ export class AKElement extends LitElement {
     constructor() {
         super();
         this.addEventListener(EVENT_LOCALE_CHANGE, this._handleLocaleChange);
-        fetchCustomCSS().then((css) => {
-            if (css === "") {
-                return;
-            }
-            new CSSStyleSheet().replace(css).then((sheet) => {
-                if (!this.shadowRoot) {
+        fetchCustomCSS().then((sheets) => {
+            sheets.map((css) => {
+                if (css === "") {
                     return;
                 }
-                this.shadowRoot.adoptedStyleSheets = [...this.shadowRoot.adoptedStyleSheets, sheet];
+                new CSSStyleSheet().replace(css).then((sheet) => {
+                    if (!this.shadowRoot) {
+                        return;
+                    }
+                    this.shadowRoot.adoptedStyleSheets = [
+                        ...this.shadowRoot.adoptedStyleSheets,
+                        sheet,
+                    ];
+                });
             });
         });
     }
