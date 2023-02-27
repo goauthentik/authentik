e1ed28c70a708bc8d847587af895116d616c0d9e
Jens Langhammer <jens@goauthentik.io>
2023-02-27 18:49:17 +0100

    web: fetch custom.css via fetch and add stylesheet
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/elements/Base.ts
+++ web/src/elements/Base.ts
@@ -2,10 +2,35 @@ import { EVENT_LOCALE_CHANGE } from "@goauthentik/common/constants";
 
 import { LitElement } from "lit";
 
+let css: Promise<string> | undefined;
+function fetchCustomCSS(): Promise<string> {
+    if (!css) {
+        css = fetch("/static/dist/custom.css")
+            .then((res) => {
+                return res.text();
+            })
+            .finally(() => {
+                return "";
+            });
+    }
+    return css;
+}
+
 export class AKElement extends LitElement {
     constructor() {
         super();
         this.addEventListener(EVENT_LOCALE_CHANGE, this._handleLocaleChange);
+        fetchCustomCSS().then((css) => {
+            if (css === "") {
+                return;
+            }
+            new CSSStyleSheet().replace(css).then((sheet) => {
+                if (!this.shadowRoot) {
+                    return;
+                }
+                this.shadowRoot.adoptedStyleSheets = [...this.shadowRoot.adoptedStyleSheets, sheet];
+            });
+        });
     }
 
     disconnectedCallback() {
