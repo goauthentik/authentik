fedabb0354d08668045d0abb1fa4dce6d10f89c8
Jens Langhammer <jens@goauthentik.io>
2023-03-03 12:41:42 +0100

    make task result better readable, exclude anonymous user
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/providers/scim/tasks.py
+++ authentik/providers/scim/tasks.py
@@ -1,6 +1,9 @@
 """SCIM Provider tasks"""
+from celery.result import allow_join_result
 from django.core.paginator import Paginator
 from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
+from guardian.shortcuts import get_anonymous_user
 from structlog.stdlib import get_logger
 
 from authentik.core.models import Group, User
@@ -23,46 +26,80 @@ def scim_sync_full(self: MonitoredTask, provider_pk: int) -> None:
     if not provider:
         return
     self.set_uid(slugify(provider.name))
+    result = TaskResult(TaskResultStatus.SUCCESSFUL, [])
+    result.messages.append(_("Starting full SCIM sync"))
     # TODO: Filtering
     LOGGER.debug("Starting SCIM sync")
-    users_paginator = Paginator(User.objects.all().order_by("pk"), PAGE_SIZE)
+    users_paginator = Paginator(
+        User.objects.all().exclude(pk=get_anonymous_user().pk).order_by("pk"), PAGE_SIZE
+    )
     groups_paginator = Paginator(Group.objects.all().order_by("pk"), PAGE_SIZE)
-    for page in users_paginator.page_range:
-        scim_sync_users.delay(page, provider_pk)
-    for page in groups_paginator.page_range:
-        scim_sync_group.delay(page, provider_pk)
-    self.set_status(TaskResult(TaskResultStatus.SUCCESSFUL))
+    with allow_join_result():
+        for page in users_paginator.page_range:
+            result.messages.append(_("Syncing page %(page)d of users" % {"page": page}))
+            for msg in scim_sync_users.delay(page, provider_pk).get():
+                result.messages.append(msg)
+        for page in groups_paginator.page_range:
+            result.messages.append(_("Syncing page %(page)d of groups" % {"page": page}))
+            for msg in scim_sync_group.delay(page, provider_pk).get():
+                result.messages.append(msg)
+    self.set_status(result)
 
 
 @CELERY_APP.task()
-def scim_sync_users(page: int, provider_pk):
+def scim_sync_users(page: int, provider_pk: int, **kwargs):
     """Sync single or multiple users to SCIM"""
     provider: SCIMProvider = SCIMProvider.objects.filter(pk=provider_pk).first()
     if not provider:
         return
     client = SCIMClient(provider)
     user_client = SCIMUserClient(client)
-    paginator = Paginator(User.objects.all().order_by("pk"), PAGE_SIZE)
+    paginator = Paginator(
+        User.objects.all().filter(**kwargs).exclude(pk=get_anonymous_user().pk).order_by("pk"),
+        PAGE_SIZE,
+    )
     LOGGER.debug("starting user sync for page", page=page)
+    messages = []
     for user in paginator.page(page).object_list:
         try:
             user_client.write(user)
         except SCIMRequestError as exc:
             LOGGER.warning("failed to sync user", exc=exc, user=user)
+            messages.append(
+                _(
+                    "Failed to sync user %(name)s: %(error)s"
+                    % {
+                        "name": user.username,
+                        "error": str(exc),
+                    }
+                )
+            )
+    return messages
 
 
 @CELERY_APP.task()
-def scim_sync_group(page: int, provider_pk):
+def scim_sync_group(page: int, provider_pk: int, **kwargs):
     """Sync single or multiple groups to SCIM"""
     provider: SCIMProvider = SCIMProvider.objects.filter(pk=provider_pk).first()
     if not provider:
         return
     client = SCIMClient(provider)
     group_client = SCIMGroupClient(client)
-    paginator = Paginator(Group.objects.all().order_by("pk"), PAGE_SIZE)
+    paginator = Paginator(Group.objects.all().filter(**kwargs).order_by("pk"), PAGE_SIZE)
     LOGGER.debug("starting group sync for page", page=page)
+    messages = []
     for group in paginator.page(page).object_list:
         try:
             group_client.write(group)
         except SCIMRequestError as exc:
             LOGGER.warning("failed to sync group", exc=exc, group=group)
+            messages.append(
+                _(
+                    "Failed to sync group %(name)s: %(error)s"
+                    % {
+                        "name": group.name,
+                        "error": str(exc),
+                    }
+                )
+            )
+    return messages
