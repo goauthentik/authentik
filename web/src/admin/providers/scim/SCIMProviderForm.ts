df0e81e5343ef733e12125e309950295a89f9830
Jens Langhammer <jens@goauthentik.io>
2023-03-07 13:19:01 +0100

    update docs
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/admin/providers/scim/SCIMProviderForm.ts
+++ web/src/admin/providers/scim/SCIMProviderForm.ts
@@ -106,7 +106,7 @@ export class SCIMProviderFormPage extends ModelForm<SCIMProvider, number> {
                             <span class="pf-c-switch__label">${t`Exclude service accounts`}</span>
                         </label>
                     </ak-form-element-horizontal>
-                    <ak-form-element-horizontal label=${t`Parent group`} name="parentGroup">
+                    <ak-form-element-horizontal label=${t`Group`} name="parentGroup">
                         <ak-search-select
                             .fetchObjects=${async (query?: string): Promise<Group[]> => {
                                 const args: CoreGroupsListRequest = {
