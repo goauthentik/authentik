f8d40462a3f89ad1a380e8cf1319a11c4c330c51
Philipp Kolberg <philipp.kolberg@t-online.de>
2023-04-30 23:04:25 +0200

    Add redis URL parser test

--- internal/outpost/proxyv2/redisstore/parser_test.go
+++ internal/outpost/proxyv2/redisstore/parser_test.go
@@ -1,3 +1,91 @@
 // SPDX-FileCopyrightText: 2020 The Gitea Authors
 // SPDX-License-Identifier: MIT
 
+package redisstore
+
+import (
+	"net/url"
+	"testing"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func uriMustGetRedisOptions(uri *url.URL) *redis.UniversalOptions {
+	opts, err := getRedisOptions(uri)
+	if err != nil {
+		panic(err)
+	}
+	return opts
+}
+
+func TestRedisUsernameOpt(t *testing.T) {
+	uri, _ := url.Parse("redis://redis:password@myredis/0")
+	opts := uriMustGetRedisOptions(uri)
+
+	if opts.Username != "redis" {
+		t.Fail()
+	}
+}
+
+func TestRedisPasswordOpt(t *testing.T) {
+	uri, _ := url.Parse("redis://redis:password@myredis/0")
+	opts := uriMustGetRedisOptions(uri)
+
+	if opts.Password != "password" {
+		t.Fail()
+	}
+}
+
+func TestSkipVerifyOpt(t *testing.T) {
+	uri, _ := url.Parse("rediss://myredis/0?skipverify=true")
+	tlsConfig := getRedisTLSOptions(uri)
+
+	if !tlsConfig.InsecureSkipVerify {
+		t.Fail()
+	}
+}
+
+func TestInsecureSkipVerifyOpt(t *testing.T) {
+	uri, _ := url.Parse("rediss://myredis/0?insecureskipverify=true")
+	tlsConfig := getRedisTLSOptions(uri)
+
+	if !tlsConfig.InsecureSkipVerify {
+		t.Fail()
+	}
+}
+
+func TestRedisSentinelUsernameOpt(t *testing.T) {
+	uri, _ := url.Parse("redis+sentinel://redis:password@myredis/0?sentinelusername=suser&sentinelpassword=spass")
+	opts := uriMustGetRedisOptions(uri).Failover()
+
+	if opts.SentinelUsername != "suser" {
+		t.Fail()
+	}
+}
+
+func TestRedisSentinelPasswordOpt(t *testing.T) {
+	uri, _ := url.Parse("redis+sentinel://redis:password@myredis/0?sentinelusername=suser&sentinelpassword=spass")
+	opts := uriMustGetRedisOptions(uri).Failover()
+
+	if opts.SentinelPassword != "spass" {
+		t.Fail()
+	}
+}
+
+func TestRedisDatabaseIndexTcp(t *testing.T) {
+	uri, _ := url.Parse("redis://redis:password@myredis/12")
+	opts := uriMustGetRedisOptions(uri)
+
+	if opts.DB != 12 {
+		t.Fail()
+	}
+}
+
+func TestRedisDatabaseIndexUnix(t *testing.T) {
+	uri, _ := url.Parse("redis+socket:///var/run/redis.sock?database=12")
+	opts := uriMustGetRedisOptions(uri)
+
+	if opts.DB != 12 {
+		t.Fail()
+	}
+}
