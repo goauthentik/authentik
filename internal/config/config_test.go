715c1cf94ea5cad9e5d9cbd36248fc9876178bd7
Philipp Kolberg <philipp.kolberg@t-online.de>
2023-05-04 12:36:45 +0200

    Update Go test for Redis URL updating

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -10,25 +10,20 @@ import (
 )
 
 func TestConfigEnv(t *testing.T) {
-	os.Setenv("AUTHENTIK_SECRET_KEY", "bar")
 	cfg = nil
-	if err := Get().fromEnv(); err != nil {
-		panic(err)
-	}
+	os.Setenv("AUTHENTIK_SECRET_KEY", "bar")
 	assert.Equal(t, "bar", Get().SecretKey)
 }
 
 func TestConfigEnv_Scheme(t *testing.T) {
+	cfg = nil
 	os.Setenv("foo", "bar")
 	os.Setenv("AUTHENTIK_SECRET_KEY", "env://foo")
-	cfg = nil
-	if err := Get().fromEnv(); err != nil {
-		panic(err)
-	}
 	assert.Equal(t, "bar", Get().SecretKey)
 }
 
 func TestConfigEnv_File(t *testing.T) {
+	cfg = nil
 	file, err := os.CreateTemp("", "")
 	if err != nil {
 		log.Fatal(err)
@@ -40,14 +35,12 @@ func TestConfigEnv_File(t *testing.T) {
 	}
 
 	os.Setenv("AUTHENTIK_SECRET_KEY", fmt.Sprintf("file://%s", file.Name()))
-	cfg = nil
-	if err := Get().fromEnv(); err != nil {
-		panic(err)
-	}
 	assert.Equal(t, "bar", Get().SecretKey)
 }
 
 func TestConfig_UpdateRedisURL(t *testing.T) {
+	cfg = nil
+	os.Setenv("AUTHENTIK_REDIS__URL", "redis://oldredis:2493/2?idletimeout=20s&skipverify=true&password=pass&username=redis")
 	os.Setenv("AUTHENTIK_REDIS__HOST", "myredis")
 	os.Setenv("AUTHENTIK_REDIS__PORT", "9637")
 	os.Setenv("AUTHENTIK_REDIS__DB", "56")
@@ -55,10 +48,9 @@ func TestConfig_UpdateRedisURL(t *testing.T) {
 	os.Setenv("AUTHENTIK_REDIS__PASSWORD", "\"'% !.;.°")
 	os.Setenv("AUTHENTIK_REDIS__TLS", "true")
 	os.Setenv("AUTHENTIK_REDIS__TLS_REQS", "none")
-	cfg = nil
-	if err := Get().fromEnv(); err != nil {
-		panic(err)
-	}
-	fmt.Println(Get().Redis.URL)
-	assert.Equal(t, "rediss://myredis:9637/56?insecureskipverify=true&password=%22%27%25+%21.%3B.%C2%B0&username=default", Get().Redis.URL)
+	assert.Equal(
+		t,
+		"rediss://myredis:9637/56?idletimeout=20s&insecureskipverify=true&password=%22%27%25+%21.%3B.%C2%B0&username=default",
+		Get().Redis.URL,
+	)
 }
