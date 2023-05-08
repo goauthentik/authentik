b5543525fb54c07612da5b7ccee780d813869450
Jens Langhammer <jens@goauthentik.io>
2023-05-08 20:16:41 +0200

    website: show all blog posts in sidebar
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -4,6 +4,7 @@ const releases = sidebar.docs
     .filter((doc) => doc.link?.slug === "releases")[0]
     .items.filter((release) => typeof release === "string");
 
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
     title: "authentik",
     tagline: "Making authentication simple.",
@@ -145,6 +146,11 @@ module.exports = {
                 gtag: {
                     trackingID: "G-9MVR9WZFZH",
                     anonymizeIP: true,
+                },
+                blog: {
+                    showReadingTime: true,
+                    blogSidebarTitle: "All our posts",
+                    blogSidebarCount: "ALL",
                 },
             },
         ],
