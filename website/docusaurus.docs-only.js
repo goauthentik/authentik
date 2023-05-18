6af85b002fcbf0523fd964af7db95449198a4c5e
risson <18313093+rissson@users.noreply.github.com>
2023-05-19 00:59:43 +0200

    website: automatically add PR links to release notes (#5682)
    * website: automatically add PR links to release notes
    
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>
    
    * small cleanup, also link in docs-only build
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * fix docs-only build
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * some more minor cleanup
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    ---------
    
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    Co-authored-by: Jens Langhammer <jens@goauthentik.io>

--- website/docusaurus.docs-only.js
+++ website/docusaurus.docs-only.js
@@ -1,106 +1,127 @@
-const mainConfig = require("./docusaurus.config");
+const config = require("./docusaurus.config");
 
-module.exports = {
-    title: "authentik",
-    tagline: "Making authentication simple.",
-    url: "https://goauthentik.io",
-    baseUrl: "/if/help/",
-    onBrokenLinks: "throw",
-    favicon: "img/icon.png",
-    organizationName: "BeryJu",
-    projectName: "authentik",
-    themeConfig: {
-        navbar: {
-            title: "authentik",
-            logo: {
-                alt: "authentik logo",
-                src: "img/icon_left_brand.svg",
-            },
-            items: [
-                {
-                    to: "docs/",
-                    activeBasePath: "docs",
-                    label: "Docs",
-                    position: "left",
+module.exports = async function () {
+    const remarkGithub = (await import("remark-github")).default;
+    const mainConfig = await config();
+    return {
+        title: "authentik",
+        tagline: "Making authentication simple.",
+        url: "https://goauthentik.io",
+        baseUrl: "/if/help/",
+        onBrokenLinks: "throw",
+        favicon: "img/icon.png",
+        organizationName: "BeryJu",
+        projectName: "authentik",
+        themeConfig: {
+            navbar: {
+                title: "authentik",
+                logo: {
+                    alt: "authentik logo",
+                    src: "img/icon_left_brand.svg",
                 },
-                {
-                    to: "integrations/",
-                    activeBasePath: "integrations",
-                    label: "Integrations",
-                    position: "left",
-                },
-                {
-                    to: "developer-docs/",
-                    activeBasePath: "developer-docs",
-                    label: "Developer Docs",
-                    position: "left",
-                },
-                {
-                    href: "https://github.com/goauthentik/authentik",
-                    label: "GitHub",
-                    position: "right",
-                },
-                {
-                    href: "https://goauthentik.io/discord",
-                    label: "Discord",
-                    position: "right",
-                },
-            ],
-        },
-        footer: mainConfig.themeConfig.footer,
-        colorMode: mainConfig.themeConfig.colorMode,
-        tableOfContents: mainConfig.themeConfig.tableOfContents,
-    },
-    presets: [
-        [
-            "@docusaurus/preset-classic",
-            {
-                docs: {
-                    id: "docs",
-                    sidebarPath: require.resolve("./sidebars.js"),
-                    editUrl:
-                        "https://github.com/goauthentik/authentik/edit/main/website/",
-                },
-                pages: false,
-                theme: {
-                    customCss: require.resolve("./src/css/custom.css"),
-                },
-            },
-        ],
-    ],
-    plugins: [
-        [
-            "@docusaurus/plugin-content-docs",
-            {
-                id: "docsIntegrations",
-                path: "integrations",
-                routeBasePath: "integrations",
-                sidebarPath: require.resolve("./sidebarsIntegrations.js"),
-                editUrl:
-                    "https://github.com/goauthentik/authentik/edit/main/website/",
-            },
-        ],
-        [
-            "@docusaurus/plugin-content-docs",
-            {
-                id: "docsDevelopers",
-                path: "developer-docs",
-                routeBasePath: "developer-docs",
-                sidebarPath: require.resolve("./sidebarsDev.js"),
-                editUrl:
-                    "https://github.com/goauthentik/authentik/edit/main/website/",
-            },
-        ],
-        [
-            "@docusaurus/plugin-client-redirects",
-            {
-                redirects: [
+                items: [
                     {
-                        to: "/docs/",
-                        from: ["/"],
+                        to: "docs/",
+                        activeBasePath: "docs",
+                        label: "Docs",
+                        position: "left",
+                    },
+                    {
+                        to: "integrations/",
+                        activeBasePath: "integrations",
+                        label: "Integrations",
+                        position: "left",
+                    },
+                    {
+                        to: "developer-docs/",
+                        activeBasePath: "developer-docs",
+                        label: "Developer Docs",
+                        position: "left",
+                    },
+                    {
+                        href: "https://github.com/goauthentik/authentik",
+                        label: "GitHub",
+                        position: "right",
+                    },
+                    {
+                        href: "https://goauthentik.io/discord",
+                        label: "Discord",
+                        position: "right",
                     },
                 ],
             },
+            footer: mainConfig.themeConfig.footer,
+            colorMode: mainConfig.themeConfig.colorMode,
+            tableOfContents: mainConfig.themeConfig.tableOfContents,
+        },
+        presets: [
+            [
+                "@docusaurus/preset-classic",
+                {
+                    docs: {
+                        id: "docs",
+                        sidebarPath: require.resolve("./sidebars.js"),
+                        editUrl:
+                            "https://github.com/goauthentik/authentik/edit/main/website/",
+                        remarkPlugins: [
+                            [
+                                remarkGithub,
+                                {
+                                    repository: "goauthentik/authentik",
+                                    // Only replace issues and PR links
+                                    buildUrl: function (
+                                        values,
+                                        defaultBuildUrl
+                                    ) {
+                                        return values.type === "issue"
+                                            ? defaultBuildUrl(values)
+                                            : false;
+                                    },
+                                },
+                            ],
+                        ],
+                    },
+                    pages: false,
+                    theme: {
+                        customCss: require.resolve("./src/css/custom.css"),
+                    },
+                },
+            ],
         ],
-    ],
+        plugins: [
+            [
+                "@docusaurus/plugin-content-docs",
+                {
+                    id: "docsIntegrations",
+                    path: "integrations",
+                    routeBasePath: "integrations",
+                    sidebarPath: require.resolve("./sidebarsIntegrations.js"),
+                    editUrl:
+                        "https://github.com/goauthentik/authentik/edit/main/website/",
+                },
+            ],
+            [
+                "@docusaurus/plugin-content-docs",
+                {
+                    id: "docsDevelopers",
+                    path: "developer-docs",
+                    routeBasePath: "developer-docs",
+                    sidebarPath: require.resolve("./sidebarsDev.js"),
+                    editUrl:
+                        "https://github.com/goauthentik/authentik/edit/main/website/",
+                },
+            ],
+            [
+                "@docusaurus/plugin-client-redirects",
+                {
+                    redirects: [
+                        {
+                            to: "/docs/",
+                            from: ["/"],
+                        },
+                    ],
+                },
+            ],
+        ],
+    };
 };
