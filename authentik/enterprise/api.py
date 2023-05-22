2e5c7c2f6a21b1190e9f7c2562c5c3d591a64c03
Jens Langhammer <jens@goauthentik.io>
2023-05-22 11:16:59 +0200

    add external users
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/enterprise/api.py
+++ authentik/enterprise/api.py
@@ -1,13 +1,32 @@
 """Enterprise API Views"""
+from dacite import from_dict
+from jwt import PyJWTError, decode
 from rest_framework.serializers import ModelSerializer
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.exceptions import ValidationError
 from authentik.core.api.used_by import UsedByMixin
-from authentik.enterprise.models import License
+from authentik.enterprise.models import License, LicenseBody, get_licensing_key
+from authentik.root.install_id import get_install_id
 
 
 class LicenseSerializer(ModelSerializer):
     """License Serializer"""
+
+    def validate_key(self, key: str) -> str:
+        try:
+            body = from_dict(LicenseBody, decode(
+                key,
+                get_licensing_key(),
+                algorithms=["RS256"],
+                options={
+                    "verify_aud": False,
+                },
+            ))
+        except PyJWTError:
+            raise ValidationError("Unable to verify license")
+        if body.install_id != get_install_id():
+            raise ValidationError("Unable to verify license")
+        return key
 
     class Meta:
         model = License
@@ -17,11 +36,13 @@ class LicenseSerializer(ModelSerializer):
             "key",
             "expiry",
             "users",
+            "external_users",
         ]
         extra_kwargs = {
             "name": {"read_only": True},
             "expiry": {"read_only": True},
             "users": {"read_only": True},
+            "external_users": {"read_only": True},
         }
 
 
--- authentik/enterprise/models.py
+++ authentik/enterprise/models.py
@@ -1,6 +1,8 @@
 """Enterprise models"""
+from dataclasses import dataclass
 from functools import lru_cache
 from typing import Optional
+from dacite import from_dict
 
 from django.db import models
 from jwt import decode
@@ -17,6 +19,14 @@ def is_licensed() -> Optional["License"]:
     # license = License.objects.filter
 
 
+@dataclass
+class LicenseBody:
+
+    install_id: str
+    users: int
+    external_users: int
+    exp: int
+
 class License(models.Model):
     """An authentik enterprise license"""
 
@@ -26,14 +36,15 @@ class License(models.Model):
     key = models.TextField()
     expiry = models.DateTimeField()
     users = models.BigIntegerField()
+    external_users = models.BigIntegerField()
 
     @property
-    def status(self) -> dict:
-        return decode(
+    def status(self) -> LicenseBody:
+        return from_dict(LicenseBody, decode(
             self.key,
             get_licensing_key(),
             algorithms=["RS256"],
             options={
                 "verify_aud": False,
             },
-        )
+        ))
