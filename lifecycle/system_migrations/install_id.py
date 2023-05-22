195f07a1f400f24b72a83fafb25d7d345e49f281
Jens Langhammer <jens@goauthentik.io>
2023-05-22 13:29:23 +0200

    add fallback when no migrations table exists
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- lifecycle/system_migrations/install_id.py
+++ lifecycle/system_migrations/install_id.py
@@ -18,12 +18,10 @@ class Migration(BaseMigration):
         )
         return not bool(self.cur.rowcount)
 
-    def run(self):
-        self.cur.execute("select count(*) from django_migrations;")
-        migrations = self.cur.fetchone()[0]
+    def upgrade(self, migrate=False):
         self.cur.execute(SQL_STATEMENT)
         self.con.commit()
-        if migrations > 0:
+        if migrate:
             # If we already have migrations in the database, assume we're upgrading an existing install
             # and set the install id to the secret key
             self.cur.execute(
@@ -34,3 +32,14 @@ class Migration(BaseMigration):
             install_id = str(uuid4())
             self.cur.execute("INSERT INTO authentik_install_id (id) VALUES (%s)", (install_id,))
         self.con.commit()
+
+    def run(self):
+        self.cur.execute(
+            "select * from information_schema.tables where table_name = 'django_migrations';"
+        )
+        if not bool(self.cur.rowcount):
+            # No django_migrations table, so generate a new id
+            return self.upgrade(migrate=False)
+        self.cur.execute("select count(*) from django_migrations;")
+        migrations = self.cur.fetchone()[0]
+        return self.upgrade(migrate=migrations > 0)
