473b8323c99e2e27af2db9f36956b38f77643b66
Ken Sternberg <ken@goauthentik.io>
2023-05-26 15:21:30 -0700

    The xliff-converter is tagged as 'xliff-converter', but has been deleted.
    \#\# Details
    
    -   Resolves #5171
    
    \#\# Changes
    
    \#\#\# New Features
    
    -   Adds a "Add an Application" to the LibraryView if there are no applications and the user is an administrator.
    
    \#\#\# Breaking Changes
    
    -   Adds breaking change which causes \<issue\>.
    
    \#\# Checklist
    
    -   [ ] Local tests pass (`ak test authentik/`)
    -   [ ] The code has been formatted (`make lint-fix`)
    
    If an API change has been made
    
    -   [ ] The API schema has been updated (`make gen-build`)
    
    If changes to the frontend have been made
    
    -   [ ] The code has been formatted (`make web`)
    -   [ ] The translation files have been updated (`make i18n-extract`)
    
    If applicable
    
    -   [ ] The documentation has been updated
    -   [ ] The documentation has been formatted (`make website`)

--- web/xliff-converter/src/__tests__/unit/cli.spec.ts
+++
@@ -1,9 +0,0 @@
-import { resolve } from 'path';
-
-describe('cli', () => {
-  it('should exist', () => {
-    const cli = require(resolve(__dirname, '../../cli'));
-
-    expect(cli).toBeTruthy();
-  });
-});
