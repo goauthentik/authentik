047eed04193865f3a3419284774c1dd6a22f3b4f
Jens Langhammer <jens@goauthentik.io>
2023-05-26 17:43:18 +0200

    website: fix go-get function (always fallback to authentik repo except hardcoded values)
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- website/netlify/functions/go-get.js
+++ website/netlify/functions/go-get.js
@@ -2,18 +2,18 @@ const gitHubNamespace = "goauthentik";
 
 exports.handler = async function (event, context) {
     let repo = "";
-    if (event.path === "/") {
-        repo = "/authentik";
-    } else if (event.path.startsWith("/api")) {
-        repo = "/client-go";
+    if (event.path.startsWith("/api")) {
+        repo = "client-go";
+    } else if (event.path.startsWith("/terraform-provider-authentik")) {
+        repo = "terraform-provider-authentik";
     } else {
-        repo = event.path;
+        repo = "authentik";
     }
     return {
         statusCode: 200,
         headers: {
             "content-type": "text/html",
         },
-        body: `<meta name="go-import" content="${event.headers.host}${event.path} git https://github.com/${gitHubNamespace}${repo}">`,
+        body: `<meta name="go-import" content="${event.headers.host}${event.path} git https://github.com/${gitHubNamespace}/${repo}">`,
     };
 };
