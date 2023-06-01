be85eecac54e051f1daebe6e8719e726c78068d3
Jens Langhammer <jens@goauthentik.io>
2023-06-01 19:35:13 +0200

    release: 2023.5.3

--- web/src/common/constants.ts
+++ web/src/common/constants.ts
@@ -3,7 +3,7 @@ export const SUCCESS_CLASS = "pf-m-success";
 export const ERROR_CLASS = "pf-m-danger";
 export const PROGRESS_CLASS = "pf-m-in-progress";
 export const CURRENT_CLASS = "pf-m-current";
-export const VERSION = "2023.5.2";
+export const VERSION = "2023.5.3";
 export const TITLE_DEFAULT = "authentik";
 export const ROUTE_SEPARATOR = ";";
 
