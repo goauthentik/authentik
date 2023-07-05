2884c0f63f0f1c99169eba3c59ec46ba7a016510
Samir Musali <samir@goauthentik.io>
2023-07-05 14:01:16 +0300

    deny: add custom message

--- authentik/stages/deny/stage.py
+++ authentik/stages/deny/stage.py
@@ -2,13 +2,23 @@
 from django.http import HttpRequest, HttpResponse
 
 from authentik.flows.stage import StageView
+from authentik.policies.exceptions import PolicyException
+from authentik.policies.expression.evaluator import PolicyEvaluator
 
 
 class DenyStageView(StageView):
     """Cancels the current flow"""
 
-    def get(self, request: HttpRequest) -> HttpResponse:
+    def get(self, request: HttpRequest, message="Denied") -> HttpResponse:
         """Cancels the current flow"""
+        evaluator = PolicyEvaluator("deny")
+        template = f'ak_message("{message}");return False'
+        try:
+            evaluator.evaluate(template)
+        except PolicyException as exc:
+            self.logger.warning(f'Failed to evaluate "{template}"', exc=exc)
+
+        self.logger.warning(message)
         return self.executor.stage_invalid()
 
     def post(self, request: HttpRequest) -> HttpResponse:
