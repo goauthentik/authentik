81d2d155901d6c7204e2f08d270f9178c9ed52d6
Jens Langhammer <jens@goauthentik.io>
2023-07-26 12:57:57 +0200

    use smaller screenshots for landing page
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- website/src/pages/index.jsx
+++ website/src/pages/index.jsx
@@ -153,13 +153,13 @@ function Home() {
                                                 firstImage={{
                                                     id: 1,
                                                     imageUrl: useBaseUrl(
-                                                        "img/screen_apps_dark.jpg",
+                                                        "img/landing_screen_apps_dark.jpg",
                                                     ),
                                                 }}
                                                 secondImage={{
                                                     id: 2,
                                                     imageUrl: useBaseUrl(
-                                                        "img/screen_apps_light.jpg",
+                                                        "img/landing_screen_apps_light.jpg",
                                                     ),
                                                 }}
                                             />
@@ -203,13 +203,13 @@ function Home() {
                                                 firstImage={{
                                                     id: 1,
                                                     imageUrl: useBaseUrl(
-                                                        "img/screen_admin_dark.jpg",
+                                                        "img/landing_screen_admin_dark.jpg",
                                                     ),
                                                 }}
                                                 secondImage={{
                                                     id: 2,
                                                     imageUrl: useBaseUrl(
-                                                        "img/screen_admin_light.jpg",
+                                                        "img/landing_screen_admin_light.jpg",
                                                     ),
                                                 }}
                                             />
