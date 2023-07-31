385112c2ae361c1b063c8fa8067fdb8cb3e04c28
Jens Langhammer <jens@goauthentik.io>
2023-07-31 17:53:36 +0200

    improve error handling, add test
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/lib/config.py
+++ authentik/lib/config.py
@@ -213,11 +213,12 @@ class ConfigLoader:
         attr: Attr = get_path_from_dict(root, path, sep=sep, default=Attr(default))
         return attr.value
 
-    def get_int(self, path: str, default = 0) -> int:
+    def get_int(self, path: str, default=0) -> int:
         """Wrapper for get that converts value into int"""
         try:
             return int(self.get(path, default))
-        except ValueError:
+        except ValueError as exc:
+            self.log("warning", "Failed to parse config as int", path=path, exc=str(exc))
             return default
 
     def get_bool(self, path: str, default=False) -> bool:
