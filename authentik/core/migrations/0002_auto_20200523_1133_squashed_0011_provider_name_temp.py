73ba88e16e789608cee4522abfb82dc33d876abf
Jens Langhammer <jens@goauthentik.io>
2023-07-31 15:35:32 +0200

    only change what is required with migrations
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/core/migrations/0002_auto_20200523_1133_squashed_0011_provider_name_temp.py
+++ authentik/core/migrations/0002_auto_20200523_1133_squashed_0011_provider_name_temp.py
@@ -124,7 +124,10 @@ class Migration(migrations.Migration):
             name="groups",
             field=models.ManyToManyField(
                 blank=True,
-                help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",
+                help_text=(
+                    "The groups this user belongs to. A user will get all permissions granted to"
+                    " each of their groups."
+                ),
                 related_name="user_set",
                 related_query_name="user",
                 to="auth.Group",
