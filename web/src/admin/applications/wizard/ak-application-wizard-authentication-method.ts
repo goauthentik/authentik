67b737102627b43087ebacc347e178c68513940a
Ken Sternberg <ken@goauthentik.io>
2023-08-07 16:09:47 -0700

    Rounding out the catalog.

--- web/src/admin/applications/wizard/ak-application-wizard-authentication-method.ts
+++ web/src/admin/applications/wizard/ak-application-wizard-authentication-method.ts
@@ -2,12 +2,17 @@ import { customElement } from "@lit/reactive-element/decorators/custom-element.j
 import { TemplateResult, html } from "lit";
 
 import ApplicationWizardPageBase from "./ApplicationWizardPageBase";
+import "./ldap/ak-application-wizard-authentication-by-ldap";
+import "./oauth/ak-application-wizard-authentication-by-oauth";
+import "./proxy/ak-application-wizard-authentication-for-reverse-proxy";
+import "./proxy/ak-application-wizard-authentication-for-single-forward-proxy";
 
 // prettier-ignore
 const handlers = new Map<string, () => TemplateResult>([
-    ["ldapprovider", () => html`<ak-application-wizard-authentication-by-ldap></ak-application-wizard-authentication-by-ldap>`],
-    ["oauth2provider", () => html`<p>Under construction</p>`],
-    ["proxyprovider", () => html`<p>Under construction</p>`],
+    ["ldapprovider`", () => html`<ak-application-wizard-authentication-by-ldap></ak-application-wizard-authentication-by-ldap>`],
+    ["oauth2provider", () => html`<ak-application-wizard-authentication-by-oauth></ak-application-wizard-authentication-by-oauth>`],
+    ["proxyprovider-proxy", () => html`<ak-application-wizard-authentication-for-reverse-proxy></ak-application-wizard-authentication-for-reverse-proxy>`],
+    ["proxyprovider-forwardsingle", () => html`<ak-application-wizard-authentication-for-single-forward-proxy></ak-application-wizard-authentication-for-single-forward-proxy>`],
     ["radiusprovider", () => html`<p>Under construction</p>`],
     ["samlprovider", () => html`<p>Under construction</p>`],
     ["scimprovider", () => html`<p>Under construction</p>`],
