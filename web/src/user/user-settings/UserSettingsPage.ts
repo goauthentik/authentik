b28f42d16eea42de76c4fb5a89bf4360dc96da22
Jens Langhammer <jens@goauthentik.io>
2023-08-08 00:45:52 +0200

    web/user: fix user settings colours on dark theme
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/user/user-settings/UserSettingsPage.ts
+++ web/src/user/user-settings/UserSettingsPage.ts
@@ -49,6 +49,18 @@ export class UserSettingsPage extends AKElement {
             PFFormControl,
             PFStack,
             css`
+                .pf-c-page {
+                    --pf-c-page--BackgroundColor: transparent;
+                }
+                .pf-c-page__main-section {
+                    --pf-c-page__main-section--BackgroundColor: transparent;
+                }
+                :host([theme="dark"]) .pf-c-page {
+                    --pf-c-page--BackgroundColor: transparent;
+                }
+                :host([theme="dark"]) .pf-c-page__main-section {
+                    --pf-c-page__main-section--BackgroundColor: transparent;
+                }
                 @media screen and (min-width: 1200px) {
                     :host {
                         width: 90rem;
