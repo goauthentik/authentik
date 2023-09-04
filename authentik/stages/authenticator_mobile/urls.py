34d8a6e86faaa8ca9fe664e18db888b36b645482
Jens Langhammer <jens@goauthentik.io>
2023-09-04 22:19:15 +0200

    add mobile only subset schema
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/stages/authenticator_mobile/urls.py
+++ authentik/stages/authenticator_mobile/urls.py
@@ -4,6 +4,13 @@ from authentik.stages.authenticator_mobile.api.device import (
     MobileDeviceViewSet,
 )
 from authentik.stages.authenticator_mobile.api.stage import AuthenticatorMobileStageViewSet
+from rest_framework import routers
+
+# Separate router which is used for the subset-schema generation
+# for the cloud-gateway we (currently) only want the mobile device endpoints
+# and don't need all other API endpoints
+router = routers.DefaultRouter()
+router.register("authenticators/mobile", MobileDeviceViewSet)
 
 api_urlpatterns = [
     ("authenticators/mobile", MobileDeviceViewSet),
@@ -14,3 +21,5 @@ api_urlpatterns = [
     ),
     ("stages/authenticator/mobile", AuthenticatorMobileStageViewSet),
 ]
+
+urlpatterns = router.urls
--- web/src/flow/stages/identification/IdentificationStage.ts
+++ web/src/flow/stages/identification/IdentificationStage.ts
@@ -255,6 +255,8 @@ export class IdentificationStage extends BaseStage<
                     name="uidField"
                     placeholder=${label}
                     autofocus=""
+                    autocapitalize="off"
+                    autocorrect="off"
                     autocomplete="username"
                     class="pf-c-form-control"
                     required
