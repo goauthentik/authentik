b9871c7401ed3fef56fbd4855032461058e81d87
Alissa Gerhard <alissa.gerhard@port-zero.com>
2023-09-08 17:03:03 +0200

    sources/ldap: fix task timeout for ldap_sync_all and ldap_sync_single

--- authentik/sources/ldap/tasks.py
+++ authentik/sources/ldap/tasks.py
@@ -26,14 +26,20 @@ SYNC_CLASSES = [
 CACHE_KEY_PREFIX = "goauthentik.io/sources/ldap/page/"
 
 
-@CELERY_APP.task()
+@CELERY_APP.task(
+    soft_time_limit=60 * 60 * CONFIG.get_int("ldap.task_timeout_hours"),
+    task_time_limit=60 * 60 * CONFIG.get_int("ldap.task_timeout_hours"),
+)
 def ldap_sync_all():
     """Sync all sources"""
     for source in LDAPSource.objects.filter(enabled=True):
         ldap_sync_single(source.pk)
 
 
-@CELERY_APP.task()
+@CELERY_APP.task(
+    soft_time_limit=60 * 60 * CONFIG.get_int("ldap.task_timeout_hours"),
+    task_time_limit=60 * 60 * CONFIG.get_int("ldap.task_timeout_hours"),
+)
 def ldap_sync_single(source_pk: str):
     """Sync a single source"""
     source: LDAPSource = LDAPSource.objects.filter(pk=source_pk).first()
