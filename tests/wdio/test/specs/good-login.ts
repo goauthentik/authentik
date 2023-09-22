918463906b2d5ab9a6b868bfe5708f549d5de6ac
Ken Sternberg <ken@goauthentik.io>
2023-09-22 13:04:46 -0700

    web/test: changed the name of one test to reflect it's 'good' status

--- tests/wdio/test/specs/good-login.ts
+++ tests/wdio/test/specs/good-login.ts
@@ -3,7 +3,7 @@ import UserLibraryPage from "../pageobjects/user-library.page.js";
 import { expect } from "@wdio/globals";
 
 describe("Log into Authentik", () => {
-    it("should login with valid credentials", async () => {
+    it("should login with valid credentials and reach the UserLibrary", async () => {
         await LoginPage.open();
         await LoginPage.login("ken@goauthentik.io", "eat10bugs");
         await expect(UserLibraryPage.pageHeader).toHaveText("My applications");
