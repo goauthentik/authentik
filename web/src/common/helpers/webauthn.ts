faac2394aa8d8fac6fcacaecf4be78ce98beb6f1
Jens Langhammer <jens@goauthentik.io>
2023-09-22 16:57:22 +0200

    include localhost
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/common/helpers/webauthn.ts
+++ web/src/common/helpers/webauthn.ts
@@ -20,7 +20,7 @@ export function checkWebAuthnSupport() {
     if ("credentials" in navigator) {
         return;
     }
-    if (window.location.protocol === "http:") {
+    if (window.location.protocol === "http:" && window.location.hostname !== "localhost") {
         throw new Error(msg("WebAuthn requires this page to be accessed via HTTPS."));
     }
     throw new Error(msg("WebAuthn not supported by browser."));
