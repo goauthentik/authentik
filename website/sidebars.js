c0099576204b907cf152a6f4e8880df962ae668b
Jens Langhammer <jens@goauthentik.io>
2023-09-26 22:04:42 +0200

    move blueprints and API to main docs
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- website/sidebars.js
+++ website/sidebars.js
@@ -310,6 +310,43 @@ const docsSidebar = {
         },
         {
             type: "category",
+            label: "Blueprints",
+            link: {
+                type: "doc",
+                id: "blueprints/index",
+            },
+            items: [
+                "blueprints/export",
+                "blueprints/v1/structure",
+                "blueprints/v1/tags",
+                "blueprints/v1/example",
+                {
+                    type: "category",
+                    label: "Models",
+                    link: {
+                        type: "doc",
+                        id: "blueprints/v1/models",
+                    },
+                    items: ["blueprints/v1/meta"],
+                },
+            ],
+        },
+        {
+            type: "category",
+            label: "API",
+            link: {
+                type: "doc",
+                id: "api/api",
+            },
+            items: [
+                "api/flow-executor",
+                "api/making-schema-changes",
+                "api/websocket",
+                "api/browser",
+            ],
+        },
+        {
+            type: "category",
             label: "Release Notes",
             link: {
                 type: "generated-index",
--- website/sidebarsDev.js
+++ website/sidebarsDev.js
@@ -14,43 +14,6 @@ module.exports = {
         },
         {
             type: "category",
-            label: "Blueprints",
-            link: {
-                type: "doc",
-                id: "blueprints/index",
-            },
-            items: [
-                "blueprints/export",
-                "blueprints/v1/structure",
-                "blueprints/v1/tags",
-                "blueprints/v1/example",
-                {
-                    type: "category",
-                    label: "Models",
-                    link: {
-                        type: "doc",
-                        id: "blueprints/v1/models",
-                    },
-                    items: ["blueprints/v1/meta"],
-                },
-            ],
-        },
-        {
-            type: "category",
-            label: "API",
-            link: {
-                type: "doc",
-                id: "api/api",
-            },
-            items: [
-                "api/flow-executor",
-                "api/making-schema-changes",
-                "api/websocket",
-                "api/browser",
-            ],
-        },
-        {
-            type: "category",
             label: "Setup",
             items: [
                 "setup/full-dev-environment",
