809d9573ab0d53587705e8627d1b3430d6bc91cc
Ken Sternberg <ken@goauthentik.io>
2023-10-03 08:45:07 -0700

    web: test oauth2 provider "guinea pig" for new components
    I used the Oauth2 provider page as my experiment in seeing if the
    horizontal-element wrappers could be used instead of the raw wrappers
    themselves, and I wanted to make sure a test existed that asserts
    that filling out THAT form in the ProvidersList and ProvidersForm
    didn't break anything.
    
    This commit updates the WDIO tests to do just that; the test is
    simple, but it does exercise the `name` field of the Provider,
    something not needed in the Wizard because it's set automatically
    based on the Application name, and it even asserts that the new
    Provider exists in the list of available Providers when it's done.

--- tests/wdio/test/pageobjects/admin.page.ts
+++ tests/wdio/test/pageobjects/admin.page.ts
@@ -4,8 +4,9 @@ import { browser } from "@wdio/globals";
 const CLICK_TIME_DELAY = 250;
 
 export default class AdminPage extends Page {
+
     public get pageHeader() {
-        return $(">>>ak-page-header h1");
+        return $('>>>ak-page-header slot[name="header"]');
     }
 
     async openApplicationsListPage() {
