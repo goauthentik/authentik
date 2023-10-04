efac31ef0ea833cad67b87d2f81d3f4d83801d96
Philipp Kolberg <philipp.kolberg@t-online.de>
2023-10-04 03:31:56 +0200

    Revert string to bool parser changes
    Keep it similar to how Golang behaves

--- authentik/lib/utils/parser.py
+++ authentik/lib/utils/parser.py
@@ -15,10 +15,10 @@ from redis.exceptions import TimeoutError as RedisTimeoutError
 
 def _to_bool(value):
     """Convert string to bool"""
-    match value.lower():
-        case "1" | "t" | "true":
+    match value:
+        case "1" | "t" | "T" | "true" | "TRUE" | "True":
             return True
-        case "0" | "f" | "false":
+        case "0" | "f" | "F" | "false" | "FALSE" | "False":
             return False
     print("Invalid boolean value found in Redis URL query, skipping")
     return False
