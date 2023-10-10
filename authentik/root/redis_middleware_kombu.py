25214002813856ccdf5d60cce0474d823600e768
Philipp Kolberg <philipp.kolberg@t-online.de>
2023-10-10 05:50:14 +0200

    Add Redis cluster support for Kombu

--- authentik/root/redis_middleware_kombu.py
+++ authentik/root/redis_middleware_kombu.py
@@ -1,5 +1,6 @@
 """Make Kombu use custom Redis connection"""
 from contextlib import contextmanager
+from queue import Empty
 from time import time
 from urllib.parse import urlparse
 
@@ -11,7 +12,10 @@ from kombu.transport.redis import Transport
 from kombu.utils import uuid
 from kombu.utils.collections import HashedSeq
 from kombu.utils.compat import _detect_environment
+from kombu.utils.encoding import bytes_to_str
 from kombu.utils.eventio import ERR, READ
+from kombu.utils.json import loads
+from redis.exceptions import MovedError
 
 from authentik.lib.utils.parser import (
     get_client,
@@ -115,10 +119,16 @@ class ClusterPoller(MultiChannelPoller):
         if self._chan_to_sock:
             return [conn for _, _, conn, _ in self._chan_to_sock]
 
-        return [
-            channel.client.connection_pool.get_connection_by_key(key, "NOOP")
-            for key in channel.active_queues
-        ]
+        conns = []
+
+        for key in channel.active_queues:
+            node = channel.client.get_node_from_key(key)
+            conns += [channel.client.get_redis_connection(node).connection_pool.get_connection("_")]
+
+        return conns
+
+    def _on_poll_start(self):
+        raise NotImplementedError()
 
     # pylint: disable=inconsistent-return-statements
     def handle_event(self, fileno, event):
@@ -150,8 +160,6 @@ class CustomChannel(Channel):
         self.client_config = None
         self.config = args[0].client.config
         super().__init__(*args, **kwargs)
-        if self.config["type"] == "cluster":
-            self.QoS = CustomQoS
         del self.Client
 
     def inject_custom_connection_class(self, connection_class):
@@ -192,7 +200,7 @@ class CustomChannel(Channel):
             NotImplementedError: as exchange_bind
                 is not implemented by the base virtual implementation.
         """
-        raise NotImplementedError()
+        raise NotImplementedError("Redis channels do not support exchange_bind.")
 
     def exchange_unbind(self, *args, **kwargs):
         """Unbind an exchange from an exchange.
@@ -201,7 +209,7 @@ class CustomChannel(Channel):
             NotImplementedError: as exchange_unbind
                 is not implemented by the base virtual implementation.
         """
-        raise NotImplementedError()
+        raise NotImplementedError("Redis channels do not support exchange_unbind.")
 
     def flow(self, active=True):
         """Enable/disable message flow.
@@ -213,19 +221,195 @@ class CustomChannel(Channel):
         raise NotImplementedError("Redis channels do not support flow.")
 
 
+class CustomClusterChannel(Channel):
+    """Enable usage of any valid Redis client as Kombu channel"""
+
+    QoS = CustomQoS
+
+    socket_keepalive = True
+
+    namespace = "default"
+    keyprefix_queue = "/{namespace}/_kombu/binding%s"
+    keyprefix_fanout = "/{namespace}/_kombu/fanout."
+    unacked_key = "/{namespace}/_kombu/unacked"
+    unacked_index_key = "/{namespace}/_kombu/unacked_index"
+    unacked_mutex_key = "/{namespace}/_kombu/unacked_mutex"
+
+    min_priority = 0
+    max_priority = 0
+    priority_steps = [min_priority]
+
+    from_transport_options = Channel.from_transport_options + (
+        "namespace",
+        "keyprefix_queue",
+        "keyprefix_fanout",
+    )
+
+    def __init__(self, conn, *args, **kwargs):
+        self.client_config = None
+        self.config = conn.client.config
+        options = conn.client.transport_options
+        namespace = options.get("namespace", self.namespace)
+        keys = [
+            "keyprefix_queue",
+            "keyprefix_fanout",
+            "unacked_key",
+            "unacked_index_key",
+            "unacked_mutex_key",
+        ]
+
+        for key in keys:
+            value = options.get(key, getattr(self, key))
+            options[key] = value.format(namespace=namespace)
+
+        super().__init__(conn, *args, **kwargs)
+        self.client.info()
+
+    def inject_custom_connection_class(self, connection_class):
+        """Connection injection that is required for Kombu async pools"""
+
+        class AsyncConnection(connection_class):
+            """Kombu injection for async connections"""
+
+            def disconnect(self):
+                """Add Kombu method to default disconnect procedure"""
+                super().disconnect()
+                self._on_connection_disconnect(self)
+
+        return AsyncConnection
+
+    def _create_client(self, asynchronous=False):
+        """Create a new Redis client using the stored connection pool"""
+        if asynchronous:
+            pool = self.async_pool
+        else:
+            pool = self.pool
+        return get_client(self.client_config, pool)
+
+    def _get_pool(self, asynchronous=False):
+        """Create a new connection pool using parsed Redis config"""
+        pool, client_config = get_connection_pool(
+            self.config,
+            use_async=False,
+            update_connection_class=self.inject_custom_connection_class if asynchronous else None,
+        )
+        self.client_config = client_config
+        return pool
+
+    def exchange_bind(self, *args, **kwargs):
+        """Bind an exchange to an exchange.
+
+        Raises:
+            NotImplementedError: as exchange_bind
+                is not implemented by the base virtual implementation.
+        """
+        raise NotImplementedError("Redis channels do not support exchange_bind.")
+
+    def exchange_unbind(self, *args, **kwargs):
+        """Unbind an exchange from an exchange.
+
+        Raises:
+            NotImplementedError: as exchange_unbind
+                is not implemented by the base virtual implementation.
+        """
+        raise NotImplementedError("Redis channels do not support exchange_unbind.")
+
+    def flow(self, active=True):
+        """Enable/disable message flow.
+
+        Raises:
+            NotImplementedError: as flow
+                is not implemented by the base virtual implementation.
+        """
+        raise NotImplementedError("Redis channels do not support flow.")
+
+    def _brpop_start(self, timeout=1):
+        """Start blocking list pop primitive."""
+        queues = self._queue_cycle.consume(len(self.active_queues))
+        if not queues:
+            return
+
+        self._in_poll = True
+        timeout = timeout or 0
+        node_to_keys = {}
+
+        for key in queues:
+            node = self.client.get_node_from_key(key)
+            node_to_keys.setdefault(node.name, []).append(key)
+
+        for chan, client, conn, cmd in self.connection.cycle._chan_to_sock:
+            expected = (self, self.client, "BRPOP")
+            keys = node_to_keys.get(client.get_node(conn.host, conn.port).name)
+
+            if keys and (chan, client, cmd) == expected:
+                for key in keys:
+                    conn.send_command("BRPOP", key, timeout)
+
+    def _brpop_read(self, **options):
+        """Read data from blocking list pop primitive."""
+        client = self.client
+        conn = options.pop("conn", None)
+
+        if conn is not None:
+            try:
+                resp = client.get_node(conn.host, conn.port).redis_connection.parse_response(
+                    conn, "BRPOP", **options
+                )
+            except self.connection_errors:
+                conn.disconnect()
+                raise
+            except MovedError as exc:
+                # copied from rediscluster/client.py
+                client.reinitialize_counter += 1
+                if client._should_reinitialized():
+                    client.nodes_manager.initialize()
+                    # Reset the counter
+                    client.reinitialize_counter = 0
+                else:
+                    client.nodes_manager.update_moved_exception(exc)
+                raise Empty()
+
+            if resp:
+                dest, item = resp
+                dest = bytes_to_str(dest).rsplit(self.sep, 1)[0]
+                self._queue_cycle.rotate(dest)
+                self.connection._deliver(loads(bytes_to_str(item)), dest)
+                return True
+
+        self._in_poll = False
+        return False
+
+    def _poll_error(self, cmd, conn, **options):
+        """Check for BRPOP error"""
+        if cmd == "BRPOP":
+            self.client.parse_response(conn, cmd)
+
+
 class CustomTransport(Transport):
     """Inject custom cluster poller and custom channel for full Redis client support"""
 
     Channel = CustomChannel
 
+    def as_uri(self, uri: str, include_password=False, mask="**") -> str:
+        """Customise the display format of the URI."""
+        raise NotImplementedError()
+
+
+class CustomClusterTransport(Transport):
+    """Inject custom cluster poller and custom channel for full Redis cluster client support"""
+
+    Channel = CustomClusterChannel
+
+    driver_type = "redis-cluster"
+    driver_name = "redis-cluster"
+
+    implements = virtual.Transport.implements.extend(
+        asynchronous=True, exchange_type=frozenset(["direct"])
+    )
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        if self.client.config["type"] == "cluster":
-            self.implements = virtual.Transport.implements.extend(
-                asynchronous=True, exchange_type=frozenset(["direct"])
-            )
-            self.cycle = ClusterPoller()
+        self.cycle = ClusterPoller()
 
     def as_uri(self, uri: str, include_password=False, mask="**") -> str:
         """Customise the display format of the URI."""
@@ -283,12 +467,21 @@ class CustomConnection(Connection):
         """Get host name/port pair separated by colon."""
         return ":".join(self.config["addrs"][0])
 
+    @property
+    def _uses_cluster(self) -> bool:
+        """Check whether Redis cluster connection is used"""
+        return self.config["type"] == "cluster"
+
     def create_transport(self):
         """Create custom transport for full Redis client support"""
+        if self._uses_cluster:
+            return CustomClusterTransport(client=self)
         return CustomTransport(client=self)
 
     def get_transport_cls(self):
         """Get the currently used transport class."""
+        if self._uses_cluster:
+            return CustomClusterTransport
         return CustomTransport
 
     @property
