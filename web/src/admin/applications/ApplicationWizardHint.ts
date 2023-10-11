ba67c08f021e6eab5f9bb4ab0c0fc60370930ee5
Ken Sternberg <ken@goauthentik.io>
2023-10-11 12:22:01 -0700

    web: hint controller
    Add the `ShowHintController`.  This ReactiveController takes a token
    in its constructor, and looks in LocalStorage for that token and
    an associated value.  If that value is not `undefined`, it sets the
    field `this.host.showHint` to the value found.
    
    It also provides a `render()` method that provides an `ak-hint-footer`
    with a checkbox and the "Don't show this message again," and responds
    to clicks on the checkbox by setting the `this.hint.showHint` and
    LocalStorage values to "false".
    
    An example web component using it has been supplied.

--- web/src/admin/applications/ApplicationWizardHint.ts
+++ web/src/admin/applications/ApplicationWizardHint.ts
@@ -1,22 +1,42 @@
 import { MessageLevel } from "@goauthentik/common/messages";
+import {
+    ShowHintController,
+    ShowHintControllerHost,
+} from "@goauthentik/components/ak-hint/ShowHintController";
 import "@goauthentik/components/ak-hint/ak-hint";
 import "@goauthentik/components/ak-hint/ak-hint-body";
 import { AKElement } from "@goauthentik/elements/Base";
 import "@goauthentik/elements/buttons/ActionButton/ak-action-button";
 import { showMessage } from "@goauthentik/elements/messages/MessageContainer";
 
-import { html } from "lit";
-import { customElement } from "lit/decorators.js";
+import { html, nothing } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
 
 import PFPage from "@patternfly/patternfly/components/Page/page.css";
 
 @customElement("ak-application-wizard-hint")
-export class AkApplicationWizardHint extends AKElement {
+export class AkApplicationWizardHint extends AKElement implements ShowHintControllerHost {
     static get styles() {
         return [PFPage];
     }
 
-    render() {
+    @property({ type: Boolean, attribute: "show-hint" })
+    forceHint: boolean = false;
+
+    @state()
+    showHint: boolean = true;
+
+    showHintController: ShowHintController;
+
+    constructor() {
+        super();
+        this.showHintController = new ShowHintController(
+            this,
+            "202310-application-wizard-announcement",
+        );
+    }
+
+    renderHint() {
         return html` <section class="pf-c-page__main-section pf-m-no-padding-mobile">
             <ak-hint>
                 <ak-hint-body>
@@ -36,7 +56,14 @@ export class AkApplicationWizardHint extends AKElement {
                         >Create with Wizard</ak-action-button
                     ></ak-hint-body
                 >
+                ${this.showHintController.render()}
             </ak-hint>
         </section>`;
     }
+
+    render() {
+        return this.showHint || this.forceHint ? this.renderHint() : nothing;
+    }
 }
+
+export default AkApplicationWizardHint;
