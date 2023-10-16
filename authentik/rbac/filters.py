0697e3d5a402f8396be27e975378e75150f63643
Jens L <jens@goauthentik.io>
2023-10-16 19:42:19 +0200

    rbac: revisions (#7188)
    * improve system migration logging
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * fix filter for internal service accounts
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * merge migration
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * bump go api
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * sources/ldap: check if we need to connect to ldap before connecting
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    ---------
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/rbac/filters.py
+++ authentik/rbac/filters.py
@@ -4,6 +4,8 @@ from rest_framework.exceptions import PermissionDenied
 from rest_framework.request import Request
 from rest_framework_guardian.filters import ObjectPermissionsFilter
 
+from authentik.core.models import UserTypes
+
 
 class ObjectFilter(ObjectPermissionsFilter):
     """Object permission filter that grants global permission higher priority than
@@ -19,6 +21,11 @@ class ObjectFilter(ObjectPermissionsFilter):
         if request.user.has_perm(permission):
             return queryset
         queryset = super().filter_queryset(request, queryset, view)
+        # Outposts (which are the only objects using internal service accounts)
+        # except requests to return an empty list when they have no objects
+        # assigned
+        if request.user.type == UserTypes.INTERNAL_SERVICE_ACCOUNT:
+            return queryset
         if not queryset.exists():
             # User doesn't have direct permission to all objects
             # and also no object permissions assigned (directly or via role)
