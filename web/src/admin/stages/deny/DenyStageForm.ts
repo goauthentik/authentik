cc3a79434161a181a3366dfb630fb0580a817ca5
Jens Langhammer <jens@goauthentik.io>
2023-10-18 11:21:43 +0200

    add form
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/admin/stages/deny/DenyStageForm.ts
+++ web/src/admin/stages/deny/DenyStageForm.ts
@@ -39,7 +39,8 @@ export class DenyStageForm extends ModelForm<DenyStage, string> {
     }
 
     renderForm(): TemplateResult {
-        return html` <span>
+        return html`
+            <span>
                 ${msg(
                     "Statically deny the flow. To use this stage effectively, disable *Evaluate when flow is planned* on the respective binding.",
                 )}
@@ -51,6 +52,23 @@ export class DenyStageForm extends ModelForm<DenyStage, string> {
                     class="pf-c-form-control"
                     required
                 />
-            </ak-form-element-horizontal>`;
+            </ak-form-element-horizontal>
+            <ak-form-group .expanded=${true}>
+                <span slot="header"> ${msg("Stage-specific settings")} </span>
+                <div slot="body" class="pf-c-form">
+                    <ak-form-element-horizontal label=${msg("Deny message")} name="denyMessage">
+                        <input
+                            type="text"
+                            value="${ifDefined(this.instance?.denyMessage || "")}"
+                            class="pf-c-form-control"
+                            required
+                        />
+                        <p class="pf-c-form__helper-text">
+                            ${msg("Message shown when this stage is run.")}
+                        </p>
+                    </ak-form-element-horizontal>
+                </div>
+            </ak-form-group>
+        `;
     }
 }
