a19003ff13c568815a7cbcb0ad18b71cb929414c
Jens Langhammer <jens@goauthentik.io>
2023-10-20 14:23:24 +0200

    sources/oauth: periodically update OAuth sources' OIDC configuration
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

---
+++ authentik/sources/oauth/tasks.py
@@ -0,0 +1,64 @@
+"""OAuth Source tasks"""
+from json import dumps
+
+from requests import RequestException
+from structlog.stdlib import get_logger
+
+from authentik.lib.utils.http import get_http_session
+from authentik.root.celery import CELERY_APP
+from authentik.sources.oauth.models import OAuthSource
+
+LOGGER = get_logger()
+
+
+@CELERY_APP.task()
+def update_well_known_jwks():
+    """Update OAuth sources' config from well_known, and JWKS info from the configured URL"""
+    session = get_http_session()
+    for source in OAuthSource.objects.all().exclude(oidc_well_known_url=""):
+        try:
+            well_known_config = session.get(source.oidc_well_known_url)
+            well_known_config.raise_for_status()
+        except RequestException as exc:
+            text = exc.response.text if exc.response else str(exc)
+            LOGGER.warning("Failed to update well_known", source=source, exc=exc, text=text)
+            continue
+        config = well_known_config.json()
+        try:
+            dirty = False
+            source_attr_key = (
+                ("authorization_url", "authorization_endpoint"),
+                ("access_token_url", "token_endpoint"),
+                ("profile_url", "userinfo_endpoint"),
+                ("oidc_jwks_url", "jwks_uri"),
+            )
+            for source_attr, config_key in source_attr_key:
+                # Check if we're actually changing anything to only
+                # save when something has changed
+                if getattr(source, source_attr) != config[config_key]:
+                    dirty = True
+                setattr(source, source_attr, config[config_key])
+        except (IndexError, KeyError) as exc:
+            LOGGER.warning(
+                "Failed to update well_known",
+                source=source,
+                exc=exc,
+            )
+            continue
+        if dirty:
+            LOGGER.info("Updating sources' OpenID Configuration", source=source)
+            source.save()
+
+    for source in OAuthSource.objects.all().exclude(oidc_jwks_url=""):
+        try:
+            jwks_config = session.get(source.oidc_jwks_url)
+            jwks_config.raise_for_status()
+        except RequestException as exc:
+            text = exc.response.text if exc.response else str(exc)
+            LOGGER.warning("Failed to update JWKS", source=source, exc=exc, text=text)
+            continue
+        config = jwks_config.json()
+        if dumps(source.oidc_jwks, sort_keys=True) != dumps(config, sort_keys=True):
+            source.oidc_jwks = config
+            LOGGER.info("Updating sources' JWKS", source=source)
+            source.save()
---
+++ authentik/sources/oauth/tests/test_tasks.py
@@ -0,0 +1,48 @@
+"""Test OAuth Source tasks"""
+from django.test import TestCase
+from requests_mock import Mocker
+
+from authentik.sources.oauth.models import OAuthSource
+from authentik.sources.oauth.tasks import update_well_known_jwks
+
+
+class TestOAuthSourceTasks(TestCase):
+    """Test OAuth Source tasks"""
+
+    def setUp(self) -> None:
+        self.source = OAuthSource.objects.create(
+            name="test",
+            slug="test",
+            provider_type="openidconnect",
+            authorization_url="",
+            profile_url="",
+            consumer_key="",
+        )
+
+    @Mocker()
+    def test_well_known_jwks(self, mock: Mocker):
+        """Test well_known update"""
+        self.source.oidc_well_known_url = "http://foo/.well-known/openid-configuration"
+        self.source.save()
+        mock.get(
+            self.source.oidc_well_known_url,
+            json={
+                "authorization_endpoint": "foo",
+                "token_endpoint": "foo",
+                "userinfo_endpoint": "foo",
+                "jwks_uri": "http://foo/jwks",
+            },
+        )
+        mock.get("http://foo/jwks", json={"foo": "bar"})
+        update_well_known_jwks()
+        self.source.refresh_from_db()
+        self.assertEqual(self.source.authorization_url, "foo")
+        self.assertEqual(self.source.access_token_url, "foo")
+        self.assertEqual(self.source.profile_url, "foo")
+        self.assertEqual(self.source.oidc_jwks_url, "http://foo/jwks")
+        self.assertEqual(
+            self.source.oidc_jwks,
+            {
+                "foo": "bar",
+            },
+        )
