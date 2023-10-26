63da101915df6b1557510cee5b00c89b82e712a3
SEIKH NABAB UDDIN <93948993+nababuddin@users.noreply.github.com>
2023-10-26 22:55:05 +0530

    Update npm_version.py
    Signed-off-by: SEIKH NABAB UDDIN <93948993+nababuddin@users.noreply.github.com>

--- scripts/npm_version.py
+++ scripts/npm_version.py
@@ -1,6 +1,8 @@
-"""Helper script to generate an NPM Version"""
+# Helper script to generate an NPM version
 from time import time
-
 from authentik import __version__
 
-print("%s-%d" % (__version__, time()))
+# Generate a unique version based on the current version and a timestamp
+npm_version = f"{__version__}-{int(time())}"
+
+print(npm_version)
