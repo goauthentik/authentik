299bc59ee62b778a6597314cea51520a40076348
Ken Sternberg <ken@goauthentik.io>
2023-10-27 09:57:55 -0700

    web: improve error handling in light components
    Rather than reproduce the error handling across all of the LightComponents,
    I've made a parent class that takes the common fields to distribute between
    the ak-form-element-horizontal and the input object itself.  This made it
    much easier to properly display errors in freeform input fields in the
    wizard, as well as working with the routine error handling in Form.ts

---
+++ web/src/components/HorizontalLightComponent.ts
@@ -0,0 +1,66 @@
+import { AKElement } from "@goauthentik/elements/Base";
+
+import { TemplateResult, html, nothing } from "lit";
+import { property } from "lit/decorators.js";
+
+export class HorizontalLightComponent extends AKElement {
+    // Render into the lightDOM. This effectively erases the shadowDOM nature of this component, but
+    // we're not actually using that and, for the meantime, we need the form handlers to be able to
+    // find the children of this component.
+    //
+    // TODO: This abstraction is wrong; it's putting *more* layers in as a way of managing the
+    // visual clutter and legibility issues of ak-form-elemental-horizontal and patternfly in
+    // general.
+    protected createRenderRoot() {
+        return this;
+    }
+
+    @property({ type: String })
+    name!: string;
+
+    @property({ type: String })
+    label = "";
+    @property({ type: Boolean })
+    required = false;
+
+    @property({ type: String })
+    help = "";
+
+    @property({ type: Object })
+    bighelp!: TemplateResult | TemplateResult[];
+
+    @property({ type: Boolean })
+    hidden = false;
+
+    @property({ type: Boolean })
+    invalid = false;
+
+    @property({ attribute: false })
+    errorMessages: string[] = [];
+
+    renderControl() {
+        throw new Error("Must be implemented in a subclass");
+    }
+
+    renderHelp() {
+        return [
+            this.help ? html`<p class="pf-c-form__helper-text">${this.help}</p>` : nothing,
+            this.bighelp ? this.bighelp : nothing,
+        ];
+    }
+
+    render() {
+        // prettier-ignore
+        return html`<ak-form-element-horizontal
+            label=${this.label}
+            ?required=${this.required}
+            ?hidden=${this.hidden}
+            name=${this.name}
+            .errorMessages=${this.errorMessages}
+            ?invalid=${this.invalid}
+            >
+              ${this.renderControl()} 
+              ${this.renderHelp()}
+        </ak-form-element-horizontal> `;
+    }
+}
