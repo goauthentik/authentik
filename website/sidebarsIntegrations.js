aadda1f314d729dd1b4f9e20b964decf7b9bee91
François-Xavier Payet <fx@payet.io>
2023-11-07 14:28:38 +0100

    website/integrations: add FreshRSS (#7301)

--- website/sidebarsIntegrations.js
+++ website/sidebarsIntegrations.js
@@ -96,6 +96,7 @@ module.exports = {
                     type: "category",
                     label: "Miscellaneous",
                     items: [
+                        "services/freshrss/index",
                         "services/gravitee/index",
                         "services/home-assistant/index",
                         "services/jellyfin/index",
