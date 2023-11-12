61a642cca888673264358f21f43cd71e399bb3f7
Jens Langhammer <jens@goauthentik.io>
2023-11-12 18:50:41 +0100

    default to checking all sources
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/sources/ldap/management/commands/ldap_check_connection.py
+++ authentik/sources/ldap/management/commands/ldap_check_connection.py
@@ -10,16 +10,15 @@ LOGGER = get_logger()
 
 
 class Command(BaseCommand):
-    """Check connectivity to ldap servers for a source"""
+    """Check connectivity to LDAP servers for a source"""
 
     def add_arguments(self, parser):
-        parser.add_argument("source_slugs", nargs="+", type=str)
+        parser.add_argument("source_slugs", nargs="?", type=str)
 
     def handle(self, **options):
-        for source_slug in options["source_slugs"]:
-            source = LDAPSource.objects.filter(slug=source_slug).first()
-            if not source:
-                LOGGER.warning("Source does not exist", slug=source_slug)
-                continue
+        sources = LDAPSource.objects.filter(enabled=True)
+        if options["source_slugs"]:
+            sources = LDAPSource.objects.filter(slug__in=options["source_slugs"])
+        for source in sources.order_by("slug"):
             status = source.check_connection()
             self.stdout.write(dumps(status, indent=4))
