f3c552a533bb30f4b669722ee446d5b2ff88b373
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2023-11-16 13:42:15 +0100

    upstream fixes
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/tenants/models.py
+++ authentik/tenants/models.py
@@ -2,24 +2,15 @@
 from uuid import uuid4
 
 from django.apps import apps
-from django.core.management import call_command
-from django.db import connections, models
+from django.db import models
 from django.dispatch import receiver
 from django.utils.translation import gettext_lazy as _
 from django_tenants.models import DomainMixin, TenantMixin, post_schema_sync
-from django_tenants.postgresql_backend.base import _check_schema_name
-from django_tenants.utils import (
-    get_creation_fakes_migrations,
-    get_tenant_base_schema,
-    get_tenant_database_alias,
-    schema_exists,
-)
 from rest_framework.serializers import Serializer
 from structlog.stdlib import get_logger
 
 from authentik.blueprints.apps import ManagedAppConfig
 from authentik.lib.models import SerializerModel
-from authentik.tenants.clone import CloneSchema
 
 LOGGER = get_logger()
 
@@ -66,51 +57,6 @@ class Tenant(TenantMixin, SerializerModel):
         help_text=_("Configure how long reputation scores should be saved for in seconds."),
         default=86400,
     )
-
-    def create_schema(self, check_if_exists=False, sync_schema=True, verbosity=1):
-        """
-        Creates the schema 'schema_name' for this tenant. Optionally checks if
-        the schema already exists before creating it. Returns true if the
-        schema was created, false otherwise.
-        """
-
-        # safety check
-        connection = connections[get_tenant_database_alias()]
-        _check_schema_name(self.schema_name)
-        cursor = connection.cursor()
-
-        if check_if_exists and schema_exists(self.schema_name):
-            return False
-
-        fake_migrations = get_creation_fakes_migrations()
-
-        if sync_schema:
-            if fake_migrations:
-                # copy tables and data from provided model schema
-                base_schema = get_tenant_base_schema()
-                clone_schema = CloneSchema()
-                clone_schema.clone_schema(base_schema, self.schema_name)
-
-                call_command(
-                    "migrate_schemas",
-                    tenant=True,
-                    fake=True,
-                    schema_name=self.schema_name,
-                    interactive=False,
-                    verbosity=verbosity,
-                )
-            else:
-                # create the schema
-                cursor.execute('CREATE SCHEMA "%s"' % self.schema_name)
-                call_command(
-                    "migrate_schemas",
-                    tenant=True,
-                    schema_name=self.schema_name,
-                    interactive=False,
-                    verbosity=verbosity,
-                )
-
-        connection.set_schema_to_public()
 
     def save(self, *args, **kwargs):
         if self.schema_name == "template":
