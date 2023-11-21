89aa6539b20608e9cddba94a499f044099abf289
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2023-11-21 18:40:46 +0100

    fix migrations for template schema 3
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/tenants/utils.py
+++ authentik/tenants/utils.py
@@ -1,9 +1,13 @@
 """Tenant utils"""
 from django.db import connection
+from django.db.utils import ProgrammingError
 
 from authentik.tenants.models import Tenant
 
 
 def get_current_tenant() -> Tenant | None:
     """Get tenant for current request"""
-    return Tenant.objects.filter(schema_name=connection.schema_name).first()
+    try:
+        return Tenant.objects.filter(schema_name=connection.schema_name).first()
+    except ProgrammingError:  # We're inside a migration and this table doesn't exist yet
+        return None
