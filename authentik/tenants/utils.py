5ede611f22c00b788169f67fefec4e4e689d0621
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2023-11-21 18:43:42 +0100

    fix type
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/tenants/utils.py
+++ authentik/tenants/utils.py
@@ -4,6 +4,6 @@ from django.db import connection
 from authentik.tenants.models import Tenant
 
 
-def get_current_tenant() -> Tenant | None:
+def get_current_tenant() -> Tenant:
     """Get tenant for current request"""
     return Tenant.objects.get(schema_name=connection.schema_name)
