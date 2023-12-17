c5b93407b6d3528dba35dc85264d0f4a60ca1edb
Jens Langhammer <jens@goauthentik.io>
2023-12-17 23:18:10 +0100

    fix channel creation
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/stages/authenticator_mobile/cloud_gateway.py
+++ authentik/stages/authenticator_mobile/cloud_gateway.py
@@ -72,6 +72,6 @@ def get_client(addr: str):
     channel = secure_channel(addr, ssl_channel_credentials())
     if settings.DEBUG:
         channel = insecure_channel(addr)
-    channel = intercept_channel(addr, AuthInterceptor(get_enterprise_token()))
+    channel = intercept_channel(channel, AuthInterceptor(get_enterprise_token()))
     client = AuthenticationPushStub(channel)
     return client
