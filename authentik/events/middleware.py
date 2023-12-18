e38aa6e4016a8dcb411e99711806ec33f786e6bd
Jens Langhammer <jens@goauthentik.io>
2023-12-18 13:19:25 +0100

    don't log mobiledevicetoken
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/events/middleware.py
+++ authentik/events/middleware.py
@@ -30,7 +30,7 @@ from authentik.policies.models import Policy, PolicyBindingModel
 from authentik.policies.reputation.models import Reputation
 from authentik.providers.oauth2.models import AccessToken, AuthorizationCode, RefreshToken
 from authentik.providers.scim.models import SCIMGroup, SCIMUser
-from authentik.stages.authenticator_mobile.models import MobileTransaction
+from authentik.stages.authenticator_mobile.models import MobileDeviceToken, MobileTransaction
 from authentik.stages.authenticator_static.models import StaticToken
 
 IGNORED_MODELS = (
@@ -56,6 +56,7 @@ IGNORED_MODELS = (
     SCIMGroup,
     Reputation,
     MobileTransaction,
+    MobileDeviceToken,
 )
 
 
