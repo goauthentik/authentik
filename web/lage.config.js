323af18aaec11d13544d1f56ee3ee71478c77cc6
Ken Sternberg <ken@goauthentik.io>
2024-01-05 12:15:35 -0800

    Stashing this to go back to harmonizing package.json

--- web/lage.config.js
+++ web/lage.config.js
@@ -4,15 +4,14 @@ module.exports = {
         "lint": [],
         "tsc": [],
         "lit-analyse": [],
+        "watch": [],
         "lint:precommit": [],
         "lint:spelling": [],
         "precommit-all": ["tsc", "lit-analyse", "lint:precommit", "lint:spelling", "prettier"],
         "build-locales": ["^build-locales"],
         "extract-locales": ["^extract-locales"],
-        "watch": ["authentik#watch"],
         "web-fix": ["^web-lint-fix", "^web-lint", "^web-check-compile", "^web-i18n-extract"],
         "prettier": ["^prettier"],
-        
-            
+        "watch-main": ["watch"],
     },
 };
