1682c052fa0a37ad36f7d0bd1447b15fc13eb36a
Jens Langhammer <jens@goauthentik.io>
2024-01-13 21:00:34 +0100

    cleanup api
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/events/api/tasks.py
+++ authentik/events/api/tasks.py
@@ -4,7 +4,7 @@ from importlib import import_module
 from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
 from drf_spectacular.types import OpenApiTypes
-from drf_spectacular.utils import OpenApiParameter, OpenApiResponse, extend_schema
+from drf_spectacular.utils import OpenApiResponse, extend_schema
 from rest_framework.decorators import action
 from rest_framework.fields import (
     CharField,
@@ -28,30 +28,40 @@ LOGGER = get_logger()
 class SystemTaskSerializer(ModelSerializer):
     """Serialize TaskInfo and TaskResult"""
 
-    task_name = CharField(source="name")
-    task_description = CharField(source="description")
-    task_start_timestamp = DateTimeField(source="start_timestamp")
-    task_finish_timestamp = DateTimeField(source="finish_timestamp")
-    task_duration = SerializerMethodField()
+    name = CharField()
+    full_name = SerializerMethodField()
+    uid = CharField(required=False)
+    description = CharField()
+    start_timestamp = DateTimeField()
+    finish_timestamp = DateTimeField()
+    duration = SerializerMethodField()
 
     status = ChoiceField(
-        # source="status",
         choices=[(x.name, x.name) for x in TaskStatus],
     )
     messages = ListField(child=CharField())
 
-    def get_task_duration(self, instance: SystemTask) -> int:
+    def get_full_name(self, instance: SystemTask) -> str:
+        """Get full name with UID"""
+        if instance.uid:
+            return f"{instance.name}:{instance.uid}"
+        return instance.name
+
+    def get_duration(self, instance: SystemTask) -> int:
         """Get the duration a task took to run"""
         return max(instance.finish_timestamp.timestamp() - instance.start_timestamp.timestamp(), 0)
 
     class Meta:
         model = SystemTask
         fields = [
-            "task_name",
-            "task_description",
-            "task_start_timestamp",
-            "task_finish_timestamp",
-            "task_duration",
+            "uuid",
+            "name",
+            "full_name",
+            "uid",
+            "description",
+            "start_timestamp",
+            "finish_timestamp",
+            "duration",
             "status",
             "messages",
         ]
@@ -62,6 +72,9 @@ class SystemTaskViewSet(ReadOnlyModelViewSet):
 
     queryset = SystemTask.objects.all()
     serializer_class = SystemTaskSerializer
+    filterset_fields = ["name", "uid", "status"]
+    ordering = ["name", "uid", "status"]
+    search_fields = ["name", "description", "uid", "status"]
 
     @permission_required(None, ["authentik_events.rerun_task"])
     @extend_schema(
@@ -71,14 +84,6 @@ class SystemTaskViewSet(ReadOnlyModelViewSet):
             404: OpenApiResponse(description="Task not found"),
             500: OpenApiResponse(description="Failed to retry task"),
         },
-        parameters=[
-            OpenApiParameter(
-                "id",
-                type=OpenApiTypes.STR,
-                location=OpenApiParameter.PATH,
-                required=True,
-            ),
-        ],
     )
     @action(detail=True, methods=["post"])
     def retry(self, request: Request, pk=None) -> Response:
