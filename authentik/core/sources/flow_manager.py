f26c0d221ee49e3c1136fd7ebaa644b19da37e59
Jens Langhammer <jens@goauthentik.io>
2024-01-27 18:16:37 +0100

    fix lint
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/core/sources/flow_manager.py
+++ authentik/core/sources/flow_manager.py
@@ -26,11 +26,7 @@ from authentik.flows.planner import (
     FlowPlanner,
 )
 from authentik.flows.stage import StageView
-from authentik.flows.views.executor import (
-    NEXT_ARG_NAME,
-    SESSION_KEY_GET,
-    SESSION_KEY_PLAN,
-)
+from authentik.flows.views.executor import NEXT_ARG_NAME, SESSION_KEY_GET, SESSION_KEY_PLAN
 from authentik.lib.utils.urls import redirect_with_qs
 from authentik.lib.views import bad_request_message
 from authentik.policies.denied import AccessDeniedResponse
