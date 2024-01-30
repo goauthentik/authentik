c332f6f0ecab4dab5b7b1cb1af7a8974d6d9d21c
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2024-01-30 13:09:17 +0100

    tenants: avoid extra queries in get_current_tenant (#8367)
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/tenants/utils.py
+++ authentik/tenants/utils.py
@@ -6,4 +6,7 @@ from authentik.tenants.models import Tenant
 
 def get_current_tenant() -> Tenant:
     """Get tenant for current request"""
+    tenant = getattr(connection, "tenant", None)
+    if tenant:
+        return tenant
     return Tenant.objects.get(schema_name=connection.schema_name)
