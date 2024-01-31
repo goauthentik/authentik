245b74eb7aef2c3f9f1497ce68f8ccee97e2af63
Jens Langhammer <jens@goauthentik.io>
2024-01-31 15:00:35 +0100

    revert #8367
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/tenants/utils.py
+++ authentik/tenants/utils.py
@@ -7,7 +7,4 @@ from authentik.tenants.models import Tenant
 
 def get_current_tenant() -> Tenant:
     """Get tenant for current request"""
-    tenant = getattr(connection, "tenant", None)
-    if tenant:
-        return tenant
     return Tenant.objects.get(schema_name=connection.schema_name)
