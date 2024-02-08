0d720b9ef5205029c1501b1fe6475482db8529b1
Jens Langhammer <jens@goauthentik.io>
2024-02-08 22:41:12 +0100

    format
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/elements/user/UserConsentList.ts
+++ web/src/elements/user/UserConsentList.ts
@@ -62,7 +62,7 @@ export class UserConsentList extends Table<UserConsent> {
     row(item: UserConsent): TemplateResult[] {
         return [
             html`${item.application.name}`,
-            html`${(item.expires && item.expiring)
+            html`${item.expires && item.expiring
                 ? html`<div>${getRelativeTime(item.expires)}</div>
                       <small>${item.expires.toLocaleString()}</small>`
                 : msg("-")}`,
