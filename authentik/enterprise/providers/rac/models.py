2752cd8743f70a46a9b1965a8644fdd71a93261b
Jens Langhammer <jens@goauthentik.io>
2024-02-11 23:27:16 +0100

    fix lint and tests
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/enterprise/providers/rac/models.py
+++ authentik/enterprise/providers/rac/models.py
@@ -201,7 +201,10 @@ class ConnectionToken(ExpiringModel):
         return settings
 
     def __str__(self):
-        return f"RAC Connection token {self.session.user} to {self.endpoint.provider.name}/{self.endpoint.name}"
+        return (
+            f"RAC Connection token {self.session.user} to "
+            f"{self.endpoint.provider.name}/{self.endpoint.name}"
+        )
 
     class Meta:
         verbose_name = _("RAC Connection token")
