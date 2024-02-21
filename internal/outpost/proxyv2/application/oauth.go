d6af62a9f819186cdaad2be849dd27bf15fff62f
Jens Langhammer <jens@goauthentik.io>
2024-02-21 20:24:08 +0100

    fix
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- internal/outpost/proxyv2/application/oauth.go
+++ internal/outpost/proxyv2/application/oauth.go
@@ -4,10 +4,8 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
-	"time"
 
 	"goauthentik.io/api/v3"
-	"goauthentik.io/internal/outpost/proxyv2/constants"
 )
 
 const (
@@ -53,32 +51,4 @@ func (a *Application) handleAuthStart(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 	http.Redirect(rw, r, a.oauthConfig.AuthCodeURL(state), http.StatusFound)
-}
-
-func (a *Application) handleAuthCallback(rw http.ResponseWriter, r *http.Request) {
-	state := a.stateFromRequest(r)
-	if state == nil {
-		a.log.Warning("invalid state")
-		a.redirect(rw, r)
-		return
-	}
-	claims, err := a.redeemCallback(r.URL, r.Context())
-	if err != nil {
-		a.log.WithError(err).Warning("failed to redeem code")
-		rw.WriteHeader(400)
-		return
-	}
-	s, err := a.sessions.Get(r, a.SessionName())
-	if err != nil {
-		a.log.WithError(err).Trace("failed to get session")
-	}
-	s.Options.MaxAge = int(time.Until(time.Unix(int64(claims.Exp), 0)).Seconds())
-	s.Values[constants.SessionClaims] = &claims
-	err = s.Save(r, rw)
-	if err != nil {
-		a.log.WithError(err).Warning("failed to save session")
-		rw.WriteHeader(400)
-		return
-	}
-	a.redirect(rw, r)
 }
