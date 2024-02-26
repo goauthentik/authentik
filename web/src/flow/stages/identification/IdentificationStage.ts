eebdf29f3e75b8dbb903510eba414cf22ef879a2
Jens Langhammer <jens@goauthentik.io>
2024-02-26 11:41:24 +0100

    also slightly reword source selection
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/flow/stages/identification/IdentificationStage.ts
+++ web/src/flow/stages/identification/IdentificationStage.ts
@@ -207,7 +207,7 @@ export class IdentificationStage extends BaseStage<
     renderInput(): TemplateResult {
         let type: "text" | "email" = "text";
         if (!this.challenge?.userFields || this.challenge.userFields.length === 0) {
-            return html`<p>${msg("Select one of the sources below to login.")}</p>`;
+            return html`<p>${msg("Select one of the options below to continue.")}</p>`;
         }
         const fields = (this.challenge?.userFields || []).sort();
         // Check if the field should be *only* email to set the input type
