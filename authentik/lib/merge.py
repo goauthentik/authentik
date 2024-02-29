02fd08ca916d14ca9fb7e768f744b680bd9a9bcf
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2024-02-29 15:36:18 +0100

    lint-fix and make gen
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/lib/merge.py
+++ authentik/lib/merge.py
@@ -1,4 +1,5 @@
 """merge utils"""
+
 from typing import Any
 
 from deepmerge import Merger
@@ -13,12 +14,12 @@ def remove_none(d: dict | list | set | tuple):
     if isinstance(d, dict):
         # list needed because the dict may change size at runtime
         for key, value in list(d.items()):
-            if isinstance(value, (list, dict, tuple, set)):
+            if isinstance(value, list | dict | tuple | set):
                 d[key] = remove_none(value)
             elif value is None or key is None:
                 del d[key]
 
-    elif isinstance(d, (list, set, tuple)):
+    elif isinstance(d, list | set | tuple):
         d = type(d)(remove_none(item) for item in d if item is not None)
 
     return d
@@ -26,7 +27,7 @@ def remove_none(d: dict | list | set | tuple):
 
 def flatten(value: Any) -> Any:
     """Flatten `value` if its a list, set or tuple"""
-    if isinstance(value, (list, set, tuple)):
+    if isinstance(value, list | set | tuple):
         if len(value) < 1:
             return None
         if isinstance(value, set):
@@ -37,7 +38,7 @@ def flatten(value: Any) -> Any:
 
 def flatten_rec(value: Any) -> Any:
     """Flatten `value` if it's a list, tuple, set, or recursively if it's a dict"""
-    if isinstance(value, (list, set, tuple)):
+    if isinstance(value, list | set | tuple):
         return flatten(value)
     if isinstance(value, dict):
         for k, v in value.items():
