5accbb730009dc5be2da2e5b098a6a39b41e0f6b
Matthias G <sirux88@gmail.com>
2024-03-05 19:55:59 +0100

    refactor CheckPasswordInlineMFA to SetSecrets

--- internal/outpost/flow/solvers_mfa.go
+++ internal/outpost/flow/solvers_mfa.go
@@ -10,39 +10,44 @@ const CodePasswordSeparator = ";"
 
 var alphaNum = regexp.MustCompile(`^[a-zA-Z0-9]*$`)
 
-// CheckPasswordInlineMFA For protocols that only support username/password, check if the password
-// contains the TOTP code
-func (fe *FlowExecutor) CheckPasswordInlineMFA() {
-	password := fe.Answers[StagePassword]
-	// We already have an authenticator answer
-	if fe.Answers[StageAuthenticatorValidate] != "" {
+// SetSecrets sets the secret answers for the flow executor for protocols that only support username/password
+// acccording to used options
+func (fe *FlowExecutor) SetSecrets(password string, mfacodebased bool) {
+	if fe.Answers[StageAuthenticatorValidate] != "" || fe.Answers[StagePassword] != "" {
 		return
 	}
-	// password doesn't contain the separator
-	if !strings.Contains(password, CodePasswordSeparator) {
-		return
-	}
-	// password ends with the separator, so it won't contain an answer
-	if strings.HasSuffix(password, CodePasswordSeparator) {
-		return
-	}
-	idx := strings.LastIndex(password, CodePasswordSeparator)
-	authenticator := password[idx+1:]
-	// Authenticator is either 6 chars (totp code) or 8 chars (long totp or static)
-	if len(authenticator) == 6 {
-		// authenticator answer isn't purely numerical, so won't be value
-		if _, err := strconv.Atoi(authenticator); err != nil {
+	fe.Answers[StagePassword] = password
+	if mfacodebased {
+		// password doesn't contain the separator
+		if !strings.Contains(password, CodePasswordSeparator) {
 			return
 		}
-	} else if len(authenticator) == 8 {
-		// 8 chars can be a long totp or static token, so it needs to be alphanumerical
-		if !alphaNum.MatchString(authenticator) {
+		// password ends with the separator, so it won't contain an answer
+		if strings.HasSuffix(password, CodePasswordSeparator) {
 			return
 		}
+		idx := strings.LastIndex(password, CodePasswordSeparator)
+		authenticator := password[idx+1:]
+		// Authenticator is either 6 chars (totp code) or 8 chars (long totp or static)
+		if len(authenticator) == 6 {
+			// authenticator answer isn't purely numerical, so won't be value
+			if _, err := strconv.Atoi(authenticator); err != nil {
+				return
+			}
+		} else if len(authenticator) == 8 {
+			// 8 chars can be a long totp or static token, so it needs to be alphanumerical
+			if !alphaNum.MatchString(authenticator) {
+				return
+			}
+		} else {
+			// Any other length, doesn't contain an answer
+			return
+		}
+		fe.Answers[StagePassword] = password[:idx]
+		fe.Answers[StageAuthenticatorValidate] = authenticator
 	} else {
-		// Any other length, doesn't contain an answer
-		return
+		// If code-based MFA is disabled StageAuthenticatorValidate answer is set to password.
+		// This allows flows with a mfa stage only.
+		fe.Answers[StageAuthenticatorValidate] = password
 	}
-	fe.Answers[StagePassword] = password[:idx]
-	fe.Answers[StageAuthenticatorValidate] = authenticator
 }
