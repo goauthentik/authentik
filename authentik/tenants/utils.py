4383fa3d6e2a9f868a25a6f8fb7cb17f84373ee1
Jens Langhammer <jens@goauthentik.io>
2024-03-06 14:47:45 +0100

    use tenant uuid instead of install_id when tenants are enabled
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/tenants/utils.py
+++ authentik/tenants/utils.py
@@ -1,10 +1,26 @@
 """Tenant utils"""
 
 from django.db import connection
+from django_tenants.utils import get_public_schema_name
 
+from authentik.lib.config import CONFIG
+from authentik.root.install_id import get_install_id
 from authentik.tenants.models import Tenant
 
 
 def get_current_tenant() -> Tenant:
     """Get tenant for current request"""
     return Tenant.objects.get(schema_name=connection.schema_name)
+
+
+def get_unique_identifier() -> str:
+    """Get a globally unique identifier that does not change"""
+    install_id = get_install_id()
+    if CONFIG.get_bool("tenants.enabled"):
+        tenant = get_current_tenant()
+        # Only use tenant's uuid if this request is not from the "public"
+        # (i.e. default) tenant
+        if tenant.schema_name == get_public_schema_name():
+            return install_id
+        return str(get_current_tenant().tenant_uuid)
+    return install_id
