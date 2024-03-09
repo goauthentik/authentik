d9ebe96269587d01fda38fc6e761bf529c8ca584
RoboMagus <68224306+RoboMagus@users.noreply.github.com>
2024-03-09 11:14:19 +0100

    Handle latest  version unknown in admin dashboard

--- web/src/admin/admin-overview/cards/VersionStatusCard.ts
+++ web/src/admin/admin-overview/cards/VersionStatusCard.ts
@@ -31,10 +31,16 @@ export class VersionStatusCard extends AdminStatusCard<Version> {
                 message: html`${msg(str`${value.versionLatest} is available!`)}`,
             });
         }
+        if (value.version_latest_valid) {
+            return Promise.resolve<AdminStatus>({
+                icon: "fa fa-check-circle pf-m-success",
+                message: html`${msg("Up-to-date!")}`,
+            });
+        }
         return Promise.resolve<AdminStatus>({
-            icon: "fa fa-check-circle pf-m-success",
-            message: html`${msg("Up-to-date!")}`,
-        });
+            icon: "fa fa-question-circle",
+            message: html`${msg("Latest version unknown")}`,
+        }); 
     }
 
     renderHeader(): TemplateResult {
