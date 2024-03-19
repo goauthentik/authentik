1447b2264ef74a8efad5a62eb3224e122518e464
Jens Langhammer <jens@goauthentik.io>
2024-03-19 11:28:18 +0100

    only run migration when needed
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- lifecycle/system_migrations/template_schema.py
+++ lifecycle/system_migrations/template_schema.py
@@ -3,7 +3,10 @@ from lifecycle.migrate import BaseMigration
 
 class Migration(BaseMigration):
     def needs_migration(self) -> bool:
-        return True
+        self.cur.execute(
+            "SELECT schema_name FROM information_schema.schemata WHERE schema_name = 'template';"
+        )
+        return not bool(self.cur.rowcount)
 
     def run(self):
         self.cur.execute("CREATE SCHEMA IF NOT EXISTS template;")
