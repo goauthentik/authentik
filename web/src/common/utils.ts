310679d27574e8ab4deef7e26ba794ed99afd3d5
Ken Sternberg <ken@goauthentik.io>
2024-03-27 08:30:50 -0700

    Fat-finger error. Thank gnu I double-check my PRs before I move them out of draft!

--- web/src/common/utils.ts
+++ web/src/common/utils.ts
@@ -83,7 +83,7 @@ export const ascii_lowercase = "abcdefghijklmnopqrstuvwxyz";
 export const ascii_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 export const ascii_letters = ascii_lowercase + ascii_uppercase;
 export const digits = "0123456789";
-export const hexdigts = digits + "abcdef" + "ABCDEF";
+export const hexdigits = digits + "abcdef" + "ABCDEF";
 export const octdigits = "01234567";
 export const punctuation = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
