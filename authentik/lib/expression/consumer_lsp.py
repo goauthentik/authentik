25cd7a94cee3926a336af9078eb60fc73f7cdd58
Jens Langhammer <jens@goauthentik.io>
2024-04-01 03:10:19 +0200

    idk
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/lib/expression/consumer_lsp.py
+++ authentik/lib/expression/consumer_lsp.py
@@ -38,11 +38,11 @@ class LSPConsumer(JsonWebsocketConsumer):
     def disconnect(self, code):
         pass
 
-
     def wrap_expression(self, expression: str, params: Iterable[str]) -> str:
         """Wrap expression in a function, call it, and save the result as `result`"""
         handler_signature = ",".join(params)
         full_expression = ""
+        full_expression += "from authentik.policies.types import PolicyRequest\n"
         full_expression += f"def handler({handler_signature}):\n"
         full_expression += indent(expression, "    ")
         full_expression += f"\nresult = handler({handler_signature})"
@@ -53,11 +53,16 @@ class LSPConsumer(JsonWebsocketConsumer):
         return super().send_json(content, close)
 
     def receive_json(self, content, **kwargs):
-        if content.get("method", "").startswith("textDocument/"):
+        if content.get("method", "") == "textDocument/didOpen":
             textDocument = content.get("params",{}).get("textDocument", {})
             text = textDocument.get("text")
             if text:
-                textDocument["text"] = self.wrap_expression(text, ["request"])
+                textDocument["text"] = self.wrap_expression(text, ["request: PolicyRequest"])
+        if content.get("method", "") == "textDocument/didChange":
+            contentChanges = content.get("params", {}).get("contentChanges", [""])
+            text = contentChanges[0].get("text", "")
+            if text:
+                contentChanges[0]["text"] = self.wrap_expression(text, ["request: PolicyRequest"])
         self.handler.consume(content)
-        print(self.handler.config.plugin_manager.get_plugins())
-        print(self.handler.config.plugin_settings())
+        # print(self.handler.config.plugin_manager.get_plugins())
+        print(self.handler.config.plugin_settings("jedi_completion"))
