9f6dca11703e110faf8caffce55c63499005f02d
Jens L <jens@goauthentik.io>
2024-04-08 12:21:26 +0200

    stages/authenticator_webauthn: add MDS support (#9114)
    * web: align style to show current user for webauthn enroll
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * ask for aaguid
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * initial MDS import
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * add API
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * add restriction
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * fix api, add actual restriction
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * default authenticator name based on aaguid
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * connect device with device type
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * fix typo in webauthn stage name
    
    this typo has been around for 3 years https://github.com/goauthentik/authentik/commit/8708e487aebb0f131ee50af3a5088da927457941#diff-bb4aee4a37f4b95c8daa7beb6bf6251d8d2b6deb8c16dce0cd7cb0d6cd71900aR16
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * add fido2 dep
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * add CI pipeline to automate updating blob
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * fix tests, include device type
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * add tests
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * exclude icon for now
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * add passkeys aaguid
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * make special unknown device type work, add docs
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    ---------
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/stages/authenticator_webauthn/urls.py
+++ authentik/stages/authenticator_webauthn/urls.py
@@ -1,13 +1,15 @@
 """API URLs"""
 
-from authentik.stages.authenticator_webauthn.api import (
-    AuthenticateWebAuthnStageViewSet,
+from authentik.stages.authenticator_webauthn.api.device_types import WebAuthnDeviceTypeViewSet
+from authentik.stages.authenticator_webauthn.api.devices import (
     WebAuthnAdminDeviceViewSet,
     WebAuthnDeviceViewSet,
 )
+from authentik.stages.authenticator_webauthn.api.stages import AuthenticatorWebAuthnStageViewSet
 
 api_urlpatterns = [
-    ("stages/authenticator/webauthn", AuthenticateWebAuthnStageViewSet),
+    ("stages/authenticator/webauthn", AuthenticatorWebAuthnStageViewSet),
+    ("stages/authenticator/webauthn_device_types", WebAuthnDeviceTypeViewSet),
     (
         "authenticators/admin/webauthn",
         WebAuthnAdminDeviceViewSet,
