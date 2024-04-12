5f23c91b63fd735cfefa9a63f5f601adc8661188
Ken Sternberg <ken@goauthentik.io>
2024-04-12 13:59:12 -0700

    web: clean up some repetitive types
    This commit centralizes two types that were defined multiple times throughout our code, and
    casts in stone those definitions, applying the correct definitions where needed.
    
    I had two types that were used repeatedly to define the interfaces for providers and context
    consumers. Because they were both one-liners, I had done what I usually curse in others: copied
    them. Worse, I hand-wrote them because they're so simple I had them memorized.

--- web/src/elements/Interface/ConfigContextController.ts
+++ web/src/elements/Interface/ConfigContextController.ts
@@ -1,23 +1,22 @@
 import { EVENT_REFRESH } from "@goauthentik/authentik/common/constants";
 import { DEFAULT_CONFIG } from "@goauthentik/common/api/config";
 import { authentikConfigContext } from "@goauthentik/elements/AuthentikContexts";
+import type { ReactiveElementHost } from "@goauthentik/elements/types.js";
 
 import { ContextProvider } from "@lit/context";
-import { ReactiveController, ReactiveControllerHost } from "lit";
+import type { ReactiveController } from "lit";
 
 import type { Config } from "@goauthentik/api";
 import { RootApi } from "@goauthentik/api";
 
 import type { AkInterface } from "./Interface";
 
-type ReactiveElementHost = Partial<ReactiveControllerHost> & AkInterface;
-
 export class ConfigContextController implements ReactiveController {
-    host!: ReactiveElementHost;
+    host!: ReactiveElementHost<AkInterface>;
 
     context!: ContextProvider<{ __context__: Config | undefined }>;
 
-    constructor(host: ReactiveElementHost) {
+    constructor(host: ReactiveElementHost<AkInterface>) {
         this.host = host;
         this.context = new ContextProvider(this.host, {
             context: authentikConfigContext,
