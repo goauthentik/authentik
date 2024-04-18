fe97c0b3b681eae5324bbeb53ef76700c0e90a5b
Jens Langhammer <jens@goauthentik.io>
2024-04-18 17:51:40 +0200

    fix
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/root/settings.py
+++ authentik/root/settings.py
@@ -7,6 +7,7 @@ from hashlib import sha512
 from pathlib import Path
 
 from celery.schedules import crontab
+from django.conf import ImproperlyConfigured
 from sentry_sdk import set_tag
 
 from authentik import ENV_GIT_HASH_KEY, __version__
@@ -213,11 +214,13 @@ DJANGO_REDIS_IGNORE_EXCEPTIONS = True
 DJANGO_REDIS_LOG_IGNORED_EXCEPTIONS = True
 match CONFIG.get("session_storage", "cache"):
     case "cache":
-        SESSION_ENGINE = "django.contrib.sessions.backends.db"
+        SESSION_ENGINE = "django.contrib.sessions.backends.cache"
     case "db":
         SESSION_ENGINE = "django.contrib.sessions.backends.db"
     case _:
-        raise ValueError("Invalid session_storage setting, allowed values are db and cache")
+        raise ImproperlyConfigured(
+            "Invalid session_storage setting, allowed values are db and cache"
+        )
 SESSION_SERIALIZER = "authentik.root.sessions.pickle.PickleSerializer"
 SESSION_CACHE_ALIAS = "default"
 # Configured via custom SessionMiddleware
