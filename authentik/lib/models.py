cd7be6a1a43fa0602f010187951f218ce2a41d70
Jens Langhammer <jens@goauthentik.io>
2024-04-23 19:06:41 +0200

    initial soft delete
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/lib/models.py
+++ authentik/lib/models.py
@@ -1,6 +1,10 @@
 """Generic models"""
 
+from typing import Any
+
 from django.db import models
+from django.db.models.signals import post_delete, pre_delete
+from django.utils import timezone
 from model_utils.managers import InheritanceManager
 from rest_framework.serializers import BaseSerializer
 
@@ -45,3 +49,52 @@ class InheritanceForeignKey(models.ForeignKey):
     """Custom ForeignKey that uses InheritanceForwardManyToOneDescriptor"""
 
     forward_related_accessor_class = InheritanceForwardManyToOneDescriptor
+
+
+class SoftDeleteQuerySet(models.query.QuerySet):
+
+    def delete(self):
+        for obj in self.all():
+            obj.delete()
+
+    def hard_delete(self):
+        return super().delete()
+
+
+class SoftDeleteManager(models.Manager):
+
+    def get_queryset(self):
+        return SoftDeleteQuerySet(self.model, using=self._db).filter(deleted_at__isnull=True)
+
+
+class SoftDeleteModel(models.Model):
+    """Model which doesn't fully delete itself, but rather saved the delete status
+    so cleanup events can run."""
+
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    objects = SoftDeleteManager()
+
+    class Meta:
+        abstract = True
+
+    @property
+    def is_deleted(self):
+        return self.deleted_at is not None
+
+    def delete(self, using: Any = ..., keep_parents: bool = ...) -> tuple[int, dict[str, int]]:
+        pre_delete.send(sender=self.__class__, instance=self)
+        now = timezone.now()
+        self.deleted_at = now
+        self.save(
+            update_fields=[
+                "deleted_at",
+            ]
+        )
+        post_delete.send(sender=self.__class__, instance=self)
+        return tuple()
+
+    def force_delete(self, using: Any = ...):
+        if not self.deleted_at:
+            raise models.ProtectedError("Refusing to force delete non-deleted model", {self})
+        return super().delete(using=using)
