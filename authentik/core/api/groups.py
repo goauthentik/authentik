962dd5c990d0d88d275a6f3f298f1b4636593d76
Jens Langhammer <jens@goauthentik.io>
2024-04-27 22:20:39 +0200

    whops that needs to be pre-fetched
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/core/api/groups.py
+++ authentik/core/api/groups.py
@@ -161,7 +161,7 @@ class GroupViewSet(UsedByMixin, ModelViewSet):
     ordering = ["name"]
 
     def get_queryset(self):
-        base_qs = Group.objects.all().select_related("parent", "roles")
+        base_qs = Group.objects.all().select_related("parent").prefetch_related("roles")
         if self.serializer_class(context={"request": self.request})._should_include_users:
             base_qs = base_qs.prefetch_related("users")
         return base_qs
