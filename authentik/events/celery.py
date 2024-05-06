da22caf12c20d59005b50e4a18131f39a08ef6f2
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2024-05-06 14:10:14 +0200

    wip
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/events/celery.py
+++ authentik/events/celery.py
@@ -6,47 +6,34 @@ from celery.events import EventReceiver
 from celery.events.state import State
 from prometheus_client import Counter, Gauge, Histogram
 
-PROMETHEUS_METRICS = None
-
-
-def get_prometheus_metrics():
-    global PROMETHEUS_METRICS
-    if PROMETHEUS_METRICS is None:
-        PROMETHEUS_METRICS = PrometheusMetrics()
-    return PROMETHEUS_METRICS
-
-
-class PrometheusMetrics:
-    def __init__(self):
-        self.events = Counter(
-            "authentik_celery_events_total", "Number of Celery events", ["worker", "type", "task"]
-        )
-        self.runtime = Histogram(
-            "authentik_tasks_runtime_seconds", "Task runtime", ["worker", "task"]
-        )
-        self.prefetch_time = Gauge(
-            "authentik_tasks_prefetch_time_seconds",
-            "Time the task spent waiting at the celery worker to be executed",
-            ["worker", "task"],
-        )
-        self.prefetched_tasks = Gauge(
-            "authentik_tasks_worker_prefetched_tasks",
-            "Number of tasks of a given type prefetched at a worker",
-            ["worker", "task"],
-        )
-        self.worker_online = Gauge("authentik_worker_online", "Worker online status", ["worker"])
-        self.worker_currently_executing_tasks = Gauge(
-            "authentik_worker_currently_executing_tasks",
-            "Number of tasks currently executng at a worker",
-            ["worker"],
-        )
+COUNTER_CELERY_EVENTS = Counter(
+    "authentik_celery_events_total", "Number of Celery events", ["worker", "type", "task"]
+)
+HISTOGRAM_TASKS_RUNTIME = Histogram(
+    "authentik_tasks_runtime_seconds", "Task runtime", ["worker", "task"]
+)
+GAUGE_TASKS_PREFETCH_TIME = Gauge(
+    "authentik_tasks_prefetch_time_seconds",
+    "Time the task spent waiting at the celery worker to be executed",
+    ["worker", "task"],
+)
+GAUGE_TASKS_WORKER_PREFETCHED = Gauge(
+    "authentik_tasks_worker_prefetched_tasks",
+    "Number of tasks of a given type prefetched at a worker",
+    ["worker", "task"],
+)
+GAUGE_WORKER_ONLINE = Gauge("authentik_worker_online", "Worker online status", ["worker"])
+GAUGE_WORKER_CURRENTLY_EXECUTING_TASKS = Gauge(
+    "authentik_worker_currently_executing_tasks",
+    "Number of tasks currently executng at a worker",
+    ["worker"],
+)
 
 
 class EventsState(State):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.counter = collections.defaultdict(collections.Counter)
-        self.metrics = get_prometheus_metrics()
 
     def event(self, event):
         super().event(event)
@@ -62,20 +49,20 @@ class EventsState(State):
             task_name = event.get("name", "")
             if not task_name and task_id in self.tasks:
                 task_name = task.name or ""
-            self.metrics.events.labels(worker=worker_name, type=event_type, task=task_name).inc()
+            COUNTER_CELERY_EVENTS.labels(worker=worker_name, type=event_type, task=task_name).inc()
 
             runtime = event.get("runtime", 0)
             if runtime:
-                self.metrics.runtime.labels(worker=worker_name, task=task_name).observe(runtime)
+                HISTOGRAM_TASKS_RUNTIME.labels(worker=worker_name, task=task_name).observe(runtime)
 
             if event_type == "task-received" and not task.eta and task.received:
-                self.metrics.prefetched_tasks.labels(worker=worker_name, task=task_name).inc()
+                GAUGE_TASKS_WORKER_PREFETCHED.labels(worker=worker_name, task=task_name).inc()
 
             if event_type == "task-started" and not task.eta and task.started and task.received:
-                self.metrics.prefetch_time.labels(worker=worker_name, task=task_name).set(
+                GAUGE_TASKS_PREFETCH_TIME.labels(worker=worker_name, task=task_name).set(
                     task.started - task.received
                 )
-                self.metrics.prefetched_tasks.labels(worker=worker_name, task=task_name).dec()
+                GAUGE_TASKS_WORKER_PREFETCHED.labels(worker=worker_name, task=task_name).dec()
 
             if (
                 event_type in ("task-succeeded", "task-failed")
@@ -83,20 +70,20 @@ class EventsState(State):
                 and task.started
                 and task.received
             ):
-                self.metrics.prefetch_time.labels(worker=worker_name, task=task_name).set(0)
+                GAUGE_TASKS_PREFETCH_TIME.labels(worker=worker_name, task=task_name).set(0)
 
         if event_type in ("worker-online", "worker-heartbeat"):
-            self.metrics.worker_online.labels(worker=worker_name).set(1)
+            GAUGE_WORKER_ONLINE.labels(worker=worker_name).set(1)
 
         if event_type == "worker-heartbeat":
             executing_tasks = event.get("active")
             if executing_tasks is not None:
-                self.metrics.worker_currently_executing_tasks.labels(worker=worker_name).set(
+                GAUGE_WORKER_CURRENTLY_EXECUTING_TASKS.labels(worker=worker_name).set(
                     executing_tasks
                 )
 
         if event_type == "worker-offline":
-            self.metrics.worker_online.labels(worker=worker_name).set(0)
+            GAUGE_WORKER_ONLINE.labels(worker=worker_name).set(0)
 
 
 class Events(threading.Thread):
