5c85c2c9e626a79180c6d0f56b44b03b3333d2f5
Ken Sternberg <ken@goauthentik.io>
2024-05-06 14:02:57 -0700

    web: issue 7864, provide a "list view" for the Applications page.
    This commit is a quick-and-dirty prototypes for what such a list
    view would look like.  None of the usual bells and whistles have
    been applied: the styles are ugly, the reveals overly quick, and
    there's probably a host of other things we could do to pretty it
    up (like search, or toggle between Alphabetical and ASCIIBetical,
    although in the context of I18N does that even mean anything
    anymore?).  But it does the job.

--- web/src/user/LibraryPage/ApplicationList.ts
+++ web/src/user/LibraryPage/ApplicationList.ts
@@ -1,12 +1,17 @@
+import { PFSize } from "@goauthentik/common/enums.js";
 import { LayoutType } from "@goauthentik/common/ui/config";
-import { AKElement } from "@goauthentik/elements/Base";
+import { AKElement, rootInterface } from "@goauthentik/elements/Base";
 
-import { css, html } from "lit";
+import { msg } from "@lit/localize";
+import { css, html, nothing } from "lit";
 import { customElement, property } from "lit/decorators.js";
+import { classMap } from "lit/directives/class-map.js";
 import { ifDefined } from "lit/directives/if-defined.js";
 
+import PFButton from "@patternfly/patternfly/components/Button/button.css";
 import PFContent from "@patternfly/patternfly/components/Content/content.css";
 import PFEmptyState from "@patternfly/patternfly/components/EmptyState/empty-state.css";
+import PFTable from "@patternfly/patternfly/components/Table/table.css";
 import PFGrid from "@patternfly/patternfly/layouts/Grid/grid.css";
 import PFBase from "@patternfly/patternfly/patternfly-base.css";
 
@@ -36,13 +41,12 @@ export class LibraryPageApplicationList extends AKElement {
     static get styles() {
         return [
             PFBase,
+            PFTable,
+            PFButton,
             PFEmptyState,
-            PFContent,
-            PFGrid,
             css`
-                .app-group-header {
-                    margin-bottom: 1em;
-                    margin-top: 1.2em;
+                .app-row a {
+                    font-weight: bold;
                 }
             `,
         ];
@@ -60,6 +64,8 @@ export class LibraryPageApplicationList extends AKElement {
     @property({ attribute: false })
     apps: AppGroupList = [];
 
+    expanded = new Set<string>();
+
     get currentLayout(): Pair {
         const layout = LAYOUTS.get(this.layout);
         if (!layout) {
@@ -70,26 +76,109 @@ export class LibraryPageApplicationList extends AKElement {
     }
 
     render() {
+        const canEdit =
+            rootInterface()?.uiConfig?.enabledFeatures.applicationEdit &&
+            rootInterface()?.me?.user.isSuperuser;
+
+        const toggleExpansion = (pk: string) => {
+            if (this.expanded.has(pk)) {
+                this.expanded.delete(pk);
+            } else {
+                this.expanded.add(pk);
+            }
+            this.requestUpdate();
+        };
+
         const [groupClass, groupGrid] = this.currentLayout;
 
-        return html`<div class="pf-l-grid pf-m-gutter">
-            ${this.apps.map(([group, apps]: AppGroupEntry) => {
-                return html`<div class="pf-l-grid__item ${groupClass}">
-                    <div class="pf-c-content app-group-header">
-                        <h2>${group}</h2>
-                    </div>
-                    <div class="pf-l-grid pf-m-gutter ${groupGrid}">
-                        ${apps.map((app: Application) => {
-                            return html`<ak-library-app
-                                class="pf-l-grid__item"
-                                .application=${app}
-                                background=${ifDefined(this.background)}
-                                ?selected=${app.slug === this.selected}
-                            ></ak-library-app>`;
-                        })}
-                    </div>
-                </div> `;
-            })}
-        </div>`;
+        const expandedClass = (pk: string) => ({
+            "pf-m-expanded": this.expanded.has(pk),
+        });
+
+        const renderExpansionCell = (app: Application) =>
+            app.metaDescription
+                ? html`<td class="pf-c-table__toggle" role="cell">
+                      <button
+                          class="pf-c-button pf-m-plain ${classMap(expandedClass(app.pk))}"
+                          @click=${() => toggleExpansion(app.pk)}
+                      >
+                          <div class="pf-c-table__toggle-icon">
+                              &nbsp;<i class="fas fa-angle-down" aria-hidden="true"></i>&nbsp;
+                          </div>
+                      </button>
+                  </td>`
+                : nothing;
+
+        const renderAppIcon = (app: Application) =>
+            html`<a
+                href="${ifDefined(app.launchUrl ?? "")}"
+                target="${ifDefined(app.openInNewTab ? "_blank" : undefined)}"
+            >
+                <ak-app-icon size=${PFSize.Small} .app=${app}></ak-app-icon>
+            </a>`;
+
+        const renderAppUrl = (app: Application) =>
+            app.launchUrl === "goauthentik.io://providers/rac/launch"
+                ? html`<ak-library-rac-endpoint-launch .app=${app}>
+                      <a slot="trigger"> ${app.name} </a>
+                  </ak-library-rac-endpoint-launch>`
+                : html`<a
+                      href="${ifDefined(app.launchUrl ?? "")}"
+                      target="${ifDefined(app.openInNewTab ? "_blank" : undefined)}"
+                      >${app.name}</a
+                  >`;
+
+        const renderAppDescription = (app: Application) =>
+            app.metaDescription
+                ? html` <tr
+                      class="pf-c-table__expandable-row ${classMap(expandedClass(app.pk))}"
+                      role="row"
+                  >
+                      <td></td>
+                      <td></td>
+                      <td colspan="3">${app.metaDescription}</td>
+                  </tr>`
+                : nothing;
+
+        const renderGroup = ([group, apps]: AppGroupEntry) => html`
+            ${group
+                ? html`<tr>
+                      <td colspan="5"><h2>${group}</h2></td>
+                  </tr>`
+                : nothing}
+            ${apps.map(
+                (app: Application) =>
+                    html`<tr>
+                            <td>${renderExpansionCell(app)}</td>
+                            <td>${renderAppIcon(app)}</td>
+                            <td class="app-row">${renderAppUrl(app)}</td>
+                            <td>${app.metaPublisher ?? ""}</td>
+                            <td>
+                                <a
+                                    class="pf-c-button pf-m-control pf-m-small pf-m-block"
+                                    href="/if/admin/#/core/applications/${app?.slug}"
+                                >
+                                    <i class="fas fa-edit"></i>&nbsp;${msg("Edit")}
+                                </a>
+                            </td>
+                        </tr>
+                        ${this.expanded.has(app.pk) ? renderAppDescription(app) : nothing} `,
+            )}
+        `;
+
+        return html`<table class="pf-c-table pf-m-compact pf-m-grid-sm pf-m-expandable">
+            <thead>
+                <tr role="row">
+                    <th></th>
+                    <th></th>
+                    <th>${msg("Application")}</th>
+                    <th>${msg("Publisher")}</th>
+                    ${canEdit ? html`<th>${msg("Edit")}</th>` : nothing}
+                </tr>
+            </thead>
+            <tbody>
+                ${this.apps.map(renderGroup)}
+            </tbody>
+        </table> `;
     }
 }
