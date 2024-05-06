0b464a8e05d0e088f4ea9259e7c45f799c209df0
Jens Langhammer <jens@goauthentik.io>
2024-05-06 03:00:47 +0200

    make sure we actually send it to all tenants
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/tenants/scheduler.py
+++ authentik/tenants/scheduler.py
@@ -1,12 +1,9 @@
 """Tenant-aware Celery beat scheduler"""
 
-from django_tenants.utils import get_public_schema_name
 from tenant_schemas_celery.scheduler import (
     TenantAwarePersistentScheduler as BaseTenantAwarePersistentScheduler,
 )
-from tenant_schemas_celery.scheduler import (
-    TenantAwareScheduleEntry,
-)
+from tenant_schemas_celery.scheduler import TenantAwareScheduleEntry
 
 
 class TenantAwarePersistentScheduler(BaseTenantAwarePersistentScheduler):
@@ -18,8 +15,8 @@ class TenantAwarePersistentScheduler(BaseTenantAwarePersistentScheduler):
 
     def apply_entry(self, entry: TenantAwareScheduleEntry, producer=None):
         # https://github.com/maciej-gol/tenant-schemas-celery/blob/master/tenant_schemas_celery/scheduler.py#L85
-        # When (as by default) tenant schemas are set, the public schema is excluded
+        # When (as by default) no tenant schemas are set, the public schema is excluded
         # so we need to explicitly include it here, otherwise the task is not executed
         if entry.tenant_schemas is None:
-            entry.tenant_schemas = [get_public_schema_name()]
+            entry.tenant_schemas = self.get_queryset().values_list("schema_name", flat=True)
         return super().apply_entry(entry, producer)
