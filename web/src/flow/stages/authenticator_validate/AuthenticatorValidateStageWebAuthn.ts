779014ae88e0a20cba574979a289fc884436d061
Jens Langhammer <jens@goauthentik.io>
2024-05-07 19:27:48 +0200

    web/flows: fix error when using consecutive webauthn validator stages
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/flow/stages/authenticator_validate/AuthenticatorValidateStageWebAuthn.ts
+++ web/src/flow/stages/authenticator_validate/AuthenticatorValidateStageWebAuthn.ts
@@ -7,7 +7,7 @@ import "@goauthentik/elements/EmptyState";
 import { BaseDeviceStage } from "@goauthentik/flow/stages/authenticator_validate/base";
 
 import { msg } from "@lit/localize";
-import { TemplateResult, html, nothing } from "lit";
+import { PropertyValues, TemplateResult, html, nothing } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
 
 import {
@@ -72,14 +72,16 @@ export class AuthenticatorValidateStageWebAuthn extends BaseDeviceStage<
         }
     }
 
-    firstUpdated(): void {
-        // convert certain members of the PublicKeyCredentialRequestOptions into
-        // byte arrays as expected by the spec.
-        const credentialRequestOptions = this.deviceChallenge
-            ?.challenge as PublicKeyCredentialRequestOptions;
-        this.transformedCredentialRequestOptions =
-            transformCredentialRequestOptions(credentialRequestOptions);
-        this.authenticateWrapper();
+    updated(changedProperties: PropertyValues<this>) {
+        if (changedProperties.has("challenge") && this.challenge !== undefined) {
+            // convert certain members of the PublicKeyCredentialRequestOptions into
+            // byte arrays as expected by the spec.
+            const credentialRequestOptions = this.deviceChallenge
+                ?.challenge as PublicKeyCredentialRequestOptions;
+            this.transformedCredentialRequestOptions =
+                transformCredentialRequestOptions(credentialRequestOptions);
+            this.authenticateWrapper();
+        }
     }
 
     async authenticateWrapper(): Promise<void> {
