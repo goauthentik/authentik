66c7283f2ba3990b813cf195ff3a664a76474864
Jens Langhammer <jens@goauthentik.io>
2024-05-08 20:24:49 +0200

    make sure autoretry is on
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/enterprise/providers/microsoft_entra/tasks.py
+++ authentik/enterprise/providers/microsoft_entra/tasks.py
@@ -2,18 +2,21 @@
 
 from authentik.enterprise.providers.microsoft_entra.models import MicrosoftEntraProvider
 from authentik.events.system_tasks import SystemTask
+from authentik.lib.sync.outgoing.exceptions import TransientSyncException
 from authentik.lib.sync.outgoing.tasks import SyncTasks
 from authentik.root.celery import CELERY_APP
 
 sync_tasks = SyncTasks(MicrosoftEntraProvider)
 
 
-@CELERY_APP.task()
+@CELERY_APP.task(autoretry_for=(TransientSyncException,), retry_backoff=True)
 def microsoft_entra_sync_objects(*args, **kwargs):
     return sync_tasks.sync_objects(*args, **kwargs)
 
 
-@CELERY_APP.task(base=SystemTask, bind=True)
+@CELERY_APP.task(
+    base=SystemTask, bind=True, autoretry_for=(TransientSyncException,), retry_backoff=True
+)
 def microsoft_entra_sync(self, provider_pk: int, *args, **kwargs):
     """Run full sync for Microsoft Entra provider"""
     return sync_tasks.sync_single(self, provider_pk, microsoft_entra_sync_objects)
@@ -24,11 +27,11 @@ def microsoft_entra_sync_all():
     return sync_tasks.sync_all(microsoft_entra_sync)
 
 
-@CELERY_APP.task()
+@CELERY_APP.task(autoretry_for=(TransientSyncException,), retry_backoff=True)
 def microsoft_entra_sync_direct(*args, **kwargs):
     return sync_tasks.sync_signal_direct(*args, **kwargs)
 
 
-@CELERY_APP.task()
+@CELERY_APP.task(autoretry_for=(TransientSyncException,), retry_backoff=True)
 def microsoft_entra_sync_m2m(*args, **kwargs):
     return sync_tasks.sync_signal_m2m(*args, **kwargs)
