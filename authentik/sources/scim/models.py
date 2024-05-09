b6dd5552fb8e7a8c58e33495dfb9bb0a60571322
Jens Langhammer <jens@goauthentik.io>
2024-05-10 01:15:34 +0200

    add ldap icon
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/sources/scim/models.py
+++ authentik/sources/scim/models.py
@@ -3,9 +3,9 @@
 from uuid import uuid4
 
 from django.db import models
+from django.templatetags.static import static
 from django.utils.translation import gettext_lazy as _
 from rest_framework.serializers import BaseSerializer
-from django.templatetags.static import static
 
 from authentik.core.models import Group, Source, Token, User
 from authentik.lib.models import SerializerModel
