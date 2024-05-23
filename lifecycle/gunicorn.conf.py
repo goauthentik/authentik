d83d6f12f1813b2fb42c6a4c14c5a90320fe8d4c
Jens Langhammer <jens@goauthentik.io>
2024-05-23 12:45:59 +0200

    format
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- lifecycle/gunicorn.conf.py
+++ lifecycle/gunicorn.conf.py
@@ -109,8 +109,8 @@ def post_worker_init(worker: DjangoUvicornWorker):
     # does not use this signal, so we can skip this safely
     if worker._worker_id != 1:
         return
-    app: "WSGIApplication" = worker.app
-    root_app: "AuthentikAsgi" = app.callable
+    app: WSGIApplication = worker.app
+    root_app: AuthentikAsgi = app.callable
     root_app.call_startup()
 
 
