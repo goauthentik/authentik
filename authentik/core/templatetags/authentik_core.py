f99de1aeed001fc35363e590667405b86488c88e
Jens Langhammer <jens@goauthentik.io>
2024-05-30 06:36:33 +0900

    use full version to match frontend
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/core/templatetags/authentik_core.py
+++ authentik/core/templatetags/authentik_core.py
@@ -4,7 +4,7 @@ from django import template
 from django.templatetags.static import static as static_loader
 from django.utils.safestring import mark_safe
 
-from authentik import __version__
+from authentik import get_full_version
 
 register = template.Library()
 
@@ -13,12 +13,15 @@ register = template.Library()
 def versioned_script(path: str) -> str:
     """Wrapper around {% static %} tag that supports setting the version"""
     returned_lines = [
-        f'<script src="{static_loader(path.replace("%v", __version__))}" type="module"></script>',
+        (
+            f'<script src="{static_loader(path.replace("%v", get_full_version()))}'
+            '" type="module"></script>'
+        ),
         # Legacy method of loading scripts used as a fallback, without the version in the filename
         # TODO: Remove after 2024.6 or later
         (
             f'<script src="{static_loader(path.replace("-%v", ""))}?'
-            f'version={__version__}" type="module"></script>'
+            f'version={get_full_version()}" type="module"></script>'
         ),
     ]
     return mark_safe("".join(returned_lines))  # nosec
