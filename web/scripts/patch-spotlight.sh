ca7f314a42d022eaedd24f2b32043a37d46cc9e7
Ken Sternberg <ken@goauthentik.io>
2024-06-18 12:29:31 -0700

    Fix for incorrect path to patch file; fix for running patch multiple times.

--- web/scripts/patch-spotlight.sh
+++ web/scripts/patch-spotlight.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-TARGET="./node_modules/@spotlightjs/overlay/dist/index-[0-9a-f]*.js";
-patch --forward --no-backup-if-mismatch -p0 $TARGET <<EOF
+TARGET="./node_modules/@spotlightjs/overlay/dist/index-"[0-9a-f]*.js
+
+if [[ $(grep -L "QX2" "$TARGET" > /dev/null 2&>1) ]]; then
+    patch --forward -V none --no-backup-if-mismatch -p0 $TARGET <<EOF
 --- a/index-5682ce90.js	2024-06-13 16:19:28
 +++ b/index-5682ce90.js	2024-06-13 16:20:23
 @@ -4958,11 +4958,10 @@
@@ -19,3 +21,4 @@ patch --forward --no-backup-if-mismatch -p0 $TARGET <<EOF
    clipboardEnabled: o = !1,
    displayDataTypes: c = !1,
 EOF
+fi
