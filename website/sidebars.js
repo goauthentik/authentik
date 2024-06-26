d7bcd7cc8bfa1fa25fd115ab1323296fece91c7f
Jens Langhammer <jens@goauthentik.io>
2024-06-26 17:57:02 +0900

    security: fix CVE-2024-38371
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- website/sidebars.js
+++ website/sidebars.js
@@ -510,6 +510,7 @@ const docsSidebar = {
             items: [
                 "security/security-hardening",
                 "security/policy",
+                "security/CVE-2024-38371",
                 "security/CVE-2024-23647",
                 "security/CVE-2024-21637",
                 "security/CVE-2023-48228",
