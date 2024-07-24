8c3a55f91222c67136413d038ea10049202e918b
David Gunter <david@davidgunter.ca>
2024-07-23 21:58:34 -0700

    Add serializer for UniquePasswordPolicy

--- authentik/policies/password/api.py
+++ authentik/policies/password/api.py
@@ -4,7 +4,7 @@ from rest_framework.viewsets import ModelViewSet
 
 from authentik.core.api.used_by import UsedByMixin
 from authentik.policies.api.policies import PolicySerializer
-from authentik.policies.password.models import PasswordPolicy
+from authentik.policies.password.models import PasswordPolicy, UniquePasswordPolicy
 
 
 class PasswordPolicySerializer(PolicySerializer):
@@ -34,6 +34,27 @@ class PasswordPolicyViewSet(UsedByMixin, ModelViewSet):
 
     queryset = PasswordPolicy.objects.all()
     serializer_class = PasswordPolicySerializer
+    filterset_fields = "__all__"
+    ordering = ["name"]
+    search_fields = ["name"]
+
+
+class UniquePasswordPolicySerializer(PolicySerializer):
+    """Password Uniqueness Policy Serializer"""
+
+    class Meta:
+        model = UniquePasswordPolicy
+        fields = PolicySerializer.Meta.fields + [
+            "password_field",
+            "num_historical_passwords",
+        ]
+
+
+class UniquePasswordPolicyViewSet(UsedByMixin, ModelViewSet):
+    """Password Uniqueness Policy Viewset"""
+
+    queryset = UniquePasswordPolicy.objects.all()
+    serializer_class = UniquePasswordPolicySerializer
     filterset_fields = "__all__"
     ordering = ["name"]
     search_fields = ["name"]
