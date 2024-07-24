9955310512897db8fe1ae7aebdc58a2c47b8cb43
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2024-07-24 17:44:33 +0200

    lint
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- tests/e2e/test_source_ldap_samba.py
+++ tests/e2e/test_source_ldap_samba.py
@@ -9,7 +9,7 @@ from authentik.blueprints.tests import apply_blueprint
 from authentik.core.models import Group, User
 from authentik.lib.generators import generate_id, generate_key
 from authentik.sources.ldap.auth import LDAPBackend
-from authentik.sources.ldap.models import LDAPSourcePropertyMapping, LDAPSource
+from authentik.sources.ldap.models import LDAPSource, LDAPSourcePropertyMapping
 from authentik.sources.ldap.sync.groups import GroupLDAPSynchronizer
 from authentik.sources.ldap.sync.membership import MembershipLDAPSynchronizer
 from authentik.sources.ldap.sync.users import UserLDAPSynchronizer
@@ -62,7 +62,9 @@ class TestSourceLDAPSamba(SeleniumTestCase):
             )
         )
         source.group_property_mappings.set(
-            LDAPSourcePropertyMapping.objects.filter(name="goauthentik.io/sources/ldap/default-name")
+            LDAPSourcePropertyMapping.objects.filter(
+                name="goauthentik.io/sources/ldap/default-name"
+            )
         )
         UserLDAPSynchronizer(source).sync_full()
         self.assertTrue(User.objects.filter(username="bob").exists())
@@ -93,7 +95,9 @@ class TestSourceLDAPSamba(SeleniumTestCase):
             )
         )
         source.group_property_mappings.set(
-            LDAPSourcePropertyMapping.objects.filter(managed="goauthentik.io/sources/ldap/default-name")
+            LDAPSourcePropertyMapping.objects.filter(
+                managed="goauthentik.io/sources/ldap/default-name"
+            )
         )
         GroupLDAPSynchronizer(source).sync_full()
         UserLDAPSynchronizer(source).sync_full()
@@ -137,7 +141,9 @@ class TestSourceLDAPSamba(SeleniumTestCase):
             )
         )
         source.group_property_mappings.set(
-            LDAPSourcePropertyMapping.objects.filter(name="goauthentik.io/sources/ldap/default-name")
+            LDAPSourcePropertyMapping.objects.filter(
+                name="goauthentik.io/sources/ldap/default-name"
+            )
         )
         UserLDAPSynchronizer(source).sync_full()
         username = "bob"
