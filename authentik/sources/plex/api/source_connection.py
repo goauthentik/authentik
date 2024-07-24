03dfcce6cd5c8f49e12d0485e078125237b12bab
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2024-07-24 14:54:59 +0200

    sources: refactor user connection api
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/sources/plex/api/source_connection.py
+++ authentik/sources/plex/api/source_connection.py
@@ -1,39 +1,28 @@
 """Plex Source connection Serializer"""
 
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
-from authentik.api.authorization import OwnerFilter, OwnerSuperuserPermissions
-from authentik.core.api.sources import UserSourceConnectionSerializer
-from authentik.core.api.used_by import UsedByMixin
+from authentik.core.api.sources import UserSourceConnectionSerializer, UserSourceConnectionViewSet
 from authentik.sources.plex.models import PlexSourceConnection
 
 
 class PlexSourceConnectionSerializer(UserSourceConnectionSerializer):
     """Plex Source connection Serializer"""
 
-    class Meta:
+    class Meta(UserSourceConnectionSerializer.Meta):
         model = PlexSourceConnection
-        fields = [
-            "pk",
-            "user",
-            "source",
+        fields = UserSourceConnectionSerializer.Meta.fields + [
             "identifier",
             "plex_token",
         ]
         extra_kwargs = {
-            "user": {"read_only": True},
+            **UserSourceConnectionSerializer.Meta.extra_kwargs,
+            "plex_token": {"write_only": True},
         }
 
 
-class PlexSourceConnectionViewSet(UsedByMixin, ModelViewSet):
+class PlexSourceConnectionViewSet(UserSourceConnectionViewSet, ModelViewSet):
     """Plex Source connection Serializer"""
 
     queryset = PlexSourceConnection.objects.all()
     serializer_class = PlexSourceConnectionSerializer
-    filterset_fields = ["source__slug"]
-    permission_classes = [OwnerSuperuserPermissions]
-    filter_backends = [OwnerFilter, DjangoFilterBackend, OrderingFilter, SearchFilter]
-    ordering = ["pk"]
-    search_fields = ["source__slug"]
