c0a494c3f8eee69d034a58cd808a6c790dd32ce9
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2024-07-26 16:18:27 +0200

    core: add primitives for source property mappings
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/providers/radius/api/property_mappings.py
+++ authentik/providers/radius/api/property_mappings.py
@@ -1,12 +1,8 @@
 """Radius Property mappings API Views"""
 
-from django_filters.filters import AllValuesMultipleFilter
-from django_filters.filterset import FilterSet
-from drf_spectacular.types import OpenApiTypes
-from drf_spectacular.utils import extend_schema_field
 from rest_framework.viewsets import ModelViewSet
 
-from authentik.core.api.property_mappings import PropertyMappingSerializer
+from authentik.core.api.property_mappings import PropertyMappingFilterSet, PropertyMappingSerializer
 from authentik.core.api.used_by import UsedByMixin
 from authentik.providers.radius.models import RadiusProviderPropertyMapping
 
@@ -19,14 +15,11 @@ class RadiusProviderPropertyMappingSerializer(PropertyMappingSerializer):
         fields = PropertyMappingSerializer.Meta.fields
 
 
-class RadiusProviderPropertyMappingFilter(FilterSet):
+class RadiusProviderPropertyMappingFilter(PropertyMappingFilterSet):
     """Filter for RadiusProviderPropertyMapping"""
 
-    managed = extend_schema_field(OpenApiTypes.STR)(AllValuesMultipleFilter(field_name="managed"))
-
-    class Meta:
+    class Meta(PropertyMappingFilterSet.Meta):
         model = RadiusProviderPropertyMapping
-        fields = "__all__"
 
 
 class RadiusProviderPropertyMappingViewSet(UsedByMixin, ModelViewSet):
