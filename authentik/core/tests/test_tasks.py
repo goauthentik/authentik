1791c429a3b3c32d8aa1135ff7e517106975b6b2
David Gunter <david@davidgunter.ca>
2024-07-27 16:22:25 -0700

    Trim the UserPasswordHistory table when user updates their own password.
    The system should aim to keep the number of historical passwords to a minimum to avoid wasting storage space.
    
    Admins can configure how many passwords they want to preserve. If multiple instances of the UniquePasswordPolicy exist, the system takes the max() value of all enabled policies to determine how many passwords should remain after trimming.

--- authentik/core/tests/test_tasks.py
+++ authentik/core/tests/test_tasks.py
@@ -1,7 +1,9 @@
 """Test tasks"""
 
+from datetime import datetime, timedelta
 from time import mktime
 
+from django.test import TestCase
 from django.utils.timezone import now
 from guardian.shortcuts import get_anonymous_user
 from rest_framework.test import APITestCase
@@ -18,9 +20,12 @@ from authentik.core.tasks import (
     clean_expired_models,
     clean_temporary_users,
     purge_password_history_table,
+    trim_user_password_history,
 )
-from authentik.core.tests.utils import create_test_admin_user
+from authentik.core.tests.utils import create_test_admin_user, create_test_user
 from authentik.lib.generators import generate_id
+from authentik.policies.models import PolicyBinding, PolicyBindingModel
+from authentik.policies.password.models import UniquePasswordPolicy
 
 
 class TestTasks(APITestCase):
@@ -65,3 +70,73 @@ class TestTasks(APITestCase):
         )
         purge_password_history_table.delay().get()
         self.assertFalse(UserPasswordHistory.objects.all())
+
+
+class TestTrimPasswordHistory(TestCase):
+    """Test password history cleanup task"""
+    def setUp(self):
+        self.user = create_test_user("test-user")
+        self.pbm = PolicyBindingModel.objects.create()
+
+    def test_trim_password_history_ok(self):
+        """Test passwors over the define limit are deleted"""
+        _now = datetime.now()
+        UserPasswordHistory.objects.bulk_create(
+            [
+                UserPasswordHistory(
+                    user=self.user,
+                    change={"old_password": "hunter1"},
+                    created_at=_now - timedelta(days=3),
+                ),
+                UserPasswordHistory(
+                    user=self.user,
+                    change={"old_password": "hunter2"},
+                    created_at=_now - timedelta(days=2),
+                ),
+                UserPasswordHistory(
+                    user=self.user, change={"old_password": "hunter3"}, created_at=_now
+                ),
+            ]
+        )
+
+        policy = UniquePasswordPolicy.objects.create(num_historical_passwords=1)
+        PolicyBinding.objects.create(
+            target=self.pbm,
+            policy=policy,
+            enabled=True,
+            order=0,
+        )
+        trim_user_password_history(self.user.pk)
+        user_pwd_history_qs = UserPasswordHistory.objects.filter(user=self.user)
+        self.assertEqual(len(user_pwd_history_qs), 1)
+
+    def test_trim_password_history_policy_diabled_no_op(self):
+        """Test no passwords removed if policy binding is disabled"""
+
+        # Insert a record to ensure it's not deleted after executing task
+        UserPasswordHistory.objects.create(user=self.user, change={"old_password": "hunter2"})
+
+        policy = UniquePasswordPolicy.objects.create(num_historical_passwords=1)
+        PolicyBinding.objects.create(
+            target=self.pbm,
+            policy=policy,
+            enabled=False,
+            order=0,
+        )
+        trim_user_password_history(self.user.pk)
+        self.assertTrue(UserPasswordHistory.objects.filter(user=self.user))
+
+    def test_trim_password_history_fewer_records_than_maximum_is_no_op(self):
+        """Test no passwords deleted if fewer passwords exist than limit"""
+
+        UserPasswordHistory.objects.create(user=self.user, change={"old_password": "hunter2"})
+
+        policy = UniquePasswordPolicy.objects.create(num_historical_passwords=2)
+        PolicyBinding.objects.create(
+            target=self.pbm,
+            policy=policy,
+            enabled=True,
+            order=0,
+        )
+        trim_user_password_history(self.user.pk)
+        self.assertTrue(UserPasswordHistory.objects.filter(user=self.user).exists())
