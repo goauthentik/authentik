4ba5716dae482d8d72854ea286518f33a6b543a4
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2024-07-29 19:44:41 +0200

    deduplicate api
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/sources/oauth/api/property_mappings.py
+++ authentik/sources/oauth/api/property_mappings.py
@@ -1,12 +1,8 @@
 """OAuth source property mappings API"""
 
-from django_filters.filters import AllValuesMultipleFilter
-from django_filters.filterset import FilterSet
-from drf_spectacular.types import OpenApiTypes
-from drf_spectacular.utils import extend_schema_field
 from rest_framework.viewsets import ModelViewSet
 
-from authentik.core.api.property_mappings import PropertyMappingSerializer
+from authentik.core.api.property_mappings import PropertyMappingFilterSet, PropertyMappingSerializer
 from authentik.core.api.used_by import UsedByMixin
 from authentik.sources.oauth.models import OAuthSourcePropertyMapping
 
@@ -14,19 +10,15 @@ from authentik.sources.oauth.models import OAuthSourcePropertyMapping
 class OAuthSourcePropertyMappingSerializer(PropertyMappingSerializer):
     """OAuthSourcePropertyMapping Serializer"""
 
-    class Meta:
+    class Meta(PropertyMappingSerializer.Meta):
         model = OAuthSourcePropertyMapping
-        fields = PropertyMappingSerializer.Meta.fields
 
 
-class OAuthSourcePropertyMappingFilter(FilterSet):
+class OAuthSourcePropertyMappingFilter(PropertyMappingFilterSet):
     """Filter for OAuthSourcePropertyMapping"""
 
-    managed = extend_schema_field(OpenApiTypes.STR)(AllValuesMultipleFilter(field_name="managed"))
-
-    class Meta:
+    class Meta(PropertyMappingFilterSet.Meta):
         model = OAuthSourcePropertyMapping
-        fields = "__all__"
 
 
 class OAuthSourcePropertyMappingViewSet(UsedByMixin, ModelViewSet):
--- authentik/sources/saml/api/property_mappings.py
+++ authentik/sources/saml/api/property_mappings.py
@@ -1,12 +1,8 @@
 """SAML source property mappings API"""
 
-from django_filters.filters import AllValuesMultipleFilter
-from django_filters.filterset import FilterSet
-from drf_spectacular.types import OpenApiTypes
-from drf_spectacular.utils import extend_schema_field
 from rest_framework.viewsets import ModelViewSet
 
-from authentik.core.api.property_mappings import PropertyMappingSerializer
+from authentik.core.api.property_mappings import PropertyMappingFilterSet, PropertyMappingSerializer
 from authentik.core.api.used_by import UsedByMixin
 from authentik.sources.saml.models import SAMLSourcePropertyMapping
 
@@ -14,19 +10,15 @@ from authentik.sources.saml.models import SAMLSourcePropertyMapping
 class SAMLSourcePropertyMappingSerializer(PropertyMappingSerializer):
     """SAMLSourcePropertyMapping Serializer"""
 
-    class Meta:
+    class Meta(PropertyMappingSerializer.Meta):
         model = SAMLSourcePropertyMapping
-        fields = PropertyMappingSerializer.Meta.fields
 
 
-class SAMLSourcePropertyMappingFilter(FilterSet):
+class SAMLSourcePropertyMappingFilter(PropertyMappingFilterSet):
     """Filter for SAMLSourcePropertyMapping"""
 
-    managed = extend_schema_field(OpenApiTypes.STR)(AllValuesMultipleFilter(field_name="managed"))
-
-    class Meta:
+    class Meta(PropertyMappingFilterSet.Meta):
         model = SAMLSourcePropertyMapping
-        fields = "__all__"
 
 
 class SAMLSourcePropertyMappingViewSet(UsedByMixin, ModelViewSet):
