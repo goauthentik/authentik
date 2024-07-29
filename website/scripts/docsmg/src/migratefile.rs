a51192025fe2f981b641b141a0df10e22e11d42a
ObamaTheLlama114 <william.chomel@gmail.com>
2024-07-28 19:36:09 -0500

    fixed issues

--- website/scripts/docsmg/src/migratefile.rs
+++ website/scripts/docsmg/src/migratefile.rs
@@ -19,3 +19,17 @@ pub fn read_migrate_file(file: PathBuf) -> anyhow::Result<Vec<(PathBuf, PathBuf)
         .collect::<Vec<_>>();
     Ok(migrations)
 }
+
+pub fn read_migrate_file_left_side(file: PathBuf) -> anyhow::Result<Vec<PathBuf>> {
+    let contents = read_to_string(file)?;
+    let lines: Vec<String> = contents
+        .split('\n')
+        .map(|x| x.to_owned())
+        .filter(|x| x != "")
+        .collect();
+    let migrations = lines
+        .iter()
+        .map(|x| x.split(" -> ").collect::<Vec<&str>>()[0].into())
+        .collect::<Vec<_>>();
+    Ok(migrations)
+}
