19b4d970f2dcccb6f9334c2c2c365072e76a567d
4d62 <github-user@sdko.org>
2024-07-30 14:11:53 -0400

    add sidebar
    Signed-off-by: 4d62 <github-user@sdko.org>

--- website/sidebarsIntegrations.js
+++ website/sidebarsIntegrations.js
@@ -143,6 +143,7 @@ module.exports = {
                         "services/ubuntu-landscape/index",
                         "services/uptime-kuma/index",
                         "services/zabbix/index",
+                        "services/whats-up-docker/index",
                     ],
                 },
                 {
