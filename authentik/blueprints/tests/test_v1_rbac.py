05b1c3dba84efadc8bc8b9d1a756b3e042c9dcec
Jens Langhammer <jens@goauthentik.io>
2024-07-30 23:31:55 +0200

    add role support to blueprints
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/blueprints/tests/test_v1_rbac.py
+++ authentik/blueprints/tests/test_v1_rbac.py
@@ -6,6 +6,7 @@ from authentik.blueprints.v1.importer import Importer
 from authentik.core.models import User
 from authentik.lib.generators import generate_id
 from authentik.lib.tests.utils import load_fixture
+from authentik.rbac.models import Role
 
 
 class TestBlueprintsV1RBAC(TransactionTestCase):
@@ -22,3 +23,18 @@ class TestBlueprintsV1RBAC(TransactionTestCase):
         user = User.objects.filter(username=uid).first()
         self.assertIsNotNone(user)
         self.assertTrue(user.has_perms(["authentik_blueprints.view_blueprintinstance"]))
+
+    def test_role_permission(self):
+        """Test permissions"""
+        uid = generate_id()
+        import_yaml = load_fixture("fixtures/rbac_role.yaml", id=uid)
+
+        importer = Importer.from_string(import_yaml)
+        self.assertTrue(importer.validate()[0])
+        self.assertTrue(importer.apply())
+        role = Role.objects.filter(name=uid).first()
+        self.assertIsNotNone(role)
+        self.assertEqual(
+            list(role.group.permissions.all().values_list("codename", flat=True)),
+            ["view_blueprintinstance"],
+        )
