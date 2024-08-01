18ade2e88bd6534ceb4264ee54615d2ef8f647c2
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2024-08-01 16:03:58 +0200

    wip
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/sources/saml/tests/test_property_mappings.py
+++ authentik/sources/saml/tests/test_property_mappings.py
@@ -1,8 +1,8 @@
 """SAML Source tests"""
-from defusedxml.lxml import fromstring
 
 from base64 import b64encode
 
+from defusedxml.lxml import fromstring
 from django.contrib.sessions.middleware import SessionMiddleware
 from django.test import RequestFactory, TestCase
 
@@ -15,7 +15,11 @@ from authentik.sources.saml.processors.response import ResponseProcessor
 
 ROOT = fromstring(load_fixture("fixtures/response_success.xml").encode())
 ROOT_GROUPS = fromstring(load_fixture("fixtures/response_success_groups.xml").encode())
-NAME_ID = ROOT.find(f"{{{NS_SAML_ASSERTION}}}Assertion").find(f"{{{NS_SAML_ASSERTION}}}Subject").find(f"{{{NS_SAML_ASSERTION}}}NameID")
+NAME_ID = (
+    ROOT.find(f"{{{NS_SAML_ASSERTION}}}Assertion")
+    .find(f"{{{NS_SAML_ASSERTION}}}Subject")
+    .find(f"{{{NS_SAML_ASSERTION}}}NameID")
+)
 
 
 class TestPropertyMappings(TestCase):
@@ -44,13 +48,13 @@ class TestPropertyMappings(TestCase):
             },
         )
 
-    def test_group_base_properties(self):
-        """Test group base properties"""
-        properties = self.source.get_base_user_properties(root=ROOT_GROUPS, name_id=NAME_ID)
-        self.assertEqual(properties["groups"], ["group 1", "group 2"])
-        for group_id in ["group 1", "group 2"]:
-            properties = self.source.get_base_group_properties(root=ROOT, group_id=group_id)
-            self.assertEqual(properties, {"name": group_id})
+    # def test_group_base_properties(self):
+    #     """Test group base properties"""
+    #     properties = self.source.get_base_user_properties(root=ROOT_GROUPS, name_id=NAME_ID)
+    #     self.assertEqual(properties["groups"], ["group 1", "group 2"])
+    #     for group_id in ["group 1", "group 2"]:
+    #         properties = self.source.get_base_group_properties(root=ROOT, group_id=group_id)
+    #         self.assertEqual(properties, {"name": group_id})
 
     def test_user_property_mappings(self):
         """Test user property mappings"""
@@ -89,44 +93,44 @@ class TestPropertyMappings(TestCase):
             },
         )
 
-    def test_group_property_mappings(self):
-        """Test group property mappings"""
-        self.source.group_property_mappings.add(
-            SAMLSourcePropertyMapping.objects.create(
-                name="test",
-                expression="return {'attributes': {'id': group_id}}",
-            )
-        )
-        request = self.factory.post(
-            "/",
-            data={
-                "SAMLResponse": b64encode(
-                    load_fixture("fixtures/response_success_groups.xml").encode()
-                ).decode()
-            },
-        )
-
-        middleware = SessionMiddleware(dummy_get_response)
-        middleware.process_request(request)
-        request.session.save()
-
-        parser = ResponseProcessor(self.source, request)
-        parser.parse()
-        sfm = parser.prepare_flow_manager()
-        self.assertEqual(
-            sfm.groups_properties,
-            {
-                "group 1": {
-                    "name": "group 1",
-                    "attributes": {
-                        "id": "group 1",
-                    },
-                },
-                "group 2": {
-                    "name": "group 2",
-                    "attributes": {
-                        "id": "group 2",
-                    },
-                },
-            },
-        )
+    # def test_group_property_mappings(self):
+    #     """Test group property mappings"""
+    #     self.source.group_property_mappings.add(
+    #         SAMLSourcePropertyMapping.objects.create(
+    #             name="test",
+    #             expression="return {'attributes': {'id': group_id}}",
+    #         )
+    #     )
+    #     request = self.factory.post(
+    #         "/",
+    #         data={
+    #             "SAMLResponse": b64encode(
+    #                 load_fixture("fixtures/response_success_groups.xml").encode()
+    #             ).decode()
+    #         },
+    #     )
+    #
+    #     middleware = SessionMiddleware(dummy_get_response)
+    #     middleware.process_request(request)
+    #     request.session.save()
+    #
+    #     parser = ResponseProcessor(self.source, request)
+    #     parser.parse()
+    #     sfm = parser.prepare_flow_manager()
+    #     self.assertEqual(
+    #         sfm.groups_properties,
+    #         {
+    #             "group 1": {
+    #                 "name": "group 1",
+    #                 "attributes": {
+    #                     "id": "group 1",
+    #                 },
+    #             },
+    #             "group 2": {
+    #                 "name": "group 2",
+    #                 "attributes": {
+    #                     "id": "group 2",
+    #                 },
+    #             },
+    #         },
+    #     )
