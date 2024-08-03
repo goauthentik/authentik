029cfe636e00bcf6358f4e554f4bbc8e19f81930
RMT <d.rong@outlook.com>
2024-08-03 15:16:46 +0800

    respect error

--- internal/outpost/proxyv2/application/session.go
+++ internal/outpost/proxyv2/application/session.go
@@ -79,6 +79,9 @@ func (a *Application) getStore(p api.ProxyOutpostConfig, externalHost *url.URL) 
 		rs.KeyPrefix(RedisKeyPrefix)
 
 		cookieDomain, err := url.Parse("//" + (*p.CookieDomain))
+		if err != nil {
+			a.log.WithError(err).Panic("failed to parse cookie domain url")
+		}
 		rs.Options(sessions.Options{
 			HttpOnly: true,
 			Secure:   strings.ToLower(externalHost.Scheme) == "https",
