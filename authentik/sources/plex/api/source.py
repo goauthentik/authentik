68af5b05724888670151dfd34fe8d195c2923b8f
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2024-08-08 11:36:24 +0200

    sources/plex: add property mappings (#10772)

--- authentik/sources/plex/api/source.py
+++ authentik/sources/plex/api/source.py
@@ -19,7 +19,7 @@ from authentik.core.api.utils import PassiveSerializer
 from authentik.flows.challenge import RedirectChallenge
 from authentik.flows.views.executor import to_stage_response
 from authentik.rbac.decorators import permission_required
-from authentik.sources.plex.models import PlexSource, PlexSourceConnection
+from authentik.sources.plex.models import PlexSource, UserPlexSourceConnection
 from authentik.sources.plex.plex import PlexAuth, PlexSourceFlowManager
 
 LOGGER = get_logger()
@@ -31,6 +31,7 @@ class PlexSourceSerializer(SourceSerializer):
     class Meta:
         model = PlexSource
         fields = SourceSerializer.Meta.fields + [
+            "group_matching_mode",
             "client_id",
             "allowed_servers",
             "allow_friends",
@@ -58,6 +59,7 @@ class PlexSourceViewSet(UsedByMixin, ModelViewSet):
         "enrollment_flow",
         "policy_engine_mode",
         "user_matching_mode",
+        "group_matching_mode",
         "client_id",
         "allow_friends",
     ]
@@ -109,7 +111,10 @@ class PlexSourceViewSet(UsedByMixin, ModelViewSet):
                 source=source,
                 request=request,
                 identifier=str(identifier),
-                user_info=user_info,
+                user_info={
+                    "info": user_info,
+                    "auth_api": auth_api,
+                },
                 policy_context={},
             )
             return to_stage_response(request, sfm.get_flow(plex_token=plex_token))
@@ -158,7 +163,7 @@ class PlexSourceViewSet(UsedByMixin, ModelViewSet):
             friends_allowed = owner_api.check_friends_overlap(identifier)
         servers_allowed = auth_api.check_server_overlap()
         if any([friends_allowed, servers_allowed]):
-            PlexSourceConnection.objects.create(
+            UserPlexSourceConnection.objects.create(
                 plex_token=plex_token,
                 user=request.user,
                 identifier=identifier,
