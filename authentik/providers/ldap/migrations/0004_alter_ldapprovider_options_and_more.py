5bb89228286250f851bd611d79fff86d65b9210e
Jens Langhammer <jens@goauthentik.io>
2024-08-14 14:29:33 +0200

    fix migration
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/providers/ldap/migrations/0004_alter_ldapprovider_options_and_more.py
+++ authentik/providers/ldap/migrations/0004_alter_ldapprovider_options_and_more.py
@@ -4,15 +4,23 @@ from django.apps.registry import Apps
 from django.db.backends.base.schema import BaseDatabaseSchemaEditor
 
 from django.db import migrations
+from django.contrib.auth.management import create_permissions
 
 
 def migrate_search_group(apps: Apps, schema_editor: BaseDatabaseSchemaEditor):
     from guardian.shortcuts import assign_perm
     from authentik.core.models import User
+    from django.apps import apps as real_apps
+
+    db_alias = schema_editor.connection.alias
+
+    # Permissions are only created _after_ migrations are run
+    # - https://github.com/django/django/blob/43cdfa8b20e567a801b7d0a09ec67ddd062d5ea4/django/contrib/auth/apps.py#L19
+    # - https://stackoverflow.com/a/72029063/1870445
+    create_permissions(real_apps.get_app_config("authentik_providers_ldap"), using=db_alias)
 
     LDAPProvider = apps.get_model("authentik_providers_ldap", "ldapprovider")
 
-    db_alias = schema_editor.connection.alias
     for provider in LDAPProvider.objects.using(db_alias).all():
         for user_pk in (
             provider.search_group.users.using(db_alias).all().values_list("pk", flat=True)
