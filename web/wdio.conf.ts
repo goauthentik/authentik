3b9d0f832d85efafef91eb2a24a1983674173125
Jens Langhammer <jens@goauthentik.io>
2024-08-23 20:41:13 +0200

    upgrade vite-tsconfig-paths
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/wdio.conf.ts
+++ web/wdio.conf.ts
@@ -1,8 +1,7 @@
 import replace from "@rollup/plugin-replace";
 import type { Options } from "@wdio/types";
 import { cwd } from "process";
-// @ts-ignore
-import * as postcssLit from "rollup-plugin-postcss-lit";
+import postcssLit from "rollup-plugin-postcss-lit";
 import type { UserConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -34,7 +33,6 @@ export const config: Options.Testrunner = {
                         "preventAssignment": true,
                     }),
                     ...(config?.plugins ?? []),
-                    // @ts-ignore
                     postcssLit(),
                     tsconfigPaths(),
                 ],
