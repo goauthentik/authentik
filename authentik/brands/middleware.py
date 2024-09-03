8c7c7c3fee73e1b6b14f2841602e10bf764c6976
Jens Langhammer <jens@goauthentik.io>
2024-09-03 19:02:46 +0200

    separate cors middleware
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/brands/middleware.py
+++ authentik/brands/middleware.py
@@ -24,13 +24,32 @@ class BrandMiddleware:
             locale = brand.default_locale
             if locale != "":
                 activate(locale)
-        response = self.get_response(request)
+        return self.get_response(request)
+
+
+class BrandCORSAPIMiddleware:
+    """CORS for API requests depending on Brand"""
+
+    get_response: Callable[[HttpRequest], HttpResponse]
+
+    def __init__(self, get_response: Callable[[HttpRequest], HttpResponse]):
+        self.get_response = get_response
+
+    def set_headers(self, request: HttpRequest, response: HttpResponse):
         response["Access-Control-Allow-Origin"] = "http://localhost:8080"
         response["Access-Control-Allow-Credentials"] = "true"
+
+    def __call__(self, request: HttpRequest) -> HttpResponse:
         if request.method == "OPTIONS":
-            response.status_code = 200
+            response = HttpResponse(
+                status=200,
+            )
+            self.set_headers(request, response)
             response["Access-Control-Allow-Headers"] = (
                 "authorization,sentry-trace,x-authentik-csrf,content-type"
             )
             response["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+            return response
+        response = self.get_response(request)
+        self.set_headers(request, response)
         return response
