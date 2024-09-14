ae60458f6fa55aac62268894ac498cc861dd6a87
Jens Langhammer <jens@goauthentik.io>
2024-09-14 21:24:00 +0200

    fix?
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- tests/wdio/test/pageobjects/forms/ldap.form.ts
+++ tests/wdio/test/pageobjects/forms/ldap.form.ts
@@ -1,7 +1,7 @@
 import Page from "../page.js";
 
 export class LdapForm extends Page {
-    async setBindFlow(_selector: string) {
+    async setBindFlow() {
         await this.searchSelect(
             'ak-search-select-view[name="authorizationFlow"]',
             "authorizationFlow",
