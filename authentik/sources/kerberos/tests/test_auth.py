d48f45ea93e3073fb2e0a2a66578b3ecf727ac0a
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2024-09-23 13:52:25 +0200

    add test for internal password update
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/sources/kerberos/tests/test_auth.py
+++ authentik/sources/kerberos/tests/test_auth.py
@@ -1,5 +1,6 @@
 """Kerberos Source Auth tests"""
 
+from django.contrib.auth.hashers import is_password_usable
 from authentik.core.models import User
 from authentik.lib.generators import generate_id
 from authentik.sources.kerberos.auth import KerberosBackend
@@ -16,8 +17,11 @@ class TestKerberosAuth(KerberosTestCase):
             slug="kerberos",
             realm=self.realm.realm,
             sync_users=False,
+            sync_users_password=False,
+            password_login_update_internal_password=True,
         )
         self.user = User.objects.create(username=generate_id())
+        self.user.set_unusable_password()
         UserKerberosSourceConnection.objects.create(
             source=self.source, user=self.user, identifier=self.realm.user_princ
         )
@@ -41,3 +45,12 @@ class TestKerberosAuth(KerberosTestCase):
             ),
             self.user,
         )
+
+    def test_internal_password_update(self):
+        """Test internal password update"""
+        backend = KerberosBackend()
+        backend.authenticate(
+            None, username=self.realm.user_princ, password=self.realm.password("user")
+        )
+        self.user.refresh_from_db()
+        self.assertTrue(is_password_usable(self.user.password))
