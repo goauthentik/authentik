d3e06484a5cef5a39a103190438feaf56f954888
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2024-09-30 12:39:14 +0200

    move to system migrations|
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/admin/models.py
+++ authentik/admin/models.py
@@ -1,23 +1,18 @@
 """authentik admin models"""
 
-from django.db import models, transaction
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
 class VersionHistory(models.Model):
-    timestamp = models.DateTimeField(auto_now_add=True)
+    id = models.BigAutoField(primary_key=True)
+    timestamp = models.DateTimeField()
     version = models.TextField()
     build = models.TextField()
 
     class Meta:
+        managed = False
+        db_table = "authentik_version_history"
         ordering = ("-timestamp",)
         verbose_name = _("Version history")
         verbose_name_plural = _("Version history")
-
-    @classmethod
-    def create_new_entry(cls, version: str, build: str):
-        with transaction.atomic():
-            last_entry = cls.objects.all().first()
-            if last_entry and last_entry.version == version and last_entry.build == build:
-                return
-            cls.objects.create(version=version, build=build)
---
+++ lifecycle/system_migrations/version_history_update.py
@@ -0,0 +1,22 @@
+# flake8: noqa
+from lifecycle.migrate import BaseMigration
+from datetime import datetime
+
+from authentik import __version__, get_build_hash
+
+
+class Migration(BaseMigration):
+    def needs_migration(self) -> bool:
+        self.cur.execute("""
+            SELECT * FROM authentik_version_history
+                WHERE version = %s AND build = %s
+                ORDER BY "timestamp" DESC
+                LIMIT 1
+        """, (__version__, get_build_hash()))
+        return not bool(self.cur.rowcount)
+
+    def run(self):
+        self.cur.execute("""
+            INSERT INTO authentik_version_history ("timestamp", version, build)
+                VALUES (%s, %s, %s)
+        """, (datetime.now(), __version__, get_build_hash()))
