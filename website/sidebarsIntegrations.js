e31ab2b0f6d9fb59d06556c4431ba98946f59273
Masked-Kunsiquat <130736043+Masked-Kunsiquat@users.noreply.github.com>
2024-10-06 18:15:02 -0400

    Update sidebarsIntegrations.js
    added espoCRM integration

--- website/sidebarsIntegrations.js
+++ website/sidebarsIntegrations.js
@@ -27,6 +27,7 @@ module.exports = {
                     items: [
                         "services/bookstack/index",
                         "services/dokuwiki/index",
+                        "services/espocrm/index",
                         "services/hedgedoc/index",
                         "services/kimai/index",
                         "services/mastodon/index",
