061f23ceb3b894c26df71aacb25b8d462513ff83
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2024-10-22 13:35:59 +0200

    fix web import
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- web/src/admin/sources/kerberos/KerberosSourceViewPage.ts
+++ web/src/admin/sources/kerberos/KerberosSourceViewPage.ts
@@ -4,7 +4,7 @@ import "@goauthentik/admin/sources/kerberos/KerberosSourceForm";
 import { DEFAULT_CONFIG } from "@goauthentik/common/api/config";
 import { EVENT_REFRESH } from "@goauthentik/common/constants";
 import "@goauthentik/components/events/ObjectChangelog";
-import MDSourceKerberosBrowser from "@goauthentik/docs/users-sources/providers/kerberos/browser.md";
+import MDSourceKerberosBrowser from "@goauthentik/docs/users-sources/sources/protocols/kerberos/browser.md";
 import { AKElement } from "@goauthentik/elements/Base";
 import "@goauthentik/elements/CodeMirror";
 import "@goauthentik/elements/Markdown";
