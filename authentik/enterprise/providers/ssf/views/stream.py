e2a2b6734b2822213d299da976726f48c44e5033
Jens Langhammer <jens@goauthentik.io>
2024-11-07 17:29:23 +0100

    more progress
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/enterprise/providers/ssf/views/stream.py
+++ authentik/enterprise/providers/ssf/views/stream.py
@@ -1,10 +1,13 @@
-from django.http import HttpResponse
-from rest_framework.fields import CharField, ChoiceField, ListField
+from django.urls import reverse
+from rest_framework.fields import CharField, ChoiceField, ListField, SerializerMethodField
 from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.serializers import ModelSerializer
 from structlog.stdlib import get_logger
+from rest_framework.mixins import CreateModelMixin, DestroyModelMixin
 
 from authentik.core.api.utils import PassiveSerializer
-from authentik.enterprise.providers.ssf.models import DeliveryMethods, EventTypes
+from authentik.enterprise.providers.ssf.models import DeliveryMethods, EventTypes, Stream
 from authentik.enterprise.providers.ssf.views.base import SSFView
 
 LOGGER = get_logger()
@@ -15,20 +18,87 @@ class StreamDeliverySerializer(PassiveSerializer):
     endpoint_url = CharField(allow_null=True)
 
 
-class StreamSerializer(PassiveSerializer):
+class StreamSerializer(ModelSerializer):
+
     delivery = StreamDeliverySerializer()
     events_requested = ListField(
         child=ChoiceField(choices=[(x.value, x.value) for x in EventTypes])
     )
+    format = CharField()
+    aud = ListField(child=CharField())
+
+    def create(self, validated_data):
+        return super().create(
+            {
+                "delivery_method": validated_data["delivery"]["method"],
+                "endpoint_url": validated_data["delivery"].get("endpoint_url"),
+                "format": validated_data["format"],
+                "provider": validated_data["provider"],
+                "events_requested": validated_data["events_requested"],
+                "aud": validated_data["aud"],
+            }
+        )
+
+    class Meta:
+        model = Stream
+        fields = [
+            "delivery",
+            "events_requested",
+            "format",
+            "aud",
+        ]
+
+
+class StreamResponseSerializer(PassiveSerializer):
+
+    stream_id = CharField(source="pk")
+    iss = SerializerMethodField()
+    aud = ListField(child=CharField())
+    delivery = SerializerMethodField()
+
+    events_requested = ListField(child=CharField())
+    events_supported = SerializerMethodField()
+    events_delivered = ListField(child=CharField(), source="events_requested")
+
+    def get_iss(self, instance: Stream) -> str:
+        request: Request = self._context["request"]
+        if not instance.provider.application:
+            return None
+        return request.build_absolute_uri(
+            reverse(
+                "authentik_providers_ssf:configuration",
+                kwargs={
+                    "application_slug": instance.provider.application.slug,
+                    "provider": instance.provider.pk,
+                },
+            )
+        )
+
+    def get_delivery(self, instance: Stream) -> StreamDeliverySerializer:
+        return {
+            "method": instance.delivery_method,
+            "endpoint_url": instance.endpoint_url,
+        }
+
+    def get_events_supported(self, instance: Stream) -> list[str]:
+        return [x.value for x in EventTypes]
 
 
 class StreamView(SSFView):
-    # def setup(self, request: HttpRequest, *args, **kwargs) -> None:
-    #     self.application = get_object_or_404(Application, slug=self.kwargs["application_slug"])
-    #     self.provider = get_object_or_404(SSFProvider, slug=self.kwargs["provider"])
-    #     # TODO: Auth
-    #     return super().setup(request, *args, **kwargs)
+    def post(self, request: Request, *args, **kwargs) -> Response:
+        stream = StreamSerializer(data=request.data)
+        stream.is_valid(raise_exception=True)
+        instance =stream.save(provider=self.provider)
+        response = StreamResponseSerializer(instance=instance, context={
+            "request": request
+        }).data
+        print(response)
+        return Response(response, status=201)
 
-    def post(self, request: Request, *args, **kwargs) -> HttpResponse:
-        payload = StreamSerializer(request.data)
-        payload.is_valid(raise_exception=True)
+    def delete(self, request: Request, *args, **kwargs) -> Response:
+        streams = Stream.objects.filter(provider=self.provider)
+        # Technically this parameter is required by the spec...
+        if "stream_id" in request.query_params:
+            streams = streams.filter(stream_id=request.query_params["stream_id"])
+        streams.delete()
+        return Response(status=204)
