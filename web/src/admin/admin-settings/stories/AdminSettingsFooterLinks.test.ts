75708f14d4aa4e6aa0d32ef2773568eb8eaa8d95
Ken Sternberg <ken@goauthentik.io>
2024-11-12 15:53:02 -0800

    Not ready for prime time.

--- web/src/admin/admin-settings/stories/AdminSettingsFooterLinks.test.ts
+++
@@ -1,7 +0,0 @@
-import { render } from "@goauthentik/elements/tests/utils.js";
-import { $, expect } from "@wdio/globals";
-
-import { html } from "lit";
-
-import "../AdminSettingsFooterLinks.js";
-import { akExpand } from "../Expand.js";
