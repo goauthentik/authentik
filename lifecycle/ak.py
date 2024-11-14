65de4ad2c805126a6703f54164a723a7e5c9a2fb
Jens Langhammer <jens@goauthentik.io>
2024-11-14 14:59:10 +0100

    use waitstatus_to_exitcode
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- lifecycle/ak.py
+++ lifecycle/ak.py
@@ -1,6 +1,6 @@
 """Wrapper for lifecycle/ak, to be installed by poetry"""
 
-from os import WEXITSTATUS, system
+from os import system, waitstatus_to_exitcode
 from pathlib import Path
 from sys import argv, exit
 
@@ -10,7 +10,7 @@ def main():
     current_path = Path(__file__)
     args = " ".join(argv[1:])
     res = system(f"{current_path.parent}/ak {args}")  # nosec
-    exit_code = WEXITSTATUS(res)
+    exit_code = waitstatus_to_exitcode(res)
     exit(exit_code)
 
 
