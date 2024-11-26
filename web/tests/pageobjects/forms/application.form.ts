e1bb27debd9c32a3c2211c02919f992f428ac810
Ken Sternberg <ken@goauthentik.io>
2024-11-26 13:37:04 -0800

    web: update to Chromedriver 131 breaks a lot of stuff
    This annoying bit of janitorial work cleans up the failure messages and resolution bugs
    that arose when updating to the latest version of Chrome.  Keeping track of all the
    weakness and breakage while the in-browser testing teams figure out how to live with
    the ShadowDOM is just really time-consuming.

--- web/tests/pageobjects/forms/application.form.ts
+++ web/tests/pageobjects/forms/application.form.ts
@@ -4,15 +4,15 @@ import Page from "../page.js";
 
 export class ApplicationForm extends Page {
     async name() {
-        return await $('ak-text-input[name="name"]').$("input");
+        return await $('>>>ak-text-input[name="name"]').$(">>>input");
     }
 
     async uiSettings() {
-        return await $("ak-form-group").$('button[aria-label="UI Settings"]');
+        return await $(">>>ak-form-group").$('button[aria-label="UI Settings"]');
     }
 
     async launchUrl() {
-        return await $('input[name="metaLaunchUrl"]');
+        return await $('>>>input[name="metaLaunchUrl"]');
     }
 }
 
