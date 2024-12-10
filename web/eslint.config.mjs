b306dcdbe7c497fd384da3edb1f9036e7d9d570c
Ken Sternberg <ken@goauthentik.io>
2024-12-10 14:20:10 -0800

    web: disable lint check for cAsEfUnNy AtTrIbUtE nAmEs.

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -41,7 +41,7 @@ export default [
         },
         files: ["src/**"],
         rules: {
-            "lit/attribute-names": "error",
+            "lit/attribute-names": "off",
             // "lit/attribute-names": "error",
             "lit/no-private-properties": "error",
             // "lit/prefer-nothing": "warn",
