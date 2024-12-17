058f279dbd74ec6385dc95439b480851c12e8f6e
Jens Langhammer <jens@goauthentik.io>
2024-12-17 18:32:01 +0100

    add initial docs
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- website/sidebars.js
+++ website/sidebars.js
@@ -128,7 +128,10 @@ export default {
                         type: "doc",
                         id: "add-secure-apps/applications/index",
                     },
-                    items: ["add-secure-apps/applications/manage_apps"],
+                    items: [
+                        "add-secure-apps/applications/manage_apps",
+                        "add-secure-apps/applications/entitlements",
+                    ],
                 },
                 {
                     type: "category",
