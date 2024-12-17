9aba691c12a48493907e1dd0c11b7f79c610d485
Jens Langhammer <jens@goauthentik.io>
2024-12-18 00:38:20 +0100

    align
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/core/api/sources.py
+++ authentik/core/api/sources.py
@@ -159,7 +159,7 @@ class SourceViewSet(
 
 
 class UserSourceConnectionSerializer(SourceSerializer):
-    """OAuth Source Serializer"""
+    """User source connection"""
 
     source_obj = SourceSerializer(read_only=True, source="source")
 
@@ -197,9 +197,9 @@ class UserSourceConnectionViewSet(
 
 
 class GroupSourceConnectionSerializer(SourceSerializer):
-    """Group Source Connection Serializer"""
+    """Group Source Connection"""
 
-    source = SourceSerializer(read_only=True)
+    source_obj = SourceSerializer(read_only=True)
 
     class Meta:
         model = GroupSourceConnection
@@ -207,12 +207,11 @@ class GroupSourceConnectionSerializer(SourceSerializer):
             "pk",
             "group",
             "source",
+            "source_obj",
             "identifier",
             "created",
         ]
         extra_kwargs = {
-            "group": {"read_only": True},
-            "identifier": {"read_only": True},
             "created": {"read_only": True},
         }
 
