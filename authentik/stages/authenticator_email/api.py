a5de3e9431c8a6a00c58e5960e4d2c62d7c61903
Marcelo Elizeche Landó <melizeche@gmail.com>
2025-01-10 06:04:11 -0300

    stages/authenticator_email: Fix imports due changes introduced in #12598

--- authentik/stages/authenticator_email/api.py
+++ authentik/stages/authenticator_email/api.py
@@ -1,11 +1,8 @@
 """AuthenticatorEmailStage API Views"""
 
-from django_filters.rest_framework.backends import DjangoFilterBackend
 from rest_framework import mixins
-from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
-from authentik.api.authorization import OwnerFilter, OwnerPermissions
 from authentik.core.api.used_by import UsedByMixin
 from authentik.core.api.utils import ModelSerializer
 from authentik.flows.api.stages import StageSerializer
@@ -68,11 +65,10 @@ class EmailDeviceViewSet(
 
     queryset = EmailDevice.objects.all()
     serializer_class = EmailDeviceSerializer
-    permission_classes = [OwnerPermissions]
-    filter_backends = [OwnerFilter, DjangoFilterBackend, OrderingFilter, SearchFilter]
     search_fields = ["name"]
     filterset_fields = ["name"]
     ordering = ["name"]
+    owner_field = "user"
 
 
 class EmailAdminDeviceViewSet(ModelViewSet):
