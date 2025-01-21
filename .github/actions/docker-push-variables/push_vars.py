5596c6394bb1a952a3c71a5aa85f73e27adc6410
Jens Langhammer <jens@goauthentik.io>
2025-01-21 01:16:12 +0100

    get cache-to from python script
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- .github/actions/docker-push-variables/push_vars.py
+++ .github/actions/docker-push-variables/push_vars.py
@@ -70,6 +70,12 @@ def get_attest_image_names(image_with_tags: list[str]):
     return ",".join(set(image_tags))
 
 
+# Generate `cache-to` param
+cache_to = ""
+if should_push:
+    cache_to = f"type=registry,ref={get_attest_image_names(image_tags)}:buildcache,mode=max"
+
+
 with open(os.environ["GITHUB_OUTPUT"], "a+", encoding="utf-8") as _output:
     print(f"shouldPush={str(should_push).lower()}", file=_output)
     print(f"sha={sha}", file=_output)
@@ -79,3 +85,4 @@ with open(os.environ["GITHUB_OUTPUT"], "a+", encoding="utf-8") as _output:
     print(f"attestImageNames={get_attest_image_names(image_tags)}", file=_output)
     print(f"imageMainTag={image_main_tag}", file=_output)
     print(f"imageMainName={image_tags[0]}", file=_output)
+    print(f"cacheTo={cache_to}", file=_output)
