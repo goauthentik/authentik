583d6e361f05896911639ddb6e1ea728624c4c7f
Jens Langhammer <jens@goauthentik.io>
2025-01-25 00:38:14 +0100

    format
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/lib/debug.py
+++ authentik/lib/debug.py
@@ -4,6 +4,7 @@ from authentik.lib.config import CONFIG
 
 LOGGER = get_logger()
 
+
 def start_debug_server(**kwargs) -> bool:
     """Attempt to start a debugpy server in the current process.
     Returns true if the server was started successfully, otherwise false"""
@@ -12,8 +13,10 @@ def start_debug_server(**kwargs) -> bool:
     try:
         import debugpy
     except ImportError:
-        LOGGER.warning("Failed to import debugpy. debugpy is not included "
-                       "in the default release dependencies and must be installed manually")
+        LOGGER.warning(
+            "Failed to import debugpy. debugpy is not included "
+            "in the default release dependencies and must be installed manually"
+        )
         return False
 
     listen: str = CONFIG.get("listen.listen_debug_py", "127.0.0.1:9901")
