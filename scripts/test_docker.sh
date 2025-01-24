1341646b5a3c7580978bf10bbeae803d460db12c
Jens Langhammer <jens@goauthentik.io>
2025-01-25 00:24:56 +0100

    move pre-release docker test to script
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

---
+++ scripts/test_docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e -x -o pipefail
+
+export PG_PASS=$(openssl rand -base64 36 | tr -d '\n')
+export AUTHENTIK_SECRET_KEY=$(openssl rand -base64 60 | tr -d '\n')
+export AUTHENTIK_TAG=$(git rev-parse HEAD | cut -c1-15)
+export COMPOSE_PROJECT_NAME="authentik-test-${AUTHENTIK_TAG}"
+
+# Ensure buildx is installed
+docker buildx install
+# For release builds we have an empty client here as we use the NPM package
+mkdir -p ./gen-ts-api
+docker build -t ghcr.io/goauthentik/server:${AUTHENTIK_TAG} .
+docker compose up --no-start
+docker compose start postgresql redis
+docker compose run -u root server test-all
+docker compose down -v
