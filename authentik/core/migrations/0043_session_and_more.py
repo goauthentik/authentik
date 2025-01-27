1de2448ac0ddf1f38ca925111de6f9eeb9c098cf
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2025-01-27 17:48:32 +0100

    wip
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/core/migrations/0043_session_and_more.py
+++ authentik/core/migrations/0043_session_and_more.py
@@ -26,8 +26,8 @@ def migrate_redis_sessions(apps, schema_editor):
     if not hasattr(cache, "keys"):
         return
     print("\nMigration Redis sessions to database, this might take a couple of minutes...")
-    # sessions_to_create = []
-    # batch = 0
+    sessions_to_create = []
+    batch = 0
     for key, session_data in progress_bar(cache.get_many(cache.keys(f"{KEY_PREFIX}*")).items()):
         print(key, session_data)
         session_key = key.removeprefix(KEY_PREFIX)
@@ -35,21 +35,19 @@ def migrate_redis_sessions(apps, schema_editor):
         print(cache.ttl(key))
         expires = now() + timedelta(seconds=cache.ttl(key))
         print(expires)
-        # sessions_to_create.append(
-        s = Session(
+        sessions_to_create.append(
+        Session(
             session_key=session_key,
             session_data=pickle.dumps(session_data, pickle.HIGHEST_PROTOCOL),
             expires=expires,
         )
-        s.save()
-        print(s)
-        # )
-    #     batch += 1
-    #     if batch >= 500:
-    #         Session.objects.using(db_alias).bulk_create(sessions_to_create)
-    #         sessions_to_create = []
-    #         batch = 0
-    # Session.objects.using(db_alias).bulk_create(sessions_to_create)
+        )
+        batch += 1
+        if batch >= 500:
+            Session.objects.using(db_alias).bulk_create(sessions_to_create)
+            sessions_to_create = []
+            batch = 0
+    Session.objects.using(db_alias).bulk_create(sessions_to_create)
 
 
 def migrate_database_sessions(apps, schema_editor):
@@ -210,5 +208,5 @@ class Migration(migrations.Migration):
         ),
         migrations.RunPython(migrate_redis_sessions, migrations.RunPython.noop),
         migrations.RunPython(migrate_database_sessions, migrations.RunPython.noop),
-        migrations.RunPython(migrate_authenticated_sessions, migrations.RunPython.noop),
+        # migrations.RunPython(migrate_authenticated_sessions, migrations.RunPython.noop),
     ]
