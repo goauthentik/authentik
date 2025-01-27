aa4c9a6cc036cc41a3e33df49a9c7e854c93b6d3
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2025-01-27 14:02:24 +0100

    start migrations
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/enterprise/providers/rac/models.py
+++ authentik/enterprise/providers/rac/models.py
@@ -148,9 +148,6 @@ class ConnectionToken(ExpiringModel):
     endpoint = models.ForeignKey(Endpoint, on_delete=models.CASCADE)
     token = models.TextField(default=default_token_key)
     settings = models.JSONField(default=dict)
-    old_session = models.ForeignKey(
-        "authentik_core.OldAuthenticatedSession", on_delete=models.CASCADE
-    )
     session = models.ForeignKey(
         "authentik_core.AuthenticatedSession", on_delete=models.CASCADE, null=True
     )
