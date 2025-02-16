e9c944c0d584f5acb39dd9bd1560c24c0574805b
Jens L. <jens@goauthentik.io>
2025-02-16 18:06:59 +0100

    web/user: fix redirects back to user settings (#13076)
    closes #13075
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/user/user-settings/details/UserPassword.ts
+++ web/src/user/user-settings/details/UserPassword.ts
@@ -1,4 +1,5 @@
 import { AndNext } from "@goauthentik/common/api/config";
+import { globalAK } from "@goauthentik/common/global";
 import { AKElement } from "@goauthentik/elements/Base";
 
 import { msg } from "@lit/localize";
@@ -31,7 +32,7 @@ export class UserSettingsPassword extends AKElement {
             <div class="pf-c-card__body">
                 <a
                     href="${ifDefined(this.configureUrl)}${AndNext(
-                        `/if/user/#/settings;${JSON.stringify({ page: "page-details" })}`,
+                        `${globalAK().api.base}if/user/#/settings;${JSON.stringify({ page: "page-details" })}`,
                     )}"
                     class="pf-c-button pf-m-primary"
                 >
