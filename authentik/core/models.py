b5b68c1919b71915662ee321e7ccfd2939a264f9
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2025-02-21 16:55:45 +0100

    wip
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/core/models.py
+++ authentik/core/models.py
@@ -1070,7 +1070,7 @@ class AuthenticatedSession(SerializerModel):
         verbose_name_plural = _("Authenticated Sessions")
 
     def __str__(self) -> str:
-        return self.session_id
+        return f"{self.session_id}"
 
     @staticmethod
     def from_request(request: HttpRequest, user: User) -> Optional["AuthenticatedSession"]:
