eb487f9d0c5a53b95bb8522a58b19574d9d9c4bc
Jens Langhammer <jens@goauthentik.io>
2025-02-25 14:11:01 +0100

    add option to temporarily override dry run
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/lib/sync/outgoing/tasks.py
+++ authentik/lib/sync/outgoing/tasks.py
@@ -106,7 +106,9 @@ class SyncTasks:
                 return
         task.set_status(TaskStatus.SUCCESSFUL, *messages)
 
-    def sync_objects(self, object_type: str, page: int, provider_pk: int, **filter):
+    def sync_objects(
+        self, object_type: str, page: int, provider_pk: int, override_dry_run=False, **filter
+    ):
         _object_type = path_to_class(object_type)
         self.logger = get_logger().bind(
             provider_type=class_to_path(self._provider_model),
@@ -117,6 +119,10 @@ class SyncTasks:
         provider = self._provider_model.objects.filter(pk=provider_pk).first()
         if not provider:
             return messages
+        # Override dry run mode if requested, however don't save the provider
+        # so that scheduled sync tasks still run in dry_run mode
+        if override_dry_run:
+            provider.dry_run = False
         try:
             client = provider.client_for_model(_object_type)
         except TransientSyncException:
