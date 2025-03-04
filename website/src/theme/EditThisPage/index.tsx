2a76bdf9c61a85c95dd1004c6e9c9c442d63ea5f
Jens Langhammer <jens@goauthentik.io>
2025-03-04 18:25:45 +0000

    website: add a better edit this page element
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

---
+++ website/src/theme/EditThisPage/index.tsx
@@ -0,0 +1,9 @@
+import React, { type ReactNode } from "react";
+import type EditThisPageType from "@theme/EditThisPage";
+import type { WrapperProps } from "@docusaurus/types";
+
+type Props = WrapperProps<typeof EditThisPageType>;
+
+export default function EditThisPageWrapper(props: Props): ReactNode {
+    return <></>;
+}
