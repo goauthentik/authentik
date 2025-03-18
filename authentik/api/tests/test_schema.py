8bf557c677a830d8768ea3c9cc217cd170237b75
Dominic R <dominic@sdko.org>
2025-03-18 18:30:24 -0400

    fix this test

--- authentik/api/tests/test_schema.py
+++ authentik/api/tests/test_schema.py
@@ -3,6 +3,7 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from yaml import safe_load
+import json
 
 
 class TestSchemaGeneration(APITestCase):
@@ -12,8 +13,9 @@ class TestSchemaGeneration(APITestCase):
         """Test generation"""
         response = self.client.get(
             reverse("authentik_api:schema"),
+            HTTP_ACCEPT="application/json",
         )
-        self.assertTrue(safe_load(response.content.decode()))
+        self.assertTrue(json.loads(response.content.decode()))
 
     def test_browser(self):
         """Test API Browser"""
