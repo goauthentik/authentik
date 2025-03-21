b604ff4988e302ec89b28e48c4ccc093c34de628
Jens Langhammer <jens@goauthentik.io>
2025-03-21 21:53:57 +0000

    add story and fix import
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/elements/sync/SyncStatusCard.ts
+++ web/src/elements/sync/SyncStatusCard.ts
@@ -1,6 +1,7 @@
 import { EVENT_REFRESH } from "@goauthentik/common/constants";
 import { getRelativeTime } from "@goauthentik/common/utils";
 import "@goauthentik/components/ak-status-label";
+import "@goauthentik/elements/buttons/ActionButton";
 import { AKElement } from "@goauthentik/elements/Base";
 import "@goauthentik/elements/EmptyState";
 import "@goauthentik/elements/events/LogViewer";
