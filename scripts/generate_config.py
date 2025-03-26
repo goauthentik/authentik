53809e3a305558d9f08a9030182c682a9711c6b4
Marcelo Elizeche Landó <marcelo@goauthentik.io>
2025-03-26 18:53:10 -0300

    fix linting to please the ci check

--- scripts/generate_config.py
+++ scripts/generate_config.py
@@ -11,49 +11,50 @@ def generate_local_config():
     # TODO: This should be generated and validated against a schema, such as Pydantic.
 
     return {
-            "debug": True,
-            "log_level": "debug",
-            "secret_key": generate_id(),
-            "postgresql": {
-                "user": "postgres",
-            },
-            "outposts": {
-                "container_image_base": "ghcr.io/goauthentik/dev-%(type)s:gh-%(build_hash)s",
-                "disable_embedded_outpost": False,
-            },
-            "blueprints_dir": "./blueprints",
-            "cert_discovery_dir": "./certs",
-            "events": {
-                "processors": {
-                    "geoip": "tests/GeoLite2-City-Test.mmdb",
-                    "asn": "tests/GeoLite2-ASN-Test.mmdb",
-                }
-            },
-            "storage": {
-                "media": {
-                    "backend": "file",
-                    "s3": {
-                        "endpoint": "http://localhost:8020",
-                        "access_key": "accessKey1",
-                        "secret_key": "secretKey1",
-                        "bucket_name": "authentik-media",
-                        "custom_domain": "localhost:8020/authentik-media",
-                        "secure_urls": False,
-                    },
+        "debug": True,
+        "log_level": "debug",
+        "secret_key": generate_id(),
+        "postgresql": {
+            "user": "postgres",
+        },
+        "outposts": {
+            "container_image_base": "ghcr.io/goauthentik/dev-%(type)s:gh-%(build_hash)s",
+            "disable_embedded_outpost": False,
+        },
+        "blueprints_dir": "./blueprints",
+        "cert_discovery_dir": "./certs",
+        "events": {
+            "processors": {
+                "geoip": "tests/GeoLite2-City-Test.mmdb",
+                "asn": "tests/GeoLite2-ASN-Test.mmdb",
+            }
+        },
+        "storage": {
+            "media": {
+                "backend": "file",
+                "s3": {
+                    "endpoint": "http://localhost:8020",
+                    "access_key": "accessKey1",
+                    "secret_key": "secretKey1",
+                    "bucket_name": "authentik-media",
+                    "custom_domain": "localhost:8020/authentik-media",
+                    "secure_urls": False,
                 },
             },
-            "tenants": {
-                "enabled": False,
-                "api_key": generate_id(),
-            },
-        }
+        },
+        "tenants": {
+            "enabled": False,
+            "api_key": generate_id(),
+        },
+    }
+
 
 if __name__ == "__main__":
     config_file_name = "local.env.yml"
 
     with open(config_file_name, "w", encoding="utf-8") as _config:
         _config.write(
-"""
+            """
 # Local authentik configuration overrides
 #
 # https://docs.goauthentik.io/docs/install-config/configuration/
@@ -64,7 +65,8 @@ if __name__ == "__main__":
 # make gen-dev-config
 # ```
 
-""")
+"""
+        )
 
         safe_dump(
             generate_local_config(),
@@ -72,7 +74,8 @@ if __name__ == "__main__":
             default_flow_style=False,
         )
 
-    print(f"""
+    print(
+        f"""
 ---
 
 Generated configuration file: {config_file_name}
