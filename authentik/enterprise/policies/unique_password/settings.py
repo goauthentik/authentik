8f56e1c59e8faf1ce23a28d83d6108fa253b9c49
Jens Langhammer <jens@goauthentik.io>
2025-03-27 16:44:44 +0100

    minor style fixes
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/enterprise/policies/unique_password/settings.py
+++ authentik/enterprise/policies/unique_password/settings.py
@@ -11,8 +11,9 @@ CELERY_BEAT_SCHEDULE = {
         "options": {"queue": "authentik_scheduled"},
     },
     "policies_unique_password_check_purge": {
-        "task": "authentik.enterprise.policies.unique_password."
-        "tasks.check_and_purge_password_history",
+        "task": (
+            "authentik.enterprise.policies.unique_password.tasks.check_and_purge_password_history"
+        ),
         "schedule": crontab(minute=fqdn_rand("policies_unique_password_purge"), hour="*/24"),
         "options": {"queue": "authentik_scheduled"},
     },
--- authentik/enterprise/policies/unique_password/urls.py
+++ authentik/enterprise/policies/unique_password/urls.py
@@ -3,5 +3,5 @@
 from authentik.enterprise.policies.unique_password.api import UniquePasswordPolicyViewSet
 
 api_urlpatterns = [
-    ("policies/unique-password", UniquePasswordPolicyViewSet),
+    ("policies/unique_password", UniquePasswordPolicyViewSet),
 ]
