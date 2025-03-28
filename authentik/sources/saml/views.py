e33decc89840e221dd717b18aa6fe0760881e9a8
Jens L. <jens@goauthentik.io>
2025-03-28 13:15:45 +0000

    core: fix non-exploitable open redirect (#13696)
    discovered by @dominic-r
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/sources/saml/views.py
+++ authentik/sources/saml/views.py
@@ -33,6 +33,7 @@ from authentik.flows.planner import (
 )
 from authentik.flows.stage import ChallengeStageView
 from authentik.flows.views.executor import NEXT_ARG_NAME, SESSION_KEY_GET, SESSION_KEY_PLAN
+from authentik.lib.utils.urls import is_url_absolute
 from authentik.lib.views import bad_request_message
 from authentik.providers.saml.utils.encoding import nice64
 from authentik.sources.saml.exceptions import MissingSAMLResponse, UnsupportedNameIDFormat
@@ -73,6 +74,8 @@ class InitiateView(View):
         final_redirect = self.request.session.get(SESSION_KEY_GET, {}).get(
             NEXT_ARG_NAME, "authentik_core:if-user"
         )
+        if not is_url_absolute(final_redirect):
+            final_redirect = "authentik_core:if-user"
         kwargs.update(
             {
                 PLAN_CONTEXT_SSO: True,
