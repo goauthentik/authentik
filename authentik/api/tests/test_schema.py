9a2436046dcbc9fd9ee56246ed7092d6a68de2b2
Dominic R <dominic@sdko.org>
2025-03-28 23:06:32 -0400

    fix schema build + not sure of prev changew hich causes all schema changes

--- authentik/api/tests/test_schema.py
+++ authentik/api/tests/test_schema.py
@@ -2,7 +2,8 @@
 
 from django.urls import reverse
 from rest_framework.test import APITestCase
-from yaml import add_representer, safe_load
+from yaml import add_multi_representer, add_representer, safe_load
+from yaml.representer import SafeRepresenter
 
 
 def represent_type(dumper, data):
@@ -12,10 +13,18 @@ def represent_type(dumper, data):
 
 def represent_str_class(dumper, data):
     """Custom representer for str class object (not string instances)"""
+    if data is str:
+        return dumper.represent_scalar("tag:yaml.org,2002:str", "string")
+    if isinstance(data, type):
+        return dumper.represent_scalar("tag:yaml.org,2002:str", str(data))
     return dumper.represent_scalar("tag:yaml.org,2002:str", str(data))
 
 
 add_representer(type, represent_type)
+# Specifically handle the str class
+SafeRepresenter.add_representer(str, represent_str_class)
+# Special case for <class 'str'>
+add_multi_representer(type, represent_type)
 
 
 class TestSchemaGeneration(APITestCase):
