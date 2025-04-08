8b62596794ada74487fd9d4f041d67d4cf97ade1
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2025-04-08 19:13:51 +0200

    Revert "core: fix non-exploitable open redirect (#13696)"
    This reverts commit c7c75dc19599c6c2ac71791a4cd23ab3ce274999.

--- authentik/core/sources/flow_manager.py
+++ authentik/core/sources/flow_manager.py
@@ -36,7 +36,7 @@ from authentik.flows.planner import (
 )
 from authentik.flows.stage import StageView
 from authentik.flows.views.executor import NEXT_ARG_NAME, SESSION_KEY_GET, SESSION_KEY_PLAN
-from authentik.lib.utils.urls import is_url_absolute, redirect_with_qs
+from authentik.lib.utils.urls import redirect_with_qs
 from authentik.lib.views import bad_request_message
 from authentik.policies.denied import AccessDeniedResponse
 from authentik.policies.utils import delete_none_values
@@ -208,8 +208,6 @@ class SourceFlowManager:
         final_redirect = self.request.session.get(SESSION_KEY_GET, {}).get(
             NEXT_ARG_NAME, "authentik_core:if-user"
         )
-        if not is_url_absolute(final_redirect):
-            final_redirect = "authentik_core:if-user"
         flow_context.update(
             {
                 # Since we authenticate the user by their token, they have no backend set
