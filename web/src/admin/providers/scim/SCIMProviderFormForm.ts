fd719bffa491bd0f4e6c6bce6421ac282f8b98d1
Immanuel von Neumann <immanuel.neumann@cplace.com>
2025-04-09 17:33:03 +0200

    providers/scim: modify user- and group syncing behavior
    rename filtergroup to groupfilters and allow multiple values
    only sync groups which are in the scimprovider's attribute \"group_filters\"
    only sync users which are entitled to view the scimprovider's application

--- web/src/admin/providers/scim/SCIMProviderFormForm.ts
+++ web/src/admin/providers/scim/SCIMProviderFormForm.ts
@@ -12,8 +12,6 @@ import "#components/ak-number-input";
 import "#elements/utils/TimeDeltaHelp";
 import "#components/ak-text-input";
 
-import { propertyMappingsProvider, propertyMappingsSelector } from "./SCIMProviderFormHelpers.js";
-
 import { DEFAULT_CONFIG } from "#common/api/config";
 
 import {
@@ -31,6 +29,12 @@ import {
 
 import YAML from "yaml";
 
+import {
+    groupsProvider,
+    groupsSelector,
+    propertyMappingsProvider,
+    propertyMappingsSelector,
+} from "./SCIMProviderFormHelpers.js";
 import { msg } from "@lit/localize";
 import { html } from "lit";
 import { ifDefined } from "lit/directives/if-defined.js";
@@ -235,33 +239,15 @@ export function renderForm({ provider = {}, errors = {}, update }: SCIMProviderF
                 >
                 </ak-switch-input>
 
-                <ak-form-element-horizontal label=${msg("Group")} name="filterGroup">
-                    <ak-search-select
-                        .fetchObjects=${async (query?: string): Promise<Group[]> => {
-                            const args: CoreGroupsListRequest = {
-                                ordering: "name",
-                                includeUsers: false,
-                            };
-                            if (query !== undefined) {
-                                args.search = query;
-                            }
-                            const groups = await new CoreApi(DEFAULT_CONFIG).coreGroupsList(args);
-                            return groups.results;
-                        }}
-                        .renderElement=${(group: Group): string => {
-                            return group.name;
-                        }}
-                        .value=${(group: Group | undefined): string | undefined => {
-                            return group ? group.pk : undefined;
-                        }}
-                        .selected=${(group: Group): boolean => {
-                            return group.pk === provider.filterGroup;
-                        }}
-                        blankable
-                    >
-                    </ak-search-select>
+                <ak-form-element-horizontal label=${msg("Group Filter")} name="groupFilters">
+                    <ak-dual-select-dynamic-selected
+                        .provider=${groupsProvider}
+                        .selector=${groupsSelector(provider?.groupFilters, null)}
+                        available-label=${msg("Available Groups")}
+                        selected-label=${msg("Selected Groups")}
+                    ></ak-dual-select-dynamic-selected>
                     <p class="pf-c-form__helper-text">
-                        ${msg("Only sync users within the selected group.")}
+                        ${msg("Groups to be synced. If empty, all groups will be synced.")}
                     </p>
                 </ak-form-element-horizontal>
             </div>
