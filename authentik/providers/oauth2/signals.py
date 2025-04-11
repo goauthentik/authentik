c0c0e8b3fdb038cf1e7d993105f0324d1b0a0dbf
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2025-04-11 13:36:22 +0200

    wip
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/providers/oauth2/signals.py
+++ authentik/providers/oauth2/signals.py
@@ -4,6 +4,7 @@ from django.dispatch import receiver
 from django.http import HttpRequest
 
 from authentik.core.models import User
+from authentik.providers.oauth2.constants import SCOPE_OFFLINE_ACCESS
 from authentik.providers.oauth2.models import AccessToken, DeviceToken, RefreshToken
 
 
@@ -13,8 +14,15 @@ def user_logged_out_oauth_access_token(sender, request: HttpRequest, user: User,
     if not request.session or not request.session.session_key:
         return
     AccessToken.objects.filter(
-        user=user, session__session__session_key=request.session.session_key
+        user=user,
+        session__session__session_key=request.session.session_key,
     ).delete()
+    for token in RefreshToken.objects.filter(
+        user=user,
+        session__session__session_key=request.session.session_key,
+    ):
+        if SCOPE_OFFLINE_ACCESS in token.scope:
+            token.delete()
 
 
 @receiver(post_save, sender=User)
@@ -22,6 +30,6 @@ def user_deactivated(sender, instance: User, **_):
     """Remove user tokens when deactivated"""
     if instance.is_active:
         return
-    AccessToken.objects.filter(session__user=instance).delete()
-    RefreshToken.objects.filter(session__user=instance).delete()
-    DeviceToken.objects.filter(session__user=instance).delete()
+    AccessToken.objects.filter(user=instance).delete()
+    RefreshToken.objects.filter(user=instance).delete()
+    DeviceToken.objects.filter(user=instance).delete()
