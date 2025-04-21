9382fdf8cd8649ddc184ed885fff3eace3ddd820
Teffen Ellis <teffen@nirri.us>
2025-04-22 01:04:16 +0200

    web: Update build paths. Fix types.

--- web/packages/monorepo/paths.js
+++ web/packages/monorepo/paths.js
@@ -2,7 +2,7 @@ import { createRequire } from "node:module";
 import { dirname, join, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const relativeDirname = dirname(fileURLToPath(import.meta.url));
 
 /**
  * @typedef {'~authentik'} MonoRepoRoot
@@ -11,20 +11,35 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 /**
  * The root of the authentik monorepo.
  */
-export const MonoRepoRoot = /** @type {MonoRepoRoot} */ (resolve(__dirname, "..", ".."));
+// TODO: Revise when this package is moved to the monorepo's `packages/monorepo` directory.
+export const MonoRepoRoot = /** @type {MonoRepoRoot} */ (
+    resolve(relativeDirname, "..", "..", "..")
+);
 
 const require = createRequire(import.meta.url);
 
 /**
  * Resolve a package name to its location in the monorepo to the single node_modules directory.
  * @param {string} packageName
+ *
  * @returns {string} The resolved path to the package.
  * @throws {Error} If the package cannot be resolved.
  */
 export function resolvePackage(packageName) {
-    const packageJSONPath = require.resolve(join(packageName, "package.json"), {
-        paths: [MonoRepoRoot],
-    });
+    const relativePackageJSONPath = join(packageName, "package.json");
 
-    return dirname(packageJSONPath);
+    /** @type {string} */
+    let absolutePackageJSONPath;
+
+    try {
+        absolutePackageJSONPath = require.resolve(relativePackageJSONPath);
+    } catch (cause) {
+        const error = new Error(`Failed to resolve package "${packageName}"`);
+
+        error.cause = cause;
+
+        throw error;
+    }
+
+    return dirname(absolutePackageJSONPath);
 }
