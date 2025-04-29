8b6ac3c8063224f95058ddc71446f8d9c381e845
Marcelo Elizeche Landó <marcelo@goauthentik.io>
2025-04-29 14:29:41 -0400

    website/docs: Password Uniqueness Policy (#13686)
    * First draft docs for policies/unique_password
    
    * simplify documentation
    
    * fix styling
    
    * Add clarification about when this policy takes effect
    
    * change wording in how it works
    
    Co-authored-by: Dominic R <dominic@sdko.org>
    Signed-off-by: Marcelo Elizeche Landó <marce@melizeche.com>
    
    * Take the user by the hand and tell them where to go
    
    * Improve wording in Configuration options
    
    * add suggestion from PR
    
    Co-authored-by: Dominic R <dominic@sdko.org>
    Signed-off-by: Marcelo Elizeche Landó <marce@melizeche.com>
    
    * Update website/docs/customize/policies/unique_password.md
    
    Signed-off-by: Tana M Berry <tanamarieberry@yahoo.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Tana M Berry <tanamarieberry@yahoo.com>
    Signed-off-by: Marcelo Elizeche Landó <marce@melizeche.com>
    
    * fix linting and wording
    
    * Add instructions for binding
    
    * Remove conf options section, add to sidebar
    
    * Update website/docs/customize/policies/unique_password.md
    
    Signed-off-by: Tana M Berry <tanamarieberry@yahoo.com>
    
    ---------
    
    Signed-off-by: Marcelo Elizeche Landó <marce@melizeche.com>
    Signed-off-by: Tana M Berry <tanamarieberry@yahoo.com>
    Co-authored-by: Dominic R <dominic@sdko.org>
    Co-authored-by: Tana M Berry <tanamarieberry@yahoo.com>

--- website/sidebars.js
+++ website/sidebars.js
@@ -396,6 +396,7 @@ export default {
                                 "customize/policies/expression/managing_flow_context_keys",
                             ],
                         },
+                        "customize/policies/unique_password",
                     ],
                 },
                 {
