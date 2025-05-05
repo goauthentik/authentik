25b8158c306366d7252826996641c5830e37db6e
Dominic R <dominic@sdko.org>
2025-05-04 22:27:16 -0400

    Revert "init"
    This reverts commit fa10dfeb1c05e464347500b8ce91a79fe9a4831e.

--- website/sidebarsIntegrations.js
+++ website/sidebarsIntegrations.js
@@ -89,7 +89,6 @@ module.exports = {
                         "services/argocd/index",
                         "services/awx-tower/index",
                         "services/cloudflare-access/index",
-                        "services/coder/index",
                         "services/globalprotect/index",
                         "services/harbor/index",
                         "services/hashicorp-vault/index",
