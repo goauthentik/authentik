bf8cc74c6af4d5bb0d1c1ed719682311ffd874f0
Jens Langhammer <jens@goauthentik.io>
2025-05-05 14:10:41 +0200

    add some tests
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/enterprise/stages/mtls/stage.py
+++ authentik/enterprise/stages/mtls/stage.py
@@ -1,7 +1,7 @@
 from urllib.parse import unquote
 
 from cryptography.exceptions import InvalidSignature
-from cryptography.x509 import Certificate, NameOID, load_pem_x509_certificate
+from cryptography.x509 import Certificate, NameOID, ObjectIdentifier, load_pem_x509_certificate
 from django.utils.translation import gettext_lazy as _
 
 from authentik.brands.models import Brand
@@ -14,14 +14,17 @@ from authentik.enterprise.stages.mtls.models import (
     UserAttributes,
 )
 from authentik.flows.challenge import AccessDeniedChallenge
+from authentik.flows.models import FlowDesignation
 from authentik.flows.planner import PLAN_CONTEXT_PENDING_USER
 from authentik.flows.stage import ChallengeStageView
 from authentik.stages.password.stage import PLAN_CONTEXT_METHOD, PLAN_CONTEXT_METHOD_ARGS
 from authentik.stages.prompt.stage import PLAN_CONTEXT_PROMPT
 
-HEADER_PROXY_FORWARDED = "HTTP_X_FORWARDED_CLIENT_CERT"
-HEADER_NGINX_FORWARDED = "HTTP_SSL_CLIENT_CERT"
-HEADER_TRAEFIK_FORWARDED = "HTTP_X_FORWARDED_TLS_CLIENT_CERT"
+# All of these headers must only be accepted from "trusted" reverse proxies
+# See internal/web/proxy.go:39
+HEADER_PROXY_FORWARDED = "X-Forwarded-Client-Cert"
+HEADER_NGINX_FORWARDED = "SSL-Client-Cert"
+HEADER_TRAEFIK_FORWARDED = "X-Forwarded-TLS-Client-Cert"
 
 
 class MTLSStageView(ChallengeStageView):
@@ -39,7 +42,7 @@ class MTLSStageView(ChallengeStageView):
             if "Cert" not in raw_cert:
                 continue
             try:
-                cert = load_pem_x509_certificate(unquote(raw_cert["Cert"]))
+                cert = load_pem_x509_certificate(unquote(raw_cert["Cert"]).encode())
                 certs.append(cert)
             except ValueError:
                 continue
@@ -51,7 +54,7 @@ class MTLSStageView(ChallengeStageView):
         if not sslcc_raw:
             return []
         try:
-            cert = load_pem_x509_certificate(unquote(sslcc_raw))
+            cert = load_pem_x509_certificate(unquote(sslcc_raw).encode())
             return [cert]
         except ValueError:
             return []
@@ -62,13 +65,20 @@ class MTLSStageView(ChallengeStageView):
         if not ftcc_raw:
             return []
         try:
-            cert = load_pem_x509_certificate(unquote(ftcc_raw))
+            cert = load_pem_x509_certificate(unquote(ftcc_raw).encode())
             return [cert]
         except ValueError:
             return []
 
     def get_ca(self) -> CertificateKeyPair | None:
-        stage: MutualTLSStage = self.executor.current_stage
+        # We can't access `certificate_authority` on `self.executor.current_stage`, as that would
+        # load the certificate into the directly referenced foreign key, which we have to pickle
+        # as part of the flow plan, and cryptography certs can't be pickled
+        stage: MutualTLSStage = (
+            MutualTLSStage.objects.filter(pk=self.executor.current_stage.pk)
+            .select_related("certificate_authority")
+            .first()
+        )
         if stage.certificate_authority:
             return stage.certificate_authority
         brand: Brand = self.request.brand
@@ -80,7 +90,6 @@ class MTLSStageView(ChallengeStageView):
         for _cert in certs:
             try:
                 _cert.verify_directly_issued_by(ca)
-                # PolicyBuilder().store()
                 return _cert
             except (InvalidSignature, TypeError, ValueError):
                 continue
@@ -93,9 +102,9 @@ class MTLSStageView(ChallengeStageView):
             case CertAttributes.SUBJECT:
                 cert_attr = cert.subject.rfc4514_string()
             case CertAttributes.COMMON_NAME:
-                cert_attr = str(cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value)
+                cert_attr = self.get_cert_attribute(cert, NameOID.COMMON_NAME)
             case CertAttributes.EMAIL:
-                cert_attr = str(cert.subject.get_attributes_for_oid(NameOID.EMAIL_ADDRESS)[0].value)
+                cert_attr = self.get_cert_attribute(cert, NameOID.EMAIL_ADDRESS)
         match stage.user_attribute:
             case UserAttributes.USERNAME:
                 user_attr = "username"
@@ -112,7 +121,8 @@ class MTLSStageView(ChallengeStageView):
         self.executor.plan.context[PLAN_CONTEXT_METHOD_ARGS].update(
             {
                 "certificate": {
-                    "serial_number": cert.serial_number
+                    "serial_number": cert.serial_number,
+                    "subject": cert.subject.rfc4514_string(),
                     # TODO: Other attributes
                 }
             }
@@ -122,15 +132,26 @@ class MTLSStageView(ChallengeStageView):
         self.executor.plan.context.setdefault(PLAN_CONTEXT_PROMPT, {})
         self.executor.plan.context[PLAN_CONTEXT_PROMPT].update(
             {
-                "email": str(cert.subject.get_attributes_for_oid(NameOID.EMAIL_ADDRESS)[0].value),
-                "name": str(cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value),
+                "email": self.get_cert_attribute(cert, NameOID.EMAIL_ADDRESS),
+                "name": self.get_cert_attribute(cert, NameOID.COMMON_NAME),
             }
         )
+
+    def get_cert_attribute(self, cert: Certificate, oid: ObjectIdentifier) -> str | None:
+        attr = cert.subject.get_attributes_for_oid(oid)
+        if len(attr) < 1:
+            return None
+        return str(attr[0].value)
 
     def dispatch(self, request, *args, **kwargs):
         stage: MutualTLSStage = self.executor.current_stage
         certs = [*self._parse_cert_xfcc(), *self._parse_cert_nginx(), *self._parse_cert_traefik()]
-        if stage.mode != TLSMode.OPTIONAL and len(certs) < 1:
+        ca = self.get_ca()
+        if not ca and stage.mode != TLSMode.OPTIONAL:
+            self.logger.warning("No Certificate authority found")
+            return super().dispatch(request, *args, **kwargs)
+        cert = self.validate_cert(ca.certificate, certs)
+        if len(certs) < 1 and stage.mode == TLSMode.REQUIRED:
             self.logger.warning("Client certificate required but no certificates given")
             return super().dispatch(
                 request,
@@ -138,18 +159,13 @@ class MTLSStageView(ChallengeStageView):
                 error_message=_("Certificate required but no certificate was given."),
                 **kwargs,
             )
-        ca = self.get_ca()
-        if not ca and stage.mode != TLSMode.OPTIONAL:
-            self.logger.warning("No Certificate authority found")
-            return super().dispatch(request, *args, **kwargs)
-        cert = self.validate_cert(ca, certs)
         if not cert and stage.mode == TLSMode.OPTIONAL:
             self.logger.info("No certificate given, continuing")
             return self.executor.stage_ok()
         existing_user = self.check_if_user(cert)
-        if existing_user and stage.mode in [TLSMode.REQUIRED_ANY, TLSMode.REQUIRED_AUTH]:
+        if existing_user and self.executor.flow.designation == FlowDesignation.AUTHENTICATION:
             self.auth_user(existing_user, cert)
-        elif not existing_user and stage.mode in [TLSMode.REQUIRED_ANY, TLSMode.REQUIRED_ENROLL]:
+        elif not existing_user and self.executor.flow.designation == FlowDesignation.ENROLLMENT:
             self.enroll_prepare_user(cert)
         else:
             self.logger.warning("Invalid configuration")
