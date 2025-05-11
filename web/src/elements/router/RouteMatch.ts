f11ba94603eb487f0c42a952b62463b740489e96
Jens L. <jens@goauthentik.io>
2025-05-11 02:40:31 +0200

    root: improve sentry distributed tracing (#14468)
    * core: include all sentry headers
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * remove spotlight patch we dont need anymore
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * always trace in debug
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * init sentry earlier
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * re-add light interface
    
    https://github.com/goauthentik/authentik/pull/14331
    
    removes 2 unneeded API calls
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * sentry integrated router
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * use new Sentry middleware to propagate headers
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * fix missing baggage
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * cleanup logs
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * use sanitized URLs for logging/tracing
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    ---------
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/elements/router/RouteMatch.ts
+++ web/src/elements/router/RouteMatch.ts
@@ -6,19 +6,35 @@ import { TemplateResult } from "lit";
 export class RouteMatch {
     route: Route;
     arguments: { [key: string]: string };
-    fullUrl?: string;
+    fullURL: string;
 
-    constructor(route: Route) {
+    constructor(route: Route, fullUrl: string) {
         this.route = route;
         this.arguments = {};
+        this.fullURL = fullUrl;
     }
 
     render(): TemplateResult {
         return this.route.render(this.arguments);
     }
 
+    /**
+     * Convert the matched Route's URL regex to a sanitized, readable URL by replacing
+     * all regex values with placeholders according to the name of their regex group.
+     *
+     * @returns The sanitized URL for logging/tracing.
+     */
+    sanitizedURL() {
+        let cleanedURL = this.fullURL;
+        for (const match of Object.keys(this.arguments)) {
+            const value = this.arguments[match];
+            cleanedURL = cleanedURL?.replace(value, `:${match}`);
+        }
+        return cleanedURL;
+    }
+
     toString(): string {
-        return `<RouteMatch url=${this.fullUrl} route=${this.route} arguments=${JSON.stringify(
+        return `<RouteMatch url=${this.sanitizedURL()} route=${this.route} arguments=${JSON.stringify(
             this.arguments,
         )}>`;
     }
