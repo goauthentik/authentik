4dcd481010d9f5242f44f616b673dc15d543e65e
Simonyi Gergő <28359278+gergosimonyi@users.noreply.github.com>
2025-05-14 15:02:29 +0200

    core: remove `OldAuthenticatedSession` content type (#14507)
    * core: remove `OldAuthenticatedSession` content type
    
    This was left out from https://github.com/goauthentik/authentik/pull/9736
    
    * remove stale content types in `repair_permissions`
    
    Co-authored-by: Jens Langhammer <jens@goauthentik.io>
    
    * run `remove_stale_contenttypes` for each tenant
    
    ---------
    
    Co-authored-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/core/management/commands/repair_permissions.py
+++ authentik/core/management/commands/repair_permissions.py
@@ -2,6 +2,7 @@
 
 from django.apps import apps
 from django.contrib.auth.management import create_permissions
+from django.core.management import call_command
 from django.core.management.base import BaseCommand, no_translations
 from guardian.management import create_anonymous_user
 
@@ -16,6 +17,10 @@ class Command(BaseCommand):
         """Check permissions for all apps"""
         for tenant in Tenant.objects.filter(ready=True):
             with tenant:
+                # See https://code.djangoproject.com/ticket/28417
+                # Remove potential lingering old permissions
+                call_command("remove_stale_contenttypes", "--no-input")
+
                 for app in apps.get_app_configs():
                     self.stdout.write(f"Checking app {app.name} ({app.label})\n")
                     create_permissions(app, verbosity=0)
