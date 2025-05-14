ad57c66a32a02352c66eca34282ef2e3cfe96abe
Jens Langhammer <jens@goauthentik.io>
2025-05-14 13:26:12 +0200

    better log
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- internal/outpost/radius/eap/packet.go
+++ internal/outpost/radius/eap/packet.go
@@ -3,8 +3,9 @@ package eap
 import (
 	"encoding/binary"
 	"errors"
-	"fmt"
 
+	"github.com/sirupsen/logrus"
+	"goauthentik.io/internal/outpost/radius/eap/debug"
 	"goauthentik.io/internal/outpost/radius/eap/tls"
 )
 
@@ -62,7 +63,7 @@ func Decode(raw []byte) (*Packet, error) {
 	}
 	packet.Payload = emptyPayload(packet.msgType)
 	packet.rawPayload = raw[5:]
-	fmt.Printf("decode raw '% x\n", raw[5:])
+	logrus.WithField("raw", debug.FormatBytes(raw[5:])).Debug("EAP decode raw")
 	err := packet.Payload.Decode(raw[5:])
 	if err != nil {
 		return nil, err
