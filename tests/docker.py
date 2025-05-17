1f54fb4e14976342115089fbc4e85d6481b844ae
Jens Langhammer <jens@goauthentik.io>
2025-05-17 14:57:28 +0200

    fix missing super calls
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- tests/docker.py
+++ tests/docker.py
@@ -38,6 +38,7 @@ class DockerTestCase(TestCase):
     def setUp(self) -> None:
         self.__client = from_env()
         self.__network = self.docker_client.networks.create(name=f"authentik-test-{generate_id()}")
+        super().setUp()
 
     @property
     def docker_client(self) -> DockerClient:
@@ -119,3 +120,4 @@ class DockerTestCase(TestCase):
             except DockerException:
                 pass
         self.__network.remove()
+        super().tearDown()
