67a176848d812de6cf6ff57d9ceda0ab708a78ce
plusls <plusls@qq.com>
2025-05-18 20:40:33 +0800

    GetHost support "X-Forwarded-Port"
    Signed-off-by: plusls <plusls@qq.com>

--- internal/utils/web/host.go
+++ internal/utils/web/host.go
@@ -5,11 +5,18 @@ import (
 )
 
 var xForwardedHost = http.CanonicalHeaderKey("X-Forwarded-Host")
+var xForwardedPort = http.CanonicalHeaderKey("X-Forwarded-Port")
 
 func GetHost(req *http.Request) string {
 	host := req.Host
-	if req.Header.Get(xForwardedHost) != "" {
-		host = req.Header.Get(xForwardedHost)
+	xff_host := req.Header.Get(xForwardedHost)
+	if xff_host != "" {
+		xff_port := req.Header.Get(xForwardedPort)
+        if xff_port != "" {
+			host = xff_host + ":" + xff_port
+		} else {
+			host = xff_host
+		}
 	}
 	return host
 }
