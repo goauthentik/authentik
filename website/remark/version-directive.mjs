64ddc1c5b64cba9e241f7f61799b2ab434e905a8
Teffen Ellis <teffen@goauthentik.io>
2025-05-21 20:58:29 +0200

    docs: Lint.

--- website/remark/version-directive.mjs
+++ website/remark/version-directive.mjs
@@ -26,8 +26,8 @@ function remarkVersionDirective() {
     /**
      * @param {Root} tree The MDAST tree to transform.
      */
-    return function (tree) {
-        visit(tree, "textDirective", function (node) {
+    return (tree) => {
+        visit(tree, "textDirective", (node) => {
             if (node.name !== "ak-version") return SKIP;
 
             const firstChild = node.children[0];
