c8a036546e30b976a7814d200dd9c9e10a218cc3
Ken Sternberg <ken@goauthentik.io>
2025-05-22 08:50:58 -0700

    web/standards: use attribute naming scheme for attributes
    ## What
    
    Renamed the 'inputHint' attribute to 'input-hint', because it is an attribute, not a property.
    Properties are camelCased, but attributes are kebab-cased.
    
    Updated all instances where this appears with the usual magic:
    
    ```
    $ perl -pi.bak -e 's/inputHint="code"/input-hint="code"/' $(rg -l 'inputHint="code"')
    ```
    
    This fix is in preparation for both the Patternfly 5 project and the Schema-Driven Forms project.

--- web/src/components/HorizontalLightComponent.ts
+++ web/src/components/HorizontalLightComponent.ts
@@ -18,25 +18,25 @@ export class HorizontalLightComponent<T> extends AKElement {
         return this;
     }
 
-    @property({ type: String })
+    @property({ type: String, reflect: true })
     name!: string;
 
-    @property({ type: String })
+    @property({ type: String, reflect: true })
     label = "";
 
-    @property({ type: Boolean })
+    @property({ type: Boolean, reflect: true })
     required = false;
 
-    @property({ type: String })
+    @property({ type: String, reflect: true })
     help = "";
 
     @property({ type: Object })
     bighelp?: TemplateResult | TemplateResult[];
 
-    @property({ type: Boolean })
+    @property({ type: Boolean, reflect: true })
     hidden = false;
 
-    @property({ type: Boolean })
+    @property({ type: Boolean, reflect: true })
     invalid = false;
 
     @property({ attribute: false })
@@ -45,7 +45,7 @@ export class HorizontalLightComponent<T> extends AKElement {
     @property({ attribute: false })
     value?: T;
 
-    @property({ type: String })
+    @property({ type: String, reflect: true, attribute: "input-hint" })
     inputHint = "";
 
     renderControl() {
