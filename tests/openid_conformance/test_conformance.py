59bf52e2ff9964babf547f63bd7e487e557fd543
Jens Langhammer <jens@goauthentik.io>
2025-05-29 23:26:34 +0200

    Revert "re-add implicit and fix?"
    This reverts commit 6a4d15fc22cf4b27ffa428be9ecc9a0e778961c6.

--- tests/openid_conformance/test_conformance.py
+++ tests/openid_conformance/test_conformance.py
@@ -3,7 +3,3 @@ from tests.openid_conformance.generator import generate
 
 class TestConformanceOIDCCBasicCertification(generate("oidcc-basic-certification-test-plan")):
     pass
-
-
-class TestConformanceOIDCCImplicitCertification(generate("oidcc-implicit-certification-test-plan")):
-    pass
