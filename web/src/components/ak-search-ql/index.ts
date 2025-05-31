30c76685bcd78d5fa80ece2c6d35eecdf9d02f1d
Jens Langhammer <jens@goauthentik.io>
2025-05-31 22:39:06 +0200

    first attempt at making it fit our UI
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/components/ak-search-ql/index.ts
+++ web/src/components/ak-search-ql/index.ts
@@ -1,18 +1,27 @@
 import { AKElement } from "@goauthentik/elements/Base";
+import "@goauthentik/elements/buttons/Dropdown";
 import { PaginatedResponse } from "@goauthentik/elements/table/Table";
 import DjangoQL from "@mrmarble/djangoql-completion";
 
 import { msg } from "@lit/localize";
-import { CSSResult, TemplateResult, css, html } from "lit";
-import { customElement, property, query } from "lit/decorators.js";
+import { CSSResult, TemplateResult, css, html, nothing } from "lit";
+import { customElement, property, query, state } from "lit/decorators.js";
 import { ifDefined } from "lit/directives/if-defined.js";
 
-import Completion from "@mrmarble/djangoql-completion/dist/index.css";
-import PFButton from "@patternfly/patternfly/components/Button/button.css";
 import PFFormControl from "@patternfly/patternfly/components/FormControl/form-control.css";
-import PFInputGroup from "@patternfly/patternfly/components/InputGroup/input-group.css";
-import PFToolbar from "@patternfly/patternfly/components/Toolbar/toolbar.css";
+import PFSearchInput from "@patternfly/patternfly/components/SearchInput/search-input.css";
 import PFBase from "@patternfly/patternfly/patternfly-base.css";
+
+export class QL extends DjangoQL {
+    createCompletionElement() {
+        this.completionEnabled = !!this.options.completionEnabled;
+        return;
+    }
+    logError(message: string): void {
+        console.warn(`authentik/ql: ${message}`);
+    }
+    textareaResize() {}
+}
 
 @customElement("ak-search-ql")
 export class QLSearch extends AKElement {
@@ -22,45 +31,26 @@ export class QLSearch extends AKElement {
     @query("[name=search]")
     searchElement?: HTMLTextAreaElement;
 
-    ql?: DjangoQL;
-
-    set apiResponse(value: PaginatedResponse<unknown> | undefined) {
-        if (!value || !value.autocomplete) {
-            return;
-        }
-        if (this.ql) {
-            this.ql.loadIntrospections(value.autocomplete);
-            return;
-        }
-        if (!this.searchElement) {
-            return;
-        }
-        this.ql = new DjangoQL({
-            introspections: value.autocomplete,
-            selector: this.searchElement,
-            autoResize: false,
-            onSubmit: (value: string) => {
-                if (!this.onSearch) return;
-                this.onSearch(value);
-            },
-        });
-        // document.adoptedStyleSheets = [
-        //     ...document.adoptedStyleSheets,
-        //     Completion,
-        // ];
-    }
+    @state()
+    menuOpen = false;
 
     @property()
     onSearch?: (value: string) => void;
 
+    ql?: QL;
+
+    set apiResponse(value: PaginatedResponse<unknown> | undefined) {
+        if (!value || !value.autocomplete || !this.ql) {
+            return;
+        }
+        this.ql.loadIntrospections(value.autocomplete);
+    }
+
     static get styles(): CSSResult[] {
         return [
             PFBase,
-            PFButton,
-            PFToolbar,
-            PFInputGroup,
             PFFormControl,
-            Completion,
+            PFSearchInput,
             css`
                 ::-webkit-search-cancel-button {
                     display: none;
@@ -72,15 +62,83 @@ export class QLSearch extends AKElement {
         ];
     }
 
+    firstUpdated() {
+        if (!this.searchElement) {
+            return;
+        }
+        this.ql = new QL({
+            completionEnabled: true,
+            introspections: {
+                current_model: "",
+                models: {},
+            },
+            selector: this.searchElement,
+            autoResize: false,
+            onSubmit: (value: string) => {
+                if (!this.onSearch) return;
+                this.onSearch(value);
+            },
+        });
+    }
+
+    refreshCompletions() {
+        if (!this.ql) {
+            return;
+        }
+        this.ql.generateSuggestions();
+        if (this.ql.suggestions.length < 1 || this.ql.loading) {
+            this.menuOpen = false;
+            return;
+        }
+        this.menuOpen = true;
+        this.requestUpdate();
+    }
+
+    renderMenu() {
+        if (!this.menuOpen || !this.ql) {
+            return nothing;
+        }
+        return html`
+            <div class="pf-c-search-input__menu">
+                <ul class="pf-c-search-input__menu-list">
+                    ${this.ql.suggestions.map((suggestion, idx) => {
+                        return html`<li class="pf-c-search-input__menu-list-item">
+                            <button
+                                class="pf-c-search-input__menu-item"
+                                type="button"
+                                @click=${() => {
+                                    this.ql?.selectCompletion(idx);
+                                    this.refreshCompletions();
+                                }}
+                            >
+                                <span class="pf-c-search-input__menu-item-text"
+                                    >${suggestion.text}</span
+                                >
+                            </button>
+                        </li>`;
+                    })}
+                </ul>
+            </div>
+        `;
+    }
+
     render(): TemplateResult {
-        return html`<textarea
-            class="pf-c-form-control ql"
-            name="search"
-            placeholder=${msg("Search...")}
-            spellcheck="false"
-        >
+        return html`<div class="pf-c-search-input">
+            <div class="pf-c-search-input__bar">
+                <span class="pf-c-search-input__text">
+                    <textarea
+                        class="pf-c-form-control ql"
+                        name="search"
+                        placeholder=${msg("Search...")}
+                        spellcheck="false"
+                        @input=${(ev: InputEvent) => this.refreshCompletions()}
+                    >
 ${ifDefined(this.value)}</textarea
-        >`;
+                    >
+                </span>
+            </div>
+            ${this.renderMenu()}
+        </div>`;
     }
 }
 
