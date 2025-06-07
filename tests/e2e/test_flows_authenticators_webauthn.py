f4df2781dbd6041115071bd4c6212afa716e559f
Jens Langhammer <jens@goauthentik.io>
2025-06-07 09:11:07 +0200

    add auth and sfe tests
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- tests/e2e/test_flows_authenticators_webauthn.py
+++ tests/e2e/test_flows_authenticators_webauthn.py
@@ -1,7 +1,5 @@
 """test flow with WebAuthn Stage"""
 
-from time import sleep
-
 from selenium.webdriver.common.virtual_authenticator import (
     Protocol,
     Transport,
@@ -13,6 +11,7 @@ from authentik.stages.authenticator_webauthn.models import (
     AuthenticatorWebAuthnStage,
     WebAuthnDevice,
 )
+from tests.e2e.test_flows_login_sfe import login_sfe
 from tests.e2e.utils import SeleniumTestCase, retry
 
 
@@ -21,14 +20,7 @@ class TestFlowsAuthenticatorWebAuthn(SeleniumTestCase):
 
     host = "localhost"
 
-    @retry()
-    @apply_blueprint(
-        "default/flow-default-authentication-flow.yaml",
-        "default/flow-default-invalidation-flow.yaml",
-    )
-    @apply_blueprint("default/flow-default-authenticator-webauthn-setup.yaml")
-    def test_webauthn_setup(self):
-        """Test WebAuthn setup"""
+    def register(self):
         options = VirtualAuthenticatorOptions(
             protocol=Protocol.CTAP2,
             transport=Transport.INTERNAL,
@@ -52,6 +44,54 @@ class TestFlowsAuthenticatorWebAuthn(SeleniumTestCase):
         )
 
         self.wait_for_url(self.if_user_url("/library"))
-        sleep(1)
-
         self.assertTrue(WebAuthnDevice.objects.filter(user=self.user, confirmed=True).exists())
+
+    @retry()
+    @apply_blueprint(
+        "default/flow-default-authentication-flow.yaml",
+        "default/flow-default-invalidation-flow.yaml",
+    )
+    @apply_blueprint("default/flow-default-authenticator-webauthn-setup.yaml")
+    def test_webauthn_setup(self):
+        """Test WebAuthn setup"""
+        self.register()
+
+    @retry()
+    @apply_blueprint(
+        "default/flow-default-authentication-flow.yaml",
+        "default/flow-default-invalidation-flow.yaml",
+    )
+    @apply_blueprint("default/flow-default-authenticator-webauthn-setup.yaml")
+    def test_webauthn_authenticate(self):
+        """Test WebAuthn authentication"""
+        self.register()
+        self.driver.delete_all_cookies()
+
+        self.driver.get(self.url("authentik_core:if-flow", flow_slug="default-authentication-flow"))
+        self.login()
+
+        self.wait_for_url(self.if_user_url("/library"))
+
+        self.assert_user(self.user)
+
+    @retry()
+    @apply_blueprint(
+        "default/flow-default-authentication-flow.yaml",
+        "default/flow-default-invalidation-flow.yaml",
+    )
+    @apply_blueprint("default/flow-default-authenticator-webauthn-setup.yaml")
+    def test_webauthn_authenticate_sfe(self):
+        """Test WebAuthn authentication (SFE)"""
+        self.register()
+        self.driver.delete_all_cookies()
+
+        self.driver.get(
+            self.url(
+                "authentik_core:if-flow",
+                flow_slug="default-authentication-flow",
+                query={"sfe": True},
+            )
+        )
+        login_sfe(self.driver, self.user)
+        self.wait_for_url(self.if_user_url("/library"))
+        self.assert_user(self.user)
