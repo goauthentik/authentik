69bedcb67dd3363f80e35fae8b653f7d12c401c4
Jens Langhammer <jens@goauthentik.io>
2025-06-17 01:36:24 +0200

    events: add option to send notifications to event user
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/events/tasks.py
+++ authentik/events/tasks.py
@@ -75,7 +75,7 @@ def event_trigger_handler(event_uuid: str, trigger_name: str):
     LOGGER.debug("e(trigger): event trigger matched", trigger=trigger)
     # Create the notification objects
     for transport in trigger.transports.all():
-        for user in trigger.group.users.all():
+        for user in trigger.destination_users(event):
             LOGGER.debug("created notification")
             notification_transport.apply_async(
                 args=[
