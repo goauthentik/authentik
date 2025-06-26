69e5b1dfbe54f17ed2b59c5ef32787a41f1c81a1
Teffen Ellis <teffen@goauthentik.io>
2025-06-26 18:16:59 +0200

    web: Clean ambiguous imports.

--- web/src/elements/ak-dual-select/stories/sb-host-provider.ts
+++ web/src/elements/ak-dual-select/stories/sb-host-provider.ts
@@ -1,7 +1,7 @@
 import { LitElement, html } from "lit";
 import { customElement } from "lit/decorators.js";
 
-import { globalVariables } from "../components/styles.css";
+import { globalVariables } from "../components/styles.js";
 
 /**
  * @element sb-dual-select-host-provider
