51e645c26ae717e872eaa25b3d49f1a248626c9e
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2025-06-30 04:09:53 +0200

    Revert "fix most tenant tests"
    This reverts commit d61995d0e2c698c9c9dce46d5c7d5c419fe71fc4.

--- packages/django-dramatiq-postgres/django_dramatiq_postgres/broker.py
+++ packages/django-dramatiq-postgres/django_dramatiq_postgres/broker.py
@@ -5,9 +5,12 @@ from queue import Empty, Queue
 from random import randint
 from typing import Any
 
+import tenacity
 from django.core.exceptions import ImproperlyConfigured
 from django.db import (
     DEFAULT_DB_ALIAS,
+    DatabaseError,
+    InterfaceError,
     OperationalError,
     connections,
     transaction,
@@ -27,6 +30,7 @@ from dramatiq.middleware import (
 )
 from pglock.core import _cast_lock_id
 from psycopg import Notify, sql
+from psycopg.errors import AdminShutdown
 
 from django_dramatiq_postgres.conf import Conf
 from django_dramatiq_postgres.models import CHANNEL_PREFIX, ChannelIdentifier, TaskBase, TaskState
@@ -114,7 +118,21 @@ class PostgresBroker(Broker):
             "message": message.encode(),
         }
 
-    @raise_connection_error
+    @tenacity.retry(
+        retry=tenacity.retry_if_exception_type(
+            (
+                AdminShutdown,
+                InterfaceError,
+                DatabaseError,
+                ConnectionError,
+                OperationalError,
+            )
+        ),
+        reraise=True,
+        wait=tenacity.wait_random_exponential(multiplier=1, max=30),
+        stop=tenacity.stop_after_attempt(10),
+        before_sleep=tenacity.before_sleep_log(logger, logging.INFO, exc_info=True),
+    )
     def enqueue(self, message: Message, *, delay: int | None = None) -> Message:
         canonical_queue_name = message.queue_name
         queue_name = canonical_queue_name
