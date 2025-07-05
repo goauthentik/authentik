963f62585ebf9ac4ebb47eb5e79bf5176f41413e
Jens Langhammer <jens@goauthentik.io>
2025-07-05 18:29:58 +0200

    cleanup
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/flow/stages/authenticator_validate/AuthenticatorValidateStage.stories.ts
+++ web/src/flow/stages/authenticator_validate/AuthenticatorValidateStage.stories.ts
@@ -30,107 +30,90 @@ const webAuthNChallenge = {
     lastUsed: null,
 };
 
-export const MultipleDeviceChallenge: StoryObj = {
-    render: ({ theme, challenge }) => {
-        return html`<ak-storybook-interface-flow theme=${theme}>
-            <ak-stage-authenticator-validate
-                .challenge=${challenge}
-            ></ak-stage-authenticator-validate>
-        </ak-storybook-interface-flow>`;
-    },
-    args: {
-        theme: "automatic",
-        challenge: {
-            pendingUser: "foo",
-            pendingUserAvatar: "https://picsum.photos/64",
-            flowInfo: {
-                title: "<ak-stage-authenticator-validate>",
-                layout: ContextualFlowInfoLayoutEnum.Stacked,
-                cancelUrl: "",
-            },
-            deviceChallenges: [
-                {
-                    deviceClass: DeviceClassesEnum.Duo,
-                    deviceUid: "",
-                    challenge: {},
-                    lastUsed: null,
+function authenticatorValidateFactory(challenge: AuthenticatorValidationChallenge): StoryObj {
+    return {
+        render: ({ theme, challenge }) => {
+            return html`<ak-storybook-interface-flow theme=${theme}>
+                <ak-stage-authenticator-validate
+                    .challenge=${challenge}
+                ></ak-stage-authenticator-validate>
+            </ak-storybook-interface-flow>`;
+        },
+        args: {
+            theme: "automatic",
+            challenge: challenge,
+        },
+        argTypes: {
+            theme: {
+                options: [UiThemeEnum.Automatic, UiThemeEnum.Light, UiThemeEnum.Dark],
+                control: {
+                    type: "select",
                 },
-                {
-                    deviceClass: DeviceClassesEnum.Webauthn,
-                    ...webAuthNChallenge,
-                },
-                {
-                    deviceClass: DeviceClassesEnum.Totp,
-                    deviceUid: "",
-                    challenge: {},
-                    lastUsed: null,
-                },
-                {
-                    deviceClass: DeviceClassesEnum.Static,
-                    deviceUid: "",
-                    challenge: {},
-                    lastUsed: null,
-                },
-                {
-                    deviceClass: DeviceClassesEnum.Sms,
-                    deviceUid: "",
-                    challenge: {},
-                    lastUsed: null,
-                },
-                {
-                    deviceClass: DeviceClassesEnum.Email,
-                    deviceUid: "",
-                    challenge: {},
-                    lastUsed: null,
-                },
-            ],
-            configurationStages: [],
-        } as AuthenticatorValidationChallenge,
-    },
-    argTypes: {
-        theme: {
-            options: [UiThemeEnum.Automatic, UiThemeEnum.Light, UiThemeEnum.Dark],
-            control: {
-                type: "select",
             },
         },
-    },
-};
+    };
+}
 
-export const WebAuthnDeviceChallenge: StoryObj = {
-    render: ({ theme, challenge }) => {
-        return html`<ak-storybook-interface-flow theme=${theme}>
-            <ak-stage-authenticator-validate
-                .challenge=${challenge}
-            ></ak-stage-authenticator-validate>
-        </ak-storybook-interface-flow>`;
+export const MultipleDeviceChallenge = authenticatorValidateFactory({
+    pendingUser: "foo",
+    pendingUserAvatar: "https://picsum.photos/64",
+    flowInfo: {
+        title: "<ak-stage-authenticator-validate>",
+        layout: ContextualFlowInfoLayoutEnum.Stacked,
+        cancelUrl: "",
     },
-    args: {
-        theme: "automatic",
-        challenge: {
-            pendingUser: "foo",
-            pendingUserAvatar: "https://picsum.photos/64",
-            errorMessage: "This is an error message",
-            flowInfo: {
-                title: "<ak-stage-authenticator-validate>",
-                layout: ContextualFlowInfoLayoutEnum.Stacked,
-                cancelUrl: "",
-            },
-            deviceChallenges: [
-                {
-                    deviceClass: DeviceClassesEnum.Webauthn,
-                    ...webAuthNChallenge,
-                },
-            ],
-            configurationStages: [],
-        } as AuthenticatorValidationChallenge,
-    },
-    argTypes: {
-        theme: {
-            options: [UiThemeEnum.Automatic, UiThemeEnum.Light, UiThemeEnum.Dark],
-            control: {
-                type: "select",
-            },
+    deviceChallenges: [
+        {
+            deviceClass: DeviceClassesEnum.Duo,
+            deviceUid: "",
+            challenge: {},
+            lastUsed: null,
         },
+        {
+            deviceClass: DeviceClassesEnum.Webauthn,
+            ...webAuthNChallenge,
+        },
+        {
+            deviceClass: DeviceClassesEnum.Totp,
+            deviceUid: "",
+            challenge: {},
+            lastUsed: null,
+        },
+        {
+            deviceClass: DeviceClassesEnum.Static,
+            deviceUid: "",
+            challenge: {},
+            lastUsed: null,
+        },
+        {
+            deviceClass: DeviceClassesEnum.Sms,
+            deviceUid: "",
+            challenge: {},
+            lastUsed: null,
+        },
+        {
+            deviceClass: DeviceClassesEnum.Email,
+            deviceUid: "",
+            challenge: {},
+            lastUsed: null,
+        },
+    ],
+    configurationStages: [],
+});
+
+export const WebAuthnDeviceChallenge = authenticatorValidateFactory({
+    pendingUser: "foo",
+    pendingUserAvatar: "https://picsum.photos/64",
+    flowInfo: {
+        title: "<ak-stage-authenticator-validate>",
+        layout: ContextualFlowInfoLayoutEnum.Stacked,
+        cancelUrl: "",
     },
-};
+    deviceChallenges: [
+        {
+            deviceClass: DeviceClassesEnum.Webauthn,
+            ...webAuthNChallenge,
+        },
+    ],
+    configurationStages: [],
+});
