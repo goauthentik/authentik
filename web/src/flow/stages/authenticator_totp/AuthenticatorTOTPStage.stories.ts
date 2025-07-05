3aa67513db5ce0f9383caca7c99a4c507574dd66
Jens Langhammer <jens@goauthentik.io>
2025-07-05 18:10:04 +0200

    fix frame stage
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/flow/stages/authenticator_totp/AuthenticatorTOTPStage.stories.ts
+++ web/src/flow/stages/authenticator_totp/AuthenticatorTOTPStage.stories.ts
@@ -24,7 +24,8 @@ export const Challenge: StoryObj = {
         challenge: {
             pendingUser: "foo",
             pendingUserAvatar: "https://picsum.photos/64",
-            configUrl: "",
+            configUrl:
+                "otpauth%3A%2F%2Ftotp%2Fauthentik%3Afoo%3Fsecret%3Dqwerqewrqewrqewrqewr%26algorithm%3DSHA1%26digits%3D6%26period%3D30%26issuer%3Dauthentik%0A",
             flowInfo: {
                 title: "Flow title",
             },
