b8e7aeb098ac1e8b926cb8c9b9fe18b0da97689a
Teffen Ellis <teffen@sister.software>
2025-07-09 20:00:02 +0200

    website: Prep for path.

--- website/docs/sidebar.mjs
+++ website/docs/sidebar.mjs
@@ -3,8 +3,8 @@
  *
  * @import { SidebarItemConfig } from "@docusaurus/plugin-content-docs-types"
  */
-import apiReference from "../docs/developer-docs/api/reference/sidebar";
 import { generateVersionDropdown } from "../src/utils.js";
+import apiReference from "./developer-docs/api/reference/sidebar.ts";
 
 /**
  * @type {SidebarItemConfig[]}
