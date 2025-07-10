3fdc4d669bda8d921e5b148072629a051e3e941d
Jens Langhammer <jens@goauthentik.io>
2025-07-10 22:24:25 +0200

    more patcher tests
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/sources/scim/tests/test_patcher.py
+++ authentik/sources/scim/tests/test_patcher.py
@@ -57,6 +57,48 @@ class TestSCIMPatcher(APITestCase):
             },
         )
 
+    def test_add_no_path(self):
+        """Test add patch with no path set"""
+        req = {
+            "schemas": ["urn:ietf:params:scim:api:messages:2.0:PatchOp"],
+            "Operations": [
+                {"op": "Add", "value": {"externalId": "aqwer"}},
+            ],
+        }
+        user = create_test_user()
+        source = SCIMSource.objects.create(slug=generate_id())
+        connection = SCIMSourceUser.objects.create(
+            user=user,
+            id=generate_id(),
+            source=source,
+            attributes={
+                "meta": {"resourceType": "User"},
+                "active": True,
+                "schemas": [
+                    "urn:ietf:params:scim:schemas:core:2.0:User",
+                    "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User",
+                ],
+                "userName": "test@t.goauthentik.io",
+                "displayName": "Test MS",
+            },
+        )
+        patcher = SCIMPatcher(connection, req["Operations"])
+        updated = patcher.apply()
+        self.assertEqual(
+            updated,
+            {
+                "meta": {"resourceType": "User"},
+                "active": True,
+                "schemas": [
+                    "urn:ietf:params:scim:schemas:core:2.0:User",
+                    "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User",
+                ],
+                "userName": "test@t.goauthentik.io",
+                "externalId": "aqwer",
+                "displayName": "Test MS",
+            },
+        )
+
     def test_replace(self):
         req = {
             "schemas": ["urn:ietf:params:scim:api:messages:2.0:PatchOp"],
@@ -98,6 +140,49 @@ class TestSCIMPatcher(APITestCase):
                 ],
                 "userName": "test@t.goauthentik.io",
                 "externalId": "test",
+                "displayName": "Test MS",
+            },
+        )
+
+    def test_replace_no_path(self):
+        """Test value replace with no path"""
+        req = {
+            "schemas": ["urn:ietf:params:scim:api:messages:2.0:PatchOp"],
+            "Operations": [
+                {"op": "Replace", "value": {"externalId": "aqwer"}},
+            ],
+        }
+        user = create_test_user()
+        source = SCIMSource.objects.create(slug=generate_id())
+        connection = SCIMSourceUser.objects.create(
+            user=user,
+            id=generate_id(),
+            source=source,
+            attributes={
+                "meta": {"resourceType": "User"},
+                "active": True,
+                "schemas": [
+                    "urn:ietf:params:scim:schemas:core:2.0:User",
+                    "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User",
+                ],
+                "userName": "test@t.goauthentik.io",
+                "externalId": "test",
+                "displayName": "Test MS",
+            },
+        )
+        patcher = SCIMPatcher(connection, req["Operations"])
+        updated = patcher.apply()
+        self.assertEqual(
+            updated,
+            {
+                "meta": {"resourceType": "User"},
+                "active": True,
+                "schemas": [
+                    "urn:ietf:params:scim:schemas:core:2.0:User",
+                    "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User",
+                ],
+                "userName": "test@t.goauthentik.io",
+                "externalId": "aqwer",
                 "displayName": "Test MS",
             },
         )
--- authentik/sources/scim/views/v2/patch.py
+++ authentik/sources/scim/views/v2/patch.py
@@ -1,6 +1,8 @@
 from copy import deepcopy
 
+from deepmerge import always_merger
 from scim2_filter_parser.attr_paths import AttrPath
+from structlog.stdlib import get_logger
 
 from authentik.providers.scim.clients.schema import PatchOp, PatchOperation
 from authentik.sources.scim.models import SCIMSourceGroup, SCIMSourceUser
@@ -18,30 +20,36 @@ class SCIMPatcher:
             if operation.op.lower() not in ["add", "remove", "replace"]:
                 raise SCIMValidationError()
             self._ops.append(operation)
+        self.logger = get_logger().bind()
+        self.working_data = deepcopy(self._connection.attributes)
 
-    def apply(self, ignored_path_prefixes: list[str] | None = None) -> dict:
-        if not ignored_path_prefixes:
-            ignored_path_prefixes = []
-        working_data = deepcopy(self._connection.attributes)
+    def apply(self) -> dict:
         for op in self._ops:
-            attr_path = AttrPath(f'{op.path} eq ""', {})
-            ctx_wk: dict = working_data
-            path_elements = [x for x in attr_path.first_path if x]
-            if len(path_elements) < 1:
-                continue
-            if path_elements[0] in ignored_path_prefixes:
-                continue
-            for path_part in path_elements[:-1]:
-                ctx_wk.setdefault(path_part, {})
-                ctx_wk = ctx_wk[path_part]
-            last_path_element = path_elements[-1]
-            if op.op == PatchOp.add:
-                if last_path_element in ctx_wk and isinstance(ctx_wk[last_path_element], list):
-                    ctx_wk[last_path_element].extend(op.value)
-                else:
-                    ctx_wk[last_path_element] = op.value
-            elif op.op == PatchOp.replace:
+            self.handle_single_op(op)
+        return self.working_data
+
+    def handle_single_op(self, op: PatchOperation):
+        if not op.path:
+            if not isinstance(op.value, dict):
+                self.logger.warning("Failed to apply patch op")
+                return
+            always_merger.merge(self.working_data, op.value)
+            return
+        attr_path = AttrPath(f'{op.path} eq ""', {})
+        ctx_wk: dict = self.working_data
+        path_elements = [x for x in attr_path.first_path if x]
+        if len(path_elements) < 1:
+            return
+        for path_part in path_elements[:-1]:
+            ctx_wk.setdefault(path_part, {})
+            ctx_wk = ctx_wk[path_part]
+        last_path_element = path_elements[-1]
+        if op.op == PatchOp.add:
+            if last_path_element in ctx_wk and isinstance(ctx_wk[last_path_element], list):
+                ctx_wk[last_path_element].extend(op.value)
+            else:
                 ctx_wk[last_path_element] = op.value
-            elif op.op == PatchOp.remove:
-                ctx_wk.pop(last_path_element, None)
-        return working_data
+        elif op.op == PatchOp.replace:
+            ctx_wk[last_path_element] = op.value
+        elif op.op == PatchOp.remove:
+            ctx_wk.pop(last_path_element, None)
