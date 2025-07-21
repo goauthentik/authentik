07ea6bb2ad7837c3af0d1aaf5656cd9c3fb0aa60
Teffen Ellis <teffen@goauthentik.io>
2025-07-21 19:18:00 +0200

    web: Flesh out Playwright.
    web: Flesh out slim tests.

---
+++ web/test/browser/session.test.ts
@@ -0,0 +1,33 @@
+import { expect, test } from "#e2e";
+import {
+    BAD_PASSWORD,
+    BAD_USERNAME,
+    GOOD_PASSWORD,
+    GOOD_USERNAME,
+} from "#e2e/fixtures/SessionFixture";
+
+test.beforeEach(async ({ session }) => {
+    await session.toLoginPage();
+});
+
+test.describe("Session management", () => {
+    test("Login with valid credentials", async ({ session, $ }) => {
+        await session.login({ username: GOOD_USERNAME, password: GOOD_PASSWORD });
+
+        await $.page.heading.expect.toHaveText("My applications");
+    });
+
+    test("Reject bad username", async ({ session }) => {
+        await session.login({ username: BAD_USERNAME, password: GOOD_PASSWORD });
+
+        await expect(session.$authFailureMessage).toBeVisible();
+        await expect(session.$authFailureMessage).toHaveText("Invalid password");
+    });
+
+    test("Reject bad password", async ({ session }) => {
+        await session.login({ username: GOOD_USERNAME, password: BAD_PASSWORD });
+
+        await expect(session.$authFailureMessage).toBeVisible();
+        await expect(session.$authFailureMessage).toHaveText("Invalid password");
+    });
+});
