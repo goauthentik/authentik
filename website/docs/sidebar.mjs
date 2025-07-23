e280159946aee3a46a9f4a971c83407d837ad31f
Teffen Ellis <592134+GirlBossRush@users.noreply.github.com>
2025-07-23 16:42:01 +0200

    website: Docusaurus 3.8 (#15097)
    website: Upgrade Docusaurus. Split API build.

--- website/docs/sidebar.mjs
+++ website/docs/sidebar.mjs
@@ -637,7 +637,7 @@ const items = [
         items: [
             {
                 type: "link",
-                href: "pathname:///api",
+                href: "https://api.goauthentik.io",
                 label: "API Overview",
                 className: "api-overview",
             },
