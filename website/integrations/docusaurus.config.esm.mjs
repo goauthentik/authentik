8766191d67450e8804564fdcf8eb6a5348996d02
Teffen Ellis <592134+GirlBossRush@users.noreply.github.com>
2025-07-23 17:54:21 +0200

    website: Docusaurus 3.8 Follow-up - API Docs (#15751)
    * website: Update API build directory.
    
    * website: Update header paths. Use separate build paths.

--- website/integrations/docusaurus.config.esm.mjs
+++ website/integrations/docusaurus.config.esm.mjs
@@ -1,7 +1,7 @@
 /**
  * @file Docusaurus Integrations config.
  *
- * @import { UserThemeConfig } from "@goauthentik/docusaurus-config";
+ * @import { UserThemeConfig, UserThemeConfigExtra } from "@goauthentik/docusaurus-config";
  * @import { Options as RedirectsPluginOptions } from "@docusaurus/plugin-client-redirects";
  */
 
@@ -77,11 +77,14 @@ export default createDocusaurusConfig(
 
         themes: ["@goauthentik/docusaurus-theme", "@docusaurus/theme-mermaid"],
 
-        themeConfig: /** @type {UserThemeConfig} */ ({
+        themeConfig: /** @type {UserThemeConfig & UserThemeConfigExtra} */ ({
             algolia: createAlgoliaConfig({
                 externalUrlRegex: /^(?:https?:\/\/)(?!integrations\.goauthentik.io)/.source,
             }),
             image: "img/social.png",
+            navbarReplacements: {
+                INTEGRATIONS_URL: "/",
+            },
             navbar: {
                 logo: {
                     alt: "authentik logo",
@@ -89,49 +92,6 @@ export default createDocusaurusConfig(
                     href: "https://goauthentik.io/",
                     target: "_self",
                 },
-                items: [
-                    {
-                        to: "https://goauthentik.io/features",
-                        label: "Features",
-                        position: "left",
-                        target: "_self",
-                    },
-                    {
-                        to: "/",
-                        label: "Integrations",
-                        position: "left",
-                    },
-                    {
-                        to: "https://docs.goauthentik.io",
-                        label: "Documentation",
-                        position: "left",
-                        target: "_self",
-                    },
-                    {
-                        to: "https://goauthentik.io/pricing/",
-                        label: "Pricing",
-                        position: "left",
-                        target: "_self",
-                    },
-                    {
-                        to: "https://goauthentik.io/blog",
-                        label: "Blog",
-                        position: "left",
-                        target: "_self",
-                    },
-                    {
-                        "href": "https://github.com/goauthentik/authentik",
-                        "data-icon": "github",
-                        "aria-label": "GitHub",
-                        "position": "right",
-                    },
-                    {
-                        "href": "https://goauthentik.io/discord",
-                        "data-icon": "discord",
-                        "aria-label": "Discord",
-                        "position": "right",
-                    },
-                ],
             },
         }),
 
