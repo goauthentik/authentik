a4646562e1a4a6e0330a8303c35e2588f1e0b10d
Teffen Ellis <teffen@goauthentik.io>
2025-07-24 17:26:52 +0200

    web: Clean up WebSocket lifecycle.

--- web/src/user/index.entrypoint.ts
+++ web/src/user/index.entrypoint.ts
@@ -269,8 +269,6 @@ export class UserInterface extends WithBrandConfig(AuthenticatedInterface) {
     @state()
     apiDrawerOpen = getURLParam("apiDrawerOpen", false);
 
-    ws: WebsocketClient;
-
     @state()
     notificationsCount = 0;
 
@@ -282,8 +280,11 @@ export class UserInterface extends WithBrandConfig(AuthenticatedInterface) {
 
     constructor() {
         configureSentry(true);
+
         super();
-        this.ws = new WebsocketClient();
+
+        WebsocketClient.connect();
+
         this.fetchConfigurationDetails();
         this.toggleNotificationDrawer = this.toggleNotificationDrawer.bind(this);
         this.toggleApiDrawer = this.toggleApiDrawer.bind(this);
@@ -299,11 +300,13 @@ export class UserInterface extends WithBrandConfig(AuthenticatedInterface) {
     }
 
     disconnectedCallback() {
+        super.disconnectedCallback();
+
         window.removeEventListener(EVENT_NOTIFICATION_DRAWER_TOGGLE, this.toggleNotificationDrawer);
         window.removeEventListener(EVENT_API_DRAWER_TOGGLE, this.toggleApiDrawer);
         window.removeEventListener(EVENT_WS_MESSAGE, this.fetchConfigurationDetails);
 
-        super.disconnectedCallback();
+        WebsocketClient.close();
     }
 
     toggleNotificationDrawer() {
