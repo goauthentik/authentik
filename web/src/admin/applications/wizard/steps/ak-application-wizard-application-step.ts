20cbc5a0db94e705031706a253e339ee2ab1539d
Teffen Ellis <teffen@goauthentik.io>
2025-07-30 02:19:29 +0200

    web: Use snake case helper.

--- web/src/admin/applications/wizard/steps/ak-application-wizard-application-step.ts
+++ web/src/admin/applications/wizard/steps/ak-application-wizard-application-step.ts
@@ -8,8 +8,6 @@ import "#elements/forms/HorizontalFormElement";
 
 import { ApplicationWizardStateUpdate, ValidationRecord } from "../types.js";
 
-import { camelToSnake } from "#common/utils";
-
 import { isSlug } from "#elements/router/utils";
 
 import { type NavigableButton, type WizardButton } from "#components/ak-wizard/types";
@@ -19,25 +17,31 @@ import { policyEngineModes } from "#admin/policies/PolicyEngineModes";
 
 import { type ApplicationRequest } from "@goauthentik/api";
 
+import { snakeCase } from "change-case";
+
 import { msg } from "@lit/localize";
 import { html } from "lit";
 import { customElement, query, state } from "lit/decorators.js";
 import { ifDefined } from "lit/directives/if-defined.js";
 
-const autoTrim = (v: unknown) => (typeof v === "string" ? v.trim() : v);
+function trimMany<T extends object, K extends keyof T>(target: T, keys: K[]): Pick<T, K> {
+    const output = {} as Record<K, unknown>;
 
-const trimMany = (o: Record<string, unknown>, vs: string[]) =>
-    Object.fromEntries(vs.map((v) => [v, autoTrim(o[v])]));
+    for (const key of keys) {
+        const value = target[key];
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const isStr = (v: any): v is string => typeof v === "string";
+        output[key] = typeof value === "string" ? value.trim() : value;
+    }
+
+    return output as Pick<T, K>;
+}
 
 @customElement("ak-application-wizard-application-step")
 export class ApplicationWizardApplicationStep extends ApplicationWizardStep {
     label = msg("Application");
 
     @state()
-    errors = new Map<string, string>();
+    errors = new Map<keyof ApplicationRequest, string>();
 
     @query("form#applicationform")
     form!: HTMLFormElement;
@@ -48,12 +52,10 @@ export class ApplicationWizardApplicationStep extends ApplicationWizardStep {
         this.enabled = true;
     }
 
-    errorMessages(name: string) {
+    protected errorMessages(name: keyof ApplicationRequest) {
         return this.errors.has(name)
             ? [this.errors.get(name)]
-            : (this.wizard.errors?.app?.[name] ??
-                  this.wizard.errors?.app?.[camelToSnake(name)] ??
-                  []);
+            : (this.wizard.errors?.app?.[name] ?? this.wizard.errors?.app?.[snakeCase(name)] ?? []);
     }
 
     get buttons(): WizardButton[] {
@@ -62,51 +64,53 @@ export class ApplicationWizardApplicationStep extends ApplicationWizardStep {
 
     get valid() {
         this.errors = new Map();
-        const values = trimMany(this.formValues ?? {}, ["metaLaunchUrl", "name", "slug"]);
 
-        if (values.name === "") {
+        const values = trimMany(this.formValues, ["metaLaunchUrl", "name", "slug"]);
+
+        if (!values.name) {
             this.errors.set("name", msg("An application name is required"));
         }
-        if (
-            !(
-                isStr(values.metaLaunchUrl) &&
-                (values.metaLaunchUrl === "" || URL.canParse(values.metaLaunchUrl))
-            )
-        ) {
+
+        if (!values.metaLaunchUrl || !URL.canParse(values.metaLaunchUrl)) {
             this.errors.set("metaLaunchUrl", msg("Not a valid URL"));
         }
-        if (!(isStr(values.slug) && values.slug !== "" && isSlug(values.slug))) {
+
+        if (!values.slug || !isSlug(values.slug)) {
             this.errors.set("slug", msg("Not a valid slug"));
         }
+
         return this.errors.size === 0;
     }
 
     override handleButton(button: NavigableButton) {
-        if (button.kind === "next") {
-            if (!this.valid) {
-                this.handleEnabling({
-                    disabled: ["provider-choice", "provider", "bindings", "submit"],
-                });
-                return;
-            }
-            const app: Partial<ApplicationRequest> = this.formValues as Partial<ApplicationRequest>;
+        if (button.kind !== "next") {
+            return super.handleButton(button);
+        }
 
-            let payload: ApplicationWizardStateUpdate = {
-                app: this.formValues,
-                errors: this.removeErrors("app"),
-            };
-            if (app.name && (this.wizard.provider?.name ?? "").trim() === "") {
-                payload = {
-                    ...payload,
-                    provider: { name: `Provider for ${app.name}` },
-                };
-            }
-            this.handleUpdate(payload, button.destination, {
-                enable: "provider-choice",
+        if (!this.valid) {
+            this.handleEnabling({
+                disabled: ["provider-choice", "provider", "bindings", "submit"],
             });
+
             return;
         }
-        super.handleButton(button);
+
+        const app = { ...this.formValues };
+
+        const payload: ApplicationWizardStateUpdate = {
+            app,
+            errors: this.removeErrors("app"),
+        };
+
+        if (!this.wizard.provider?.name?.trim() && app.name) {
+            payload.provider = {
+                name: `Provider for ${app.name}`,
+            };
+        }
+
+        this.handleUpdate(payload, button.destination, {
+            enable: "provider-choice",
+        });
     }
 
     renderForm(app: Partial<ApplicationRequest>, errors: ValidationRecord) {
