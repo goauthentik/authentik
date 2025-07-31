8bed58b171bda8bf98f5f154cbc612d97834fdac
Alexander Tereshkin <a@tereshk.in>
2025-07-31 18:42:59 +0300

    sources/telegram: update serielizer field types references to be in line with convention

--- authentik/sources/telegram/stage.py
+++ authentik/sources/telegram/stage.py
@@ -3,8 +3,8 @@ import hmac
 from datetime import datetime, timedelta
 
 from django.utils.translation import gettext_lazy as _
-from rest_framework import serializers
-from rest_framework.fields import BooleanField, CharField
+from rest_framework.fields import BooleanField, CharField, IntegerField, URLField
+from rest_framework.serializers import ValidationError
 
 from authentik.flows.challenge import Challenge, ChallengeResponse
 from authentik.stages.identification.stage import LoginChallengeMixin
@@ -19,17 +19,17 @@ class TelegramLoginChallenge(LoginChallengeMixin, Challenge):
 class TelegramChallengeResponse(ChallengeResponse):
     component = CharField(default="ak-source-telegram")
 
-    id = serializers.IntegerField()
-    first_name = serializers.CharField(max_length=255, required=False)
-    last_name = serializers.CharField(max_length=255, required=False)
-    username = serializers.CharField(max_length=255, required=False)
-    photo_url = serializers.URLField(required=False)
-    auth_date = serializers.IntegerField(required=True)
-    hash = serializers.CharField(max_length=64, required=True)
+    id = IntegerField()
+    first_name = CharField(max_length=255, required=False)
+    last_name = CharField(max_length=255, required=False)
+    username = CharField(max_length=255, required=False)
+    photo_url = URLField(required=False)
+    auth_date = IntegerField(required=True)
+    hash = CharField(max_length=64, required=True)
 
     def validate_auth_date(self, auth_date):
         if datetime.fromtimestamp(auth_date) < datetime.now() - timedelta(minutes=5):
-            raise serializers.ValidationError(_("Authentication date is too old"))
+            raise ValidationError(_("Authentication date is too old"))
         return auth_date
 
     def validate(self, attrs):
@@ -44,5 +44,5 @@ class TelegramChallengeResponse(ChallengeResponse):
             "sha256",
         ).hexdigest()
         if not hmac.compare_digest(digest, attrs["hash"]):
-            raise serializers.ValidationError(_("Invalid hash"))
+            raise ValidationError(_("Invalid hash"))
         return attrs
