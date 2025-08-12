1bc2daae98aea4d2e66dd8b12b971c99dff8b2cc
Teffen Ellis <592134+GirlBossRush@users.noreply.github.com>
2025-08-12 16:34:08 +0200

    web: Form validation regressions, consistency fixes (#15894)
    * web: Clean up naming of inputs.
    
    * web: Flesh out label components, slots.
    
    * web: Expand clickable area of labels.
    
    * web: Fix issue where launch URL is required.
    
    * web: Surface string errors verbatim.
    
    * web: Fix issues surrounding client-side error reporting, form validation,
    server-side error reporting.
    
    * web: Clarify property visibility.
    
    * web: Fix issue where provider errors are not surfaced.
    
    * web: Fix issue where wizard steps do not consistently use form validation.
    
    * web: Fix issue where render root is not preferred.
    
    * web: Fix import path.
    
    * web: Fix selectors.
    
    * keep labels aligned
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * web: Fix field nesting.
    
    * web: Fix issue where required secret text inputs fail validation when editing existing entities.
    
    - We need to make the component have a better understanding of this concept.
    
    * web: Fix slot alignment on legacy elements.
    
    ---------
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    Co-authored-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/elements/utils/inputs.ts
+++ web/src/elements/utils/inputs.ts
@@ -7,6 +7,11 @@ export type NamedElement<T = Element> = T & {
     name: string;
 };
 
+/**
+ * Type predicate to check if an element currently has a `name` attribute.
+ *
+ * @see {@linkcode isNameableElement} to check if an element is nameable.
+ */
 export function isNamedElement(element: Element): element is NamedElement {
     if (!(element instanceof HTMLElement)) {
         return false;
@@ -16,26 +21,56 @@ export function isNamedElement(element: Element): element is NamedElement {
 }
 
 /**
+ * A set of elements that can be named, i.e. have a `name` attribute.
+ *
+ * @deprecated This should be replaced with a less brittle approach.
+ */
+const NameableElements = new Set([
+    "INPUT",
+    "TEXTAREA",
+    "SELECT",
+    "AK-CODEMIRROR",
+    "AK-CHIP-GROUP",
+    "AK-SEARCH-SELECT",
+    "AK-RADIO",
+]);
+
+/**
+ * Type predicate to check if an element is nameable.
+ *
+ * @see {@linkcode isNamedElement} to check if an element currently has a `name` attribute.
+ */
+export function isNameableElement(element: Element): element is NamedElement {
+    if (!(element instanceof HTMLElement)) {
+        return false;
+    }
+
+    return NameableElements.has(element.tagName);
+}
+
+/**
  * Create a map of files provided by input elements within the given iterable.
  */
 export function createFileMap<T extends PropertyKey = PropertyKey>(
-    fileInputParents?: Iterable<NamedElement<LitElement>> | null,
+    fileInputParents?: Iterable<LitElement> | null,
 ): Map<T, File> {
     const record = new Map<T, File>();
 
     for (const element of fileInputParents || []) {
         element.requestUpdate();
 
+        if (!isNamedElement(element)) continue;
+
         const inputElement = element.querySelector<HTMLInputElement>("input[type=file]");
 
         if (!inputElement) continue;
 
         const file = inputElement.files?.[0];
-        const name = element.name;
+        const name = element.name as T;
 
         if (!file || !name) continue;
 
-        record.set(name as T, file);
+        record.set(name, file);
     }
 
     return record;
