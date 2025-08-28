03f23f5a9d6f265f6a533e66d39f797cbbfb5fc5
dewi-tik <dewi@goauthentik.io>
2025-08-28 11:18:10 +0100

    Adds mention of port config

--- web/src/admin/providers/rac/EndpointForm.ts
+++ web/src/admin/providers/rac/EndpointForm.ts
@@ -91,7 +91,9 @@ export class EndpointForm extends ModelForm<Endpoint, string> {
                     class="pf-c-form-control"
                     required
                 />
-                <p class="pf-c-form__helper-text">${msg("Hostname/IP to connect to.")}</p>
+                <p class="pf-c-form__helper-text">
+                    ${msg("Hostname/IP to connect to. Optionally specify the port.")}
+                </p>
             </ak-form-element-horizontal>
             <ak-form-element-horizontal
                 label=${msg("Maximum concurrent connections")}
