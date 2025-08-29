5b25c4f3f6032c26c7fa8bbf734a02a3a8760a9c
Teffen Ellis <teffen@goauthentik.io>
2025-08-29 14:59:28 +0200

    website: use warning.
    - This script is outdated.

--- web/scripts/lint-lockfile.sh
+++ web/scripts/lint-lockfile.sh
@@ -10,12 +10,12 @@ fi
 CMD=$(jq -r '.packages | to_entries[] | select((.key | contains("node_modules")) and (.value | has("resolved") | not)) | .key' < "$1")
 
 if [ -n "$CMD" ]; then
-    echo "ERROR package-lock.json entries missing 'resolved' field:"
+    echo "Warning: package-lock.json entries missing 'resolved' field:"
     echo ""
     # Shellcheck erroneously believes that shell string substitution can be used here, but that
     # feature lacks a "start of line" discriminator.
     # shellcheck disable=SC2001
     echo "$CMD" | sed 's/^/    /g'
     echo ""
-    exit 1
-fi    
+    exit 0
+fi
