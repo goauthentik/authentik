171e83b088a3be9af67126344262470cc4df9708
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2025-09-24 18:52:58 +0200

    lib: typing
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/lib/merge.py
+++ authentik/lib/merge.py
@@ -1,6 +1,6 @@
 """merge utils"""
 
-from deepmerge import Merger
+from deepmerge import Merger  # type: ignore[attr-defined]
 
 MERGE_LIST_UNIQUE = Merger(
     [(list, ["append_unique"]), (dict, ["merge"]), (set, ["union"])], ["override"], ["override"]
--- authentik/lib/utils/urls.py
+++ authentik/lib/utils/urls.py
@@ -1,5 +1,6 @@
 """URL-related utils"""
 
+from typing import Any
 from urllib.parse import urlparse
 
 from django.http import HttpResponse, QueryDict
@@ -10,12 +11,16 @@ from structlog.stdlib import get_logger
 LOGGER = get_logger()
 
 
-def is_url_absolute(url):
+def is_url_absolute(url: str | bytes | bytearray | None) -> bool:
     """Check if domain is absolute to prevent user from being redirect somewhere else"""
     return bool(urlparse(url).netloc)
 
 
-def redirect_with_qs(view: str, get_query_set: QueryDict | None = None, **kwargs) -> HttpResponse:
+def redirect_with_qs(
+    view: str,
+    get_query_set: QueryDict | None = None,
+    **kwargs: Any,
+) -> HttpResponse:
     """Wrapper to redirect whilst keeping GET Parameters"""
     try:
         target = reverse(view, kwargs=kwargs)
@@ -29,7 +34,7 @@ def redirect_with_qs(view: str, get_query_set: QueryDict | None = None, **kwargs
     return redirect(target)
 
 
-def reverse_with_qs(view: str, query: QueryDict | None = None, **kwargs) -> str:
+def reverse_with_qs(view: str, query: QueryDict | None = None, **kwargs: Any) -> str:
     """Reverse a view to it's url but include get params"""
     url = reverse(view, **kwargs)
     if query:
