bbfcc9c6abe7239dc9755bb0a4bda3828facbd02
Jens Langhammer <jens@goauthentik.io>
2025-09-24 14:36:43 +0200

    generate config to use replicas
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

---
+++ .github/actions/setup/generate_config.py
@@ -0,0 +1,17 @@
+from os import getenv
+
+from yaml import safe_dump
+
+from authentik.lib.generators import generate_id
+
+config = {
+    "log_level": "debug",
+    "secret_key": generate_id(),
+}
+
+profiles = getenv("PROFILES")
+if profiles and "postgres_replica" in profiles:
+    config["postgresql"] = {"read_replicas": {"0": {"host": "localhost", "port": 5433}}}
+
+with open("local.env.yml", "w") as _config:
+    safe_dump(config, _config, default_flow_style=False)
