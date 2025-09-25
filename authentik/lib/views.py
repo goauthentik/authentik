123cca34a1d82e60e6a7856cb1eedeb3f3a8a82c
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2025-09-25 13:42:02 +0200

    wip
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/lib/views.py
+++ authentik/lib/views.py
@@ -1,6 +1,7 @@
 """authentik helper views"""
 
 from django.http import HttpRequest
+from django.template.base import Template
 from django.template.response import TemplateResponse
 from django.utils.translation import gettext_lazy as _
 
@@ -8,8 +9,8 @@ from django.utils.translation import gettext_lazy as _
 def bad_request_message(
     request: HttpRequest,
     message: str,
-    title="Bad Request",
-    template="if/error.html",
+    title: str = "Bad Request",
+    template: list[str] | tuple[str, ...] | tuple[()] | str | Template = "if/error.html",
 ) -> TemplateResponse:
     """Return generic error page with message, with status code set to 400"""
     return TemplateResponse(
