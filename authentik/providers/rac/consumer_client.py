f22ee6f37aa6a9be9aee3ce34e0cb1c0c80b6c3f
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2025-09-30 16:31:25 +0200

    lint
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/providers/rac/consumer_client.py
+++ authentik/providers/rac/consumer_client.py
@@ -1,6 +1,7 @@
 """RAC Client consumer"""
 
 from hashlib import sha256
+
 from asgiref.sync import async_to_sync
 from channels.db import database_sync_to_async
 from channels.exceptions import ChannelFull, DenyConnection
