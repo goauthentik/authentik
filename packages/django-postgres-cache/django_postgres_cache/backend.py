d8d8e6c4008a83124b22a624e4993796e932bb0f
Jens Langhammer <jens@goauthentik.io>
2025-10-02 01:27:31 +0200

    simplify to use ORM
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- packages/django-postgres-cache/django_postgres_cache/backend.py
+++ packages/django-postgres-cache/django_postgres_cache/backend.py
@@ -1,7 +1,6 @@
 from typing import Any
 
 from django.core.cache.backends.db import DatabaseCache as BaseDatabaseCache
-from django.db import connections, router
 from django.db.utils import ProgrammingError
 from django.utils.module_loading import import_string
 from django.utils.timezone import now
@@ -35,45 +34,18 @@ class DatabaseCache(BaseDatabaseCache):
 
     def _keys(self, keys_pattern: str, version: int | None = None) -> list[str]:
         keys_pattern = self.make_key(keys_pattern.replace("*", ".*"), version=version)
-        db = router.db_for_read(self.cache_model_class)
-        connection = connections[db]
-        quote_name = connection.ops.quote_name
-        table = quote_name(self._table)
 
-        with connection.cursor() as cursor:
-            cursor.execute(
-                "SELECT {} FROM {} WHERE {} ~ {}".format(  # nosec
-                    quote_name("cache_key"),
-                    table,
-                    quote_name("cache_key"),
-                    "%s",
-                ),
-                [keys_pattern],
+        return [
+            self.reverse_key_func(key)
+            for key in CacheEntry.objects.filter(cache_key__regex=keys_pattern).values_list(
+                "cache_key", flat=True
             )
-            rows = cursor.fetchall()
-
-        return [self.reverse_key_func(row[0]) for row in rows]
+        ]
 
     def ttl(self, key: str, version: int | None = None) -> int | None:
         """Get TTL left for a given key and version"""
         key = self.make_and_validate_key(key, version=version)
-        db = router.db_for_read(self.cache_model_class)
-        connection = connections[db]
-        quote_name = connection.ops.quote_name
-        table = quote_name(self._table)
-
-        with connection.cursor() as cursor:
-            cursor.execute(
-                "SELECT {} FROM {} WHERE {} = {}".format(  # nosec
-                    quote_name("expires"),
-                    table,
-                    quote_name("cache_key"),
-                    "%s",
-                ),
-                [key],
-            )
-            rows = cursor.fetchall()
-
-        if not rows:
+        entry = CacheEntry.objects.filter(cache_key=key).first()
+        if not entry:
             return None
-        return int((rows[0].expires - now()).total_seconds())
+        return int((entry.expires - now()).total_seconds())
