76f7ed1c46ec81c43d57b109c2c8eb1a6538d862
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2025-10-06 13:45:13 +0200

    fix mypy
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/commands/management/commands/migrate.py
+++ authentik/commands/management/commands/migrate.py
@@ -4,4 +4,4 @@ from authentik.commands.management.commands import MigrationAutodetector
 
 
 class Command(BaseCommand):
-    autodetector = MigrationAutodetector
+    autodetector = MigrationAutodetector  # type: ignore[assignment]
