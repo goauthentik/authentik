28b65bc0ec0fb01bdc0aa4a8473dfcecb24fbe95
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2025-10-07 15:03:11 +0200

    tasks: add preprocess, running and postprocess statuses
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- web/src/elements/tasks/TaskStatus.ts
+++ web/src/elements/tasks/TaskStatus.ts
@@ -34,7 +34,21 @@ export class TaskStatus extends AKElement {
             case TasksTasksListAggregatedStatusEnum.Consumed:
             case TaskAggregatedStatusEnum.Consumed:
             case LastTaskStatusEnum.Consumed:
+                return html`<ak-label color=${PFColor.Blue}
+                    >${msg("Task has been up by a worker")}</ak-label
+                >`;
+            case TasksTasksListAggregatedStatusEnum.Preprocess:
+            case TaskAggregatedStatusEnum.Preprocess:
+            case LastTaskStatusEnum.Preprocess:
+                return html`<ak-label color=${PFColor.Blue}>${msg("Pre-processing")}</ak-label>`;
+            case TasksTasksListAggregatedStatusEnum.Running:
+            case TaskAggregatedStatusEnum.Running:
+            case LastTaskStatusEnum.Running:
                 return html`<ak-label color=${PFColor.Blue}>${msg("Running")}</ak-label>`;
+            case TasksTasksListAggregatedStatusEnum.Postprocess:
+            case TaskAggregatedStatusEnum.Postprocess:
+            case LastTaskStatusEnum.Postprocess:
+                return html`<ak-label color=${PFColor.Blue}>${msg("Post-processing")}</ak-label>`;
             case TasksTasksListAggregatedStatusEnum.Done:
             case TaskAggregatedStatusEnum.Done:
             case LastTaskStatusEnum.Done:
