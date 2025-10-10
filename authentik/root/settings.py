7dbdb4c613aa525f49dbc6c90b258740f10d2216
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2025-10-10 16:35:13 +0200

    tasks: store messages in separate table (#17359)

--- authentik/root/settings.py
+++ authentik/root/settings.py
@@ -418,7 +418,7 @@ DRAMATIQ = {
         ("authentik.tasks.middleware.CurrentTask", {}),
         ("authentik.tasks.middleware.TenantMiddleware", {}),
         ("authentik.tasks.middleware.ModelDataMiddleware", {}),
-        ("authentik.tasks.middleware.MessagesMiddleware", {}),
+        ("authentik.tasks.middleware.TaskLogMiddleware", {}),
         ("authentik.tasks.middleware.LoggingMiddleware", {}),
         ("authentik.tasks.middleware.DescriptionMiddleware", {}),
         ("authentik.tasks.middleware.WorkerHealthcheckMiddleware", {}),
