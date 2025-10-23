e31d914d0742bec4b43f39067d700984f93e8c48
Marcelo Elizeche Landó <marcelo@goauthentik.io>
2025-10-22 22:26:34 -0300

    Add flows-invitation-enrollment.yaml blueprint example, make serializer add default anonymous user in blueprint context

--- authentik/stages/invitation/api.py
+++ authentik/stages/invitation/api.py
@@ -2,8 +2,10 @@
 
 from django_filters.filters import BooleanFilter
 from django_filters.filterset import FilterSet
+from guardian.shortcuts import get_anonymous_user
 from rest_framework.viewsets import ModelViewSet
 
+from authentik.blueprints.v1.importer import SERIALIZER_CONTEXT_BLUEPRINT
 from authentik.core.api.groups import PartialUserSerializer
 from authentik.core.api.used_by import UsedByMixin
 from authentik.core.api.utils import JSONDictField, ModelSerializer
@@ -61,6 +63,13 @@ class InvitationSerializer(ModelSerializer):
             "flow",
             "flow_obj",
         ]
+
+    def save(self, **kwargs):
+        """Set created_by to anonymous user when in blueprint context"""
+        if SERIALIZER_CONTEXT_BLUEPRINT in self.context:
+            if "created_by" not in kwargs:
+                kwargs["created_by"] = get_anonymous_user()
+        return super().save(**kwargs)
 
 
 class InvitationViewSet(UsedByMixin, ModelViewSet):
