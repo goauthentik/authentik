63a8a0d709d41616cade242f82881421a63f94b5
Teffen Ellis <teffen@goauthentik.io>
2025-11-03 05:23:27 +0100

    web: Hide avatars when set to "none"

--- web/src/elements/utils/images.ts
+++ web/src/elements/utils/images.ts
@@ -3,3 +3,7 @@ import { ResolvedUITheme } from "#common/theme";
 export function themeImage(rawPath: string, theme: ResolvedUITheme) {
     return rawPath.replaceAll("%(theme)s", theme);
 }
+
+export function isDefaultAvatar(path?: string | null): boolean {
+    return !!path?.endsWith("user_default.png");
+}
