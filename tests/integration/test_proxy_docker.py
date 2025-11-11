5e174b863223f65d8c9275b5aafa75da5ff9e824
Jens Langhammer <jens@goauthentik.io>
2025-11-11 17:09:17 +0100

    bump dind version
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- tests/integration/test_proxy_docker.py
+++ tests/integration/test_proxy_docker.py
@@ -30,7 +30,7 @@ class TestProxyDocker(DockerTestCase, ChannelsLiveServerTestCase):
         super().setUp()
         self.ssl_folder = mkdtemp()
         self.run_container(
-            image="docker.io/library/docker:24.0.7-dind-alpine3.18",
+            image="docker.io/library/docker:28.5.2-dind-alpine3.22",
             network_mode="host",
             privileged=True,
             healthcheck=Healthcheck(
