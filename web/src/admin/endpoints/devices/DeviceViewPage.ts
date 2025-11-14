d187783530f8f9a9cd57d9a11d5c78e140fe6d21
Jens Langhammer <jens@goauthentik.io>
2025-11-14 19:07:45 +0100

    very basic UI
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/admin/endpoints/devices/DeviceViewPage.ts
+++ web/src/admin/endpoints/devices/DeviceViewPage.ts
@@ -8,8 +8,15 @@ import { setPageDetails } from "#components/ak-page-navbar";
 import { EndpointDevice, EndpointsApi } from "@goauthentik/api";
 
 import { msg } from "@lit/localize";
-import { PropertyValues } from "lit";
+import { CSSResult, html, PropertyValues } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
+
+import PFButton from "@patternfly/patternfly/components/Button/button.css";
+import PFCard from "@patternfly/patternfly/components/Card/card.css";
+import PFDescriptionList from "@patternfly/patternfly/components/DescriptionList/description-list.css";
+import PFPage from "@patternfly/patternfly/components/Page/page.css";
+import PFGrid from "@patternfly/patternfly/layouts/Grid/grid.css";
+import PFBase from "@patternfly/patternfly/patternfly-base.css";
 
 @customElement("ak-endpoints-device-view")
 export class DeviceViewPage extends AKElement {
@@ -21,6 +28,8 @@ export class DeviceViewPage extends AKElement {
 
     @state()
     protected error?: APIError;
+
+    static styles: CSSResult[] = [PFBase, PFCard, PFPage, PFGrid, PFButton, PFDescriptionList];
 
     protected fetchDevice(id: string) {
         new EndpointsApi(DEFAULT_CONFIG)
@@ -45,6 +54,42 @@ export class DeviceViewPage extends AKElement {
             header: this.device?.data.network?.hostname ?? msg("Loading device..."),
             description: this.device?.data.os?.version,
         });
+    }
+
+    render() {
+        return html`<section class="pf-c-page__main-section">
+            <div class="pf-l-grid pf-m-gutter">
+                <div class="pf-l-grid__item pf-m-3-col pf-c-card">
+                    <div class="pf-c-card__body">
+                        <dl class="pf-c-description-list pf-m-horizontal">
+                            <div class="pf-c-description-list__group">
+                                <dt class="pf-c-description-list__term">
+                                    <span class="pf-c-description-list__text">${msg("Name")}</span>
+                                </dt>
+                                <dd class="pf-c-description-list__description">
+                                    <div class="pf-c-description-list__text">
+                                        ${this.device?.data.network?.hostname}
+                                    </div>
+                                </dd>
+                            </div>
+
+                            <div class="pf-c-description-list__group">
+                                <dt class="pf-c-description-list__term">
+                                    <span class="pf-c-description-list__text"
+                                        >${msg("Serial")}</span
+                                    >
+                                </dt>
+                                <dd class="pf-c-description-list__description">
+                                    <div class="pf-c-description-list__text">
+                                        ${this.device?.data.hardware?.serial}
+                                    </div>
+                                </dd>
+                            </div>
+                        </dl>
+                    </div>
+                </div>
+            </div>
+        </section>`;
     }
 }
 
