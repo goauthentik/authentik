c6f46706d33a7e55a598212bf84adb0aa941194b
Teffen Ellis <teffen@goauthentik.io>
2025-11-14 02:17:23 +0100

    Fix issues surrounding color layering, overlay states, contrast, etc.

--- web/src/admin/providers/ssf/SSFProviderViewPage.ts
+++ web/src/admin/providers/ssf/SSFProviderViewPage.ts
@@ -80,8 +80,8 @@ export class SSFProviderViewPage extends AKElement {
         if (!this.provider) {
             return nothing;
         }
-        return html` <main>
-            <ak-tabs>
+        return html`<main part="main">
+            <ak-tabs part="tabs">
                 <div
                     role="tabpanel"
                     tabindex="0"
