64607246c34c1f7ce0ab512003e4a308a95e1101
Jens Langhammer <jens@goauthentik.io>
2025-11-17 21:56:36 +0100

    remove chrome from this PR
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/enterprise/endpoints/connectors/google_chrome/connector.py
+++
@@ -1,7 +0,0 @@
-from authentik.endpoints.connector import BaseConnector, EnrollmentMethods
-
-
-class GoogleChromeConnector(BaseConnector):
-
-    def supported_enrollment_methods(self) -> list[EnrollmentMethods]:
-        return [EnrollmentMethods.AUTOMATIC_USER]
--- authentik/enterprise/settings.py
+++ authentik/enterprise/settings.py
@@ -4,7 +4,6 @@ TENANT_APPS = [
     "authentik.enterprise.audit",
     "authentik.enterprise.endpoints.connectors.agent",
     "authentik.enterprise.endpoints.connectors.fleet",
-    "authentik.enterprise.endpoints.connectors.google_chrome",
     "authentik.enterprise.policies.unique_password",
     "authentik.enterprise.providers.google_workspace",
     "authentik.enterprise.providers.microsoft_entra",
