c4064ee8b93275d4b67f3b0199982b0b23213bc1
Teffen Ellis <teffen@goauthentik.io>
2025-11-19 17:10:36 +0100

    Flesh out auto-categories.

---
+++ website/docs/categories.mjs
@@ -0,0 +1,14 @@
+/**
+ * @type {ReadonlyArray<readonly [string, string]>}
+ */
+export default [
+    ["core", "Core Concepts"],
+    ["enterprise", "Enterprise"],
+    ["install-config", "Installation and Configuration"],
+    ["add-secure-apps", "Add and Secure Applications"],
+    ["customize", "Customize your instance"],
+    ["users-sources", "Manage Users and Sources"],
+    ["sys-mgmt", "System Management"],
+    ["security", "Security"],
+    ["troubleshooting", "Troubleshooting"],
+];
