b52609141d3d118d8aa64ee3d0dca1ad2a63c55c
Teffen Ellis <teffen@sister.software>
2025-11-23 22:21:16 +0100

    Bump live reload deps. Fix linter.

--- packages/esbuild-plugin-live-reload/eslint.config.mjs
+++ packages/esbuild-plugin-live-reload/eslint.config.mjs
@@ -1,3 +1,30 @@
+/**
+ * @file ESLint Configuration
+ *
+ * @import { Config } from "eslint/config";
+ */
+
 import { createESLintPackageConfig } from "@goauthentik/eslint-config";
 
-export default createESLintPackageConfig()
+import { defineConfig } from "eslint/config";
+
+// @ts-check
+
+/**
+ * @type {Config[]}
+ */
+const eslintConfig = defineConfig(
+    createESLintPackageConfig({
+        parserOptions: {
+            tsconfigRootDir: import.meta.dirname,
+        },
+    }),
+    {
+        rules: {
+            "no-console": "off",
+        },
+        files: ["shared/**"],
+    },
+);
+
+export default eslintConfig;
