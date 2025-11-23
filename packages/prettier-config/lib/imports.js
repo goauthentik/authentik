201d0486f34f1f069a87848b7dbfa8ed38145dc8
Teffen Ellis <teffen@sister.software>
2025-11-23 22:20:04 +0100

    Bump Prettier deps. Fix ESlint config.

--- packages/prettier-config/lib/imports.js
+++ packages/prettier-config/lib/imports.js
@@ -20,7 +20,7 @@ const AK_KEEP_UNUSED_IMPORTS = !!process.env.AK_KEEP_UNUSED_IMPORTS;
 function resolveModule(name) {
     try {
         return require.resolve(name);
-    } catch (error) {
+    } catch (_error) {
         return null;
     }
 }
@@ -61,12 +61,12 @@ function normalizeImports(filepath, input) {
             [
                 // ---
                 `(?:import|from)`,
-                `\\\(?\\n?\\s*`,
-                `"(?<suffix>@goauthentik\/${submodule}\/)`,
+                `\\(?\\n?\\s*`,
+                `"(?<suffix>@goauthentik/${submodule}/)`,
 
                 `(?<path>[^"'.]+)`,
                 `(?:.[^"']+)?["']`,
-                `\\n?\\s*\\\)?;`,
+                `\\n?\\s*\\)?;`,
             ].join(""),
             "gm",
         );
