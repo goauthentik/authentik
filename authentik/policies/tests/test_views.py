5739d26bbbdb89865fc9c25cad6c14ec94479502
Jens L. <jens@goauthentik.io>
2025-11-26 00:20:35 +0100

    policies: use flow planner directly in PolicyAccessView to directly set flow context (#18372)
    * policies: use flow planner directly in PolicyAccessView to directly set flow context
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * fix oauth tests
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * fix call to get default flow
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * fix more tests
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * fix handling of ?next
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * slightly more tests
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * fix issue in random test
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    ---------
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/policies/tests/test_views.py
+++ authentik/policies/tests/test_views.py
@@ -1,17 +1,19 @@
 from django.contrib.auth.models import AnonymousUser
 from django.contrib.sessions.middleware import SessionMiddleware
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.test import RequestFactory, TestCase
 from django.urls import reverse
 
-from authentik.core.models import Application, Provider
-from authentik.core.tests.utils import create_test_flow, create_test_user
-from authentik.flows.models import FlowDesignation
+from authentik.blueprints.tests import apply_blueprint
+from authentik.core.models import Application, Group, Provider
+from authentik.core.tests.utils import create_test_brand, create_test_flow, create_test_user
+from authentik.flows.models import Flow, FlowDesignation
 from authentik.flows.planner import FlowPlan
 from authentik.flows.views.executor import SESSION_KEY_PLAN
 from authentik.lib.generators import generate_id
 from authentik.lib.tests.utils import dummy_get_response
 from authentik.policies.apps import BufferedPolicyAccessViewFlag
+from authentik.policies.models import PolicyBinding
 from authentik.policies.views import (
     QS_BUFFER_ID,
     SESSION_KEY_BUFFER,
@@ -51,6 +53,85 @@ class TestPolicyViews(TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(res.content, b"foo")
 
+    def test_pav_unauthenticated_no_flow(self):
+        """Test simple policy access view (unauthenticated access, no authentication flow)"""
+        Flow.objects.filter(designation=FlowDesignation.AUTHENTICATION).delete()
+        provider = Provider.objects.create(
+            name=generate_id(),
+        )
+        app = Application.objects.create(name=generate_id(), slug=generate_id(), provider=provider)
+
+        class TestView(PolicyAccessView):
+            def resolve_provider_application(self):
+                self.provider = provider
+                self.application = app
+
+            def get(self, *args, **kwargs):
+                return HttpResponse("foo")
+
+        req = self.factory.get("/")
+        req.user = AnonymousUser()
+        req.brand = create_test_brand()
+        middleware = SessionMiddleware(dummy_get_response)
+        middleware.process_request(req)
+        with self.assertRaises(Http404):
+            TestView.as_view()(req)
+
+    @apply_blueprint("default/flow-default-authentication-flow.yaml")
+    def test_pav_unauthenticated_flow_no_acccess(self):
+        """Test simple policy access view (unauthenticated access,
+        authentication flow with policy)"""
+        provider = Provider.objects.create(
+            name=generate_id(),
+        )
+        app = Application.objects.create(name=generate_id(), slug=generate_id(), provider=provider)
+        flow = Flow.objects.get(slug="default-authentication-flow")
+        PolicyBinding.objects.create(
+            target=flow, group=Group.objects.create(name=generate_id()), order=0
+        )
+
+        class TestView(PolicyAccessView):
+            def resolve_provider_application(self):
+                self.provider = provider
+                self.application = app
+
+            def get(self, *args, **kwargs):
+                return HttpResponse("foo")
+
+        req = self.factory.get("/")
+        req.user = AnonymousUser()
+        req.brand = create_test_brand(flow_authentication=flow)
+        middleware = SessionMiddleware(dummy_get_response)
+        middleware.process_request(req)
+        with self.assertRaises(Http404):
+            TestView.as_view()(req)
+
+    @apply_blueprint("default/flow-default-authentication-flow.yaml")
+    def test_pav_unauthenticated_next_param(self):
+        """Test simple policy access view (unauthenticated access, with checking next param)"""
+        provider = Provider.objects.create(
+            name=generate_id(),
+        )
+        app = Application.objects.create(name=generate_id(), slug=generate_id(), provider=provider)
+        flow = Flow.objects.get(slug="default-authentication-flow")
+
+        class TestView(PolicyAccessView):
+            def resolve_provider_application(self):
+                self.provider = provider
+                self.application = app
+
+            def get(self, *args, **kwargs):
+                return HttpResponse("foo")
+
+        req = self.factory.get("/")
+        req.user = AnonymousUser()
+        req.brand = create_test_brand(flow_authentication=flow)
+        middleware = SessionMiddleware(dummy_get_response)
+        middleware.process_request(req)
+        res = TestView.as_view()(req)
+        self.assertEqual(res.status_code, 302)
+        self.assertEqual(res.url, "/if/flow/default-authentication-flow/?next=%2F")
+
     @patch_flag(BufferedPolicyAccessViewFlag, True)
     def test_pav_buffer(self):
         """Test simple policy access view"""
@@ -79,13 +160,14 @@ class TestPolicyViews(TestCase):
         self.assertTrue(res.url.startswith(reverse("authentik_policies:buffer")))
 
     @patch_flag(BufferedPolicyAccessViewFlag, True)
+    @apply_blueprint("default/flow-default-authentication-flow.yaml")
     def test_pav_buffer_skip(self):
         """Test simple policy access view (skip buffer)"""
         provider = Provider.objects.create(
             name=generate_id(),
         )
         app = Application.objects.create(name=generate_id(), slug=generate_id(), provider=provider)
-        flow = create_test_flow(FlowDesignation.AUTHENTICATION)
+        flow = Flow.objects.get(slug="default-authentication-flow")
 
         class TestView(BufferedPolicyAccessView):
             def resolve_provider_application(self):
@@ -97,13 +179,16 @@ class TestPolicyViews(TestCase):
 
         req = self.factory.get("/?skip_buffer=true")
         req.user = AnonymousUser()
+        req.brand = create_test_brand(flow_authentication=flow)
         middleware = SessionMiddleware(dummy_get_response)
         middleware.process_request(req)
         req.session[SESSION_KEY_PLAN] = FlowPlan(flow.pk)
         req.session.save()
         res = TestView.as_view()(req)
         self.assertEqual(res.status_code, 302)
-        self.assertTrue(res.url.startswith(reverse("authentik_flows:default-authentication")))
+        self.assertTrue(
+            res.url.startswith(reverse("authentik_core:if-flow", kwargs={"flow_slug": flow.slug}))
+        )
 
     def test_buffer(self):
         """Test buffer view"""
