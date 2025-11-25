48254669c8d30a384000d3fc0535aca44ea8b888
Jens Langhammer <jens@goauthentik.io>
2025-11-25 02:08:17 +0100

    include system config in agent config
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/endpoints/connectors/agent/api/agent.py
+++ authentik/endpoints/connectors/agent/api/agent.py
@@ -5,6 +5,7 @@ from rest_framework.fields import (
     SerializerMethodField,
 )
 
+from authentik.api.v3.config import ConfigSerializer, ConfigView
 from authentik.core.api.utils import PassiveSerializer
 from authentik.crypto.apps import MANAGED_KEY
 from authentik.crypto.models import CertificateKeyPair
@@ -25,12 +26,17 @@ class AgentConfigSerializer(PassiveSerializer):
     nss_gid_offset = IntegerField()
     auth_terminate_session_on_expiry = BooleanField()
 
+    system_config = SerializerMethodField()
+
     def get_jwks(self, instance: AgentConnector) -> dict:
         kp = CertificateKeyPair.objects.filter(managed=MANAGED_KEY).first()
         return {"keys": [JWKSView.get_jwk_for_key(kp, "sig")]}
 
     def get_refresh_interval(self, instance: AgentConnector) -> int:
         return int(timedelta_from_string(instance.refresh_interval).total_seconds())
+
+    def get_system_config(self, instance: AgentConnector) -> ConfigSerializer:
+        return ConfigView.get_config(self.context["request"]).data
 
 
 class EnrollSerializer(PassiveSerializer):
