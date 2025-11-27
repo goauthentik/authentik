1fb71371cba2349c1e4f9eb9a75f57ecdb09e410
Jens L. <jens@goauthentik.io>
2025-11-27 19:05:57 +0100

    endpoints: AuthN and AuthZ (#18350)
    * start agent auth
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * also check windows system disk (hardcode C: for now)
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * add process table
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * include jwks
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * nonce
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * snap
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * missing exp and username (temp values)
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * fix missing meta
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * rework auth and migrations
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * include system config in agent config
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * fix a bunch of broken stuff
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * update ui
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * format
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * add device to login event
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * add ssh
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * fix tests
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * start adding tests
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * add tests
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * policies
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * remove domain name
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * fix tests
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * fix leftover
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * add device to flow context
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * dont allow access without policies
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * some ui changes
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * re-invent the wheel again
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * start updating tests
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * t
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * Revert "t"
    
    This reverts commit b74db5f5d44a334083a8311799a4ea0782b55e34.
    
    * Revert "start updating tests"
    
    This reverts commit b2524c00b2bb9834faf6bc30625fd08a6f7992e9.
    
    * Revert "re-invent the wheel again"
    
    This reverts commit c7cdf4c018b09610c479569390732b7c54611baf.
    
    * fixup
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * fix ui
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * re-migrate
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * update tests
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * fix web build
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * f
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * fix tests
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * add device users and device groups
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    * expand users
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    ---------
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

---
+++ authentik/endpoints/connectors/agent/api/agent.py
@@ -0,0 +1,55 @@
+from rest_framework.fields import (
+    BooleanField,
+    CharField,
+    IntegerField,
+    SerializerMethodField,
+)
+
+from authentik.api.v3.config import ConfigSerializer, ConfigView
+from authentik.core.api.utils import PassiveSerializer
+from authentik.crypto.apps import MANAGED_KEY
+from authentik.crypto.models import CertificateKeyPair
+from authentik.endpoints.connectors.agent.models import AgentConnector
+from authentik.lib.utils.time import timedelta_from_string
+from authentik.providers.oauth2.views.jwks import JWKSView
+
+
+class AgentConfigSerializer(PassiveSerializer):
+
+    refresh_interval = SerializerMethodField()
+
+    authorization_flow = CharField()
+    jwks = SerializerMethodField()
+
+    nss_uid_offset = IntegerField()
+    nss_gid_offset = IntegerField()
+    auth_terminate_session_on_expiry = BooleanField()
+
+    system_config = SerializerMethodField()
+
+    def get_jwks(self, instance: AgentConnector) -> dict:
+        kp = CertificateKeyPair.objects.filter(managed=MANAGED_KEY).first()
+        return {"keys": [JWKSView.get_jwk_for_key(kp, "sig")]}
+
+    def get_refresh_interval(self, instance: AgentConnector) -> int:
+        return int(timedelta_from_string(instance.refresh_interval).total_seconds())
+
+    def get_system_config(self, instance: AgentConnector) -> ConfigSerializer:
+        return ConfigView.get_config(self.context["request"]).data
+
+
+class EnrollSerializer(PassiveSerializer):
+
+    device_serial = CharField(required=True)
+    device_name = CharField(required=True)
+
+
+class AgentTokenResponseSerializer(PassiveSerializer):
+
+    token = CharField(required=True)
+    expires_in = IntegerField(required=0)
+
+
+class AgentAuthenticationResponse(PassiveSerializer):
+
+    url = CharField()
--- authentik/endpoints/connectors/agent/api/connectors.py
+++ authentik/endpoints/connectors/agent/api/connectors.py
@@ -6,11 +6,8 @@ from drf_spectacular.utils import OpenApiResponse, extend_schema
 from rest_framework.decorators import action
 from rest_framework.exceptions import PermissionDenied, ValidationError
 from rest_framework.fields import (
-    BooleanField,
     CharField,
     ChoiceField,
-    IntegerField,
-    SerializerMethodField,
 )
 from rest_framework.relations import PrimaryKeyRelatedField
 from rest_framework.request import Request
@@ -20,6 +17,11 @@ from rest_framework.viewsets import ModelViewSet
 from authentik.core.api.used_by import UsedByMixin
 from authentik.core.api.utils import PassiveSerializer
 from authentik.endpoints.api.connectors import ConnectorSerializer
+from authentik.endpoints.connectors.agent.api.agent import (
+    AgentConfigSerializer,
+    AgentTokenResponseSerializer,
+    EnrollSerializer,
+)
 from authentik.endpoints.connectors.agent.auth import (
     AgentAuth,
     AgentEnrollmentAuth,
@@ -32,37 +34,23 @@ from authentik.endpoints.connectors.agent.models import (
 )
 from authentik.endpoints.facts import DeviceFacts, OSFamily
 from authentik.endpoints.models import Device
-from authentik.lib.utils.time import timedelta_from_string
+from authentik.lib.utils.reflection import ConditionalInheritance
 
 
 class AgentConnectorSerializer(ConnectorSerializer):
 
     class Meta(ConnectorSerializer.Meta):
         model = AgentConnector
-        fields = "__all__"
-
-
-class AgentConfigSerializer(PassiveSerializer):
-
-    nss_uid_offset = IntegerField()
-    nss_gid_offset = IntegerField()
-    authentication_flow = CharField()
-    auth_terminate_session_on_expiry = BooleanField()
-    refresh_interval = SerializerMethodField()
-
-    def get_refresh_interval(self, instance: AgentConnector) -> int:
-        return int(timedelta_from_string(instance.refresh_interval).total_seconds())
-
-
-class EnrollSerializer(PassiveSerializer):
-
-    device_serial = CharField()
-    device_name = CharField()
-
-
-class EnrollResponseSerializer(PassiveSerializer):
-
-    token = CharField()
+        fields = ConnectorSerializer.Meta.fields + [
+            "snapshot_expiry",
+            "auth_terminate_session_on_expiry",
+            "refresh_interval",
+            "authorization_flow",
+            "nss_uid_offset",
+            "nss_gid_offset",
+            "challenge_key",
+            "jwt_federation_providers",
+        ]
 
 
 class MDMConfigSerializer(PassiveSerializer):
@@ -88,68 +76,19 @@ class MDMConfigResponseSerializer(PassiveSerializer):
     config = CharField(required=True)
 
 
-class AgentConnectorViewSet(UsedByMixin, ModelViewSet):
+class AgentConnectorViewSet(
+    ConditionalInheritance(
+        "authentik.enterprise.endpoints.connectors.agent.api.connectors.AgentConnectorViewSetMixin"
+    ),
+    UsedByMixin,
+    ModelViewSet,
+):
 
     queryset = AgentConnector.objects.all()
     serializer_class = AgentConnectorSerializer
     search_fields = ["name"]
     ordering = ["name"]
     filterset_fields = ["name", "enabled"]
-
-    @extend_schema(
-        request=EnrollSerializer(),
-        responses={200: EnrollResponseSerializer},
-    )
-    @action(
-        methods=["POST"],
-        detail=False,
-        authentication_classes=[AgentEnrollmentAuth],
-    )
-    def enroll(self, request: Request):
-        token: EnrollmentToken = request.auth
-        data = EnrollSerializer(data=request.data)
-        data.is_valid(raise_exception=True)
-        device, _ = Device.objects.get_or_create(
-            identifier=data.validated_data["device_serial"],
-            defaults={
-                "name": data.validated_data["device_name"],
-                "expiring": False,
-                "group": token.device_group,
-            },
-        )
-        connection, _ = AgentDeviceConnection.objects.update_or_create(
-            device=device,
-            connector=token.connector,
-        )
-        token = DeviceToken.objects.create(device=connection, expiring=False)
-        return Response(
-            {
-                "token": token.key,
-            }
-        )
-
-    @extend_schema(
-        responses=AgentConfigSerializer(),
-        request=OpenApiTypes.NONE,
-    )
-    @action(methods=["GET"], detail=False, authentication_classes=[AgentAuth])
-    def agent_config(self, request: Request):
-        token: DeviceToken = request.auth
-        connector: AgentConnector = token.device.connector.agentconnector
-        return Response(AgentConfigSerializer(connector).data)
-
-    @extend_schema(
-        request=DeviceFacts(),
-        responses={204: OpenApiResponse(description="Successfully checked in")},
-    )
-    @action(methods=["POST"], detail=False, authentication_classes=[AgentAuth])
-    def check_in(self, request: Request):
-        token: DeviceToken = request.auth
-        data = DeviceFacts(data=request.data)
-        data.is_valid(raise_exception=True)
-        connection: AgentDeviceConnection = token.device
-        connection.create_snapshot(data.validated_data)
-        return Response(status=204)
 
     @extend_schema(
         request=MDMConfigSerializer(),
@@ -167,3 +106,59 @@ class AgentConnectorViewSet(UsedByMixin, ModelViewSet):
         ctrl = connector.controller(connector)
         payload = ctrl.generate_mdm_config(data.validated_data["platform"], request, token)
         return Response({"config": payload})
+
+    @extend_schema(
+        request=EnrollSerializer(),
+        responses={200: AgentTokenResponseSerializer},
+    )
+    @action(
+        methods=["POST"],
+        detail=False,
+        authentication_classes=[AgentEnrollmentAuth],
+    )
+    def enroll(self, request: Request):
+        token: EnrollmentToken = request.auth
+        data = EnrollSerializer(data=request.data)
+        data.is_valid(raise_exception=True)
+        device, _ = Device.objects.get_or_create(
+            identifier=data.validated_data["device_serial"],
+            defaults={
+                "name": data.validated_data["device_name"],
+                "expiring": False,
+                "access_group": token.device_group,
+            },
+        )
+        connection, _ = AgentDeviceConnection.objects.update_or_create(
+            device=device,
+            connector=token.connector,
+        )
+        token = DeviceToken.objects.create(device=connection, expiring=False)
+        return Response(
+            {
+                "token": token.key,
+                "expires_in": 0,
+            }
+        )
+
+    @extend_schema(
+        request=OpenApiTypes.NONE,
+        responses=AgentConfigSerializer(),
+    )
+    @action(methods=["GET"], detail=False, authentication_classes=[AgentAuth])
+    def agent_config(self, request: Request):
+        token: DeviceToken = request.auth
+        connector: AgentConnector = token.device.connector.agentconnector
+        return Response(AgentConfigSerializer(connector, context={"request": request}).data)
+
+    @extend_schema(
+        request=DeviceFacts(),
+        responses={204: OpenApiResponse(description="Successfully checked in")},
+    )
+    @action(methods=["POST"], detail=False, authentication_classes=[AgentAuth])
+    def check_in(self, request: Request):
+        token: DeviceToken = request.auth
+        data = DeviceFacts(data=request.data)
+        data.is_valid(raise_exception=True)
+        connection: AgentDeviceConnection = token.device
+        connection.create_snapshot(data.validated_data)
+        return Response(status=204)
