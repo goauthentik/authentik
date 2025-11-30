3b0432700d2b5e6ce226ecc0092a9d01996c13db
Jens Langhammer <jens@goauthentik.io>
2025-11-30 02:57:29 +0100

    implement the thing
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/enterprise/endpoints/connectors/agent/views/apple_token.py
+++ authentik/enterprise/endpoints/connectors/agent/views/apple_token.py
@@ -1,28 +1,34 @@
-from datetime import timedelta
+from typing import Any
 
+from django.db.models.functions import SHA256
 from django.http import HttpRequest, HttpResponse
+from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.utils.timezone import now
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
-from jwt import PyJWT, decode
+from jwt import decode, encode, get_unverified_header
 from rest_framework.exceptions import ValidationError
 from structlog.stdlib import get_logger
 
-from authentik.core.models import AuthenticatedSession, Session, User
+from authentik.core.models import AuthenticatedSession, Session, User, default_token_duration
 from authentik.core.sessions import SessionStore
+from authentik.crypto.apps import MANAGED_KEY
+from authentik.crypto.models import CertificateKeyPair
 from authentik.endpoints.connectors.agent.models import (
     AgentConnector,
     AgentDeviceConnection,
     AgentDeviceUserBinding,
     AppleNonce,
+    DeviceAuthenticationToken,
+    DeviceToken,
 )
 from authentik.enterprise.endpoints.connectors.agent.http import JWEResponse
 from authentik.events.models import Event, EventAction
 from authentik.events.signals import SESSION_LOGIN_EVENT
 from authentik.providers.oauth2.constants import TOKEN_TYPE
 from authentik.providers.oauth2.id_token import IDToken
-from authentik.providers.oauth2.models import RefreshToken
+from authentik.providers.oauth2.models import JWTAlgorithms
 from authentik.root.middleware import SessionMiddleware
 
 LOGGER = get_logger()
@@ -35,42 +41,18 @@ class TokenView(View):
     connector: AgentConnector
 
     def post(self, request: HttpRequest) -> HttpResponse:
-        version = request.POST.get("platform_sso_version")
         assertion = request.POST.get("assertion", request.POST.get("request"))
         if not assertion:
             return HttpResponse(status=400)
 
-        decode_unvalidated = PyJWT().decode_complete(assertion, options={"verify_signature": False})
-        LOGGER.debug(decode_unvalidated["header"])
-        expected_kid = decode_unvalidated["header"]["kid"]
+        self.jwt_request = self.validate_request_token(assertion)
 
-        self.device_connection = (
-            AgentDeviceConnection.objects.filter(apple_sign_key_id=expected_kid)
-            .select_related("device")
-            .first()
-        )
-        self.connector = self.device_connection.connector
-
-        # Properly decode the JWT with the key from the device
-        decoded = decode(
-            assertion,
-            self.device_connection.apple_signing_key,
-            algorithms=["ES256"],
-            options={"verify_aud": False},
-        )
-        LOGGER.debug(decoded)
-
-        LOGGER.debug("got device", device=self.device_connection.device)
-
-        # Check that the nonce hasn't been used before
-        nonce = AppleNonce.objects.filter(nonce=decoded["request_nonce"]).first()
-        if not nonce:
-            return HttpResponse(status=400)
-        nonce.delete()
-
+        version = request.POST.get("platform_sso_version")
+        grant_type = request.POST.get("grant_type")
         handler_func = (
-            f"handle_v{version}_{decode_unvalidated["header"]["typ"]}".replace("-", "_")
+            f"handle_v{version}_{grant_type}".replace("-", "_")
             .replace("+", "_")
+            .replace(":", "_")
             .replace(".", "_")
         )
         handler = getattr(self, handler_func, None)
@@ -78,26 +60,63 @@ class TokenView(View):
             LOGGER.debug("Handler not found", handler=handler_func)
             return HttpResponse(status=400)
         LOGGER.debug("sending to handler", handler=handler_func)
-        return handler(decoded)
+        return handler()
 
-    def validate_device_user_response(
-        self, assertion: str
-    ) -> tuple[AgentDeviceUserBinding, dict] | None:
+    def validate_request_token(self, assertion: str) -> dict[str, Any]:
+        # Decode without validation to get header
+        header = get_unverified_header(assertion)
+        LOGGER.debug("token header", header=header)
+        expected_kid = header["kid"]
+
+        self.device_connection = (
+            AgentDeviceConnection.objects.filter(apple_sign_key_id=expected_kid)
+            .select_related("device")
+            .first()
+        )
+        self.connector = self.device_connection.connector
+        LOGGER.debug("got device", device=self.device_connection.device)
+
+        expected_aud = self.request.build_absolute_uri(
+            reverse("authentik_enterprise_endpoints_connectors_agent:psso-token")
+        )
+        # Properly decode the JWT with the key from the device
+        decoded = decode(
+            assertion,
+            self.device_connection.apple_signing_key,
+            algorithms=["ES256"],
+            audience=expected_aud,
+            issuer=str(self.connector.pk),
+        )
+        self.remote_nonce = decoded.get("nonce")
+
+        # Check that the nonce hasn't been used before
+        nonce = AppleNonce.filter_not_expired(nonce=decoded["request_nonce"]).first()
+        if not nonce:
+            raise ValidationError("Invalid nonce")
+        nonce.delete()
+        return decoded
+
+    def validate_embedded_assertion(self, assertion: str) -> tuple[AgentDeviceUserBinding, dict]:
         """Decode an embedded assertion and validate it by looking up the matching device user"""
-        decode_unvalidated = PyJWT().decode_complete(assertion, options={"verify_signature": False})
-        expected_kid = decode_unvalidated["header"]["kid"]
+        decode_unvalidated = get_unverified_header(assertion)
+        expected_kid = decode_unvalidated["kid"]
 
         device_user = AgentDeviceUserBinding.objects.filter(
             device=self.device_connection.device, apple_enclave_key_id=expected_kid
         ).first()
         if not device_user:
-            return None
-        return device_user, decode(
+            LOGGER.warning("Could not find device user binding for user")
+            raise ValidationError("Invalid request")
+        decoded: dict[str, Any] = decode(
             assertion,
             device_user.apple_secure_enclave_key,
-            audience="apple-platform-sso",
+            audience=str(self.device_connection.pk),
             algorithms=["ES256"],
         )
+        if decoded.get("nonce") != self.jwt_request.get("nonce"):
+            LOGGER.warning("Mis-matched nonce to outer assertion")
+            raise ValidationError("Invalid nonce")
+        return device_user, decoded
 
     def create_auth_session(self, user: User):
         event = Event.new(EventAction.LOGIN).from_http(self.request, user=user)
@@ -109,39 +128,55 @@ class TokenView(View):
         session = SessionMiddleware.encode_session(store.session_key, user)
         return session
 
-    def handle_v1_0_platformsso_login_request_jwt(self, decoded: dict):
-        user = None
-        if decoded["grant_type"] == "urn:ietf:params:oauth:grant-type:jwt-bearer":
-            # Decode and validate inner assertion
-            user, inner = self.validate_device_user_response(decoded["assertion"])
-            if inner["nonce"] != decoded["nonce"]:
-                LOGGER.warning("Mis-matched nonce to outer assertion")
-                raise ValidationError("Invalid request")
+    def create_id_token(self, user: User, **kwargs):
+        id_token = IDToken(
+            iss=self.request.build_absolute_uri(
+                reverse("authentik_enterprise_endpoints_connectors_agent:psso-token")
+            ),
+            sub=user.username,
+            aud=str(self.device_connection.device.pk),
+            exp=int(default_token_duration().timestamp()),
+            iat=int(now().timestamp()),
+            **kwargs,
+        )
+        kp = CertificateKeyPair.objects.filter(managed=MANAGED_KEY).first()
+        return encode(
+            id_token.to_dict(),
+            kp.private_key,
+            headers={
+                "kid": kp.kid,
+            },
+            algorithm=JWTAlgorithms.from_private_key(kp.private_key),
+        )
 
-        refresh_token = RefreshToken(
+    def handle_v1_0_urn_ietf_params_oauth_grant_type_jwt_bearer(self):
+        user, inner = self.validate_embedded_assertion(self.jwt_request["assertion"])
+        id_token = self.create_id_token(user.user)
+        device_auth_token = (
+            DeviceToken.filter_not_expired(
+                device=self.device_connection.device,
+            )
+            .annotate(key_hash=SHA256("key"))
+            .filter(key_hash=self.jwt_request["x-ak-dti"])
+            .first()
+        )
+        if not device_auth_token:
+            LOGGER.warning("failed to find device token")
+            raise ValidationError("Invalid request")
+        auth_token = DeviceAuthenticationToken.objects.create(
+            device=self.device_connection.device,
+            connector=self.connector,
             user=user.user,
-            scope=decoded["scope"],
-            expires=now() + timedelta(hours=8),
-            # provider=self.provider,
-            auth_time=now(),
-            session=None,
+            device_token=device_auth_token,
         )
-        id_token = IDToken.new(
-            # self.provider,
-            refresh_token,
-            self.request,
-        )
-        id_token.nonce = decoded["nonce"]
-        refresh_token.id_token = id_token
-        refresh_token.save()
         return JWEResponse(
             {
-                "refresh_token": refresh_token.token,
-                "refresh_token_expires_in": int((refresh_token.expires - now()).total_seconds()),
-                "id_token": refresh_token.id_token.to_jwt(self.provider),
+                "refresh_token": auth_token.token,
+                "refresh_token_expires_in": int((auth_token.expires - now()).total_seconds()),
+                "id_token": id_token,
                 "token_type": TOKEN_TYPE,
                 "session_key": self.create_auth_session(user.user),
             },
             device=self.device_connection,
-            apv=decoded["jwe_crypto"]["apv"],
+            apv=self.jwt_request["jwe_crypto"]["apv"],
         )
