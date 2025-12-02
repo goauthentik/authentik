5f7db3c6adac7043b706b24b283f88937025a515
Jens Langhammer <jens@goauthentik.io>
2025-12-02 18:33:47 +0100

    fix uuid format issues
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/admin/events/utils.ts
+++ web/src/admin/events/utils.ts
@@ -65,11 +65,18 @@ export function renderEventUser(
     }
     if (event.context.device) {
         return html`${body}<small>
-                <a href="#/endpoints/devices/${event.context.device.pk}">
+                <a href="#/endpoints/devices/${formatUUID(event.context.device.pk)}">
                     ${msg(str`Via ${event.context.device.name}`)}
                 </a>
             </small>`;
     }
 
     return body;
+}
+
+export function formatUUID(hex: string): string {
+    if (hex.length < 32) {
+        return hex;
+    }
+    return `${hex.substring(0, 8)}-${hex.substring(8, 12)}-${hex.substring(12, 16)}-${hex.substring(16, 20)}-${hex.substring(20, 32)}`;
 }
