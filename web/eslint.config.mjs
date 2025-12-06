01b91e9dd11aa1926b9e74ce87dbe1b450ffa3e4
Teffen Ellis <teffen@goauthentik.io>
2025-12-06 06:44:52 +0100

    Add ESLint rule.

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -11,6 +11,82 @@ import { defineConfig } from "eslint/config";
 // @ts-check
 
 /**
+ * @typedef RestrictedImportEntry An entry describing a restricted import and it's remedy.
+ * @property name {string} The restricted import name.
+ * @property message {string} The message to show when the import is restricted.
+ */
+
+/**
+ * @typedef RestrictedImportPattern An entry describing a restricted import pattern and it's remedy.
+ * @property regex {string} The restricted import pattern regex.
+ * @property message {string} The message to show when the import is restricted.
+ */
+
+/**
+ * @typedef RestrictedImportsOptions An entry describing a restricted import rule.
+ * @property patterns {RestrictedImportPattern[]} The restricted import patterns.
+ * @property [paths] {RestrictedImportEntry[]} Optional restricted import paths.
+ */
+
+const submodules = new Set(
+    /** @type {const} */ ([
+        "styles",
+        "common",
+        "elements",
+        "components",
+        "admin",
+        "user",
+        "flow",
+        "rac",
+    ]),
+);
+
+/**
+ * @typedef {(typeof submodules) extends Set<infer U> ? U : never} SubModule
+ */
+
+/**
+ *
+ * @param {SubModule} subpath
+ * @param {Iterable<SubModule>} allowed
+ * @returns {Config}
+ */
+function defineImportRestrictions(subpath, allowed) {
+    const allowedSet = new Set(allowed);
+    const restricted = submodules
+        // ---
+        .difference(allowedSet)
+        .add(subpath);
+
+    /**
+     * @type {RestrictedImportsOptions}
+     */
+    const options = {
+        patterns: Array.from(restricted, (mod) => ({
+            regex: `#${mod}/.+`,
+            message: `Cross-submodule import from #${mod} to #${subpath} is restricted. Consider moving the imported file to a #common if shared usage is intended.`,
+        })),
+    };
+
+    return {
+        rules: {
+            "no-restricted-imports": ["warn", options],
+        },
+        files: Array.from(allowedSet, (mod) => `src/${mod}/**/*`),
+    };
+}
+
+/**
+ * @type {Map<SubModule, SubModule[]>}
+ */
+const submoduleOrganization = new Map([
+    ["common", ["styles"]],
+    ["elements", ["styles", "common"]],
+    ["admin", ["styles", "common", "elements", "components"]],
+    ["user", ["styles", "common", "elements", "components"]],
+]);
+
+/**
  * ESLint configuration for authentik's monorepo.
  * @type {Config[]}
  */
@@ -50,6 +126,9 @@ const eslintConfig = defineConfig(
             "no-param-reassign": "off",
         },
     },
+    ...Array.from(submoduleOrganization, ([target, allowed]) => {
+        return defineImportRestrictions(target, allowed);
+    }),
     {
         rules: {
             "vars-on-top": "off",
