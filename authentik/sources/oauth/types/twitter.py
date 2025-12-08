f578f7d585914fddd49bcce0e613bf6ee4b55e86
Mohammed Muien <muien5080@gmail.com>
2025-12-08 18:08:10 +0530

    Fix Twitter OAuth integration: add users.email scope, include id field, clean email parsing

--- authentik/sources/oauth/types/twitter.py
+++ authentik/sources/oauth/types/twitter.py
@@ -15,7 +15,7 @@ class TwitterOAuthRedirect(OAuthRedirect):
     """Twitter OAuth2 Redirect"""
 
     def get_additional_parameters(self, source):  # pragma: no cover
-        scopes = ["users.read", "tweet.read"]
+        scopes = ["users.read", "tweet.read","users.email"]
         # If admin has defined custom scopes in the provider UI, merge them
         configured = getattr(source, "scope", []) or []
         for s in configured:
@@ -47,17 +47,17 @@ class TwitterType(SourceType):
 
     authorization_url = "https://twitter.com/i/oauth2/authorize"
     access_token_url = "https://api.twitter.com/2/oauth2/token"  # nosec
-    profile_url = "https://api.twitter.com/2/users/me?user.fields=verified,username,name,profile_image_url,confirmed_email"
+    profile_url = (
+    "https://api.twitter.com/2/users/me"
+    "?user.fields=id,name,username,confirmed_email,profile_image_url,verified"
+    )
 
     pkce = PKCEMethod.S256
 
     def get_base_user_properties(self, info: dict[str, Any], **kwargs) -> dict[str, Any]:
         data = info.get("data", {})
-        email = (
-            data.get("confirmed_email")
-            or data.get("email")
-            or None
-        )
+        email = data.get("confirmed_email")
+
         return {
             "username": data.get("username"),
             "email": email,
