d8cc2c6d4f937e22a22f7ac169986dc4ba4bfbda
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2025-12-09 17:57:53 +0100

    also add configuration error event
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/admin/files/apps.py
+++ authentik/admin/files/apps.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+
 from authentik.blueprints.apps import ManagedAppConfig
+from authentik.lib.config import CONFIG
 
 
 class AuthentikFilesConfig(ManagedAppConfig):
@@ -6,3 +9,18 @@ class AuthentikFilesConfig(ManagedAppConfig):
     label = "authentik_admin_files"
     verbose_name = "authentik Files"
     default = True
+
+    def ready(self):
+        super().ready()
+
+        from authentik.events.models import Event, EventAction
+
+        if (
+            CONFIG.get("storage.media.backend", CONFIG.get("storage.backend", "file")) == "file"
+            and Path("/media").exists()
+        ):
+            Event.new(
+                EventAction.CONFIGURATION_ERROR,
+                message="/media has been moved to /data/media. "
+                "Check the release notes for migration steps.",
+            ).save()
