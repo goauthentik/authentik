bf60bfa005d14f1173d6b7605f2c9ad9bd2b5e35
dewi-tik <dewi@goauthentik.io>
2025-12-16 17:20:56 +0200

    Fix helper text

--- web/src/admin/endpoints/connectors/agent/EnrollmentTokenForm.ts
+++ web/src/admin/endpoints/connectors/agent/EnrollmentTokenForm.ts
@@ -82,7 +82,9 @@ export class EnrollmentTokenForm extends WithBrandConfig(ModelForm<EnrollmentTok
                     .group=${this.instance?.deviceGroup}
                 ></ak-endpoints-device-group-search>
                 <p class="pf-c-form__helper-text">
-                    ${msg("Select a group for the device to be added to upon enrollment.")}
+                    ${msg(
+                        "Select a device access group for the device to be added to upon enrollment.",
+                    )}
                 </p>
             </ak-form-element-horizontal>
             <ak-form-element-horizontal name="expiring">
