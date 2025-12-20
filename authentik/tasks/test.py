7fa25dd8c4a67d73e1d3a2508f67e94dad9ed30c
Jens Langhammer <jens@goauthentik.io>
2025-12-20 15:22:23 +0100

    tasks: improved tests
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/tasks/test.py
+++ authentik/tasks/test.py
@@ -9,24 +9,25 @@ from dramatiq.results.middleware import Results
 from dramatiq.worker import Worker, _ConsumerThread, _WorkerThread
 
 from authentik.tasks.broker import PostgresBroker
-from authentik.tasks.middleware import MetricsMiddleware
+
+TESTING_QUEUE = "testing"
 
 
 class TestWorker(Worker):
-    def __init__(self, queue_name: str, broker: Broker):
+    def __init__(self, broker: Broker):
         super().__init__(broker=broker)
         self.work_queue = PriorityQueue()
         self.consumers = {
-            queue_name: _ConsumerThread(
+            TESTING_QUEUE: _ConsumerThread(
                 broker=self.broker,
-                queue_name=queue_name,
+                queue_name=TESTING_QUEUE,
                 prefetch=2,
                 work_queue=self.work_queue,
                 worker_timeout=1,
             ),
         }
-        self.consumers[queue_name].consumer = self.broker.consume(
-            queue_name=queue_name,
+        self.consumers[TESTING_QUEUE].consumer = self.broker.consume(
+            queue_name=TESTING_QUEUE,
             prefetch=2,
             timeout=1,
         )
@@ -39,18 +40,25 @@ class TestWorker(Worker):
 
         self.broker.emit_before("worker_boot", self)
         self.broker.emit_after("worker_boot", self)
+        self.broker.emit_after("process_boot")
 
     def process_message(self, message: MessageProxy):
         self.work_queue.put(message)
-        self.consumers[message.queue_name].consumer.in_processing.add(message.message_id)
+        self.consumers[TESTING_QUEUE].consumer.in_processing.add(message.message_id)
         self._worker.process_message(message)
 
 
 class TestBroker(PostgresBroker):
+    worker: TestWorker | None = None
+
+    def start(self):
+        self.worker = TestWorker(broker=self)
+
     def enqueue(self, *args, **kwargs):
-        message = super().enqueue(*args, **kwargs)
-        worker = TestWorker(message.queue_name, broker=self)
-        worker.process_message(MessageProxy(message))
+        message = super().enqueue(*args, **kwargs).copy(queue_name=TESTING_QUEUE)
+        if not self.worker:
+            return message
+        self.worker.process_message(MessageProxy(message))
         return message
 
 
@@ -68,8 +76,6 @@ def use_test_broker():
         middleware: dramatiq.middleware.middleware.Middleware = import_string(middleware_class)(
             **middleware_kwargs,
         )
-        if isinstance(middleware, MetricsMiddleware):
-            continue
         if isinstance(middleware, Retries):
             middleware.max_retries = 0
         if isinstance(middleware, Results):
@@ -78,5 +84,5 @@ def use_test_broker():
                 **Conf().result_backend_kwargs,
             )
         broker.add_middleware(middleware)
-
+    broker.start()
     dramatiq.set_broker(broker)
