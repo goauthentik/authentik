5e30f5c795ef8b40e233452a20d21b502c21e1cf
Jens Langhammer <jens@goauthentik.io>
2025-12-22 13:44:03 +0100

    fix implicit
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- tests/openid_conformance/test_implicit.py
+++ tests/openid_conformance/test_implicit.py
@@ -2,9 +2,9 @@ from tests.e2e.utils import retry
 from tests.openid_conformance.base import TestOpenIDConformance
 
 
-class TestOpenIDConformanceBasic(TestOpenIDConformance):
+class TestOpenIDConformanceImplicit(TestOpenIDConformance):
 
     @retry()
-    def test_oidcc_basic_certification_test(self):
-        test_plan_name = "oidcc-basic-certification-test-plan"
+    def test_oidcc_implicit_certification_test_plan(self):
+        test_plan_name = "oidcc-implicit-certification-test-plan"
         self.run_test(test_plan_name, self.test_plan_config)
