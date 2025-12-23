5bbeb45dd592a6b0e922230191d395f9ba748a5c
Jens Langhammer <jens@goauthentik.io>
2025-12-23 12:03:23 +0100

    different test
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/api/tests/test_schema.py
+++ authentik/api/tests/test_schema.py
@@ -1,6 +1,6 @@
 """Schema generation tests"""
 
-from hashlib import sha512
+from pathlib import Path
 
 from django.core.management import call_command
 from django.urls import reverse
@@ -8,11 +8,6 @@ from rest_framework.test import APITestCase
 from yaml import safe_load
 
 from authentik.lib.config import CONFIG
-
-
-def file_hash(path: str) -> str:
-    with open(path) as _f:
-        return sha512(_f.read().encode()).hexdigest()
 
 
 class TestSchemaGeneration(APITestCase):
@@ -34,18 +29,15 @@ class TestSchemaGeneration(APITestCase):
 
     def test_build_schema(self):
         """Test schema build command"""
-        blueprint_file = "blueprints/schema.json"
-        api_file = "schema.yml"
-        before_blueprint = file_hash(blueprint_file)
-        before_api = file_hash(api_file)
+        blueprint_file = Path("blueprints/schema.json")
+        api_file = Path("schema.yml")
+        blueprint_file.unlink()
+        api_file.unlink()
         with (
             CONFIG.patch("debug", True),
             CONFIG.patch("tenants.enabled", True),
             CONFIG.patch("outposts.disable_embedded_outpost", True),
         ):
             call_command("build_schema")
-        after_blueprint = file_hash(blueprint_file)
-        after_api = file_hash(api_file)
-
-        self.assertEqual(before_blueprint, after_blueprint, "Blueprint schema changed")
-        self.assertEqual(before_api, after_api, "API schema changed")
+        self.assertTrue(blueprint_file.exists())
+        self.assertTrue(api_file.exists())
