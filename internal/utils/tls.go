b7a1fab12e3eed36f640939caef563db62509a6d
Jens Langhammer <jens@goauthentik.io>
2025-12-28 13:19:55 +0100

    internal: update TLS Suite
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- internal/utils/tls.go
+++ internal/utils/tls.go
@@ -3,24 +3,27 @@ package utils
 import "crypto/tls"
 
 func GetTLSConfig() *tls.Config {
+	// Based on
+	// https://ssl-config.mozilla.org/#server=go&version=1.25&config=intermediate&guideline=5.7
 	tlsConfig := &tls.Config{
 		MinVersion: tls.VersionTLS12,
-		MaxVersion: tls.VersionTLS12,
+		CurvePreferences: []tls.CurveID{
+			tls.X25519,
+			tls.CurveP256,
+			tls.CurveP384,
+		},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			// Insecure SWEET32 attack ciphers, TLS config uses a fallback
+			// tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
+			// tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+		},
 	}
-
-	// Insecure SWEET32 attack ciphers, TLS config uses a fallback
-	insecureCiphersIds := []uint16{
-		tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
-		tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
-	}
-	defaultSecureCiphers := []uint16{}
-	for _, cs := range tls.CipherSuites() {
-		for _, icsId := range insecureCiphersIds {
-			if cs.ID != icsId {
-				defaultSecureCiphers = append(defaultSecureCiphers, cs.ID)
-			}
-		}
-	}
-	tlsConfig.CipherSuites = defaultSecureCiphers
 	return tlsConfig
 }
