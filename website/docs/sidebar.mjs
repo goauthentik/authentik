921750d47303ed4707c688e16158f942622f042b
Jens Langhammer <jens@goauthentik.io>
2025-12-31 17:36:43 +0100

    rename, update
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- website/docs/sidebar.mjs
+++ website/docs/sidebar.mjs
@@ -730,7 +730,7 @@ const items = [
                             type: "doc",
                             id: "endpoint-devices/authentik-agent/release-notes/index",
                         },
-                        items: ["endpoint-devices/authentik-agent/release-notes/v0.35.0"],
+                        items: ["endpoint-devices/authentik-agent/release-notes/v0.35"],
                     },
                 ],
             },
