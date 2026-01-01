2518fb9ac3622ccecce05429af0642980322b772
Jens Langhammer <jens@goauthentik.io>
2026-01-01 16:27:38 +0100

    fix misleading variable
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- website/docusaurus-theme/releases/version.mjs
+++ website/docusaurus-theme/releases/version.mjs
@@ -1,4 +1,4 @@
-export const YEAR_MS = 2 * 360 * 24 * 60 * 60 * 1000;
+export const TWO_YEAR_MS = 2 * 365 * 24 * 60 * 60 * 1000;
 
 /**
  * @param {import("semver").SemVer} parsed
@@ -7,7 +7,7 @@ export function validateVersion(parsed) {
     const versionDate = new Date(parsed.major, parsed.minor - 1);
     const now = new Date();
 
-    if (now.getTime() - versionDate.getTime() >= YEAR_MS) {
+    if (now.getTime() - versionDate.getTime() >= TWO_YEAR_MS) {
         throw new Error(
             `Semver version ${versionDate.getFullYear()}.${versionDate.getMonth()} is older than 2 years`,
         );
