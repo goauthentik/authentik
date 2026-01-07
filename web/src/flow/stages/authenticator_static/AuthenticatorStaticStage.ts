936c3227e6961af828de76468bbe12b9f5c956a4
Teffen Ellis <teffen@goauthentik.io>
2026-01-07 17:56:34 +0100

    Clean up flow user details.

--- web/src/flow/stages/authenticator_static/AuthenticatorStaticStage.ts
+++ web/src/flow/stages/authenticator_static/AuthenticatorStaticStage.ts
@@ -1,6 +1,7 @@
 import "#flow/FormStatic";
 import "#flow/components/ak-flow-card";
 
+import { FlowUserDetails } from "#flow/FormStatic";
 import { BaseStage } from "#flow/stages/base";
 
 import {
@@ -11,7 +12,6 @@ import {
 import { msg } from "@lit/localize";
 import { css, CSSResult, html, TemplateResult } from "lit";
 import { customElement } from "lit/decorators.js";
-import { ifDefined } from "lit/directives/if-defined.js";
 
 import PFButton from "@patternfly/patternfly/components/Button/button.css";
 import PFForm from "@patternfly/patternfly/components/Form/form.css";
@@ -53,17 +53,8 @@ export class AuthenticatorStaticStage extends BaseStage<
     render(): TemplateResult {
         return html`<ak-flow-card .challenge=${this.challenge}>
             <form class="pf-c-form" @submit=${this.submitForm}>
-                <ak-form-static
-                    class="pf-c-form__group"
-                    userAvatar="${this.challenge.pendingUserAvatar}"
-                    user=${this.challenge.pendingUser}
-                >
-                    <div slot="link">
-                        <a href="${ifDefined(this.challenge.flowInfo?.cancelUrl)}"
-                            >${msg("Not you?")}</a
-                        >
-                    </div>
-                </ak-form-static>
+                ${FlowUserDetails({ challenge: this.challenge })}
+
                 <ul class="pf-c-form__group token-list">
                     ${this.challenge.codes.map((token) => {
                         return html`<li>${token}</li>`;
