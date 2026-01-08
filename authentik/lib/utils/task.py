15744b18e93f1591072b54b8d056c4761a9ade1e
Jens Langhammer <jens@goauthentik.io>
2026-01-08 19:56:08 +0100

    handle mode file not found
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/lib/utils/task.py
+++ authentik/lib/utils/task.py
@@ -1,12 +1,20 @@
+from functools import lru_cache
 from pathlib import Path
 from tempfile import gettempdir
 from typing import Any
 
 from dramatiq import Actor
 
-mode = (Path(gettempdir()) / "authentik-mode").read_text().strip()
+
+@lru_cache
+def current_mode():
+    try:
+        return (Path(gettempdir()) / "authentik-mode").read_text().strip()
+    except OSError:
+        return "server"
+
 
 def run_task[T: Any](t: Actor[(str), T], *args, **kwargs) -> T:
-    if mode == "worker":
+    if current_mode() == "worker":
         return callable(*args, **kwargs)
     return callable().send(*args, **kwargs).get_result(block=True)
