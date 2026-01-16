3a046e588a8987f28ce8846c24c5ddf249a18bcc
Dominic R <dominic@sdko.org>
2026-01-15 19:09:04 -0500

    core: add nosec comment for empty password string in signal

--- authentik/core/models.py
+++ authentik/core/models.py
@@ -570,7 +570,7 @@ class User(SerializerModel, AttributesMixin, AbstractUser):
 
             if not sender:
                 sender = self
-            password_changed.send(sender=sender, user=self, password="", request=request)
+            password_changed.send(sender=sender, user=self, password="", request=request)  # nosec B106
         self.password = password_hash
         self.password_change_date = now()
 
