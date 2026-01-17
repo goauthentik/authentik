97e337eb163e3f6850f2788a7c8552afb25d0b9b
Jens Langhammer <jens@goauthentik.io>
2026-01-17 03:16:07 +0100

    add timeout
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- tests/e2e/utils.py
+++ tests/e2e/utils.py
@@ -236,7 +236,7 @@ class SeleniumTestCase(DockerTestCase, StaticLiveServerTestCase):
     def _join_broker(self):
         broker = get_broker()
         for queue in broker.get_declared_queues():
-            broker.join(queue)
+            broker.join(queue, timeout=1)
         broker.stop()
 
     def tearDown(self):
