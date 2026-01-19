39dc5c8f0bf2ad345b3b67845b93b294632c6a5f
Jens Langhammer <jens@goauthentik.io>
2026-01-19 03:17:11 +0100

    aight
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/enterprise/providers/ws_federation/processors/metadata.py
+++ authentik/enterprise/providers/ws_federation/processors/metadata.py
@@ -16,6 +16,20 @@ class MetadataProcessor(BaseMetadataProcessor):
         self.add_role_descriptor_sts(entity_descriptor)
         super().add_children(entity_descriptor)
 
+    def add_endpoint(self, parent: Element, name: str):
+        endpoint = SubElement(
+            parent, f"{{{NS_WS_FED_PROTOCOL}}}{name}", nsmap=NS_MAP
+        )
+        endpoint_ref = SubElement(endpoint, f"{{{NS_ADDRESSING}}}EndpointReference", nsmap=NS_MAP)
+
+        address = SubElement(endpoint_ref, f"{{{NS_ADDRESSING}}}Address", nsmap=NS_MAP)
+        address.text = self.http_request.build_absolute_uri(
+            reverse(
+                "authentik_providers_ws_federation:wsfed",
+                kwargs={"application_slug": self.provider.application.slug},
+            )
+        )
+
     def add_role_descriptor_sts(self, entity_descriptor: Element):
         role_descriptor = SubElement(
             entity_descriptor, f"{{{NS_SAML_METADATA}}}RoleDescriptor", nsmap=NS_MAP
@@ -27,15 +41,5 @@ class MetadataProcessor(BaseMetadataProcessor):
         if signing_descriptor is not None:
             role_descriptor.append(signing_descriptor)
 
-        endpoint = SubElement(
-            role_descriptor, f"{{{NS_WS_FED_PROTOCOL}}}SecurityTokenServiceEndpoint", nsmap=NS_MAP
-        )
-        endpoint_ref = SubElement(endpoint, f"{{{NS_ADDRESSING}}}EndpointReference", nsmap=NS_MAP)
-
-        address = SubElement(endpoint_ref, f"{{{NS_ADDRESSING}}}Address", nsmap=NS_MAP)
-        address.text = self.http_request.build_absolute_uri(
-            reverse(
-                "authentik_providers_ws_federation:wsfed",
-                kwargs={"application_slug": self.provider.application.slug},
-            )
-        )
+        self.add_endpoint(role_descriptor, "SecurityTokenServiceEndpoint")
+        self.add_endpoint(role_descriptor, "PassiveRequestorEndpoint")
