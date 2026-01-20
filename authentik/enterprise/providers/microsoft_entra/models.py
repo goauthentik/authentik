a755bd0e4af243e38de8935d91ffa303f779dd46
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2026-01-20 18:17:19 +0100

    lib/sync/outgoing: handle deletions even if object does not exist in database (#18968)

--- authentik/enterprise/providers/microsoft_entra/models.py
+++ authentik/enterprise/providers/microsoft_entra/models.py
@@ -141,6 +141,18 @@ class MicrosoftEntraProvider(OutgoingSyncProvider, BackchannelProvider):
             return Group.objects.all().order_by("pk")
         raise ValueError(f"Invalid type {type}")
 
+    @classmethod
+    def get_object_mappings(cls, obj: User | Group) -> list[tuple[str, str]]:
+        if isinstance(obj, User):
+            return list(
+                obj.microsoftentraprovideruser_set.values_list("provider__pk", "microsoft_id")
+            )
+        if isinstance(obj, Group):
+            return list(
+                obj.microsoftentraprovidergroup_set.values_list("provider__pk", "microsoft_id")
+            )
+        raise ValueError(f"Invalid type {type(obj)}")
+
     def microsoft_credentials(self):
         return {
             "credentials": ClientSecretCredential(
