7965525842fe2b77317c6a91c75e4d70bf87104c
Jens Langhammer <jens@goauthentik.io>
2026-01-22 21:05:39 +0100

    wip
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    # Conflicts:
    #	authentik/core/signals.py
    #	authentik/stages/identification/stage.py
    #	web/src/flow/stages/RedirectStage.ts

---
+++ web/src/flow/tabs/orchestrator.ts
@@ -0,0 +1,45 @@
+import { Broadcast } from "#flow/tabs/broadcast";
+import { TabID } from "#flow/tabs/TabID";
+
+import { ConsoleLogger } from "#logger/browser";
+
+const lockKey = "authentik-tab-locked";
+const logger = ConsoleLogger.prefix("mtab/orchestrate");
+
+export function multiTabOrchestrateLeave() {
+    Broadcast.shared.akExitTab();
+    TabID.shared.clear();
+}
+
+export async function multiTabOrchestrateResume() {
+    if (localStorage.getItem(lockKey)) {
+        logger.debug("Tabs locked, leaving.");
+        multiTabOrchestrateLeave();
+        return;
+    }
+    logger.debug("Locking tabs");
+    localStorage.setItem(lockKey, new Date().getTime().toString());
+
+    const tabs = await Broadcast.shared.akTabDiscover();
+    logger.debug("Got list of tabs", tabs);
+
+    for (const tab of tabs) {
+        logger.debug("Telling tab to continue", tab);
+        Broadcast.shared.akResumeTab(tab);
+        const done = Promise.withResolvers<void>();
+        const checker = setInterval(() => {
+            if (Broadcast.shared.exitedTabIds.includes(tab)) {
+                logger.debug("tab exited", tab);
+                setTimeout(() => {
+                    logger.debug("continue exited", tab);
+                    done.resolve();
+                }, 1000);
+                clearInterval(checker);
+            }
+        }, 1);
+        await done.promise;
+        logger.debug("Tab done, continuing", tab);
+    }
+    logger.debug("All tabs done.");
+    localStorage.removeItem(lockKey);
+}
