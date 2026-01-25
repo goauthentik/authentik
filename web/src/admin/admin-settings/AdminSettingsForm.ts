5ea9eca13c87d6fb6ef57e717c204940e92ff3a4
Jens Langhammer <jens@goauthentik.io>
2026-01-25 23:49:56 +0100

    policies: remove BufferedPolicyAccessView
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>
    
    # Conflicts:
    #	authentik/policies/views.py
    #	authentik/providers/oauth2/views/authorize.py
    #	schema.yml
    #	tests/e2e/test_provider_saml.py

--- web/src/admin/admin-settings/AdminSettingsForm.ts
+++ web/src/admin/admin-settings/AdminSettingsForm.ts
@@ -267,15 +267,6 @@ export class AdminSettingsForm extends Form<SettingsRequest> {
             >
                 <div class="pf-c-form">
                     <ak-switch-input
-                        name="flags.policiesBufferedAccessView"
-                        ?checked=${settings?.flags.policiesBufferedAccessView ?? false}
-                        label=${msg("Buffer PolicyAccessView requests")}
-                        help=${msg(
-                            "When enabled, parallel requests for application authorization will be buffered instead of conflicting with other flows.",
-                        )}
-                    >
-                    </ak-switch-input>
-                    <ak-switch-input
                         name="flags.flowsRefreshOthers"
                         ?checked=${settings?.flags.flowsRefreshOthers ?? false}
                         label=${msg("Refresh other flow tabs upon authentication")}
