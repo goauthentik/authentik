b47016cd3095a3e8db18b4d3ba575ea9ff8f91ea
Tana M Berry <tana@goauthentik.io>
2026-01-27 09:34:27 -0600

    thanks Teffen

--- website/docusaurus-theme/theme/utils/mermaid_icons.js
+++ website/docusaurus-theme/theme/utils/mermaid_icons.js
@@ -1,33 +1,19 @@
-import { icons } from "@iconify-json/fa7-regular";
-import { icons } from "@iconify-json/fa7-solid";
-import { icons } from "@iconify-json/fluent-mdl2";
-import { icons } from "@iconify-json/mdi";
+import * as fa7Regular from "@iconify-json/fa7-regular";
+import * as fa7Solid from "@iconify-json/fa7-solid";
+import * as mdi from "@iconify-json/mdi";
 import mermaid from "mermaid";
 
 mermaid.registerIconPacks([
     {
-        name: icons.prefix, // To use the prefix defined in the icon pack
-        icons,
+        name: fa7Regular.icons.prefix,
+        icons: fa7Regular.icons,
     },
-]);
-
-mermaid.registerIconPacks([
     {
-        name: icons.prefix, // To use the prefix defined in the icon pack
-        icons,
+        name: fa7Solid.icons.prefix,
+        icons: fa7Solid.icons,
     },
-]);
-
-mermaid.registerIconPacks([
     {
-        name: icons.prefix, // To use the prefix defined in the icon pack
-        icons,
-    },
-]);
-
-mermaid.registerIconPacks([
-    {
-        name: icons.prefix, // To use the prefix defined in the icon pack
-        icons,
+        name: mdi.icons.prefix,
+        icons: mdi.icons,
     },
 ]);
