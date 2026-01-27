cc49cceccbc8f91383cbe7fecb68afbcda74773f
Dominic R <dominic@sdko.org>
2026-01-26 19:13:06 -0500

    web/flows: fix stray "}" rendered in AccessDeniedStage
    Closes:  https://github.com/goauthentik/authentik/issues/19774

--- web/src/flow/stages/access_denied/AccessDeniedStage.ts
+++ web/src/flow/stages/access_denied/AccessDeniedStage.ts
@@ -50,7 +50,6 @@ export class AccessDeniedStage extends BaseStage<
                           </a>
                       </fieldset>`
                     : nothing}
-                }
             </form>
         </ak-flow-card>`;
     }
