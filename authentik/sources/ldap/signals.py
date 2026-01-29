363c1dfdbebd608b37c22431c72d71d881ccb647
Dominic R <dominic@sdko.org>
2026-01-28 21:31:39 -0500

    sources/kerberos,ldap: Gergo's review

--- authentik/sources/ldap/signals.py
+++ authentik/sources/ldap/signals.py
@@ -39,6 +39,8 @@ def ldap_password_validate(sender, password: str, plan_context: dict[str, Any], 
 @receiver(password_changed)
 def ldap_sync_password(sender, user: User, password: str, **_):
     """Connect to ldap and update password."""
+    if not password:  # No raw password available (e.g. set via hash)
+        return
     sources = LDAPSource.objects.filter(sync_users_password=True, enabled=True)
     if not sources.exists():
         return
