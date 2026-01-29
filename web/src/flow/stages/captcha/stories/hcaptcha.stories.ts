cec59b73502411b237189f12e1646f0de66dd824
Teffen Ellis <592134+GirlBossRush@users.noreply.github.com>
2026-01-29 07:36:24 +0100

    Flesh out stories.

--- web/src/flow/stages/captcha/stories/hcaptcha.stories.ts
+++ web/src/flow/stages/captcha/stories/hcaptcha.stories.ts
@@ -9,8 +9,62 @@ export default {
     title: "Flow / Stages / <ak-stage-captcha> / hCaptcha",
 } satisfies Meta<typeof import("../CaptchaStage.js").CaptchaStage>;
 
-export const ChallengeHCaptcha = flowFactory("ak-stage-captcha", {
-    jsUrl: "https://js.hcaptcha.com/1/api.js",
-    siteKey: "10000000-ffff-ffff-ffff-000000000001",
-    interactive: true,
-});
+export const VisibleChallengePasses = flowFactory(
+    "ak-stage-captcha",
+    {
+        jsUrl: "https://js.hcaptcha.com/1/api.js",
+        siteKey: "10000000-ffff-ffff-ffff-000000000001",
+        interactive: true,
+    },
+    {
+        name: "Visible Challenge - Always Passes",
+    },
+);
+
+export const EnterpriseAccountSafe = flowFactory(
+    "ak-stage-captcha",
+    {
+        jsUrl: "https://js.hcaptcha.com/1/api.js",
+        siteKey: "20000000-ffff-ffff-ffff-000000000002",
+        interactive: true,
+    },
+    {
+        name: "Enterprise Account - Safe",
+    },
+);
+
+export const EnterpriseAccountBotDetected = flowFactory(
+    "ak-stage-captcha",
+    {
+        jsUrl: "https://js.hcaptcha.com/1/api.js",
+        siteKey: "30000000-ffff-ffff-ffff-000000000003",
+        interactive: true,
+    },
+    {
+        name: "Enterprise Account - Bot Detected",
+    },
+);
+
+export const InvisibleChallengePasses = flowFactory(
+    "ak-stage-captcha",
+    {
+        jsUrl: "https://js.hcaptcha.com/1/api.js",
+        siteKey: "10000000-ffff-ffff-ffff-000000000001",
+        interactive: false,
+    },
+    {
+        name: "Invisible Challenge - Always Passes",
+    },
+);
+
+export const InvisibleEnterpriseAccountBotDetected = flowFactory(
+    "ak-stage-captcha",
+    {
+        jsUrl: "https://js.hcaptcha.com/1/api.js",
+        siteKey: "30000000-ffff-ffff-ffff-000000000003",
+        interactive: false,
+    },
+    {
+        name: "Invisible Enterprise Account - Bot Detected",
+    },
+);
