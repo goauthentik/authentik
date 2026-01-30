1dc964b98e35b8605659cfda7ff6b420188608f7
Dominic R <dominic@sdko.org>
2026-01-30 15:49:15 -0500

    core: Please work

--- authentik/events/migrations/0017_alter_event_action.py
+++ authentik/events/migrations/0017_alter_event_action.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("authentik_events", "0016_merge_20260130_0135"),
+        ("authentik_events", "0016_notificationrule_destination_security_email_and_more"),
     ]
 
     operations = [
