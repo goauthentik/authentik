8ad572ba3502bcb6a042fa76e757a8f6fc8e7512
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2026-02-02 19:14:14 +0100

    wip
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- crates/axum/src/accept/proxy_protocol.rs
+++ crates/axum/src/accept/proxy_protocol.rs
@@ -1,9 +1,16 @@
-use std::{net::SocketAddr, time::Duration};
+use std::ops::Deref;
+use std::task::{Context, Poll};
+use std::{net::SocketAddr, pin::Pin, time::Duration};
 
 use axum::{Extension, middleware::AddExtension};
 use axum_server::accept::{Accept, DefaultAcceptor};
+use bytes::{Buf, BufMut, BytesMut};
+use eyre::Result;
+use eyre::eyre;
 use futures::future::BoxFuture;
-use tokio::io::{AsyncRead, AsyncWrite};
+use pin_project_lite::pin_project;
+use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
+use tokio::net::TcpStream;
 use tokio_rustls::{rustls::pki_types::CertificateDer, server::TlsStream};
 use tower::Layer;
 
@@ -98,3 +105,82 @@ where
 //         })
 //     }
 // }
+
+pin_project! {
+    pub struct ProxyProtocolStream<T> {
+        #[pin]
+        inner: T,
+        buf: BytesMut,
+        orig_source: Option<SocketAddr>,
+        orig_destination: Option<SocketAddr>,
+    }
+}
+
+impl<T> ProxyProtocolStream<T> {
+    fn get_pin_mut(self: Pin<&mut Self>) -> Pin<&mut T> {
+        self.project().inner
+    }
+
+    pub fn try_into_inner(self) -> Result<T> {
+        if self.buf.is_empty() {
+            Ok(self.inner)
+        } else {
+            Err(eyre!(
+                "Cannot return inner stream because buffer is not empty"
+            ))
+        }
+    }
+}
+
+impl<T> AsRef<T> for ProxyProtocolStream<T> {
+    fn as_ref(&self) -> &T {
+        &self.inner
+    }
+}
+
+impl Deref for ProxyProtocolStream<TcpStream> {
+    type Target = TcpStream;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl<T: AsyncRead> ProxyProtocolStream<T> {
+    pub async fn new(stream: T) -> Result<Self> {
+        // TODO: read the proxy protocol here
+        Ok(Self {
+            inner: stream,
+            buf: BytesMut::with_capacity(100),
+            orig_source: None,
+            orig_destination: None,
+        })
+    }
+}
+
+impl<T: AsyncRead> AsyncRead for ProxyProtocolStream<T> {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<std::io::Result<()>> {
+        let this = self.project();
+        if this.buf.is_empty() {
+            this.inner.poll_read(cx, buf)
+        } else {
+            // send remaining data from buffer
+            let to_copy = this.buf.remaining().min(buf.remaining());
+            buf.put_slice(&this.buf[0..to_copy]);
+            this.buf.advance(to_copy);
+
+            // there is still space in output buffer
+            // let's try if we have some bytes to add there
+            if buf.has_remaining_mut()
+                && let Poll::Ready(Err(e)) = this.inner.poll_read(cx, buf)
+            {
+                return Poll::Ready(Err(e));
+            }
+            Poll::Ready(Ok(()))
+        }
+    }
+}
