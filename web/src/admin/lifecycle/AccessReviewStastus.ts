ff114169e5a5456725081326082964871f26bb92
Alexander Tereshkin <a@tereshk.in>
2026-02-08 12:14:07 +0200

    enterprise/lifecycle: use ts-pattern and return `nothing` instead of empty html

--- web/src/admin/lifecycle/AccessReviewStastus.ts
+++ web/src/admin/lifecycle/AccessReviewStastus.ts
@@ -3,8 +3,10 @@ import { PFColor } from "#elements/Label";
 
 import { ReviewStateEnum } from "@goauthentik/api";
 
+import { match } from "ts-pattern";
+
 import { msg } from "@lit/localize";
-import { CSSResult, html, TemplateResult } from "lit";
+import { CSSResult, html, nothing } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
 import PFButton from "@patternfly/patternfly/components/Button/button.css";
@@ -14,21 +16,28 @@ export class AccessReviewStatus extends AKElement {
     public static styles: CSSResult[] = [PFButton];
 
     @property()
-    status?: ReviewStateEnum;
+    status: ReviewStateEnum = ReviewStateEnum.UnknownDefaultOpenApi;
 
-    render(): TemplateResult {
-        switch (this.status) {
-            case ReviewStateEnum.Pending:
-                return html` <ak-label color=${PFColor.Orange}>${msg("Pending review")}</ak-label>`;
-            case ReviewStateEnum.Reviewed:
-                return html` <ak-label color=${PFColor.Green}>${msg("Reviewed")}</ak-label>`;
-            case ReviewStateEnum.Overdue:
-                return html` <ak-label color=${PFColor.Red}>${msg("Overdue")}</ak-label>`;
-            case ReviewStateEnum.Canceled:
-                return html` <ak-label color=${PFColor.Grey}>${msg("Canceled")}</ak-label>`;
-            default:
-                return html``;
-        }
+    render() {
+        return match(this.status)
+            .with(
+                ReviewStateEnum.Pending,
+                () => html` <ak-label color=${PFColor.Orange}>${msg("Pending review")}</ak-label>`,
+            )
+            .with(
+                ReviewStateEnum.Reviewed,
+                () => html`<ak-label color=${PFColor.Green}>${msg("Reviewed")}</ak-label>`,
+            )
+            .with(
+                ReviewStateEnum.Overdue,
+                () => html`<ak-label color=${PFColor.Red}>${msg("Overdue")}</ak-label>`,
+            )
+            .with(
+                ReviewStateEnum.Canceled,
+                () => html`<ak-label color=${PFColor.Grey}>${msg("Canceled")}</ak-label>`,
+            )
+            .with(ReviewStateEnum.UnknownDefaultOpenApi, () => nothing)
+            .exhaustive();
     }
 }
 
