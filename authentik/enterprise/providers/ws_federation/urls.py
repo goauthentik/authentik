cb9e4e003c2528d8fe36bf699ac5c5790e3beea6
Jens L. <jens@goauthentik.io>
2026-02-17 17:20:47 +0000

    enterprise/providers/ws_federation: fix incorrect metadata download URL (#20173)
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/enterprise/providers/ws_federation/urls.py
+++ authentik/enterprise/providers/ws_federation/urls.py
@@ -4,12 +4,19 @@ from django.urls import path
 
 from authentik.enterprise.providers.ws_federation.api.providers import WSFederationProviderViewSet
 from authentik.enterprise.providers.ws_federation.views import WSFedEntryView
+from authentik.providers.saml.views.metadata import MetadataDownload
 
 urlpatterns = [
     path(
         "",
         WSFedEntryView.as_view(),
         name="wsfed",
+    ),
+    # Metadata
+    path(
+        "<slug:application_slug>/metadata/",
+        MetadataDownload.as_view(),
+        name="metadata-download",
     ),
 ]
 
