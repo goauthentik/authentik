242802dd2126eaa301430608fb4de45d60c7961c
Felix Prasse <felix.prasse@suse.com>
2026-02-26 10:23:02 +0100

    Do not show the "log back in" button if the url is blank

--- web/src/user/LibraryPage/LibraryPageImpl.utils.ts
+++ web/src/user/LibraryPage/LibraryPageImpl.utils.ts
@@ -1,18 +1,7 @@
 import type { Application } from "@goauthentik/api";
+import { isLaunchUrlValid } from "#common/utils";
 
-const isFullUrlRe = new RegExp("://");
-const isHttpRe = new RegExp("http(s?)://");
-const isAuthentikSpecialRe = new RegExp("goauthentik.io://");
-const isNotFullUrl = (url: string) => !isFullUrlRe.test(url);
-const isHttp = (url: string) => isHttpRe.test(url);
-const isAuthentikSpecial = (url: string) => isAuthentikSpecialRe.test(url);
-
-export const appHasLaunchUrl = (app: Application): boolean => {
+export const appHasLaunchUrl = (app: Application) => {
     const url = app.launchUrl;
-
-    return !!(
-        typeof url === "string" &&
-        url &&
-        (isHttp(url) || isNotFullUrl(url) || isAuthentikSpecial(url))
-    );
+    return isLaunchUrlValid(url)
 };
