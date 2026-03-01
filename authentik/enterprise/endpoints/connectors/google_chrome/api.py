15e9224163e7fdfa4202f3edf9e063b54a344ce9
Jens Langhammer <jens@goauthentik.io>
2026-03-01 23:29:11 +0100

    include chrome url
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/enterprise/endpoints/connectors/google_chrome/api.py
+++ authentik/enterprise/endpoints/connectors/google_chrome/api.py
@@ -1,5 +1,8 @@
 """GoogleChromeConnector API Views"""
 
+from django.urls import reverse
+from rest_framework.fields import SerializerMethodField
+from rest_framework.request import Request
 from rest_framework.viewsets import ModelViewSet
 
 from authentik.core.api.used_by import UsedByMixin
@@ -11,11 +14,20 @@ from authentik.enterprise.endpoints.connectors.google_chrome.models import Googl
 class GoogleChromeConnectorSerializer(EnterpriseRequiredMixin, ConnectorSerializer):
     """GoogleChromeConnector Serializer"""
 
+    chrome_url = SerializerMethodField()
+
+    def get_chrome_url(self, _: GoogleChromeConnector) -> str | None:
+        """Full URL to be used in Google Workspace configuration"""
+        request: Request = self.context.get("request", None)
+        if not request:
+            return True
+        return request.build_absolute_uri(
+            reverse("authentik_endpoints_connectors_google_chrome:chrome")
+        )
+
     class Meta:
         model = GoogleChromeConnector
-        fields = ConnectorSerializer.Meta.fields + [
-            "credentials",
-        ]
+        fields = ConnectorSerializer.Meta.fields + ["credentials", "chrome_url"]
 
 
 class GoogleChromeConnectorViewSet(UsedByMixin, ModelViewSet):
