7f9f2a13c21f975f88ee1d9937da02f0afa0d832
Ken Sternberg <ken@goauthentik.io>
2026-03-11 09:56:34 -0700

    That was a stupid spelling error.

--- web/paths/node.js
+++ web/paths/node.js
@@ -90,7 +90,7 @@ export const EntryPoint = /** @type {const} */ ({
         in: resolve(PackageRoot, "src", "styles", "authentik", "static.global.css"),
         out: resolve(DistDirectory, "styles", "static"),
     },
-    FlowsStyles: {
+    FlowStyles: {
         in: resolve(PackageRoot, "src", "styles", "authentik", "flows.global.css"),
         out: resolve(DistDirectory, "styles", "flow"),
     },
