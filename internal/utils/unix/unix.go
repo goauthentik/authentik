1ddc5963629b8063ae6a6a6fd5f5b2f55a972f47
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2026-03-19 14:22:41 +0100

    better unix listener
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- internal/utils/unix/unix.go
+++ internal/utils/unix/unix.go
@@ -2,15 +2,14 @@ package unix
 
 import (
 	"net"
-	"time"
 )
 
 type Listener struct {
-	ln *net.UnixListener
+	*net.UnixListener
 }
 
 type Conn struct {
-	c net.Conn
+	net.Conn
 }
 
 func Listen(path string) (*Listener, error) {
@@ -23,36 +22,16 @@ func Listen(path string) (*Listener, error) {
 		return nil, err
 	}
 	return &Listener{
-		ln: ln,
+		ln,
 	}, nil
 }
 
 func (l *Listener) Accept() (net.Conn, error) {
-	c, err := l.ln.Accept()
+	c, err := l.Accept()
 	if err != nil {
 		return nil, err
 	}
-	return &Conn{c: c}, nil
-}
-
-func (l *Listener) Close() error {
-	return l.ln.Close()
-}
-
-func (l *Listener) Addr() net.Addr {
-	return l.ln.Addr()
-}
-
-func (c *Conn) Read(b []byte) (int, error) {
-	return c.c.Read(b)
-}
-
-func (c *Conn) Write(b []byte) (n int, err error) {
-	return c.c.Write(b)
-}
-
-func (c *Conn) Close() error {
-	return c.c.Close()
+	return &Conn{c}, nil
 }
 
 func (c *Conn) LocalAddr() net.Addr {
@@ -61,16 +40,4 @@ func (c *Conn) LocalAddr() net.Addr {
 
 func (c *Conn) RemoteAddr() net.Addr {
 	return &net.TCPAddr{IP: net.IPv6loopback, Port: 0}
-}
-
-func (c *Conn) SetDeadline(t time.Time) error {
-	return c.c.SetDeadline(t)
-}
-
-func (c *Conn) SetReadDeadline(t time.Time) error {
-	return c.c.SetReadDeadline(t)
-}
-
-func (c *Conn) SetWriteDeadline(t time.Time) error {
-	return c.c.SetWriteDeadline(t)
 }
