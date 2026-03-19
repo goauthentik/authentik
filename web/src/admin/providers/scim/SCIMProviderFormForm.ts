590418e0a60d841ed17f0642cac254ff8bb6cf46
Felix Prasse <felix.prasse@suse.com>
2026-03-19 18:18:46 +0100

    Add SCIM compatibility mode for qualified.com
    Qualified does not have a service provider config endpoint but supports filtering (to a degree).
    
    Enable filtering here so that we can sync existing users.

--- web/src/admin/providers/scim/SCIMProviderFormForm.ts
+++ web/src/admin/providers/scim/SCIMProviderFormForm.ts
@@ -198,6 +198,11 @@ export function renderForm({ provider = {}, errors = {}, update }: SCIMProviderF
                             value: CompatibilityModeEnum.Sfdc,
                             description: html`${msg("Altered behavior for usage with Salesforce.")}`,
                         },
+                        {
+                            label: msg("Qualified"),
+                            value: CompatibilityModeEnum.Qualified,
+                            description: html`${msg("Altered behavior for usage with Qualified.")}`,
+                        },
                     ]}
                     help=${msg(
                         "Alter authentik's behavior for vendor-specific SCIM implementations.",
