327c01dfd43dbda92321ed7646e8acafb4ece4fd
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2026-03-23 00:56:30 +0100

    better print error
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- src/axum/error.rs
+++ src/axum/error.rs
@@ -18,7 +18,7 @@ where E: Into<Report>
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
-        warn!("error occurred: {:?}", self.0);
+        warn!(error = ?self.0, "error occurred");
         (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong").into_response()
     }
 }
