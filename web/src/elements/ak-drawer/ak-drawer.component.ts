1aaced234c4e7ffed36a96d67e07500cec13f5a7
Ken Sternberg <ken@goauthentik.io>
2026-03-25 10:00:44 -0700

    .

--- web/src/elements/ak-drawer/ak-drawer.component.ts
+++ web/src/elements/ak-drawer/ak-drawer.component.ts
@@ -1,4 +1,5 @@
 import { AKElement } from "#elements/Base";
+import { classList } from "#elements/directives/class-list";
 
 import { css, html } from "lit";
 import { property } from "lit/decorators.js";
@@ -9,6 +10,45 @@ const Style = css`
     slot {
         display: content;
     }
+
+    [data-theme="dark"] {
+        --pf-c-drawer__panel--BackgroundColor: var(--ak-dark-background);
+    }
+
+    .pf-c-drawer {
+        /* TODO: Revisit this after native <dialog> modals are implemented. */
+        --pf-c-drawer__content--ZIndex: auto;
+    }
+
+    .pf-c-drawer__body {
+        display: flex;
+        flex-flow: column;
+    }
+
+    .pf-c-drawer__content {
+        --pf-c-drawer__content--BackgroundColor: transparent;
+    }
+
+    .pf-c-drawer {
+        .pf-c-drawer__panel {
+            background-color: var(--pf-c-drawer__panel--BackgroundColor);
+
+            transition-behavior: allow-discrete;
+
+            gap: var(--pf-global--spacer--sm);
+
+            @media (width > 768px) {
+                flex-flow: row;
+
+                .pf-c-drawer__panel_content {
+                    flex: 1 1 auto;
+                    max-width: 33dvw;
+                }
+            }
+        }
+    }
+
+    /* #region Dark Theme */
 `;
 
 export class Drawer extends AKElement {
@@ -22,7 +62,7 @@ export class Drawer extends AKElement {
 
         return html`
             <div class="pf-c-page__drawer">
-                <div class="pf-c-drawer" id="flow-drawer">
+                <div class="pf-c-drawer ${classList(open)}" id="flow-drawer">
                     <div class="pf-c-drawer__main">
                         <div class="pf-c-drawer__content">
                             <div class="pf-c-drawer__body">
