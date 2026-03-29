c2390b93277af4ccad6a0d84ad5469487ff716ab
Jens Langhammer <jens@goauthentik.io>
2026-03-29 23:29:54 +0200

    update service monitor when labels mismatch
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/outposts/controllers/k8s/service_monitor.py
+++ authentik/outposts/controllers/k8s/service_monitor.py
@@ -9,6 +9,7 @@ from kubernetes.client import ApiextensionsV1Api, CustomObjectsApi
 from authentik.outposts.controllers.base import FIELD_MANAGER
 from authentik.outposts.controllers.k8s.base import KubernetesObjectReconciler
 from authentik.outposts.controllers.k8s.service import MetricsServiceReconciler
+from authentik.outposts.controllers.k8s.triggers import NeedsUpdate
 
 if TYPE_CHECKING:
     from authentik.outposts.controllers.kubernetes import KubernetesController
@@ -78,6 +79,11 @@ class PrometheusServiceMonitorReconciler(KubernetesObjectReconciler[PrometheusSe
     @staticmethod
     def reconciler_name() -> str:
         return "prometheus servicemonitor"
+
+    def reconcile(self, current: PrometheusServiceMonitor, reference: PrometheusServiceMonitor):
+        if current.spec.selector.matchLabels != reference.spec.selector.matchLabels:
+            raise NeedsUpdate()
+        super().reconcile(current, reference)
 
     @property
     def noop(self) -> bool:
