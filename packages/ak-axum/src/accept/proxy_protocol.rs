a522b8be76a7e2731f87b1a6b5ebcb629714f912
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2026-04-01 19:46:57 +0200

    fmt
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- packages/ak-axum/src/accept/proxy_protocol.rs
+++ packages/ak-axum/src/accept/proxy_protocol.rs
@@ -1,13 +1,12 @@
 use std::{io, time::Duration};
 
+use ak_common::tokio::proxy_protocol::{ProxyProtocolStream, header::Header};
 use axum::{Extension, middleware::AddExtension};
 use axum_server::accept::{Accept, DefaultAcceptor};
 use futures::future::BoxFuture;
 use tokio::io::{AsyncRead, AsyncWrite};
 use tower::Layer as _;
 use tracing::instrument;
-
-use ak_common::tokio::proxy_protocol::{ProxyProtocolStream, header::Header};
 
 #[derive(Clone, Debug)]
 pub struct ProxyProtocolState {
