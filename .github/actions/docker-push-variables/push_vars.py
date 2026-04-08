733ea261f2450217b3c5993b9aa4573aa130277b
Simonyi Gergő <gergo@goauthentik.io>
2026-04-08 11:33:01 +0200

    ci: fix `docker-push-variables
    The error is
    
    SyntaxError: f-string expression part cannot include a backslash

--- .github/actions/docker-push-variables/push_vars.py
+++ .github/actions/docker-push-variables/push_vars.py
@@ -118,4 +118,4 @@ with open(os.environ["GITHUB_OUTPUT"], "a+", encoding="utf-8") as _output:
     print(f"imageMainTag={image_main_tag}", file=_output)
     print(f"imageMainName={image_tags[0]}", file=_output)
     print(f"cacheTo={cache_to}", file=_output)
-    print(f"imageBuildArgs={'\n'.join(image_build_args)}", file=_output)
+    print(f"imageBuildArgs={"\n".join(image_build_args)}", file=_output)
