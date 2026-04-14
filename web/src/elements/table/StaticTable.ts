1cc53cfca808f8d30506d15f5e4f79ada4854e6d
Teffen Ellis <592134+GirlBossRush@users.noreply.github.com>
2026-04-14 05:38:39 +0200

    Fix check access form invoker, styles.

--- web/src/elements/table/StaticTable.ts
+++ web/src/elements/table/StaticTable.ts
@@ -4,31 +4,31 @@ import { PaginatedResponse, Table } from "#elements/table/Table";
 import { SlottedTemplateResult } from "#elements/types";
 
 import { PropertyValues } from "lit";
-import { html, nothing } from "lit-html";
 import { property } from "lit/decorators.js";
 
 export abstract class StaticTable<T extends object> extends Table<T> {
     protected override searchEnabled = false;
 
     @property({ attribute: false })
-    items?: T[] = [];
+    public items: T[] | null = [];
 
     protected override async apiEndpoint(): Promise<PaginatedResponse<T, object>> {
         return createPaginatedResponse(this.items ?? []);
     }
 
     protected override renderToolbar(): SlottedTemplateResult {
-        return html`${this.renderObjectCreate()}`;
+        return this.renderObjectCreate();
     }
 
     protected override renderTablePagination(): SlottedTemplateResult {
-        return nothing;
+        return null;
     }
 
     protected override willUpdate(changedProperties: PropertyValues<this>): void {
         if (changedProperties.has("items")) {
             this.fetch();
         }
+
         super.willUpdate(changedProperties);
     }
 }
