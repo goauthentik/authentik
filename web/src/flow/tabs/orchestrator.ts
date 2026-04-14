4c9c5f98f0739b8853fa34145c3bb755bddfc3ef
Teffen Ellis <592134+GirlBossRush@users.noreply.github.com>
2026-04-14 22:27:52 +0200

    web: Continuous login tidy.

--- web/src/flow/tabs/orchestrator.ts
+++ web/src/flow/tabs/orchestrator.ts
@@ -19,42 +19,54 @@ export async function multiTabOrchestrateResume() {
     if (!globalAK().brand.flags.flowsContinuousLogin) {
         return;
     }
-    const lockTabId = localStorage.getItem(lockKey);
+
+    const lockTabID = localStorage.getItem(lockKey);
     const tabs = await Broadcast.shared.akTabDiscover();
+
     logger.debug("Got list of tabs", tabs);
 
-    if (lockTabId && tabs.has(lockTabId)) {
+    if (lockTabID && tabs.has(lockTabID)) {
         logger.debug("Tabs locked, leaving.");
         multiTabOrchestrateLeave();
         return;
     }
+
     logger.debug("Locking tabs");
     localStorage.setItem(lockKey, TabID.shared.current);
 
     for (const tab of tabs) {
         logger.debug("Telling tab to continue", tab);
         Broadcast.shared.akResumeTab(tab);
+
         const done = Promise.withResolvers<void>();
-        const timers: { timeout?: ReturnType<typeof setTimeout> } = {};
-        const checker = setInterval(() => {
-            if (Broadcast.shared.exitedTabIds.includes(tab)) {
+
+        let timeout = -1;
+
+        const checker = requestAnimationFrame(() => {
+            if (Broadcast.shared.exitedTabIDs.includes(tab)) {
                 logger.debug("tab exited", tab);
-                clearTimeout(timers.timeout);
-                setTimeout(() => {
+                self.clearTimeout(timeout);
+
+                self.setTimeout(() => {
                     logger.debug("continue exited", tab);
                     done.resolve();
                 }, 1000);
-                clearInterval(checker);
+
+                cancelAnimationFrame(checker);
             }
-        }, 1);
-        timers.timeout = setTimeout(() => {
+        });
+
+        timeout = self.setTimeout(() => {
             logger.warn("Timed out waiting for tab to exit, moving on", tab);
-            clearInterval(checker);
+            cancelAnimationFrame(checker);
             done.resolve();
         }, TAB_EXIT_TIMEOUT_MS);
+
         await done.promise;
+
         logger.debug("Tab done, continuing", tab);
     }
+
     logger.debug("All tabs done.");
     localStorage.removeItem(lockKey);
 }
