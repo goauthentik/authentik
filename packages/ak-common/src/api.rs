193131c06db4fce992b83bebfea5a48c0e6e725d
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2026-04-15 18:06:57 +0200

    add tests
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- packages/ak-common/src/api.rs
+++ packages/ak-common/src/api.rs
@@ -18,12 +18,16 @@ impl ServerConfig {
             .host
             .clone()
             .ok_or_else(|| eyre!("environment variable `AUTHENTIK_HOST` not set"))?;
-        let host = host.parse()?;
+        let mut host: Url = host.parse()?;
         let token = config::get()
             .token
             .clone()
             .ok_or_else(|| eyre!("environment variable `AUTHENTIK_TOKEN` not set"))?;
         let insecure = config::get().insecure.unwrap_or(false);
+
+        if !host.path().ends_with('/') {
+            host.path_segments_mut().expect("URL cannot be a base").push("");
+        }
 
         Ok(Self {
             host,
@@ -37,7 +41,7 @@ impl ServerConfig {
 pub fn make_config() -> Result<Configuration> {
     let server_config = ServerConfig::new()?;
 
-    let base_path = format!("{}api/v3", server_config.host);
+    let base_path = server_config.host.join("api/v3")?.into();
 
     let client = reqwest::ClientBuilder::new()
         .tls_danger_accept_invalid_hostnames(server_config.insecure)
@@ -52,4 +56,125 @@ pub fn make_config() -> Result<Configuration> {
         user_agent: Some(user_agent_outpost()),
         ..Default::default()
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::config;
+
+    use super::ServerConfig;
+    use serde_json::json;
+    use super::make_config;
+
+    #[test]
+    fn server_config_no_trailing_slash() {
+        config::init().expect("failed to init config");
+        config::set(json!({
+            "host": "http://localhost:9000",
+            "token": "token",
+        }))
+        .expect("failed to set config");
+
+        let server_config = ServerConfig::new().expect("failed to create server config");
+
+        assert_eq!(server_config.host.as_str(), "http://localhost:9000/");
+    }
+
+    #[test]
+    fn server_config_with_trailing_slash() {
+        config::init().expect("failed to init config");
+        config::set(json!({
+            "host": "http://localhost:9000/",
+            "token": "token",
+        }))
+        .expect("failed to set config");
+
+        let server_config = ServerConfig::new().expect("failed to create server config");
+
+        assert_eq!(server_config.host.as_str(), "http://localhost:9000/");
+    }
+
+    #[test]
+    fn server_config_with_path_no_trailing_slash() {
+        config::init().expect("failed to init config");
+        config::set(json!({
+            "host": "http://localhost:9000/authentik",
+            "token": "token",
+        }))
+        .expect("failed to set config");
+
+        let server_config = ServerConfig::new().expect("failed to create server config");
+
+        assert_eq!(server_config.host.as_str(), "http://localhost:9000/authentik/");
+    }
+
+    #[test]
+    fn server_config_with_path_and_trailing_slash() {
+        config::init().expect("failed to init config");
+        config::set(json!({
+            "host": "http://localhost:9000/authentik/",
+            "token": "token",
+        }))
+        .expect("failed to set config");
+
+        let server_config = ServerConfig::new().expect("failed to create server config");
+
+        assert_eq!(server_config.host.as_str(), "http://localhost:9000/authentik/");
+    }
+
+    #[test]
+    fn make_config_no_trailing_slash() {
+        config::init().expect("failed to init config");
+        config::set(json!({
+            "host": "http://localhost:9000",
+            "token": "token",
+        }))
+        .expect("failed to set config");
+
+        let api_config = make_config().expect("failed to make config");
+
+        assert_eq!(api_config.base_path, "http://localhost:9000/api/v3");
+    }
+
+    #[test]
+    fn make_config_with_trailing_slash() {
+        config::init().expect("failed to init config");
+        config::set(json!({
+            "host": "http://localhost:9000/",
+            "token": "token",
+        }))
+        .expect("failed to set config");
+
+        let api_config = make_config().expect("failed to make config");
+
+        assert_eq!(api_config.base_path, "http://localhost:9000/api/v3");
+    }
+
+    #[test]
+    fn make_config_with_path_no_trailing_slash() {
+        config::init().expect("failed to init config");
+        config::set(json!({
+            "host": "http://localhost:9000/authentik",
+            "token": "token",
+        }))
+        .expect("failed to set config");
+
+        let api_config = make_config().expect("failed to make config");
+
+        assert_eq!(api_config.base_path, "http://localhost:9000/authentik/api/v3");
+    }
+
+    #[test]
+    fn make_config_with_path_and_trailing_slash() {
+        config::init().expect("failed to init config");
+        config::set(json!({
+            "host": "http://localhost:9000/authentik/",
+            "token": "token",
+        }))
+        .expect("failed to set config");
+
+        let api_config = make_config().expect("failed to make config");
+
+        assert_eq!(api_config.base_path, "http://localhost:9000/authentik/api/v3");
+    }
 }
