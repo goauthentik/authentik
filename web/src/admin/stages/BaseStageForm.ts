91327ae8c100a3fcf436578f5f5ef9d699035e43
Dominic R <dominic@sdko.org>
2026-04-22 18:59:30 -0400

    Drop shared stage API field

--- web/src/admin/stages/BaseStageForm.ts
+++ web/src/admin/stages/BaseStageForm.ts
@@ -1,8 +1,5 @@
-import { DEFAULT_CONFIG } from "#common/api/config";
-
 import { ModelForm } from "#elements/forms/ModelForm";
 
-import { StagesApi } from "@goauthentik/api";
 import type { Stage } from "@goauthentik/api/dist/models/Stage";
 
 import { msg } from "@lit/localize";
@@ -10,8 +7,6 @@ import { msg } from "@lit/localize";
 export abstract class BaseStageForm<T extends Stage> extends ModelForm<T, string> {
     public static override verboseName = msg("Stage");
     public static override verboseNamePlural = msg("Stages");
-
-    protected stagesAPI = new StagesApi(DEFAULT_CONFIG);
 
     public override getSuccessMessage(): string {
         return this.instance
