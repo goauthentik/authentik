2fdc4fbe395cdba22b2607fbdc4a59052e7c763c
Jens Langhammer <jens@goauthentik.io>
2026-05-01 17:14:06 +0200

    t
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- tests/openid_conformance/base.py
+++ tests/openid_conformance/base.py
@@ -7,11 +7,11 @@ from selenium.webdriver.support import expected_conditions as ec
 
 from authentik.blueprints.tests import apply_blueprint, reconcile_app
 from authentik.providers.oauth2.models import OAuth2Provider
+from tests.live import E2ETestCase
 from tests.openid_conformance.conformance import Conformance
-from tests.selenium import SeleniumTestCase
 
 
-class TestOpenIDConformance(SeleniumTestCase):
+class TestOpenIDConformance(E2ETestCase):
 
     conformance: Conformance
 
