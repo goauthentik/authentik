1eb641de1bfdfb07c88092adb77feb4d0cbabcd2
Connor Peshek <connor@connorpeshek.me>
2026-05-04 11:08:35 -0500

    udpate

--- tests/openid_conformance/test_config.py
+++ tests/openid_conformance/test_config.py
@@ -1,8 +1,15 @@
+from authentik.providers.oauth2.models import IssuerMode, OAuth2Provider
 from tests.decorators import retry
 from tests.openid_conformance.base import TestOpenIDConformance
 
 
 class TestOpenIDConformanceConfig(TestOpenIDConformance):
+
+    def setUp(self):
+        super().setUp()
+        OAuth2Provider.objects.filter(name__startswith="oidc-conformance-").update(
+            issuer_mode=IssuerMode.PER_PROVIDER,
+        )
 
     @retry()
     def test_oidcc_config_certification_test_plan(self):
