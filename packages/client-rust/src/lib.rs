d3d9165e3d7c3ce64505fff407f1325ccf680999
Jens Langhammer <jens@goauthentik.io>
2026-05-05 13:56:45 +0200

    format
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- packages/client-rust/src/lib.rs
+++ packages/client-rust/src/lib.rs
@@ -1,11 +1,11 @@
 #![allow(unused_imports)]
 #![allow(clippy::too_many_arguments)]
 
-extern crate serde_repr;
+extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
+extern crate serde_repr;
 extern crate url;
-extern crate reqwest;
 
 pub mod apis;
 pub mod models;
