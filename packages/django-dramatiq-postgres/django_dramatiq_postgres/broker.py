a1ab4625707f9682b9ffb3d1328894115d1a462b
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2026-05-07 17:35:21 +0200

    Cherry-pick #22110 to version-2026.2 (with conflicts)
    This cherry-pick has conflicts that need manual resolution.
    
    Original PR: #22110
    Original commit: b420e4fdbddba00e93faadeffdb54c9a9912d750

--- packages/django-dramatiq-postgres/django_dramatiq_postgres/broker.py
+++ packages/django-dramatiq-postgres/django_dramatiq_postgres/broker.py
@@ -23,7 +23,11 @@ from django.utils.functional import cached_property
 from django.utils.module_loading import import_string
 from dramatiq.broker import Broker, Consumer, MessageProxy
 from dramatiq.common import compute_backoff, current_millis, dq_name, q_name, xq_name
+<<<<<<< HEAD
 from dramatiq.errors import ConnectionError, QueueJoinTimeout
+=======
+from dramatiq.errors import BrokerConnectionError, DecodeError, QueueJoinTimeout
+>>>>>>> b420e4fdb (packages/django-dramatiq-postgres/broker: avoid task processing stopping on decode error (#22110))
 from dramatiq.message import Message
 from dramatiq.middleware import (
     Middleware,
@@ -370,7 +374,18 @@ class _PostgresConsumer(Consumer):
         )
         if task is None:
             return None
-        message = Message.decode(cast(bytes, task.message))
+        try:
+            message = Message.decode(cast(bytes, task.message))
+        except DecodeError:
+            self.logger.error(
+                "Failed to decode task, rejecting", queue=self.queue_name, message_id=message_id
+            )
+            self.query_set.filter(message_id=message_id, queue_name=self.queue_name).update(
+                state=TaskState.REJECTED,
+                mtime=timezone.now(),
+                eta=None,
+            )
+            return None
         message.options["task"] = task
         self.in_processing.add(str(message_id))
         return message
