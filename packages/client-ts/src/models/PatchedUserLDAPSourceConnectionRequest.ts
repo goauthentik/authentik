f4e4bfcbe556fd8448d02c09471f6fc5b2b55e01
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2026-06-01 18:42:06 +0200

    root: fix schema and API clients (#22735)
    * regenerate schema
    
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>
    
    * update ts client
    
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>
    
    ---------
    
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- packages/client-ts/src/models/PatchedUserLDAPSourceConnectionRequest.ts
+++ packages/client-ts/src/models/PatchedUserLDAPSourceConnectionRequest.ts
@@ -20,12 +20,6 @@
 export interface PatchedUserLDAPSourceConnectionRequest {
     /**
      *
-     * @type {number}
-     * @memberof PatchedUserLDAPSourceConnectionRequest
-     */
-    user?: number;
-    /**
-     *
      * @type {string}
      * @memberof PatchedUserLDAPSourceConnectionRequest
      */
@@ -61,7 +55,6 @@ export function PatchedUserLDAPSourceConnectionRequestFromJSONTyped(
         return json;
     }
     return {
-        user: json["user"] == null ? undefined : json["user"],
         source: json["source"] == null ? undefined : json["source"],
         identifier: json["identifier"] == null ? undefined : json["identifier"],
     };
@@ -82,7 +75,6 @@ export function PatchedUserLDAPSourceConnectionRequestToJSONTyped(
     }
 
     return {
-        user: value["user"],
         source: value["source"],
         identifier: value["identifier"],
     };
