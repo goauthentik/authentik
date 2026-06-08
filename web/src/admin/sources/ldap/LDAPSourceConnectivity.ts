d999a7fa12a3cbf52857607ee600b5c453065680
Ken Sternberg <ken@goauthentik.io>
2026-06-08 14:11:30 -0700

    Fix rendering bug.

--- web/src/admin/sources/ldap/LDAPSourceConnectivity.ts
+++ web/src/admin/sources/ldap/LDAPSourceConnectivity.ts
@@ -6,6 +6,7 @@ import { SlottedTemplateResult } from "#elements/types";
 import { msg } from "@lit/localize";
 import { CSSResult, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
+import { map } from "lit/directives/map.js";
 
 import PFList from "@patternfly/patternfly/components/List/list.css";
 
@@ -24,25 +25,24 @@ export class LDAPSourceConnectivity extends AKElement {
         if (!this.connectivity) {
             return html`${msg("No connectivity status available.")}`;
         }
+
+        const servers = Object.entries(this.connectivity);
+
         return html`<ul class="pf-c-list">
-            ${Object.keys(this.connectivity).map((serverKey) => {
-                let serverLabel = html`${serverKey}`;
-                if (serverKey === "__all__") {
-                    serverLabel = html`<b>${msg("Global status")}</b>`;
-                }
-                const server = this.connectivity![serverKey];
-                const content = html`${serverLabel}: ${server.status}`;
-                let tooltip = html`${content}`;
-                if (server.status === "ok") {
-                    tooltip = html`<pf-tooltip position="top">
-                        <ul slot="content" class="pf-c-list">
-                            <li>${msg("Vendor")}: ${server.vendor}</li>
-                            <li>${msg("Version")}: ${server.version}</li>
-                        </ul>
-                        ${content}
-                    </pf-tooltip>`;
-                }
-                return html`<li>${tooltip}</li>`;
+            ${map(servers, ([key, server]) => {
+                const label = key === "__all__" ? html`<b>${msg("Global status")}</b>` : key;
+                const content = html`${label}: ${server.status}`;
+                return html`<li>
+                    ${server.status === "ok"
+                        ? html`<pf-tooltip position="top">
+                              <ul slot="content" class="pf-c-list">
+                        <li>${msg("Vendor")}: ${server.vendor}</Li>
+                        <li>${msg("Version")}: ${server.version}</li>
+                              </ul>
+                              ${content}
+                          </pf-tooltip>`
+                        : content}
+                </li>`;
             })}
         </ul>`;
     }
