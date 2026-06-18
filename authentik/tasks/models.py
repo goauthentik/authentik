5cf38ee495e5d27e2f3895b0dcfe8a315284be35
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2026-06-18 14:19:31 +0200

    packages/django-postgres-dramatiq: add support for task dependencies (#23064)
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/tasks/models.py
+++ authentik/tasks/models.py
@@ -20,6 +20,7 @@ from authentik.tenants.models import Tenant
 class TaskStatus(models.TextChoices):
     """Task aggregated status. Reported by the task runners"""
 
+    WAITING_FOR_DEPENDENCIES = TaskState.WAITING_FOR_DEPENDENCIES
     QUEUED = TaskState.QUEUED
     CONSUMED = TaskState.CONSUMED
     PREPROCESS = TaskState.PREPROCESS
@@ -33,6 +34,15 @@ class TaskStatus(models.TextChoices):
 
 
 class Task(InternallyManagedMixin, SerializerModel, TaskBase):
+    # Overridden from TaskBase to use a through model
+    dependencies = models.ManyToManyField(
+        "self",
+        verbose_name=_("Tasks that must complete for this task to run."),
+        symmetrical=False,
+        through="TaskDependency",
+        through_fields=("task", "dependency"),
+    )
+
     tenant = models.ForeignKey(
         Tenant,
         on_delete=models.CASCADE,
@@ -147,6 +157,20 @@ class Task(InternallyManagedMixin, SerializerModel, TaskBase):
 
     def error(self, message: str | Exception, **attributes) -> None:
         self.log(self.uid, TaskStatus.ERROR, message, **attributes)
+
+
+class TaskDependency(InternallyManagedMixin, models.Model):
+    pk = models.CompositePrimaryKey("task", "dependency")
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="+")
+    dependency = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="+")
+
+    class Meta:
+        default_permissions = []
+        verbose_name = _("Task dependency")
+        verbose_name_plural = _("Task dependencies")
+
+    def __str__(self):
+        return f"Task {self.pk[0]} dependency on {self.pk[1]}"
 
 
 class TaskLog(InternallyManagedMixin, models.Model):
