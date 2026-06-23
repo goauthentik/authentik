0aa575ffbdb466263663cc254ec5d87c115ad491
Simonyi Gergő <gergo@goauthentik.io>
2026-06-23 15:42:13 +0200

    outposts: explicit m2m

--- authentik/outposts/models.py
+++ authentik/outposts/models.py
@@ -281,7 +281,7 @@ class Outpost(ScheduledModel, SerializerModel, ManagedModel):
 
     _config = models.JSONField(default=default_outpost_config)
 
-    providers = models.ManyToManyField(Provider)
+    providers = models.ManyToManyField(Provider, through="OutpostProvider")
 
     @property
     def serializer(self) -> Serializer:
@@ -455,6 +455,17 @@ class Outpost(ScheduledModel, SerializerModel, ManagedModel):
     class Meta:
         verbose_name = _("Outpost")
         verbose_name_plural = _("Outposts")
+
+
+class OutpostProvider(models.Model):
+    outpost = models.ForeignKey(Outpost, on_delete=models.CASCADE)
+    provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (("outpost", "provider"),)
+
+    def __str__(self):
+        return f"OutpostProvider for outpost {self.outpost_id} and provider {self.provider}."
 
 
 @dataclass
