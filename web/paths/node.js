73f689772485229808c8c3fad5bca8f44515e55c
Ken Sternberg <ken@goauthentik.io>
2026-06-24 16:13:37 -0700

    Turns out, the '.global' tag is needed to trigger the correct referencing behavior.

--- web/paths/node.js
+++ web/paths/node.js
@@ -83,15 +83,15 @@ export const EntryPoint = /** @type {const} */ ({
         out: resolve(DistDirectory, "poly"),
     },
     InterfaceStyles: {
-        in: resolve(PackageRoot, "src", "styles", "interface.css"),
+        in: resolve(PackageRoot, "src", "styles", "interface.global.css"),
         out: resolve(DistDirectory, "styles", "interface"),
     },
     StaticStyles: {
-        in: resolve(PackageRoot, "src", "styles", "static.css"),
+        in: resolve(PackageRoot, "src", "styles", "static.global.css"),
         out: resolve(DistDirectory, "styles", "static"),
     },
     FlowStyles: {
-        in: resolve(PackageRoot, "src", "styles", "flows.css"),
+        in: resolve(PackageRoot, "src", "styles", "flows.global.css"),
         out: resolve(DistDirectory, "styles", "flow"),
     },
 });
