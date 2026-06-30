c66237710faef3dca2b7d413b4f714e93bb42f74
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2026-06-30 18:06:13 +0200

    root: move server and worker healthcheck to rust
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

---
+++ src/healthcheck.rs
@@ -0,0 +1,47 @@
+use ak_common::Mode;
+use argh::FromArgs;
+use eyre::{Result, eyre};
+use reqwest::blocking::Client;
+use tracing::info;
+
+#[derive(Debug, FromArgs, PartialEq)]
+/// Run healthcheck
+#[argh(subcommand, name = "healthcheck")]
+#[expect(
+    clippy::empty_structs_with_brackets,
+    reason = "argh doesn't support unit structs"
+)]
+pub(super) struct Cli {
+    #[argh(positional)]
+    mode: String,
+}
+
+pub(super) fn run(args: &Cli) -> Result<()> {
+    let mode: Mode = args.mode.parse()?;
+    info!(%mode, "checking health");
+
+    let response = match mode {
+        #[cfg(feature = "core")]
+        Mode::AllInOne | Mode::Server | Mode::Worker => Client::builder()
+            .user_agent("goauthentik.io/healthcheck")
+            .unix_socket(crate::server::socket_path())
+            .build()?
+            .get("http://localhost/-/health/live/")
+            .send()?,
+        #[cfg(feature = "proxy")]
+        Mode::Proxy => Client::builder()
+            .user_agent("goauthentik.io/healthcheck")
+            .unix_socket(crate::metrics::socket_path())
+            .build()?
+            .get("http://localhost/outpost.goauthentik.io/ping")
+            .send()?,
+    };
+
+    if !response.status().is_success() {
+        return Err(eyre!("unhealthy status code {}", response.status()));
+    }
+
+    info!("successfully checked health");
+
+    Ok(())
+}
