8f7cf021fff22172083a162ce15705d6c6742fbf
Jens Langhammer <jens@goauthentik.io>
2026-07-02 15:50:52 +0100

    ui
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/admin/access-requests/AccessRequestListPage.ts
+++ web/src/admin/access-requests/AccessRequestListPage.ts
@@ -1,11 +1,14 @@
+import "../../components/ak-status-label";
+
 import { aki } from "#common/api/client";
 import { PaginatedResponse } from "#common/api/responses";
 
-import { RowType } from "#elements/table/Table";
+import { RowType, Timestamp } from "#elements/table/Table";
 import { TableColumn } from "#elements/table/TableColumn";
 import { TablePage } from "#elements/table/TablePage";
+import { SlottedTemplateResult } from "#elements/types";
 
-import { GrantRequest, PamApi } from "@goauthentik/api";
+import { GrantRequest, PamApi, RequestStatus } from "@goauthentik/api";
 
 import { msg } from "@lit/localize";
 import { html } from "lit-html";
@@ -16,14 +19,35 @@ export class AccessRequestListPage extends TablePage<GrantRequest> {
     public pageTitle: string = msg("Access Requests");
     public pageDescription: string = msg("");
     public pageIcon: string = "";
+    public expandable: boolean = true;
+    public searchEnabled: boolean = true;
     protected async apiEndpoint(): Promise<PaginatedResponse<GrantRequest, object>> {
         return aki(PamApi).pamGrantRequestsList({
             ...(await this.defaultEndpointConfig()),
         });
     }
-    protected columns: TableColumn[] = [[""]];
+    protected columns: TableColumn[] = [
+        [msg("User"), "createdBy"],
+        [msg("Created"), "created"],
+        [msg("Status"), "status"],
+    ];
+    protected renderExpanded(item: GrantRequest): SlottedTemplateResult {
+        return html`${JSON.stringify(item.data)}<br>${item.targets}`;
+    }
     protected row(item: GrantRequest): RowType[] {
-        return [html``];
+        return [
+            html`<a href="#/identity/users/${item.createdBy.pk}">
+                <div>${item.createdBy.username}</div>
+                <small>${item.createdBy.name}</small>
+            </a>`,
+            Timestamp(item.created),
+            html`<ak-status-label
+                .good=${item.status === RequestStatus.Approved}
+                good-label=${msg("Approved")}
+                bad-label=${msg("Pending")}
+                type="info"
+            ></ak-status-label>`,
+        ];
     }
 }
 
