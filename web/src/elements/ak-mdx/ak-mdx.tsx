03a9cf2ff25ef3a84ae70b878e20d2864ab8865a
Teffen Ellis <592134+GirlBossRush@users.noreply.github.com>
2026-07-11 04:39:05 +0200

    web: wire up @goauthentik/lit-jsx as the JSX import source

--- web/src/elements/ak-mdx/ak-mdx.tsx
+++ web/src/elements/ak-mdx/ak-mdx.tsx
@@ -1,3 +1,4 @@
+/** @jsxImportSource react */
 import "#elements/Alert";
 
 import { globalAK } from "#common/global";
--- web/src/elements/ak-mdx/components/MDXWrapper.tsx
+++ web/src/elements/ak-mdx/components/MDXWrapper.tsx
@@ -1,3 +1,4 @@
+/** @jsxImportSource react */
 import React from "react";
 
 export interface MDXWrapperProps {
