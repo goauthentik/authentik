bce55dbe60136befff0dda5f1434c038d9a45cb6
Jens Langhammer <jens@goauthentik.io>
2026-07-13 12:03:08 +0100

    fix authorize error calls
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/providers/oauth2/views/authorize.py
+++ authentik/providers/oauth2/views/authorize.py
@@ -331,19 +331,19 @@ class OAuthAuthorizationParams:
         if SCOPE_BOUND_KEY in self.scope and not self.dpop_jkt:
             raise AuthorizeError(
                 self.redirect_uri,
-                "invalid_request",
-                self.grant_type,
-                self.state,
-                "dpop_jkt is required when bound_key scope is requested",
+                error="invalid_request",
+                grant_type=self.grant_type,
+                state=self.state,
+                description="dpop_jkt is required when bound_key scope is requested",
             )
         # dpop_jkt should only be set if requesting the key binding scope
         if SCOPE_BOUND_KEY not in self.scope and self.dpop_jkt:
             raise AuthorizeError(
                 self.redirect_uri,
-                "invalid_request",
-                self.grant_type,
-                self.state,
-                "dpop_jkt is set when bound_key scope is not requested",
+                error="invalid_request",
+                grant_type=self.grant_type,
+                state=self.state,
+                description="dpop_jkt is set when bound_key scope is not requested",
             )
 
     def check_dpop_jkt(self):
@@ -351,10 +351,10 @@ class OAuthAuthorizationParams:
         if self.dpop_jkt and not is_valid_jkt(self.dpop_jkt):
             raise AuthorizeError(
                 self.redirect_uri,
-                "invalid_request",
-                self.grant_type,
-                self.state,
-                "dpop_jkt must be a base64url-encoded SHA-256 JWK thumbprint",
+                error="invalid_request",
+                grant_type=self.grant_type,
+                state=self.state,
+                description="dpop_jkt must be a base64url-encoded SHA-256 JWK thumbprint",
             )
 
     def check_nonce(self):
