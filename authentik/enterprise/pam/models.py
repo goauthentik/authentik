a69ccb47d0be362843310c06ca52b1d1b1c8faef
Jens Langhammer <jens@goauthentik.io>
2026-07-15 15:25:56 +0100

    remove ai for now
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/enterprise/pam/models.py
+++ authentik/enterprise/pam/models.py
@@ -10,44 +10,6 @@ from authentik.lib.models import InternallyManagedMixin, SerializerModel
 from authentik.policies.models import PolicyBinding, PolicyBindingModel
 
 
-class Persona(ExpiringModel, User):
-    # Inherited:
-    #  - parent.email
-    #  - parent.name (customisable)
-    # Modified:
-    #  - parent.username
-    #  - parent.groups
-
-    parent = models.ForeignKey(User, on_delete=models.CASCADE, related_name="personas")
-
-    @staticmethod
-    def create_for_user(name: str, user: User) -> Persona:
-        return Persona.objects.create(username=name, name=user.name, parent=user)
-
-    class Meta(ExpiringModel.Meta):
-        verbose_name = _("Persona")
-        verbose_name_plural = _("Personas")
-
-    def __str__(self):
-        return f"Persona {self.username} for {self.parent_id}"
-
-
-class Grant(ExpiringModel, CreatedUpdatedModel):
-    """Grant for a persona to access `target`, given after manual/automatic approval."""
-
-    uuid = models.UUIDField(default=uuid4, primary_key=True)
-
-    persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
-    target = models.ForeignKey(PolicyBindingModel, on_delete=models.CASCADE)
-
-    class Meta(ExpiringModel.Meta):
-        verbose_name = _("Grant")
-        verbose_name_plural = _("Grants")
-
-    def __str__(self):
-        return f"Grant {self.uuid}"
-
-
 class RequestStatus(models.TextChoices):
 
     CREATED = "created"
@@ -56,7 +18,7 @@ class RequestStatus(models.TextChoices):
 
 
 class GrantRequest(SerializerModel, ExpiringModel, CreatedUpdatedModel):
-    """Request of a user to access target(s) via Persona"""
+    """Request of a user to access target(s)"""
 
     uuid = models.UUIDField(default=uuid4, primary_key=True)
 
@@ -146,7 +108,9 @@ class PolicyBindingModelRequestRule(SerializerModel, CreatedUpdatedModel, Policy
 
     @property
     def serializer(self):
-        from authentik.enterprise.pam.api.request_rules import PolicyBindingModelRequestRuleSerializer
+        from authentik.enterprise.pam.api.request_rules import (
+            PolicyBindingModelRequestRuleSerializer,
+        )
 
         return PolicyBindingModelRequestRuleSerializer
 
