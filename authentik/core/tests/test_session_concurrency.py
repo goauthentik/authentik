a7fcd5bc7ba2b0c67485361d7082800a4eab06fc
Dominic R <dominic@goauthentik.io>
2026-07-17 12:56:41 -0400

    core: isolate session concurrency tests

---
+++ authentik/core/tests/test_session_concurrency.py
@@ -0,0 +1,40 @@
+"""Concurrent session user switching tests."""
+
+from concurrent.futures import ThreadPoolExecutor
+from threading import Barrier
+
+from django.db import close_old_connections
+from django.test import TransactionTestCase
+from django.utils.crypto import get_random_string
+
+from authentik.core import user_switching
+from authentik.core.models import AuthenticatedSession
+from authentik.core.tests.utils import create_test_session, create_test_user
+from authentik.core.user_switching import UserSwitchingSession
+
+
+class TestUserSwitchingConcurrency(TransactionTestCase):
+    """Test concurrent activation of sessions in one browser."""
+
+    def test_concurrent_activation_has_one_current_session(self):
+        token = get_random_string(user_switching.TOKEN_LENGTH)
+        sessions = [create_test_session(create_test_user()) for _ in range(2)]
+        barrier = Barrier(len(sessions))
+
+        def activate(session_key: str) -> None:
+            close_old_connections()
+            try:
+                barrier.wait()
+                user_switching.activate_session(session_key, token)
+            finally:
+                close_old_connections()
+
+        with ThreadPoolExecutor(max_workers=len(sessions)) as executor:
+            list(executor.map(activate, [session.session_id for session in sessions]))
+
+        switching_session = UserSwitchingSession.objects.get(token=token)
+        self.assertIn(switching_session.current_session_id, {session.pk for session in sessions})
+        self.assertEqual(
+            AuthenticatedSession.objects.filter(user_switching_session_id=token).count(),
+            len(sessions),
+        )
