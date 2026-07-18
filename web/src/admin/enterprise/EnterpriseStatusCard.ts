d3bbaaca59a83e000e16ca4d44eb69874b07b390
Ken Sternberg <ken@goauthentik.io>
2026-07-17 17:10:05 -0700

    web/maint: clean-up ak-enterprise-status-card
    ## What
    
    This commit updates ak-enterprise-status-card with the following improvements:
    
    - `@state()` replaced with `@property({ attribute: false })`, which gets us better linting and typechecking for client code
    - `renderSummaryBadge()` uses a map expression instead of switch statement. Removes some boilerplate and clutter
    - Slightly adjusted some margins, cleaning up the card. It’s a little shorter now, too.
    - Fixed some misconceptions around what `msg(str` actually does. The remaining `msg` strings are all that lit-localize actually uses.
    - Duplicate progress bars replaced with a single function that does the percentage and severity in a new scope, creating a tighter and more visually appealing codebase.
    - Includes a complete Storybook story file with multiple stories for Valid, Expiring, Exceeded, Unlicensed and Loading
    
    ## Why
    
    This mostly started as a test-case for ak-progress-bar. I needed a case where I could see the progress bar in use, and we didn’t have many of them. I decided to add a Storybook story for the Enterprise Status card, because it was a standalone component that used two of them, so it would be a good place to see how they looked stacked one atop another.
    
    While I was rigging the Storybook, the linter complained that `@state()` wasn’t appropriate for properties that were being passed to the component. So I cleaned those. While trying to balance the whitespace around the `∞` symbol, I got frustrated with all the repetition and unbalanced code, and, well, things sorta spiraled out from there.
    
    Anyway, it’s nicer now.

--- web/src/admin/enterprise/EnterpriseStatusCard.ts
+++ web/src/admin/enterprise/EnterpriseStatusCard.ts
@@ -5,25 +5,31 @@ import { PFColor } from "#elements/Label";
 
 import { LicenseForecast, LicenseSummary, LicenseSummaryStatusEnum } from "@goauthentik/api";
 
-import { msg, str } from "@lit/localize";
-import { CSSResult, html, nothing } from "lit";
+import { msg } from "@lit/localize";
+import { css, CSSResult, html, nothing } from "lit";
 import { customElement, property } from "lit/decorators.js";
+import { classMap } from "lit/directives/class-map.js";
 
 import PFCard from "@patternfly/patternfly/components/Card/card.css";
 import PFDescriptionList from "@patternfly/patternfly/components/DescriptionList/description-list.css";
 import PFProgress from "@patternfly/patternfly/components/Progress/progress.css";
 import PFSplit from "@patternfly/patternfly/layouts/Split/split.css";
 
-const badgeDetails = {
-    [LicenseSummaryStatusEnum.Expired]: [PFColor.Red, msg("Expired")],
-    [LicenseSummaryStatusEnum.ExpirySoon]: [PFColor.Orange, msg("Expiring soon")],
-    [LicenseSummaryStatusEnum.Unlicensed]: [PFColor.Gray, msg("Unlicensed")],
-    [LicenseSummaryStatusEnum.ReadOnly]: [PFColor.Red, msg("Read Only")],
-    [LicenseSummaryStatusEnum.LimitExceededAdmin]: [PFColor.Orange, msg("User Count Exceeded")],
-    [LicenseSummaryStatusEnum.LimitExceededUser]: [PFColor.Red, msg("User Count Exceeded")],
-    [LicenseSummaryStatusEnum.Valid]: [PFColor.Green, msg("Valid")],
-    [LicenseSummaryStatusEnum.UnknownDefaultOpenApi]: [null, null],
-};
+const badgeDetails = new Map<LicenseSummaryStatusEnum | undefined, [PFColor, string]>([
+    [LicenseSummaryStatusEnum.Expired, [PFColor.Red, msg("Expired")]],
+    [LicenseSummaryStatusEnum.ExpirySoon, [PFColor.Orange, msg("Expiring soon")]],
+    [LicenseSummaryStatusEnum.Unlicensed, [PFColor.Gray, msg("Unlicensed")]],
+    [LicenseSummaryStatusEnum.ReadOnly, [PFColor.Red, msg("Read Only")]],
+    [LicenseSummaryStatusEnum.LimitExceededAdmin, [PFColor.Orange, msg("User Count Exceeded")]],
+    [LicenseSummaryStatusEnum.LimitExceededUser, [PFColor.Red, msg("User Count Exceeded")]],
+    [LicenseSummaryStatusEnum.Valid, [PFColor.Green, msg("Valid")]],
+]);
+
+const Style = css`
+    .pf-l-split {
+        --pf-l-split--m-gutter--MarginRight: 3rem;
+    }
+`;
 
 @customElement("ak-enterprise-status-card")
 export class EnterpriseStatusCard extends AKElement {
@@ -33,12 +39,12 @@ export class EnterpriseStatusCard extends AKElement {
     @property({ attribute: false })
     summary?: LicenseSummary;
 
-    static styles: CSSResult[] = [PFDescriptionList, PFCard, PFSplit, PFProgress];
+    static styles: CSSResult[] = [PFDescriptionList, PFCard, PFSplit, PFProgress, Style];
 
     renderSummaryBadge() {
-        const status = this.summary?.status ?? LicenseSummaryStatusEnum.UnknownDefaultOpenApi;
-        const [color, message] = badgeDetails[status] ?? [null, null];
-        return color ? html`<ak-label color=${color}>${message}</ak-label>` : nothing;
+        const status = badgeDetails.get(this.summary?.status);
+        if (!status) return nothing;
+        return html`<ak-label color=${status[0]}>${status[1]}</ak-label>`;
     }
 
     calcUserPercentage(licensed: number, current: number) {
@@ -57,24 +63,29 @@ export class EnterpriseStatusCard extends AKElement {
         const currentExternalUsers = this.forecast.externalUsers;
         const licensedInternalUsers = this.summary.internalUsers;
         const licensedExternalUsers = this.summary.externalUsers;
+        const isLicensed = this.summary.status !== LicenseSummaryStatusEnum.Unlicensed;
 
-        let internalUserPercentage = 0;
-        let externalUserPercentage = 0;
-        if (this.summary.status !== LicenseSummaryStatusEnum.Unlicensed) {
-            internalUserPercentage = this.calcUserPercentage(
-                licensedInternalUsers,
-                currentInternalUsers,
-            );
-            externalUserPercentage = this.calcUserPercentage(
-                licensedExternalUsers,
-                currentExternalUsers,
-            );
-        }
+        const progressBar = (label, current, licensed) => {
+            const percentage = licensed ? this.calcUserPercentage(licensed, current) : 0;
+            const severity = classMap({
+                "pf-m-success": isLicensed && percentage <= 80,
+                "pf-m-danger": isLicensed && percentage > 100,
+                "pf-m-warning": isLicensed && percentage > 80 && percentage <= 100,
+            });
+
+            return html`
+                <ak-progress-bar class="${severity}" value=${percentage}>
+                    <span slot="description">${label} (${current} / ${licensed})</span>
+                    <span slot="status"> ${percentage < Infinity ? `${percentage}` : "∞"}% </span>
+                </ak-progress-bar>
+            `;
+        };
+
         return html`<div class="pf-c-card">
             <div class="pf-c-card__title">${msg("Current license status")}</div>
             <div class="pf-c-card__body">
                 <div class="pf-l-split pf-m-gutter">
-                    <dl class="pf-l-split__item pf-c-description-list pf-m-horizontal">
+                    <dl class="pf-l-split__item pf-c-description-list">
                         <div class="pf-c-description-list__group">
                             <dt class="pf-c-description-list__term">
                                 <span class="pf-c-description-list__text"
@@ -89,40 +100,16 @@ export class EnterpriseStatusCard extends AKElement {
                         </div>
                     </dl>
                     <div class="pf-l-split__item pf-m-fill">
-                        <ak-progress-bar
-                            class="${internalUserPercentage > 100
-                                ? "pf-m-danger"
-                                : ""} ${internalUserPercentage >= 80 ? "pf-m-warning" : ""}"
-                            value=${internalUserPercentage}
-                        >
-                            <span slot="description">
-                                ${msg(
-                                    str`Internal user usage (${currentInternalUsers}/${licensedInternalUsers})`,
-                                )}
-                            </span>
-                            <span slot="status">
-                                ${msg(
-                                    str`${internalUserPercentage < Infinity ? internalUserPercentage : "∞"}%`,
-                                )}
-                            </span>
-                        </ak-progress-bar>
-                        <ak-progress-bar
-                            class="${externalUserPercentage > 100
-                                ? "pf-m-danger"
-                                : ""} ${externalUserPercentage >= 80 ? "pf-m-warning" : ""}"
-                            value=${externalUserPercentage}
-                        >
-                            <span slot="description">
-                                ${msg(
-                                    str`External user usage (${currentExternalUsers}/${licensedExternalUsers})`,
-                                )}
-                            </span>
-                            <span slot="status">
-                                ${msg(
-                                    str`${externalUserPercentage < Infinity ? externalUserPercentage : "∞"}%`,
-                                )}
-                            </span>
-                        </ak-progress-bar>
+                        ${progressBar(
+                            msg("Internal user usage"),
+                            currentInternalUsers,
+                            licensedInternalUsers
+                        )}
+                        ${progressBar(
+                            msg("External user usage"),
+                            currentExternalUsers,
+                            licensedExternalUsers
+                        )}
                     </div>
                 </div>
             </div>
