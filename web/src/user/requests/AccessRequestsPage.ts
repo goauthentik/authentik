88cd98db55cecb683b12072eefdd12f3fa114751
Jens Langhammer <jens@goauthentik.io>
2026-07-20 20:12:27 +0100

    add banner
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/user/requests/AccessRequestsPage.ts
+++ web/src/user/requests/AccessRequestsPage.ts
@@ -4,26 +4,64 @@ import "#user/requests/BrowseRequestable";
 import "#user/requests/MyGrantRequestsList";
 import "#user/requests/PendingReviewList";
 
+import { aki } from "#common/api/client";
+import { PaginatedResponse } from "#common/api/responses";
+
 import { AKSkipToContent } from "#elements/a11y/ak-skip-to-content";
 import { AKElement } from "#elements/Base";
+import { paramURL } from "#elements/router/RouterOutlet";
 import { SlottedTemplateResult } from "#elements/types";
 
-import { msg } from "@lit/localize";
-import { CSSResult, html } from "lit";
-import { customElement } from "lit/decorators.js";
+import Styles from "#user/user-settings/styles.css";
 
+import { GrantRequest, RequestsApi } from "@goauthentik/api";
+
+import { msg } from "@lit/localize";
+import { CSSResult, html, nothing } from "lit";
+import { customElement, state } from "lit/decorators.js";
+
+import PFBanner from "@patternfly/patternfly/components/Banner/banner.css";
 import PFContent from "@patternfly/patternfly/components/Content/content.css";
 import PFPage from "@patternfly/patternfly/components/Page/page.css";
-import Styles from "#user/user-settings/styles.css";
 
 @customElement("ak-access-requests-page")
 export class AccessRequestsPage extends AKElement {
-    static styles: CSSResult[] = [PFPage, PFContent, Styles];
+    static styles: CSSResult[] = [PFPage, PFBanner, PFContent, Styles];
+
+    @state()
+    toReview?: PaginatedResponse<GrantRequest>;
+
+    override async connectedCallback(): Promise<void> {
+        aki(RequestsApi)
+            .requestsGrantRequestsPendingReviewList({})
+            .then((rev) => {
+                this.toReview = rev;
+            })
+            .finally(() => {
+                super.connectedCallback();
+            });
+    }
 
     protected override render(): SlottedTemplateResult {
         return html`<div class="pf-c-page">
             <div class="pf-c-page__main">
-                <ak-tabs role="main" aria-label=${msg("Access requests")} ${AKSkipToContent.ref} vertical>
+                ${(this.toReview?.pagination.count || 0) > 0
+                    ? html`<div class="pf-c-banner pf-m-info">
+                          ${msg("Reviews to request: ")}
+                          <a
+                              href=${paramURL("/requests", {
+                                  page: "page-for-review",
+                              })}
+                              >${msg("Review")}</a
+                          >
+                      </div>`
+                    : nothing}
+                <ak-tabs
+                    role="main"
+                    aria-label=${msg("Access requests")}
+                    ${AKSkipToContent.ref}
+                    vertical
+                >
                     <div
                         id="page-browse"
                         role="tabpanel"
