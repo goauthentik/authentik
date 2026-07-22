89d581af6f169e88fe7797c4d7713d4200e7c158
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2026-07-22 16:24:40 +0200

    fixup
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- internal/gounicorn/gounicorn.go
+++ internal/gounicorn/gounicorn.go
@@ -66,13 +66,8 @@ func (g *GoUnicorn) initCmd() {
 			panic(fmt.Errorf("failed to create temporary pid file: %v", err))
 		}
 		g.pidFile = pidFile.Name()
-		if coveragePath, ok := os.LookupEnv("AUTHENTIK_COVERAGE"); ok {
-			command = "coverage"
-			args = []string{"run", "--data-file", coveragePath, "-m", "gunicorn", "-c", "./lifecycle/gunicorn.conf.py", "authentik.root.asgi:application"}
-		} else {
-			command = "gunicorn"
-			args = []string{"-c", "./lifecycle/gunicorn.conf.py", "authentik.root.asgi:application"}
-		}
+		command = "gunicorn"
+		args = []string{"-c", "./lifecycle/gunicorn.conf.py", "authentik.root.asgi:application"}
 		if g.pidFile != "" {
 			args = append(args, "--pid", g.pidFile)
 		}
