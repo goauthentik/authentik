93d80b96c2ce27a062fed465eae9b9ce3491352a
Jens L. <jens@goauthentik.io>
2026-07-27 13:47:01 +0100

    Cherry-pick #24376 to version-2026.5 (with conflicts)
    This cherry-pick has conflicts that need manual resolution.
    
    Original PR: #24376
    Original commit: 94b42b52c0df86a04a79875b3136333ddd84b282
    
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- packages/client-ts/src/models/CurrentBrandFlags.ts
+++ packages/client-ts/src/models/CurrentBrandFlags.ts
@@ -19,18 +19,6 @@
  */
 export interface CurrentBrandFlags {
     /**
-     * Configure if applications without any policy/group/user bindings should be accessible to any user.
-     * @type {boolean}
-     * @memberof CurrentBrandFlags
-     */
-    coreDefaultAppAccess: boolean;
-    /**
-     * Include additional information in audit logs, may incur a performance penalty.
-     * @type {boolean}
-     * @memberof CurrentBrandFlags
-     */
-    enterpriseAuditIncludeExpandedDiff: boolean;
-    /**
      * Upon successful authentication, re-start authentication in other open tabs.
      * @type {boolean}
      * @memberof CurrentBrandFlags
@@ -49,13 +37,6 @@ export interface CurrentBrandFlags {
  * Check if a given object implements the CurrentBrandFlags interface.
  */
 export function instanceOfCurrentBrandFlags(value: object): value is CurrentBrandFlags {
-    if (!("coreDefaultAppAccess" in value) || value["coreDefaultAppAccess"] === undefined)
-        return false;
-    if (
-        !("enterpriseAuditIncludeExpandedDiff" in value) ||
-        value["enterpriseAuditIncludeExpandedDiff"] === undefined
-    )
-        return false;
     if (!("flowsContinuousLogin" in value) || value["flowsContinuousLogin"] === undefined)
         return false;
     if (!("flowsRefreshOthers" in value) || value["flowsRefreshOthers"] === undefined) return false;
@@ -74,8 +55,6 @@ export function CurrentBrandFlagsFromJSONTyped(
         return json;
     }
     return {
-        coreDefaultAppAccess: json["core_default_app_access"],
-        enterpriseAuditIncludeExpandedDiff: json["enterprise_audit_include_expanded_diff"],
         flowsContinuousLogin: json["flows_continuous_login"],
         flowsRefreshOthers: json["flows_refresh_others"],
     };
@@ -94,8 +73,6 @@ export function CurrentBrandFlagsToJSONTyped(
     }
 
     return {
-        core_default_app_access: value["coreDefaultAppAccess"],
-        enterprise_audit_include_expanded_diff: value["enterpriseAuditIncludeExpandedDiff"],
         flows_continuous_login: value["flowsContinuousLogin"],
         flows_refresh_others: value["flowsRefreshOthers"],
     };
