2facac1df2fee64ec7417be3af454b09b6b205ca
Connor Peshek <connor@connorpeshek.me>
2026-07-28 23:01:15 -0500

    stages/authenticator_webauthn: remove prevent_duplicate_devices

--- packages/client-ts/src/models/AuthenticatorWebAuthnStageRequest.ts
+++ packages/client-ts/src/models/AuthenticatorWebAuthnStageRequest.ts
@@ -77,12 +77,6 @@ export interface AuthenticatorWebAuthnStageRequest {
      */
     deviceTypeRestrictions?: Array<string>;
     /**
-     * When enabled, a given device can only be registered once.
-     * @type {boolean}
-     * @memberof AuthenticatorWebAuthnStageRequest
-     */
-    preventDuplicateDevices?: boolean;
-    /**
      *
      * @type {number}
      * @memberof AuthenticatorWebAuthnStageRequest
@@ -142,10 +136,6 @@ export function AuthenticatorWebAuthnStageRequestFromJSONTyped(
                 : (json["hints"] as Array<any>).map(WebAuthnHintEnumFromJSON),
         deviceTypeRestrictions:
             json["device_type_restrictions"] == null ? undefined : json["device_type_restrictions"],
-        preventDuplicateDevices:
-            json["prevent_duplicate_devices"] == null
-                ? undefined
-                : json["prevent_duplicate_devices"],
         maxAttempts: json["max_attempts"] == null ? undefined : json["max_attempts"],
     };
 }
@@ -178,7 +168,6 @@ export function AuthenticatorWebAuthnStageRequestToJSONTyped(
                 ? undefined
                 : (value["hints"] as Array<any>).map(WebAuthnHintEnumToJSON),
         device_type_restrictions: value["deviceTypeRestrictions"],
-        prevent_duplicate_devices: value["preventDuplicateDevices"],
         max_attempts: value["maxAttempts"],
     };
 }
