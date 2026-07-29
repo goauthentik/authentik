fe8abd00e10a7b09662aebbf2446181eb0f0ba69
Jens Langhammer <jens@goauthentik.io>
2026-07-29 18:48:28 +0100

    show policy bindings
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- web/src/admin/providers/oauth2/OAuth2ProviderViewPage.ts
+++ web/src/admin/providers/oauth2/OAuth2ProviderViewPage.ts
@@ -13,6 +13,7 @@ import "#elements/ak-mdx/index";
 import "#elements/buttons/ModalButton";
 import "#elements/buttons/SpinnerButton/index";
 import "#elements/Divider";
+import "#admin/policies/BoundPoliciesList";
 
 import { aki } from "#common/api/client";
 import { EVENT_REFRESH } from "#common/constants";
@@ -529,15 +530,16 @@ export class OAuth2ProviderViewPage extends AKElement {
         return html`<div
             class="pf-c-page__main-section pf-m-no-padding-mobile pf-l-grid pf-m-gutter"
         >
-            <div class="pf-c-card pf-l-grid__item pf-m-12-col">
+            <div class="pf-c-card pf-l-grid__item pf-m-4-col">
                 <div class="pf-c-card__title">${msg("Dynamic Client Registration")}</div>
                 <div class="pf-c-card__body">
                     ${renderDescriptionList([
                         [
-                            msg("Create application"),
-                            html`${dcr.createApplication ? msg("Yes") : msg("No")}`,
+                            msg("Default application group"),
+                            html`${dcr.defaultApplicationGroup !== ""
+                                ? dcr.defaultApplicationGroup
+                                : "-"}`,
                         ],
-                        [msg("Default client type"), html`${TypeToLabel(dcr.defaultClientType)}`],
                         [
                             msg("Allowed grant types"),
                             html`${(dcr.allowedGrantTypes || []).length > 0
@@ -557,6 +559,19 @@ export class OAuth2ProviderViewPage extends AKElement {
                         ],
                     ])}
                 </div>
+            </div>
+            <div class="pf-c-card pf-l-grid__item pf-m-8-col">
+                <div class="pf-c-card__title">${msg("Dynamic application policies")}</div>
+                <ak-bound-policies-list
+                    target=${this.dcrConfig.pbmUuid}
+                    .policyEngineMode=${this.dcrConfig.policyEngineMode}
+                >
+                    <span slot="description">
+                        ${msg(
+                            "Bindings configured here will be copied to dynamically registered applications. If no bindings are created, bindings of this providers' application are copied.",
+                        )}
+                    </span>
+                </ak-bound-policies-list>
             </div>
         </div>`;
     }
