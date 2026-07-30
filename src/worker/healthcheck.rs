463646b4b98a577636325d7123307b4a41bb5403
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2026-07-30 15:25:40 +0200

    worker: fix healthcheck paths (#24481)
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- src/worker/healthcheck.rs
+++ src/worker/healthcheck.rs
@@ -63,8 +63,8 @@ async fn fallback() -> StatusCode {
 pub(super) fn build_router(workers: Arc<Workers>) -> Router {
     wrap_router(
         Router::new()
-            .route("/-/heath/ready/", any(health_ready))
-            .route("/-/heath/live/", any(health_live))
+            .route("/-/health/ready/", any(health_ready))
+            .route("/-/health/live/", any(health_live))
             .fallback(fallback)
             .with_state(workers),
         true,
