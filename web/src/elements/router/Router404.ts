0e3c024d2631e179e3d75172a05b0021f8ac1496
Teffen Ellis <teffen@Teffens-MacBook-Pro.local>
2026-08-11 19:42:58 +0100

    web: Remove the legacy hash router.

--- web/src/elements/router/Router404.ts
+++ web/src/elements/router/Router404.ts
@@ -1,4 +1,5 @@
 import { AKElement } from "#elements/Base";
+import { toCurrentInterface } from "#elements/router/core/interfaces";
 
 import { msg, str } from "@lit/localize";
 import { CSSResult, html, TemplateResult } from "lit";
@@ -22,7 +23,7 @@ export class Router404 extends AKElement {
                 <div class="pf-c-empty-state__body">
                     ${msg(str`The URL "${this.url}" was not found.`)}
                 </div>
-                <a href="#/" class="pf-c-button pf-m-primary" type="button"
+                <a href=${toCurrentInterface()} class="pf-c-button pf-m-primary" type="button"
                     >${msg("Return home")}</a
                 >
             </div>
