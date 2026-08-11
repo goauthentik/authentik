67444342118aadd3d00a38a53a5a1855dc26bf5b
Teffen Ellis <teffen@Teffens-MacBook-Pro.local>
2026-08-11 16:06:50 +0100

    web: Mount the path-routing outlet in the user interface.

--- web/src/user/ak-interface-user.ts
+++ web/src/user/ak-interface-user.ts
@@ -2,7 +2,7 @@ import "#components/ak-nav-buttons";
 import "#elements/banner/EnterpriseStatusBanner";
 import "#components/notifications/APIDrawer";
 import "#components/notifications/NotificationDrawer";
-import "#elements/router/RouterOutlet";
+import "#elements/router/core/RouterView";
 import "#components/ak-nav-tabs";
 
 import { globalAK } from "#common/global";
@@ -15,6 +15,7 @@ import { WithBrandConfig } from "#elements/mixins/branding";
 import { WithCapabilitiesConfig } from "#elements/mixins/capabilities";
 import { WithLicenseSummary } from "#elements/mixins/license";
 import { canAccessAdmin, WithSession } from "#elements/mixins/session";
+import { formatInterfacePrefix, toUserInterface } from "#elements/router/core/interfaces";
 import { ifPresent } from "#elements/utils/attributes";
 import { ThemedImage } from "#elements/utils/images";
 
@@ -27,7 +28,7 @@ import {
 } from "#components/notifications/utils";
 
 import Styles from "#user/ak-interface-user.css";
-import { ROUTES } from "#user/Routes";
+import { DEFAULT_PATH, ROUTES } from "#user/Routes";
 
 import { ConsoleLogger } from "#logger/browser";
 
@@ -175,7 +176,7 @@ class UserInterface extends WithLicenseSummary(
                     class="pf-c-page__header"
                 >
                     <div part="brand" class="pf-c-page__header-brand">
-                        <a href="#/" class="pf-c-page__header-brand-link">
+                        <a href=${toUserInterface()} class="pf-c-page__header-brand-link">
                             ${ThemedImage({
                                 src: this.brandingLogo,
                                 alt: this.brandingTitle,
@@ -202,14 +203,18 @@ class UserInterface extends WithLicenseSummary(
                         <div class="pf-c-drawer__main">
                             <div class="pf-c-drawer__content">
                                 <div class="pf-c-drawer__body">
-                                    <ak-router-outlet
+                                    <ak-router-view
                                         class="pf-l-bullseye__item pf-c-page__main"
                                         tabindex="-1"
                                         id="main-content"
-                                        default-url="/library"
                                         .routes=${ROUTES}
+                                        .prefix=${formatInterfacePrefix(
+                                            globalAK().api.relBase,
+                                            "user",
+                                        )}
+                                        .defaultPath=${DEFAULT_PATH}
                                     >
-                                    </ak-router-outlet>
+                                    </ak-router-view>
                                 </div>
                             </div>
                             ${renderNotificationDrawerPanel(this.drawer)}
