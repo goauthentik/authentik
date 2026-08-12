7e3563acf67d57472e029820117bfcf982b92642
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2026-08-12 18:34:19 +0200

    more removal
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/root/tests/test_ws_client.py
+++ authentik/root/tests/test_ws_client.py
@@ -1,7 +1,6 @@
 from asgiref.sync import sync_to_async
 from channels.routing import URLRouter
 from channels.testing import WebsocketCommunicator
-from django.http import HttpRequest
 from django.test import TransactionTestCase
 
 from authentik.core.tests.utils import create_test_user
@@ -14,45 +13,11 @@ from authentik.events.models import (
 )
 from authentik.lib.generators import generate_id
 from authentik.root import websocket
-from authentik.stages.password import BACKEND_INBUILT
-from authentik.stages.user_login.stage import COOKIE_NAME_KNOWN_DEVICE
 
 
 class TestClientWS(TransactionTestCase):
-
     def setUp(self):
         self.user = create_test_user()
-
-    async def _alogin_cookie(self, user, **kwargs):
-        """Similar to `client.aforce_login` but allow setting of cookies"""
-        from django.contrib.auth import alogin
-
-        # Create a fake request to store login details.
-        request = HttpRequest()
-        session = await self.client.asession()
-        request.session = session
-        request.COOKIES.update(kwargs)
-
-        await alogin(request, user, BACKEND_INBUILT)
-        # Save the session values.
-        await request.session.asave()
-        self.client._set_login_cookies(request)
-
-    async def test_auth_blank(self):
-        dev_id = generate_id()
-        communicator = WebsocketCommunicator(
-            URLRouter(websocket.websocket_urlpatterns),
-            "/ws/client/",
-            headers=[(b"cookie", f"{COOKIE_NAME_KNOWN_DEVICE}={dev_id}".encode())],
-        )
-        connected, _ = await communicator.connect()
-        self.assertTrue(connected)
-
-        await self._alogin_cookie(self.user, **{COOKIE_NAME_KNOWN_DEVICE: dev_id})
-
-        await communicator.receive_nothing()
-        await communicator.receive_json_from()
-        await communicator.disconnect()
 
     async def test_notification(self):
         communicator = WebsocketCommunicator(
