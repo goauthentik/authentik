f6142f357fbccc45cdad815966225726feece123
Simonyi Gergő <gergo@goauthentik.io>
2026-09-01 01:43:55 +0200

    ues copied `new_value` instead of original `value`

--- authentik/tenants/api/settings.py
+++ authentik/tenants/api/settings.py
@@ -33,8 +33,8 @@ class FlagJSONField(JSONDictField):
             if _flag.visibility == "system":
                 new_value.pop(_flag.key, None)
             # Explicitly present unset flags as if they were set to default
-            if _flag.key not in value:
-                value[_flag.key] = _flag.default
+            if _flag.key not in new_value:
+                new_value[_flag.key] = _flag.default
         return super().to_representation(new_value)
 
     def run_validators(self, value: dict):
