47cffa64c4c1c4e0c5cb59240678a8da12248a7f
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2026-09-01 17:32:37 +0200

    wip
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- authentik/sources/ldap/middleware.py
+++ authentik/sources/ldap/middleware.py
@@ -1,12 +1,24 @@
+from collections.abc import Iterable
+
+from dramatiq.actor import Actor
+
 from authentik.lib.sync.middleware import SyncMiddleware
 from authentik.sources.ldap.models import LDAPSourceSync
-from authentik.sources.ldap.tasks import ldap_sync, ldap_sync_page, ldap_sync_trigger_outgoing_sync
 
 
 class LDAPSyncMiddleware(SyncMiddleware):
     SyncModel = LDAPSourceSync
-    actors = [
-        ldap_sync,
-        ldap_sync_page,
-        ldap_sync_trigger_outgoing_sync,
-    ]
+
+    @staticmethod
+    def actors() -> Iterable[Actor]:
+        from authentik.sources.ldap.tasks import (
+            ldap_sync,
+            ldap_sync_page,
+            ldap_sync_trigger_outgoing_sync,
+        )
+
+        return (
+            ldap_sync,
+            ldap_sync_page,
+            ldap_sync_trigger_outgoing_sync,
+        )
