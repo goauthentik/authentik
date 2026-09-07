7c6744109b3f5bb6a4f908ddb1dbd53242fbe0e7
Simonyi Gergő <gergo@goauthentik.io>
2026-09-07 11:26:49 +0200

    Revert "remove `requied` from flags API"
    This reverts commit c8fcfde74a2eef3333ed1b20403074e126647655.
    
    After more testing, I don't believe this is required. The two bugfixes
    should be enough.

--- authentik/tenants/api/settings.py
+++ authentik/tenants/api/settings.py
@@ -61,6 +61,7 @@ class FlagsJSONExtension(OpenApiSerializerFieldExtension):
 
     def map_serializer_field(self, auto_schema, direction):
         props = {}
+        required = []
         for flag in Flag.available():
             _flag = flag()
             props[_flag.key] = build_basic_type(get_args(_flag.__orig_bases__[0])[0])
@@ -68,7 +69,9 @@ class FlagsJSONExtension(OpenApiSerializerFieldExtension):
                 props[_flag.key]["description"] = _flag.description
             if _flag.deprecated:
                 props[_flag.key]["deprecated"] = _flag.deprecated
-        return build_object_type(props)
+            if not _flag.deprecated:
+                required.append(_flag.key)
+        return build_object_type(props, required=required)
 
 
 class SettingsSerializer(ModelSerializer):
