2f5d73234960f572a4ea1f734f143d460f1f3902
Ken Sternberg <ken@goauthentik.io>
2026-09-08 15:52:31 -0700

    Friggin... well, not prettier this time.  Black this time.

--- authentik/flows/api/flows_diagram.py
+++ authentik/flows/api/flows_diagram.py
@@ -1,11 +1,7 @@
 from dataclasses import dataclass, field
-from enum import StrEnum
-
-from django.utils.translation import gettext as _
 
 from django.db import models
 from django.db.models import Model, QuerySet
-
 from guardian.shortcuts import get_objects_for_user
 from rest_framework.fields import CharField, ChoiceField, IntegerField
 
@@ -131,25 +127,17 @@ class FlowDiagram:
         type: DiagramEdgeTypes = DiagramEdgeTypes.PROCEED,
     ) -> None:
         for source in sources:
-            self.graph.edges.append(
-                DiagramEdge(source.identifier, target.identifier, type)
-            )
+            self.graph.edges.append(DiagramEdge(source.identifier, target.identifier, type))
 
-    def get_policy_bindings(
-        self, target: PolicyBindingModel
-    ) -> QuerySet[PolicyBinding]:
+    def get_policy_bindings(self, target: PolicyBindingModel) -> QuerySet[PolicyBinding]:
         return (
-            get_objects_for_user(
-                self.user, "authentik_policies.view_policybinding"
-            )
+            get_objects_for_user(self.user, "authentik_policies.view_policybinding")
             .filter(target=target)
             .exclude(policy__isnull=True)
             .order_by("order")
         )
 
-    def get_authentication_requirement(
-        self, flow_start: DiagramNode, end: DiagramNode
-    ) -> None:
+    def get_authentication_requirement(self, flow_start: DiagramNode, end: DiagramNode) -> None:
         if self.flow.authentication == FlowAuthenticationRequirement.NONE:
             return
 
@@ -160,25 +148,17 @@ class FlowDiagram:
                 name=self.flow.authentication,
             )
         )
-        self.add_edges(
-            [requirement], end, DiagramEdgeTypes.REQUIREMENT_UNFULFILLED
-        )
-        self.add_edges(
-            [requirement], flow_start, DiagramEdgeTypes.REQUIREMENT_FULFILLED
-        )
+        self.add_edges([requirement], end, DiagramEdgeTypes.REQUIREMENT_UNFULFILLED)
+        self.add_edges([requirement], flow_start, DiagramEdgeTypes.REQUIREMENT_FULFILLED)
 
-    def get_flow_policies(
-        self, flow_start: DiagramNode, end: DiagramNode
-    ) -> None:
+    def get_flow_policies(self, flow_start: DiagramNode, end: DiagramNode) -> None:
         bindings = list(self.get_policy_bindings(self.flow))
 
         if not bindings:
             return
 
         pre = self.add_node(
-            DiagramNode(
-                identifier="flow_pre", type=DiagramNodeTypes.PRE_FLOW_POLICIES
-            )
+            DiagramNode(identifier="flow_pre", type=DiagramNodeTypes.PRE_FLOW_POLICIES)
         )
 
         for index, binding in enumerate(bindings):
@@ -194,9 +174,7 @@ class FlowDiagram:
         last_stage: DiagramNode | None = None
 
         stages = (
-            get_objects_for_user(
-                self.user, "authentik_flows.view_flowstagebinding"
-            )
+            get_objects_for_user(self.user, "authentik_flows.view_flowstagebinding")
             .filter(target=self.flow)
             .order_by("order")
         )
@@ -215,9 +193,7 @@ class FlowDiagram:
                     )
                 )
 
-            stage = self.add_node(
-                stage_node(f"stage_{stage_index}", stage_binding)
-            )
+            stage = self.add_node(stage_node(f"stage_{stage_index}", stage_binding))
 
             for policy in policies:
                 self.add_edges(parents, policy)
@@ -227,9 +203,7 @@ class FlowDiagram:
             else:
                 self.add_edges(parents, stage)
 
-            self.add_edges(
-                previous_policies, stage, DiagramEdgeTypes.POLICY_DENIED
-            )
+            self.add_edges(previous_policies, stage, DiagramEdgeTypes.POLICY_DENIED)
 
             parents, previous_policies, last_stage = [stage], policies, stage
 
