d22e16f8c8efb3423f8b4c6d23121af78ee4f1ac
Jacob Sachs <jsachs@users.noreply.github.com>
2026-09-15 20:58:02 -0400

    dangling import that was removed
    Signed-off-by: Jacob Sachs <jsachs@users.noreply.github.com>

--- authentik/policies/signals.py
+++ authentik/policies/signals.py
@@ -1,7 +1,5 @@
 """authentik policy signals"""
 
-from uuid import UUID
-
 from django.core.cache import cache
 from django.db import connection
 from django.db.models.signals import post_save, pre_delete
