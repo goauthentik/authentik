021d58c86f2fe4b1c5bc9cecd9a603b7142eb185
Jens Langhammer <jens@goauthentik.io>
2026-09-16 15:05:21 +0100

    core: add server-log endpoint for debugging
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

---
+++ web/src/components/log.ts
@@ -0,0 +1,19 @@
+import { aki } from "#common/api/client";
+
+import { DebugApi } from "@goauthentik/api";
+
+let _serverLog = async (_: string): Promise<void> => {};
+if (process.env.NODE_ENV === "development") {
+    _serverLog = async (msg): Promise<void> => {
+        const api = await aki(DebugApi);
+        api.debugLogCreate({
+            serverLogRequest: {
+                message: msg,
+            },
+        });
+    };
+}
+
+export function serverLog(msg: string) {
+    return _serverLog(msg);
+}
