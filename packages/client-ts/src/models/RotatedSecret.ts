4550ea91ae487477a06958c727f361b12ef10947
Dominic R <dominic@goauthentik.io>
2026-09-16 18:47:11 -0400

    api: generate compatible secret type filters

--- packages/client-ts/src/models/RotatedSecret.ts
+++ packages/client-ts/src/models/RotatedSecret.ts
@@ -1,5 +1,3 @@
-/* tslint:disable */
-/* eslint-disable */
 /**
  * authentik
  * Making authentication simple.
@@ -14,13 +12,11 @@
 
 /**
  * A rotated value, hidden when the caller cannot view it.
+ *
  * @export
  * @interface RotatedSecret
  */
 export interface RotatedSecret {
-    /**
-     *
-     */
     readonly value: string | null;
 }
 
--- packages/client-ts/src/models/Secret.ts
+++ packages/client-ts/src/models/Secret.ts
@@ -1,5 +1,3 @@
-/* tslint:disable */
-/* eslint-disable */
 /**
  * authentik
  * Making authentication simple.
@@ -18,33 +16,21 @@ import { SecretTypeEnumFromJSON, SecretTypeEnumToJSON } from "./SecretTypeEnum";
 
 /**
  * Create and configure a secret without exposing its value.
+ *
  * @export
  * @interface Secret
  */
 export interface Secret {
-    /**
-     *
-     */
     readonly pk: string;
-    /**
-     *
-     */
     name: string;
-    /**
-     *
-     */
     type?: SecretTypeEnum;
     /**
-     * Objects that are managed by authentik. These objects are created and updated automatically. This flag only indicates that an object can be overwritten by migrations. You can still modify the objects via the API, but expect changes to be overwritten in a later update.
+     * Objects that are managed by authentik. These objects are created and updated automatically.
+     * This flag only indicates that an object can be overwritten by migrations. You can still
+     * modify the objects via the API, but expect changes to be overwritten in a later update.
      */
     readonly managed: string | null;
-    /**
-     *
-     */
     readonly created: Date;
-    /**
-     *
-     */
     readonly lastUpdated: Date;
 }
 
